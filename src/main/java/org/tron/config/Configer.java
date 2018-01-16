1f96d93b04a850fd3a444821b315a614f06007e8
sasaxie <xiexiaodong917@163.com>
2018-01-16 12:57:48 +0800

    remove some useless code

--- src/main/java/org/tron/config/Configer.java
+++ src/main/java/org/tron/config/Configer.java
@@ -1,29 +1,25 @@
 /*
- * java-tron is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * Copyright (c) [2016] [ <ether.camp> ]
+ * This file is part of the ethereumJ library.
+ *
+ * The ethereumJ library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * java-tron is distributed in the hope that it will be useful,
+ * The ethereumJ library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ethereumJ library. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.tron.config;
 
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
-import java.io.File;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Reader;
-import java.io.Writer;
-import java.util.Properties;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.spongycastle.util.encoders.Hex;
@@ -31,65 +27,68 @@ import org.tron.core.Constant;
 import org.tron.crypto.ECKey;
 import org.tron.utils.ByteArray;
 
+import java.io.*;
+import java.util.Properties;
+
 public class Configer {
-  private static final Logger logger = LoggerFactory.getLogger("Configer");
-  private final static String DATABASE_DIRECTORY = Constant.DATABASE_DIR;
-  public static String TRON_CONF = Constant.NORMAL_CONF;
-  private static String generatedNodePrivateKey;
+    private static final Logger logger = LoggerFactory.getLogger("Configer");
+    private final static String DATABASE_DIRECTORY = Constant.DATABASE_DIR;
+    public static String TRON_CONF = Constant.NORMAL_CONF;
+    private static String generatedNodePrivateKey;
 
-  static {
-    try {
-      File file = new File(Configer.getConf().getString
-          (DATABASE_DIRECTORY), "nodeId.properties");
-      Properties props = new Properties();
-      if (file.canRead()) {
-        try (Reader r = new FileReader(file)) {
-          props.load(r);
+    static {
+        try {
+            File file = new File(Configer.getConf().getString
+                    (DATABASE_DIRECTORY), "nodeId.properties");
+            Properties props = new Properties();
+            if (file.canRead()) {
+                try (Reader r = new FileReader(file)) {
+                    props.load(r);
+                }
+            } else {
+                ECKey key = new ECKey();
+
+                byte[] privKeyBytes = key.getPrivKeyBytes();
+
+                String nodeIdPrivateKey = ByteArray.toHexString(privKeyBytes);
+
+                props.setProperty("nodeIdPrivateKey", nodeIdPrivateKey);
+                props.setProperty("nodeId", Hex.toHexString(key.getNodeId
+                        ()));
+                file.getParentFile().mkdirs();
+                try (Writer w = new FileWriter(file)) {
+                    props.store(w, "Generated NodeID.");
+                }
+                logger.info("New nodeID generated: " + props.getProperty
+                        ("nodeId"));
+                logger.info("Generated nodeID and its private key stored " +
+                        "in " + file);
+            }
+            generatedNodePrivateKey = props.getProperty("nodeIdPrivateKey");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
         }
-      } else {
-        ECKey key = new ECKey();
-
-        byte[] privKeyBytes = key.getPrivKeyBytes();
-
-        String nodeIdPrivateKey = ByteArray.toHexString(privKeyBytes);
-
-        props.setProperty("nodeIdPrivateKey", nodeIdPrivateKey);
-        props.setProperty("nodeId", Hex.toHexString(key.getNodeId
-            ()));
-        file.getParentFile().mkdirs();
-        try (Writer w = new FileWriter(file)) {
-          props.store(w, "Generated NodeID.");
-        }
-        logger.info("New nodeID generated: " + props.getProperty
-            ("nodeId"));
-        logger.info("Generated nodeID and its private key stored " +
-            "in " + file);
-      }
-      generatedNodePrivateKey = props.getProperty("nodeIdPrivateKey");
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-  }
-
-  public static Config getConf() {
-    return ConfigFactory.load(TRON_CONF);
-  }
-
-  public static Config getConf(String conf) {
-
-    if (conf == null || "".equals(conf)) {
-      return ConfigFactory.load(TRON_CONF);
-    } else {
-      return ConfigFactory.load(conf);
     }
 
-  }
+    public static Config getConf() {
+        return ConfigFactory.load(TRON_CONF);
+    }
 
-  public static ECKey getMyKey() {
-    return ECKey.fromPrivate(Hex.decode(generatedNodePrivateKey));
-  }
+    public static Config getConf(String conf) {
 
-  public static String getGNPK() {
-    return generatedNodePrivateKey;
-  }
+        if (conf == null || "".equals(conf)) {
+            return ConfigFactory.load(TRON_CONF);
+        } else {
+            return ConfigFactory.load(conf);
+        }
+
+    }
+
+    public static ECKey getMyKey() {
+        return ECKey.fromPrivate(Hex.decode(generatedNodePrivateKey));
+    }
+
+    public static String getGNPK() {
+        return generatedNodePrivateKey;
+    }
 }
