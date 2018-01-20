0ae1a93e751ed281fbea66d239a675d61125b10e
Sean Robbins <sean.m.robbins@gmail.com>
2018-01-20 01:19:55 +0000

    Reformat TypeConversionTest to Google Java Style guide

--- src/test/java/org/tron/utils/TypeConversionTest.java
+++ src/test/java/org/tron/utils/TypeConversionTest.java
@@ -15,37 +15,41 @@
 
 package org.tron.utils;
 
+import static org.tron.common.utils.TypeConversion.bytesToHexString;
+import static org.tron.common.utils.TypeConversion.bytesToLong;
+import static org.tron.common.utils.TypeConversion.hexStringToBytes;
+import static org.tron.common.utils.TypeConversion.longToBytes;
+
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import static org.tron.common.utils.TypeConversion.*;
-
 
 public class TypeConversionTest {
-    private static final Logger logger = LoggerFactory.getLogger("Test");
 
-    @Test
-    public void testLongToBytes() {
-        byte[] result = longToBytes(123L);
-        logger.info("long 123 to bytes is: {}", result);
-    }
+  private static final Logger logger = LoggerFactory.getLogger("Test");
 
-    @Test
-    public void testBytesToLong() {
-        long result = bytesToLong(new byte[]{0, 0, 0, 0, 0, 0, 0, 124});
-        logger.info("bytes 124 to long is: {}", result);
-    }
+  @Test
+  public void testLongToBytes() {
+    byte[] result = longToBytes(123L);
+    logger.info("long 123 to bytes is: {}", result);
+  }
 
-    @Test
-    public void testBytesToHexString() {
-        String result = bytesToHexString(new byte[]{0, 0, 0, 0, 0, 0, 0, 125});
-        logger.info("bytes 125 to hex string is: {}", result);
-    }
+  @Test
+  public void testBytesToLong() {
+    long result = bytesToLong(new byte[]{0, 0, 0, 0, 0, 0, 0, 124});
+    logger.info("bytes 124 to long is: {}", result);
+  }
 
-    @Test
-    public void testHexStringToBytes() {
-        byte[] result = hexStringToBytes("7f");
-        logger.info("hex string 7f to bytes is: {}", result);
-    }
+  @Test
+  public void testBytesToHexString() {
+    String result = bytesToHexString(new byte[]{0, 0, 0, 0, 0, 0, 0, 125});
+    logger.info("bytes 125 to hex string is: {}", result);
+  }
+
+  @Test
+  public void testHexStringToBytes() {
+    byte[] result = hexStringToBytes("7f");
+    logger.info("hex string 7f to bytes is: {}", result);
+  }
 }
