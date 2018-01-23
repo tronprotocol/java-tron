95cb39c5e148b95ca64a77b2b9ae4fe04cab9e7f
zhaohong229 <zhaohong229@gmail.com>
2018-01-23 17:22:13 +0800

    add witness service

---
+++ src/main/java/org/tron/core/services/WitnessService.java
@@ -0,0 +1,29 @@
+package org.tron.core.services;
+
+import org.tron.common.application.Service;
+
+public class WitnessService implements Service {
+
+    @Override
+    public void start() {
+        GereateBlockLoop loop = new GereateBlockLoop();
+        Thread Gereatethread = new Thread(loop);
+        Gereatethread.start();
+    }
+
+    @Override
+    public void stop() {
+
+    }
+    private void generateBlockLoop() {
+
+    }
+    private class GereateBlockLoop implements Runnable {
+
+        @Override
+        public void run() {
+
+        }
+    }
+
+}
