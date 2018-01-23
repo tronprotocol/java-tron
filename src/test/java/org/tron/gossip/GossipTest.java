120b6b9852f3affa0ed952adc42c2e974c97a855
sasaxie <xiexiaodong917@163.com>
2018-01-23 17:10:35 +0800

    test: 1. add gossip unit test

--- src/test/java/org/tron/gossip/GossipTest.java
+++ src/test/java/org/tron/gossip/GossipTest.java
@@ -15,38 +15,67 @@
 
 package org.tron.gossip;
 
-import org.junit.Assert;
-import org.junit.BeforeClass;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.apache.gossip.LocalMember;
 import org.junit.Test;
-import org.tron.common.overlay.example.LocalNode;
-import org.tron.common.overlay.message.Message;
-import org.tron.common.overlay.message.Type;
+import org.tron.common.overlay.gossip.LocalNode;
+import org.tron.core.BlockUtils;
+import org.tron.core.net.message.BlockMessage;
+import org.tron.core.net.message.Message;
+import org.tron.protos.core.TronBlock;
 
+import java.io.UnsupportedEncodingException;
+import java.util.List;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
 
 public class GossipTest {
-    private final static String CLUSTER = "mycluster";
-    private static LocalNode standNode = null;
+    private static LocalNode standNode = LocalNode.getInstance();
     private Semaphore lock = new Semaphore(0);
-    private Object sharedData = null;
-
-    @BeforeClass
-    public static void init() {
-        standNode = new LocalNode(CLUSTER, "udp://localhost:10000", "0");
-    }
 
     @Test
     public void testGossipBroadcast() throws InterruptedException {
         standNode.getGossipManager().registerSharedDataSubscriber((key, oldValue, newValue) -> {
-            if (key.equals("block")) {
-                sharedData = newValue;
+            byte[] newValueBytes = null;
+            try {
+                newValueBytes = newValue.toString().getBytes("ISO-8859-1");
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
             }
+
+            TronBlock.Block block = null;
+
+            try {
+                block = TronBlock.Block.parseFrom(newValueBytes);
+            } catch (InvalidProtocolBufferException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(BlockUtils.toPrintString(block));
         });
 
-        Message message = new Message("test", Type.BLOCK);
+        Message message = new BlockMessage(BlockUtils.newBlock(null, ByteString.copyFrom(new byte[]{1}), ByteString
+                .copyFrom(new byte[]{2}), 3L));
         standNode.broadcast(message);
         lock.tryAcquire(10, TimeUnit.SECONDS);
-        Assert.assertEquals("test", sharedData);
+    }
+
+    @Test
+    public void testGossipGetLiveMembers() {
+        List<LocalMember> memberList = standNode.getLiveMembers();
+
+        for (LocalMember l : memberList) {
+            System.out.println(l);
+        }
+    }
+
+    @Test
+    public void testGossipGetDeadMembers() {
+        List<LocalMember> memberList = standNode.getDeadMembers();
+
+        for (LocalMember l : memberList) {
+            System.out.println(l);
+        }
     }
 }
