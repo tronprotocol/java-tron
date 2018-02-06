d11c512cb74cc8eb8233edcb8c68ce1dc8144e93
Heng Zhang <zhangheng1536@gmail.com>
2018-02-06 17:38:22 +0800

    feat: add help command function

--- src/main/java/org/tron/program/Args.java
+++ src/main/java/org/tron/program/Args.java
@@ -1,4 +1,19 @@
 package org.tron.program;
 
+import com.beust.jcommander.Parameter;
+
 public class Args {
+
+  @Parameter(names = {"-d", "--outputDirectory"}, description = "Directory")
+  private String outputDirectory = new String("");
+  @Parameter(names = {"-h", "--help"}, help = true, description = "Directory")
+  private boolean help = false;
+
+  public String getOutputDirectory() {
+    return outputDirectory;
+  }
+
+  public boolean isHelp() {
+    return help;
+  }
 }
--- src/main/java/org/tron/program/FullNode.java
+++ src/main/java/org/tron/program/FullNode.java
@@ -1,5 +1,6 @@
 package org.tron.program;
 
+import com.beust.jcommander.JCommander;
 import org.tron.common.application.Application;
 import org.tron.common.application.ApplicationFactory;
 import org.tron.core.services.RpcApiService;
@@ -7,9 +8,22 @@ import org.tron.core.services.WitnessService;
 
 public class FullNode {
 
+
   public static void main(String args[]) {
+
+    Args cfgArgs = new Args();
+    JCommander.newBuilder()
+        .addObject(cfgArgs)
+        .build()
+        .parse(args);
+    if (cfgArgs.isHelp()) {
+      System.out.println("help = [" + cfgArgs.isHelp() + "]");
+      return;
+    }
+    System.out.println("debug = [" + cfgArgs.getOutputDirectory() + "]");
+
     Application tApp = ApplicationFactory.create();
-    tApp.init("/configPath", new Args());
+    tApp.init(cfgArgs.getOutputDirectory(), new Args());
     RpcApiService rpcApiService = new RpcApiService(tApp);
     tApp.addService(rpcApiService);
     tApp.addService(new WitnessService(tApp));
