638c07a69aba30e45679f0fb73d4e4c077b5be66
troncoin <we151760we@163.com>
2018-02-07 17:26:39 +0800

    Add exception analysis before generating the block and Update the witnessServer code

---
+++ src/main/java/org/tron/core/witness/BlockProductionCondition.java
@@ -0,0 +1,13 @@
+package org.tron.core.witness;
+
+public enum  BlockProductionCondition {
+  PRODUCED,           // Successfully generated block
+  NOT_SYNCED,
+  NOT_MY_TURN,        // It isn't my turn
+  NOT_TIME_YET,       // Not yet arrived
+  NO_PRIVATE_KEY,
+  LOW_PARTICIPATION,
+  LAG,
+  CONSECUTIVE,
+  EXCEPTION_PRODUCING_BLOCK
+}
