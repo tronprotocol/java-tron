253f57203cef1a943e08cbdf050240de52c319fa
Sheip <sdargutev@gmail.com>
2018-02-12 21:21:34 +0100

    fix bug in LevelDbDataSourceImpl.closeDB, that can cause deadlock and memory leak when calling this method multiple times.

--- src/test/java/org/tron/common/storage/leveldb/LevelDbDataSourceImplTest.java
+++ src/test/java/org/tron/common/storage/leveldb/LevelDbDataSourceImplTest.java
@@ -1,38 +1,39 @@
 /*
- * Copyright (c) [2016] [ <ether.camp> ]
- * This file is part of the ethereumJ library.
+ * Copyright (c) [2016] [ <ether.camp> ] This file is part of the ethereumJ library.
  *
- * The ethereumJ library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * The ethereumJ library is free software: you can redistribute it and/or modify it under the terms
+ * of the GNU Lesser General Public License as published by the Free Software Foundation, either
+ * version 3 of the License, or (at your option) any later version.
  *
- * The ethereumJ library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
+ * The ethereumJ library is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+ * PURPOSE. See the GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with the ethereumJ library. If not, see <http://www.gnu.org/licenses/>.
+ * You should have received a copy of the GNU Lesser General Public License along with the ethereumJ
+ * library. If not, see <http://www.gnu.org/licenses/>.
  */
 
 package org.tron.common.storage.leveldb;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-
-import org.junit.Ignore;
+import org.junit.Before;
 import org.junit.Test;
 import org.tron.common.utils.ByteArray;
 import org.tron.core.Constant;
 
-@Ignore
 public class LevelDbDataSourceImplTest {
+
+  private LevelDbDataSourceImpl dataSource;
+
+  @Before
+  public void setup() {
+    dataSource = new LevelDbDataSourceImpl(Constant.TEST, Constant.OUTPUT_DIR, "test");
+  }
 
   @Test
   public void testGet() {
-    LevelDbDataSourceImpl dataSource = new LevelDbDataSourceImpl(Constant.TEST, Constant.OUTPUT_DIR,
-        "test");
     dataSource.initDB();
     String key1 = "000134yyyhy";
     byte[] key = key1.getBytes();
@@ -44,8 +45,6 @@ public class LevelDbDataSourceImplTest {
 
   @Test
   public void testPut() {
-    LevelDbDataSourceImpl dataSource = new LevelDbDataSourceImpl(Constant.TEST, Constant.OUTPUT_DIR,
-        "test");
     dataSource.initDB();
     String key1 = "000134yyyhy";
     byte[] key = key1.getBytes();
@@ -63,11 +62,21 @@ public class LevelDbDataSourceImplTest {
 
   @Test
   public void testRest() {
-
-    LevelDbDataSourceImpl dataSource = new LevelDbDataSourceImpl(Constant.TEST_CONF,
-        Constant.OUTPUT_DIR, "test");
     dataSource.resetDB();
     dataSource.closeDB();
+  }
+
+  @Test(timeout = 100)
+  public void testLockReleased() {
+    dataSource.initDB();
+    // normal close
+    dataSource.closeDB();
+    // closing already closed db.
+    dataSource.closeDB();
+    // closing again to make sure the lock is free. If not test will hang.
+    dataSource.closeDB();
+
+    assertFalse("Database is still alive after closing.", dataSource.isAlive());
   }
 
 }
