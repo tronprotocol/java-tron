8db8757e9957daa8ffbdfd11a92180847b1c85a5
xiechang33 <354035097@qq.com>
2018-03-05 20:44:25 +0800

    test:improve unit test

--- src/test/java/org/tron/storage/leveldb/LevelDbDataSourceImplTest.java
+++ src/test/java/org/tron/storage/leveldb/LevelDbDataSourceImplTest.java
@@ -66,7 +66,7 @@ public class LevelDbDataSourceImplTest {
   @Test
   public void testRest() {
     LevelDbDataSourceImpl dataSource = new LevelDbDataSourceImpl(Constant.OUTPUT_DIR, "test2");
-    dataSource.initDB();
+    dataSource.resetDb();
     assertEquals(0, dataSource.allKeys().size());
     dataSource.closeDB();
   }
