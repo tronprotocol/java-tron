a8f324ea1583d611d707ced512f52dcc144bbe54
zhaohong <zhaohong229@gmail.com>
2018-03-06 11:52:51 +0800

    fix the build error sha256hash.

--- src/main/java/org/tron/common/application/ApplicationImpl.java
+++ src/main/java/org/tron/common/application/ApplicationImpl.java
@@ -34,9 +34,8 @@ public class ApplicationImpl implements Application, NodeDelegate {
   @Override
   public List<Sha256Hash> getBlockHashes(List<Sha256Hash> blockChainSummary) {
     //todo: return the blocks it should be have.
-
     List<Sha256Hash> retBlockHashes = new ArrayList<>();
-    Sha256Hash lastKnownBlkHash = Sha256Hash.ZERO_HASH;
+    Sha256Hash lastKnownBlkHash = Sha25Hash.ZERO_HASH;
 
     if (!blockChainSummary.isEmpty()) {
       //todo: find a block we all know between the summary and my db.
