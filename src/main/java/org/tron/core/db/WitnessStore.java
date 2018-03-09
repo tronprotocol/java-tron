240169f7d785060c7acbb16164f485013279ba7a
Matt Yue <yrp1990@gmail.com>
2018-03-09 20:15:31 +0800

    associate undo mechanism with database

--- src/main/java/org/tron/core/db/WitnessStore.java
+++ src/main/java/org/tron/core/db/WitnessStore.java
@@ -3,9 +3,9 @@ package org.tron.core.db;
 import com.google.protobuf.ByteString;
 import java.util.List;
 import java.util.stream.Collectors;
+import org.apache.commons.lang3.ArrayUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.tron.core.capsule.AccountCapsule;
 import org.tron.core.capsule.WitnessCapsule;
 
 public class WitnessStore extends TronDatabase<WitnessCapsule> {
@@ -18,11 +18,24 @@ public class WitnessStore extends TronDatabase<WitnessCapsule> {
 
   @Override
   public void put(byte[] key, WitnessCapsule item) {
+    logger.info("voteAddress is {} ", item.getAddress());
 
+    byte[] value = dbSource.getData(key);
+    if (ArrayUtils.isNotEmpty(value)) {
+      onModify(key, value);
+    }
+
+    dbSource.putData(item.getAddress().toByteArray(), item.getData());
+
+    if (ArrayUtils.isEmpty(value)) {
+      onCreate(key);
+    }
   }
 
   @Override
   public void delete(byte[] key) {
+    // This should be called just before an object is removed.
+    onDelete(key);
 
   }
 
