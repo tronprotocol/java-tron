2bed6f20cb99aac18dbf1759e48581b9810f62c8
olivier <olenheim@hotmail.com>
2018-03-16 13:12:31 +0800

    fix when initGeniss the head is null's problem

--- src/main/java/org/tron/core/db/Manager.java
+++ src/main/java/org/tron/core/db/Manager.java
@@ -178,11 +178,10 @@ public class Manager {
     this.numHashCache = new LevelDbDataSourceImpl(
         Args.getInstance().getOutputDirectory(), "block" + "_NUM_HASH");
     this.numHashCache.initDB();
-    this.initHeadBlock(Sha256Hash.wrap(this.dynamicPropertiesStore.getLatestBlockHeaderHash()));
-    this.khaosDb = new KhaosDatabase("block" + "_KDB");
-    this.khaosDb.start(head);
 
+    this.khaosDb = new KhaosDatabase("block" + "_KDB");
     this.pendingTrxs = new ArrayList<>();
+
     try {
       this.initGenesis();
     } catch (ContractValidateException e) {
@@ -198,6 +197,9 @@ public class Manager {
       logger.error(e.getMessage());
       System.exit(-1);
     }
+
+    this.initHeadBlock(Sha256Hash.wrap(this.dynamicPropertiesStore.getLatestBlockHeaderHash()));
+    this.khaosDb.start(head);
   }
 
   public BlockId getGenesisBlockId() {
@@ -406,7 +408,8 @@ public class Manager {
 
     BlockCapsule newBlock = this.khaosDb.push(block);
     //DB don't need lower block
-    if (newBlock.getNum() <= head.getNum()) {
+    if (!(head == null && newBlock.getNum() == 1)
+        || newBlock.getNum() <= head.getNum()) {
       return;
     }
 
