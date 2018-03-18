60e433b4f85cb88e507b6c361e760bfca6ff9d46
Sean Robbins <sean.m.robbins@gmail.com>
2018-03-18 17:05:41 +0000

    Remove ValidationException from imports

--- src/main/java/org/tron/core/net/node/NodeDelegateImpl.java
+++ src/main/java/org/tron/core/net/node/NodeDelegateImpl.java
@@ -7,7 +7,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.LongStream;
-import javax.xml.bind.ValidationException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.tron.common.utils.Sha256Hash;
