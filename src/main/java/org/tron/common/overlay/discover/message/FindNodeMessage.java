846bd2b6970520702c7f9fc092321d544838d48e
sasaxie <xiexiaodong917@163.com>
2018-03-20 00:00:08 +0800

    add discover message

--- src/main/java/org/tron/common/overlay/discover/message/FindNodeMessage.java
+++ src/main/java/org/tron/common/overlay/discover/message/FindNodeMessage.java
@@ -1,87 +1,51 @@
-/*
- * Copyright (c) [2016] [ <ether.camp> ]
- * This file is part of the ethereumJ library.
- *
- * The ethereumJ library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The ethereumJ library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with the ethereumJ library. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.tron.common.overlay.discover.message;
 
-import org.ethereum.crypto.ECKey;
-import org.ethereum.util.ByteUtil;
-import org.ethereum.util.RLP;
-import org.ethereum.util.RLPItem;
-import org.ethereum.util.RLPList;
-import org.spongycastle.util.encoders.Hex;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.tron.core.net.message.MessageTypes;
+import org.tron.protos.Message;
 
-import static org.ethereum.util.ByteUtil.longToBytesNoLeadZeroes;
+public class FindNodeMessage extends DiscoverMessage {
 
-public class FindNodeMessage extends Message {
+  private Message.FindNeighbours findNeighbours;
 
-    byte[] target;
-    long expires;
+  public FindNodeMessage(byte[] data) {
+    super(data);
+  }
 
-    @Override
-    public void parse(byte[] data) {
-
-        RLPList list = (RLPList) RLP.decode2OneItem(data, 0);
-
-        RLPItem target = (RLPItem) list.get(0);
-        RLPItem expires = (RLPItem) list.get(1);
-
-        this.target = target.getRLPData();
-        this.expires = ByteUtil.byteArrayToLong(expires.getRLPData());
+  private void unPack() {
+    try {
+      this.findNeighbours = Message.FindNeighbours.parseFrom(data);
+    } catch (InvalidProtocolBufferException e) {
+      e.printStackTrace();
     }
+    unpacked = true;
+  }
 
+  private void pack() {
+    this.data = this.findNeighbours.toByteArray();
+  }
 
-    public static FindNodeMessage create(byte[] target, ECKey privKey) {
-
-        long expiration = 90 * 60 + System.currentTimeMillis() / 1000;
-
-        /* RLP Encode data */
-        byte[] rlpToken = RLP.encodeElement(target);
-
-        byte[] rlpExp = longToBytesNoLeadZeroes(expiration);
-        rlpExp = RLP.encodeElement(rlpExp);
-
-        byte[] type = new byte[]{3};
-        byte[] data = RLP.encodeList(rlpToken, rlpExp);
-
-        FindNodeMessage findNode = new FindNodeMessage();
-        findNode.encode(type, data, privKey);
-        findNode.target = target;
-        findNode.expires = expiration;
-
-        return findNode;
+  @Override
+  public byte[] getData() {
+    if (this.data == null) {
+      this.pack();
     }
+    return this.data;
+  }
 
-    public byte[] getTarget() {
-        return target;
-    }
+  @Override
+  public String toString() {
 
-    public long getExpires() {
-        return expires;
-    }
+    long currTime = System.currentTimeMillis() / 1000;
 
-    @Override
-    public String toString() {
+    String out = String.format("[FindNodeMessage] \n");
 
-        long currTime = System.currentTimeMillis() / 1000;
+    return out;
+  }
 
-        String out = String.format("[FindNodeMessage] \n target: %s \n expires in %d seconds \n %s\n",
-                Hex.toHexString(target), (expires - currTime), super.toString());
-
-        return out;
-    }
+  @Override
+  public MessageTypes getType() {
+    return null;
+  }
 
 }
--- src/main/java/org/tron/common/overlay/discover/message/PongMessage.java
+++ src/main/java/org/tron/common/overlay/discover/message/PongMessage.java
@@ -1,108 +1,48 @@
-/*
- * Copyright (c) [2016] [ <ether.camp> ]
- * This file is part of the ethereumJ library.
- *
- * The ethereumJ library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * The ethereumJ library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with the ethereumJ library. If not, see <http://www.gnu.org/licenses/>.
- */
 package org.tron.common.overlay.discover.message;
 
-import org.ethereum.crypto.ECKey;
-import org.ethereum.util.ByteUtil;
-import org.ethereum.util.RLP;
-import org.ethereum.util.RLPItem;
-import org.ethereum.util.RLPList;
-import org.spongycastle.util.encoders.Hex;
+import com.google.protobuf.InvalidProtocolBufferException;
+import org.tron.core.net.message.MessageTypes;
+import org.tron.protos.Message;
 
-import static org.ethereum.util.ByteUtil.longToBytes;
-import static org.ethereum.util.ByteUtil.stripLeadingZeroes;
+public class PongMessage extends DiscoverMessage {
 
-public class PongMessage extends Message {
+  private Message.PongMessage pongMessage;
 
-    byte[] token; // token is the MDC of the ping
-    long expires;
+  public PongMessage(byte[] data) {
+    super(data);
+  }
 
-    public static PongMessage create(byte[] token, Node toNode, ECKey privKey) {
-
-        long expiration = 90 * 60 + System.currentTimeMillis() / 1000;
-
-        byte[] rlpToList = toNode.getBriefRLP();
-
-        /* RLP Encode data */
-        byte[] rlpToken = RLP.encodeElement(token);
-        byte[] tmpExp = longToBytes(expiration);
-        byte[] rlpExp = RLP.encodeElement(stripLeadingZeroes(tmpExp));
-
-        byte[] type = new byte[]{2};
-        byte[] data = RLP.encodeList(rlpToList, rlpToken, rlpExp);
-
-        PongMessage pong = new PongMessage();
-        pong.encode(type, data, privKey);
-
-        pong.token = token;
-        pong.expires = expiration;
-
-        return pong;
+  private void unPack() {
+    try {
+      this.pongMessage = Message.PongMessage.parseFrom(data);
+    } catch (InvalidProtocolBufferException e) {
+      e.printStackTrace();
+    }
+    unpacked = true;
     }
 
-    public static PongMessage create(byte[] token, ECKey privKey) {
-        return create(token, privKey, 3 + System.currentTimeMillis() / 1000);
+  private void pack() {
+    this.data = this.pongMessage.toByteArray();
     }
 
-    static PongMessage create(byte[] token, ECKey privKey, long expiration) {
-
-        /* RLP Encode data */
-        byte[] rlpToken = RLP.encodeElement(token);
-        byte[] rlpExp = RLP.encodeElement(ByteUtil.longToBytes(expiration));
-
-        byte[] type = new byte[]{2};
-        byte[] data = RLP.encodeList(rlpToken, rlpExp);
-
-        PongMessage pong = new PongMessage();
-        pong.encode(type, data, privKey);
-
-        pong.token = token;
-        pong.expires = expiration;
-
-        return pong;
+  @Override
+  public byte[] getData() {
+    if (this.data == null) {
+      this.pack();
     }
+    return this.data;
+  }
 
+  @Override
+  public MessageTypes getType() {
+    return null;
+  }
 
-    @Override
-    public void parse(byte[] data) {
-        RLPList list = (RLPList) RLP.decode2OneItem(data, 0);
+  @Override
+  public String toString() {
 
-        this.token = list.get(0).getRLPData();
-        RLPItem expires = (RLPItem) list.get(1);
-        this.expires = ByteUtil.byteArrayToLong(expires.getRLPData());
-    }
+    String out = String.format("[PongMessage]\n");
 
-
-    public byte[] getToken() {
-        return token;
-    }
-
-    public long getExpires() {
-        return expires;
-    }
-
-    @Override
-    public String toString() {
-        long currTime = System.currentTimeMillis() / 1000;
-
-        String out = String.format("[PongMessage] \n token: %s \n expires in %d seconds \n %s\n",
-                Hex.toHexString(token), (expires - currTime), super.toString());
-
-        return out;
-    }
+    return out;
+  }
 }
