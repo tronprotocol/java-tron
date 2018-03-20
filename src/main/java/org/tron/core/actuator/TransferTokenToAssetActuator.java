369d7e86b639495c57540459618d585c26cc7040
jack forest <785748374@qq.com>
2018-03-20 18:08:27 +0800

    transfer token to asset contractor

--- src/main/java/org/tron/core/actuator/TransferTokenToAssetActuator.java
+++ src/main/java/org/tron/core/actuator/TransferTokenToAssetActuator.java
@@ -15,13 +15,18 @@
 
 package org.tron.core.actuator;
 
+import com.google.common.base.Preconditions;
 import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
+import org.tron.core.capsule.AccountCapsule;
+import org.tron.core.capsule.AssetIssueCapsule;
 import org.tron.core.capsule.TransactionResultCapsule;
 import org.tron.core.db.Manager;
 import org.tron.core.exception.ContractExeException;
 import org.tron.core.exception.ContractValidateException;
+import org.tron.protos.Contract;
+import org.tron.protos.Protocol;
 
 public class TransferTokenToAssetActuator extends AbstractActuator {
 
@@ -30,13 +35,85 @@ public class TransferTokenToAssetActuator extends AbstractActuator {
   }
 
   @Override
-  public boolean execute(TransactionResultCapsule result) throws ContractExeException {
-    return false;
+  public boolean execute(TransactionResultCapsule ret) throws ContractExeException {
+    long fee = calcFee();
+
+    try {
+      Contract.TransferTokenToAssetContract token2AssetContract =
+              contract.unpack(Contract.TransferTokenToAssetContract.class);
+
+      int cost = token2AssetContract.getTrxNum();
+
+      //subtract from owner address
+      byte[] ownerAddressBytes = token2AssetContract.getOwnerAddress().toByteArray();
+      AccountCapsule ownerAccount = this.dbManager.getAccountStore().get(ownerAddressBytes);
+      ownerAccount.setBalance(ownerAccount.getBalance() - cost);
+
+      //calculate the exchange amount
+      AssetIssueCapsule assetIssueCapsule =
+              this.dbManager.getAssetIssueStore().get(token2AssetContract.getName().toByteArray());
+      //the exchangeAmount will actually be a floating-point?
+      int exchangeAmount = cost * assetIssueCapsule.getTrxNum() / assetIssueCapsule.getNum();
+
+      //add to to_address
+      byte[] toAddressBytes = token2AssetContract.getToAddress().toByteArray();
+      AccountCapsule toAccount = this.dbManager.getAccountStore().get(toAddressBytes);
+      toAccount.setBalance(toAccount.getBalance() + exchangeAmount);
+
+      //write to db
+      dbManager.getAccountStore().put(ownerAddressBytes, ownerAccount);
+      dbManager.getAccountStore().put(toAddressBytes, toAccount);
+
+      ret.setStatus(fee, Protocol.Transaction.Result.code.SUCESS);
+
+      return true;
+    }
+    catch (InvalidProtocolBufferException e) {
+      ret.setStatus(fee, Protocol.Transaction.Result.code.FAILED);
+      e.printStackTrace();
+      throw new ContractExeException(e.getMessage());
+    }
   }
 
   @Override
   public boolean validate() throws ContractValidateException {
-    return false;
+    if (!this.contract.is(Contract.TransferTokenToAssetContract.class)) {
+      throw new ContractValidateException();
+    }
+
+    try {
+      final Contract.TransferTokenToAssetContract token2AssetContract =
+              this.contract.unpack(Contract.TransferTokenToAssetContract.class);
+
+      Preconditions.checkNotNull(token2AssetContract.getOwnerAddress(), "OwnerAddress is null");
+      Preconditions.checkNotNull(token2AssetContract.getToAddress(), "ToAddress is null");
+      Preconditions.checkNotNull(token2AssetContract.getName(), "trx name is null");
+      if(token2AssetContract.getTrxNum() < 0){
+        throw new ContractValidateException("Trx Num can not be negative!");
+      }
+
+      byte[] addressBytes = token2AssetContract.getOwnerAddress().toByteArray();
+      //Whether the account exist
+      if (! this.dbManager.getAccountStore().has(addressBytes)) {
+        throw new ContractValidateException("Account does not exist!");
+      }
+
+      AccountCapsule ac = this.dbManager.getAccountStore().get(addressBytes);
+      //Whether the balance is enough
+      if(ac.getBalance() < token2AssetContract.getTrxNum()){
+        throw new ContractValidateException();
+      }
+
+      //Whether have the mapping
+      if( !this.dbManager.getAssetIssueStore().has(token2AssetContract.getName().toByteArray()) ){
+        throw new ContractValidateException();
+      }
+    }
+    catch (InvalidProtocolBufferException e) {
+      throw new ContractValidateException();
+    }
+
+    return true;
   }
 
   @Override
