03a068decebe2b0e2804c40e6fbdeddd75d0abf4
Heng Zhang <zhangheng1536@gmail.com>
2018-03-21 15:49:45 +0800

    fix : check fee.

--- src/main/java/org/tron/core/actuator/AssetIssueActuator.java
+++ src/main/java/org/tron/core/actuator/AssetIssueActuator.java
@@ -34,6 +34,7 @@ import org.tron.protos.Protocol.Transaction.Result.code;
 
 @Slf4j
 public class AssetIssueActuator extends AbstractActuator {
+
   AssetIssueActuator(Any contract, Manager dbManager) {
     super(contract, dbManager);
   }
@@ -54,7 +55,7 @@ public class AssetIssueActuator extends AbstractActuator {
       dbManager.getAssetIssueStore()
           .put(assetIssueCapsule.getName().toByteArray(), assetIssueCapsule);
 
-      dbManager.adjustBalance(assetIssueContract.getOwnerAddress().toByteArray(), fee);
+      dbManager.adjustBalance(assetIssueContract.getOwnerAddress().toByteArray(), -calcFee());
       ret.setStatus(fee, code.SUCESS);
 
       AccountCapsule accountCapsule = dbManager.getAccountStore()
