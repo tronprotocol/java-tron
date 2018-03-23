2744457872890d12947fb508f55104e5d9e37277
sasaxie <xiexiaodong917@163.com>
2018-03-23 16:01:22 +0800

    add TronMessageFactory case

--- src/main/java/org/tron/core/net/message/TronMessageFactory.java
+++ src/main/java/org/tron/core/net/message/TronMessageFactory.java
@@ -14,16 +14,30 @@ public class TronMessageFactory extends MessageFactory {
     switch (receivedTypes) {
       case TRX:
         return new TransactionMessage(packed);
-      case TRXS:
-        return new TransactionsMessage(packed);
       case BLOCK:
         return new BlockMessage(packed);
+      case TRXS:
+        return new TransactionsMessage(packed);
       case BLOCKS:
         return new BlocksMessage(packed);
       case BLOCKHEADERS:
         return new BlockHeadersMessage(packed);
       case INVENTORY:
         return new InventoryMessage(packed);
+      case FETCH_INV_DATA:
+        return new FetchInvDataMessage(packed);
+      case SYNC_BLOCK_CHAIN:
+        return new SyncBlockChainMessage(packed);
+      case BLOCK_CHAIN_INVENTORY:
+        return new ChainInventoryMessage(packed);
+      case ITEM_NOT_FOUND:
+        return new ItemNotFound();
+      case FETCH_BLOCK_HEADERS:
+        return new FetchBlockHeadersMessage(packed);
+      case BLOCK_INVENTORY:
+        return new BlockInventoryMessage(packed);
+      case TRX_INVENTORY:
+        return new TransactionInventoryMessage(packed);
       default:
         throw new IllegalArgumentException("No such message");
     }
