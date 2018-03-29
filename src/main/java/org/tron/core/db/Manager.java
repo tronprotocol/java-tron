9832f8981310804e3a4f3bf89c25f2bcd0fe391b
zhaohong <zhaohong229@gmail.com>
2018-03-29 16:55:47 +0800

    fix && improve: pending transaction should be restore when exception occurs.

--- src/main/java/org/tron/core/db/Manager.java
+++ src/main/java/org/tron/core/db/Manager.java
@@ -87,6 +87,7 @@ public class Manager {
   @Getter
   private BlockCapsule head;
   private RevokingDatabase revokingStore;
+  @Getter
   private DialogOptional<Dialog> dialog = DialogOptional.empty();
 
   @Getter
@@ -116,6 +117,10 @@ public class Manager {
 
   public List<TransactionCapsule> getPendingTransactions() {
     return this.pendingTransactions;
+  }
+
+  public List<TransactionCapsule> getPoppedTransactions() {
+    return this.popedTransactions;
   }
 
 
@@ -524,94 +529,53 @@ public class Manager {
       throws ValidateSignatureException, ContractValidateException,
       ContractExeException, UnLinkedBlockException {
 
-    List<TransactionCapsule> pendingTrxsTmp = new ArrayList<>(pendingTransactions);
-    //TODO: optimize performance here.
-    pendingTransactions.clear();
-    dialog.reset();
+    try (PendingManager pm = new PendingManager(this)) {
+      //todo: check block's validity
+      if (!block.generatedByMyself) {
+        if (!block.validateSignature()) {
+          logger.info("The siganature is not validated.");
+          //TODO: throw exception here.
+          return;
+        }
 
-    //todo: check block's validity
-    if (!block.generatedByMyself) {
-      if (!block.validateSignature()) {
-        logger.info("The siganature is not validated.");
-        //TODO: throw exception here.
-        return;
+        if (!block.calcMerkleRoot().equals(block.getMerkleRoot())) {
+          logger.info("The merkler root doesn't match, Calc result is " + block.calcMerkleRoot()
+              + " , the headers is " + block.getMerkleRoot());
+          // TODO:throw exception here.
+          return;
+        }
       }
-
-      if (!block.calcMerkleRoot().equals(block.getMerkleRoot())) {
-        logger.info("The merkler root doesn't match, Calc result is " + block.calcMerkleRoot()
-            + " , the headers is " + block.getMerkleRoot());
-        //TODO: throw exception here.
-        return;
+      try {
+        validateWitnessSchedule(block); // direct return ,need test
+      } catch (Exception ex) {
+        logger.error("validateWitnessSchedule error", ex);
       }
+      BlockCapsule newBlock = this.khaosDb.push(block);
+      //DB don't need lower block
+      if (head == null) {
+        if (newBlock.getNum() != 0) {
+          return;
+        }
+      } else {
+        if (newBlock.getNum() <= head.getNum()) {
+          return;
+        }
+        //switch fork
+        if (!newBlock.getParentHash().equals(head.getBlockId())) {
+          switchFork(newBlock);
+        }
+        try (Dialog tmpDialog = revokingStore.buildDialog()) {
+          this.processBlock(newBlock);
+          tmpDialog.commit();
+        } catch (RevokingStoreIllegalStateException e) {
+          logger.debug(e.getMessage(), e);
+        }
+      }
+      blockStore.put(block.getBlockId().getBytes(), block);
+      this.numHashCache.putData(ByteArray.fromLong(block.getNum()), block.getBlockId().getBytes());
+      //refreshHead(newBlock);
+      logger.info("save block: " + newBlock);
     }
-
-    try {
-      validateWitnessSchedule(block); // direct return ,need test
-    } catch (Exception ex) {
-      logger.error("validateWitnessSchedule error", ex);
-    }
-
-    BlockCapsule newBlock = this.khaosDb.push(block);
-    //DB don't need lower block
-    if (head == null) {
-      if (newBlock.getNum() != 0) {
-        return;
-      }
-    } else {
-      if (newBlock.getNum() <= head.getNum()) {
-        return;
-      }
-      //switch fork
-      if (!newBlock.getParentHash().equals(head.getBlockId())) {
-        switchFork(newBlock);
-      }
-
-      try (Dialog tmpDialog = revokingStore.buildDialog()) {
-        this.processBlock(newBlock);
-        tmpDialog.commit();
-      } catch (RevokingStoreIllegalStateException e) {
-        logger.debug(e.getMessage(), e);
-      }
-    }
-
-    //filter trxs
-    pendingTrxsTmp.stream()
-        .filter(trx -> transactionStore.get(trx.getTransactionId().getBytes()) == null)
-        .forEach(trx -> {
-          try {
-            pushTransactions(trx);
-          } catch (ValidateSignatureException e) {
-            logger.debug(e.getMessage(), e);
-          } catch (ContractValidateException e) {
-            logger.debug(e.getMessage(), e);
-          } catch (ContractExeException e) {
-            logger.debug(e.getMessage(), e);
-          } catch (HighFreqException e) {
-            logger.debug(e.getMessage(), e);
-          }
-        });
-    popedTransactions.stream()
-        .filter(trx -> transactionStore.get(trx.getTransactionId().getBytes()) == null)
-        .forEach(trx -> {
-          try {
-            pushTransactions(trx);
-          } catch (ValidateSignatureException e) {
-            e.printStackTrace();
-          } catch (ContractValidateException e) {
-            e.printStackTrace();
-          } catch (ContractExeException e) {
-            e.printStackTrace();
-          } catch (HighFreqException e) {
-            e.printStackTrace();
-          }
-        });
-
-    popedTransactions.clear();
-
-    blockStore.put(block.getBlockId().getBytes(), block);
-    this.numHashCache.putData(ByteArray.fromLong(block.getNum()), block.getBlockId().getBytes());
-    //refreshHead(newBlock);
-    logger.info("save block: " + newBlock);
   }
 
   public void updateDynamicProperties(BlockCapsule block) {
