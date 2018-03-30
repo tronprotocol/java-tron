5debd6287d017a8cbd6d729a3b76694d530be480
Marcus <zhaohong229@gmail.com>
2018-03-30 16:09:23 +0800

    Update Parameter.java

--- src/main/java/org/tron/core/config/Parameter.java
+++ src/main/java/org/tron/core/config/Parameter.java
@@ -8,7 +8,7 @@ public interface Parameter {
     long ASSET_ISSUE_FEE = 1024000000; // 1024 trx 1024*10^6
     long VOTE_WITNESS_FEE = 10000; // 10000 drop
     long CREATE_ACCOUNT_FEE = 10000; // 10000 drop
-    long WITNESS_PAY_PER_BLOCK = 32000000;  // 3trx
+    long WITNESS_PAY_PER_BLOCK = 32000000;  // 32trx
     int BLOCK_PRODUCED_INTERVAL = 3; // 3sec
 
     double SOLIDIFIED_THRESHOLD = 0.3;
