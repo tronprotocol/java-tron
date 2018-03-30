464460b20d02fbc7e38d9e433339347618f45159
xiechang33 <354035097@qq.com>
2018-03-30 10:45:57 +0800

    improve unit test AccountStoreTest

--- src/test/java/org/tron/core/db/AccountStoreTest.java
+++ src/test/java/org/tron/core/db/AccountStoreTest.java
@@ -22,12 +22,6 @@ public class AccountStoreTest {
   private static final byte[] data = TransactionStoreTest.randomBytes(32);
   private static byte[] address = TransactionStoreTest.randomBytes(32);
   private static byte[] accountName = TransactionStoreTest.randomBytes(32);
-  private static final String ACCOUNT_SUN_ADDRESS
-      = "4948c2e8a756d9437037dcd8c7e0c73d560ca38d";
-  private static final String ACCOUNT_BLACKHOLE_ADDRESS
-      = "548794500882809695a8a687866e76d4271a146a";
-  private static final String ACCOUNT_ZION_ADDRESS
-      = "55ddae14564f82d5b94c7a131b5fcfd31ad6515a";
 
   @AfterClass
   public static void destroy() {
@@ -60,34 +54,7 @@ public class AccountStoreTest {
     ;
     Assert.assertTrue(AccountStoreTest.has(data));
   }
-
-  @Test
-  public void getSunBlackZion() {
-    //test getSun,getBlackhole and getZion Method
-    AccountCapsule accountCapsuleSun = new AccountCapsule(ByteString.copyFrom(address),
-        ByteString.copyFrom(accountName),
-        AccountType.forNumber(1));
-    AccountStoreTest.put(ByteString.copyFrom(ByteArray.fromHexString(ACCOUNT_SUN_ADDRESS))
-        .toByteArray(), accountCapsuleSun);
-    AccountCapsule accountCapsuleBlack = accountCapsuleSun;
-
-    Assert.assertEquals(accountCapsuleSun.getInstance(), AccountStoreTest.getSun().getInstance());
-
-    AccountStoreTest.put(ByteString.copyFrom(ByteArray.fromHexString(ACCOUNT_BLACKHOLE_ADDRESS))
-        .toByteArray(), accountCapsuleBlack);
-
-    Assert.assertEquals(accountCapsuleBlack.getInstance(),
-        AccountStoreTest.getBlackhole().getInstance());
-
-    AccountCapsule accountCapsuleZion = accountCapsuleSun;
-
-    AccountStoreTest.put(ByteString.copyFrom(ByteArray.fromHexString(ACCOUNT_ZION_ADDRESS))
-        .toByteArray(), accountCapsuleZion);
-
-    Assert.assertEquals(accountCapsuleZion.getInstance(),
-        AccountStoreTest.getZion().getInstance());
-  }
-
+  
 
   public static byte[] randomBytes(int length) {
     //generate the random number
