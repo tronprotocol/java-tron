cf0f5978d9510b72188a4612bc6ca1329e9b3095
olivier <olenheim@hotmail.com>
2018-04-02 23:15:13 +0800

    fix sync bug

--- src/main/java/org/tron/core/net/node/NodeDelegateImpl.java
+++ src/main/java/org/tron/core/net/node/NodeDelegateImpl.java
@@ -102,9 +102,10 @@ public class NodeDelegateImpl implements NodeDelegate {
 
     if (blockChainSummary.isEmpty() ||
         (blockChainSummary.size() == 1
-        && blockChainSummary.get(0).equals(dbManager.getHeadBlockId()))) {
+        && blockChainSummary.get(0).equals(dbManager.getGenesisBlockId()))) {
       unForkedBlockId = dbManager.getGenesisBlockId();
-    } else if (blockChainSummary.size() == 1) {
+    } else if (blockChainSummary.size() == 1
+        && blockChainSummary.get(0).getNum() == 0) {
      return new LinkedList<BlockId>(){{
         add(dbManager.getGenesisBlockId());
       }};
