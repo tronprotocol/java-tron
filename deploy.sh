bd800f85247ca21caa893b8ea7a8f67d38694ee5
Heng Zhang <zhangheng1536@gmail.com>
2018-04-03 21:01:53 +0800

    feat: add travis to deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 ./gradlew clean shadowJar
-ssh deploy@47.93.9.236 -p 22008 touch bbb.txt
-ssh deploy@47.93.9.236 -p 22008 mkdir tron
-scp -P 22008 build/libs/java-tron.jar deploy@47.93.9.236:/home/deploy/tron/
-scp -P 22008 start.sh deploy@47.93.9.236:/home/deploy/tron/
-# ssh deploy@47.93.9.236 -p 22008 sh tron/start.sh
+ssh tron@47.93.9.236 -p 22008 touch bbb.txt
+ssh tron@47.93.9.236 -p 22008 mkdir tron
+scp -P 22008 build/libs/java-tron.jar tron@47.93.9.236:/home/tron/tron/
+scp -P 22008 start.sh tron@47.93.9.236:/home/tron/tron/
+# ssh tron@47.93.9.236 -p 22008 sh tron/start.sh
 ls -a
