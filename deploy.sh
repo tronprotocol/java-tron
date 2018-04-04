8ace2af69c2f6210d30ba29fc6a0bbef3da2e246
huzhenyuan <huzhenyuan@peiwo.cn>
2018-04-04 12:08:17 +0800

    feat: add travis to deploy.

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-# ./gradlew clean shadowJar
+./gradlew clean shadowJar
 ssh tron@47.93.9.236 -p 22008 mkdir java-tron
 scp -P 22008 build/libs/java-tron.jar tron@47.93.9.236:/home/tron/java-tron/
 scp -P 22008 start.sh tron@47.93.9.236:/home/tron/java-tron/
-# ssh tron@47.93.9.236 -p 22008 sh java-tron/start.sh
+ssh tron@47.93.9.236 -p 22008 sh java-tron/start.sh
 ls -a
