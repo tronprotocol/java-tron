7c1b69eefbdb3ccd2d08b9b9b68946fcff64580c
zergweak <zhang_anwen@163.com>
2018-04-04 18:54:26 +0800

    remove test code

--- src/main/java/org/tron/core/db/Manager.java
+++ src/main/java/org/tron/core/db/Manager.java
@@ -289,7 +289,6 @@ public class Manager {
     if (this.containBlock(this.genesisBlock.getBlockId())) {
       Args.getInstance().setChainId(this.genesisBlock.getBlockId().toString());
     } else {
-      String test = Args.getInstance().getOutputDirectory();
       if (this.hasBlocks()) {
         logger.error("genesis block modify, please delete database directory({}) and restart",
             Args.getInstance().getOutputDirectory());
