c5a48930b3177bd8aee32d48019ef4da108475e6
huzhenyuan <huzhenyuan@peiwo.cn>
2018-04-04 20:48:46 +0800

    start test node as witness node

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kill -9 `cat /home/tron/pid.txt`
-nohup  java -jar /home/tron/java-tron/java-tron.jar -c /home/tron/config.conf > /home/tron/tron-shell.log 2>&1 & echo $! >/home/tron/pid.txt
+nohup  java -jar /home/tron/java-tron/java-tron.jar --witness -c /home/tron/config.conf > /home/tron/tron-shell.log 2>&1 & echo $! >/home/tron/pid.txt
