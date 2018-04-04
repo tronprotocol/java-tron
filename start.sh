bb587321a5b888925a04dc6bec4c17bc3b344f78
huzhenyuan <huzhenyuan@peiwo.cn>
2018-04-04 19:10:08 +0800

    modify config.conf path for test server

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 kill -9 `cat pid.txt`
-nohup  java -jar /home/tron/java-tron/java-tron.jar -c /home/tron/java-tron/config.conf > tron-shell.log 2>&1 & echo $! >pid.txt
+nohup  java -jar /home/tron/java-tron/java-tron.jar -c /home/tron/config.conf > tron-shell.log 2>&1 & echo $! >pid.txt
