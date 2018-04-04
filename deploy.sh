ee05a1d322dafaedc33dee7f2a5fd6ddcbd309f0
huzhenyuan <huzhenyuan@peiwo.cn>
2018-04-04 14:12:53 +0800

    use HOST_IP for deploy to all machine

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 ./gradlew clean shadowJar
-ssh tron@47.93.9.236 -p 22008 mkdir java-tron
-scp -P 22008 build/libs/java-tron.jar tron@47.93.9.236:/home/tron/java-tron/
-scp -P 22008 start.sh tron@47.93.9.236:/home/tron/java-tron/
-ssh tron@47.93.9.236 -p 22008 sh java-tron/start.sh
-ls -a
+
+HOST_IP_1=47.93.9.236
+HOST_IP_2=47.93.33.201
+HOST_IP_3=123.56.10.6
+HOST_IP_4=39.107.80.135
+HOST_IP_5=47.93.184.2
+
+for HOST_IP in $HOST_IP_1 $HOST_IP_2 $HOST_IP_3 $HOST_IP_4 $HOST_IP_5
+do
+    ssh tron@$HOST_IP -p 22008 mkdir java-tron
+    scp -P 22008 build/libs/java-tron.jar tron@$HOST_IP:/home/tron/java-tron/
+    scp -P 22008 start.sh tron@$HOST_IP:/home/tron/java-tron/
+    ssh tron@$HOST_IP -p 22008 sh java-tron/start.sh
+    ls -a
+done
