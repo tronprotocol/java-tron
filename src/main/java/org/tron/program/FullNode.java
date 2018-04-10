a31906501ac8cecc9a266f9457edd622adcb8bf1
Matt Yue <yrp1990@gmail.com>
2018-04-10 18:51:04 +0800

    del test code

--- src/main/java/org/tron/program/FullNode.java
+++ src/main/java/org/tron/program/FullNode.java
@@ -1,6 +1,5 @@
 package org.tron.program;
 
-import java.util.List;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,10 +8,8 @@ import org.tron.common.application.ApplicationFactory;
 import org.tron.core.Constant;
 import org.tron.core.config.DefaultConfig;
 import org.tron.core.config.args.Args;
-import org.tron.core.db.api.StoreAPI;
 import org.tron.core.services.RpcApiService;
 import org.tron.core.services.WitnessService;
-import org.tron.protos.Protocol.Block;
 
 @Slf4j
 public class FullNode {
@@ -27,7 +24,6 @@ public class FullNode {
 
     ApplicationContext context = new AnnotationConfigApplicationContext(DefaultConfig.class);
 
-    List<Block> blocks = context.getBean(StoreAPI.class).getLatestBlocks(1);
     if (cfgArgs.isHelp()) {
       logger.info("Here is the help message.");
       return;
