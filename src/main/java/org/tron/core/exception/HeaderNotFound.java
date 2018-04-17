734159eb0c0f1e1ef034d50616bcdd32127f403b
Matt Yue <yrp1990@gmail.com>
2018-04-17 13:05:10 +0800

    1. merge develop; 2. when initializing db, revokingStore.size should be 0

---
+++ src/main/java/org/tron/core/exception/HeaderNotFound.java
@@ -0,0 +1,12 @@
+package org.tron.core.exception;
+
+public class HeaderNotFound extends TronException {
+
+  public HeaderNotFound() {
+    super();
+  }
+
+  public HeaderNotFound(String message) {
+    super(message);
+  }
+}
