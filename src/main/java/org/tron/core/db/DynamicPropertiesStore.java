93722514d41850e967383631babacb849ce73976
nanfengpo <lsxxxxxx@126.com>
2018-04-17 22:44:02 +0800

    remove witness shuffle

--- src/main/java/org/tron/core/db/DynamicPropertiesStore.java
+++ src/main/java/org/tron/core/db/DynamicPropertiesStore.java
@@ -16,6 +16,7 @@ public class DynamicPropertiesStore extends TronStoreWithRevoking<BytesCapsule> 
 
   private static final long MAINTENANCE_TIME_INTERVAL = 24 * 3600 * 1000;// (ms)
   private static final long MAINTENANCE_SKIP_SLOTS = 2;
+  private static final int SINGLE_REPEAT = 2;
 
   private static final byte[] LATEST_BLOCK_HEADER_TIMESTAMP = "latest_block_header_timestamp"
       .getBytes();
@@ -236,6 +237,10 @@ public class DynamicPropertiesStore extends TronStoreWithRevoking<BytesCapsule> 
 
   public long getMaintenanceSkipSlots() {
     return MAINTENANCE_SKIP_SLOTS;
+  }
+
+  public int getSingleRepeat() {
+    return SINGLE_REPEAT;
   }
 
   private void setNextMaintenanceTime(DateTime nextMaintenanceTime) {
