11dcea394f98c0459577b3b192993ba868431d79
wubin01 <wb_bupt@163.com>
2018-04-20 19:02:10 +0800

    mdf nodeimpl

--- src/main/java/org/tron/core/net/node/NodeImpl.java
+++ src/main/java/org/tron/core/net/node/NodeImpl.java
@@ -4,6 +4,8 @@ import static org.tron.core.config.Parameter.NodeConstant.MAX_BLOCKS_ALREADY_FET
 import static org.tron.core.config.Parameter.NodeConstant.MAX_BLOCKS_IN_PROCESS;
 import static org.tron.core.config.Parameter.NodeConstant.MAX_BLOCKS_SYNC_FROM_ONE_PEER;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
 import com.google.common.collect.Iterables;
 import io.netty.util.internal.ConcurrentSet;
 import java.util.ArrayList;
@@ -69,6 +71,14 @@ public class NodeImpl extends PeerConnectionDelegate implements Node {
   @Autowired
   @Lazy
   private SyncPool pool;
+
+  Cache<Sha256Hash, TransactionMessage> TrxCache = CacheBuilder.newBuilder()
+          .maximumSize(10000).expireAfterWrite(60, TimeUnit.SECONDS)
+          .recordStats().build();
+
+  Cache<Sha256Hash, BlockMessage> BlockCache = CacheBuilder.newBuilder()
+          .maximumSize(1000).expireAfterWrite(60, TimeUnit.SECONDS)
+          .recordStats().build();
 
   class InvToSend {
 
@@ -241,10 +251,10 @@ public class NodeImpl extends PeerConnectionDelegate implements Node {
     if (msg instanceof BlockMessage) {
       logger.info("Ready to broadcast a block, Its hash is " + msg.getMessageId());
       freshBlockId.offer(((BlockMessage) msg).getBlockId());
-      blockToAdvertise.add(((BlockMessage) msg).getBlockId());
+      BlockCache.put(msg.getMessageId(), (BlockMessage) msg);
       type = InventoryType.BLOCK;
     } else if (msg instanceof TransactionMessage) {
-      trxToAdvertise.add(msg.getMessageId());
+      TrxCache.put(msg.getMessageId(), (TransactionMessage)msg);
       type = InventoryType.TRX;
     } else {
       return;
@@ -776,24 +786,36 @@ public class NodeImpl extends PeerConnectionDelegate implements Node {
     MessageTypes type = fetchInvDataMsg.getInvMessageType();
 
     //TODO:maybe can use message cache here
-    final BlockCapsule[] blocks = {del.getGenesisBlock()};
+    BlockCapsule block = null;
     //get data and send it one by one
-    fetchInvDataMsg.getHashList()
-        .forEach(hash -> {
-          if (del.contain(hash, type)) {
-            Message msg = del.getData(hash, type);
-            if (type.equals(MessageTypes.BLOCK)) {
-              blocks[0] = ((BlockMessage) msg).getBlockCapsule();
-            }
-            peer.sendMessage(msg);
-          } else {
-            peer.sendMessage(new ItemNotFound());
-          }
-        });
+    for (Sha256Hash hash : fetchInvDataMsg.getHashList()){
 
-    if (blocks[0] != null) {
-      peer.setHeadBlockWeBothHave(blocks[0].getBlockId());
-      peer.setHeadBlockTimeWeBothHave(blocks[0].getTimeStamp());
+      Message msg;
+
+      if (type == MessageTypes.BLOCK){
+        msg = BlockCache.getIfPresent(hash);
+      }else {
+        msg = TrxCache.getIfPresent(hash);
+      }
+
+      if (msg == null){
+        msg = del.getData(hash, type);
+      }
+
+      if (msg != null) {
+        if (type.equals(MessageTypes.BLOCK)) {
+          block = ((BlockMessage) msg).getBlockCapsule();
+        }
+        peer.sendMessage(msg);
+      } else {
+        logger.error("fetch message {} {} failed.", type, hash);
+        peer.sendMessage(new ItemNotFound());
+      }
+    }
+
+    if (block != null) {
+      peer.setHeadBlockWeBothHave(block.getBlockId());
+      peer.setHeadBlockTimeWeBothHave(block.getTimeStamp());
     }
   }
 
