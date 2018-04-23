909d5d049c17d588ef15236f222d2e8f4904cd3c
zergweak <zhang_anwen@163.com>
2018-04-23 18:28:19 +0800

    modify getTransactionId

--- src/main/java/org/tron/core/capsule/TransactionCapsule.java
+++ src/main/java/org/tron/core/capsule/TransactionCapsule.java
@@ -322,7 +322,7 @@ public class TransactionCapsule implements ProtoCapsule<Transaction> {
 
 
   public Sha256Hash getTransactionId() {
-    return Sha256Hash.of(this.transaction.toByteArray());
+    return Sha256Hash.of(this.transaction.getRawData().toByteArray());
   }
 
   @Override
