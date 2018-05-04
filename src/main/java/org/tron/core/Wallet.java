acc2bb18fb2dc842a29f9ccc2c3e598cdb6a941a
renchengchang <rencc@sina.com>
2018-05-04 12:26:40 +0800

    check transaction size before broadcast

--- src/main/java/org/tron/core/Wallet.java
+++ src/main/java/org/tron/core/Wallet.java
@@ -43,13 +43,7 @@ import org.tron.core.capsule.TransactionCapsule;
 import org.tron.core.capsule.WitnessCapsule;
 import org.tron.core.db.AccountStore;
 import org.tron.core.db.Manager;
-import org.tron.core.exception.ContractExeException;
-import org.tron.core.exception.ContractValidateException;
-import org.tron.core.exception.DupTransactionException;
-import org.tron.core.exception.StoreException;
-import org.tron.core.exception.TaposException;
-import org.tron.core.exception.ValidateBandwidthException;
-import org.tron.core.exception.ValidateSignatureException;
+import org.tron.core.exception.*;
 import org.tron.core.net.message.TransactionMessage;
 import org.tron.core.net.node.NodeImpl;
 import org.tron.protos.Contract.AccountCreateContract;
@@ -209,6 +203,9 @@ public class Wallet {
     TransactionCapsule trx = new TransactionCapsule(signaturedTransaction);
     try {
       Message message = new TransactionMessage(signaturedTransaction);
+      if (message.getData().length > Constant.TRANSACTION_MAX_BYTE_SIZE) {
+        throw new TooBigTransactionException("too big transaction, the size is " + message.getData().length + " bytes");
+      }
       dbManager.pushTransactions(trx);
       p2pNode.broadcast(message);
       return true;
@@ -224,6 +221,8 @@ public class Wallet {
       logger.debug("dup trans", e);
     } catch (TaposException e) {
       logger.debug("tapos error", e);
+    } catch (TooBigTransactionException e) {
+      logger.debug("transaction error", e);
     }
     return false;
   }
