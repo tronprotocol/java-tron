0974e1d90753b5aafb5369ed4880276b07c204d2
sean-liu55 <liushaohua9017@163.com>
2018-05-05 11:22:12 +0800

    revert

--- src/main/java/org/tron/core/actuator/VoteWitnessActuator.java
+++ src/main/java/org/tron/core/actuator/VoteWitnessActuator.java
@@ -94,6 +94,7 @@ public class VoteWitnessActuator extends AbstractActuator {
         sum = LongMath.checkedAdd(sum, vote.getVoteCount());
       }
 
+      sum = LongMath.checkedMultiply(sum, 1000000L); //trx -> drop. The vote count is based on TRX
       if (sum > share) {
         throw new ContractValidateException(
             "The total number of votes[" + sum + "] is greater than the share[" + share + "]");
