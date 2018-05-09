932713961bb80db14c3d118e160dd19b67be07a3
zergweak <zhang_anwen@163.com>
2018-05-09 11:23:51 +0800

    modify updateaccount and add name validate and add unit test case

--- src/main/java/org/tron/core/actuator/UpdateAccountActuator.java
+++ src/main/java/org/tron/core/actuator/UpdateAccountActuator.java
@@ -4,42 +4,82 @@ import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
 import lombok.extern.slf4j.Slf4j;
+import org.tron.core.Wallet;
 import org.tron.core.capsule.AccountCapsule;
 import org.tron.core.capsule.TransactionResultCapsule;
+import org.tron.core.capsule.utils.TransactionUtil;
+import org.tron.core.db.AccountStore;
 import org.tron.core.db.Manager;
 import org.tron.core.exception.ContractExeException;
 import org.tron.core.exception.ContractValidateException;
 import org.tron.protos.Contract.AccountUpdateContract;
+import org.tron.protos.Protocol.Transaction.Result.code;
 
 @Slf4j
 public class UpdateAccountActuator extends AbstractActuator {
 
+  AccountUpdateContract accountUpdateContract;
+  byte[] accountName;
+  byte[] ownerAddress;
+  long fee;
+
   UpdateAccountActuator(Any contract, Manager dbManager) {
     super(contract, dbManager);
-  }
-
-  @Override
-  public boolean execute(TransactionResultCapsule result) throws ContractExeException {
-    long fee = calcFee();
     try {
-
-      AccountUpdateContract accountUpdateContract = contract.unpack(AccountUpdateContract.class);
-      AccountCapsule account =
-          dbManager.getAccountStore().get(accountUpdateContract.getOwnerAddress().toByteArray());
-
-      account.setAccountName(accountUpdateContract.getAccountName().toByteArray());
-      dbManager.getAccountStore().put(accountUpdateContract.getOwnerAddress().toByteArray(),
-          account);
-      return true;
+      accountUpdateContract = contract.unpack(AccountUpdateContract.class);
+      accountName = accountUpdateContract.getAccountName().toByteArray();
+      ownerAddress = accountUpdateContract.getOwnerAddress().toByteArray();
+      fee = calcFee();
     } catch (InvalidProtocolBufferException e) {
-      logger.debug(e.getMessage(), e);
-      throw new ContractExeException(e.getMessage());
+      logger.error(e.getMessage(), e);
+    } catch (Exception e) {
+      logger.error(e.getMessage(), e);
     }
   }
 
   @Override
+  public boolean execute(TransactionResultCapsule ret) throws ContractExeException {
+    try {
+      AccountStore accountStore = dbManager.getAccountStore();
+      AccountCapsule account = accountStore.get(ownerAddress);
+
+      account.setAccountName(accountName);
+      accountStore.put(ownerAddress, account);
+      ret.setStatus(fee, code.SUCESS);
+    } catch (Exception e) {
+      logger.debug(e.getMessage(), e);
+      ret.setStatus(fee, code.FAILED);
+      throw new ContractExeException(e.getMessage());
+    }
+    return true;
+  }
+
+  @Override
   public boolean validate() throws ContractValidateException {
-    // todo validate freq.
+    try {
+      if (this.dbManager == null) {
+        throw new ContractValidateException("No dbManager!");
+      }
+      if (accountUpdateContract == null) {
+        throw new ContractValidateException(
+            "contract type error,expected type [AccountUpdateContract],real type[" + contract
+                .getClass() + "]");
+      }
+
+      if (!TransactionUtil.validAccountName(accountName)) {
+        throw new ContractValidateException("Invalidate accountName");
+      }
+      if (!Wallet.addressValid(ownerAddress)) {
+        throw new ContractValidateException("Invalidate ownerAddress");
+      }
+      //TO do  唯一性校验
+      if (!dbManager.getAccountStore().has(ownerAddress)) {
+        throw new ContractValidateException("Account has not existed");
+      }
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      throw new ContractValidateException(ex.getMessage());
+    }
     return true;
   }
 
