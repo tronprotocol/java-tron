4784f694054d3fce2c37d55f1bc6e23b6bd00787
wubin01 <wb_bupt@163.com>
2018-05-10 11:06:25 +0800

    add trxs msg

--- src/main/java/org/tron/core/net/node/NodeImpl.java
+++ src/main/java/org/tron/core/net/node/NodeImpl.java
@@ -118,8 +118,6 @@ public class NodeImpl extends PeerConnectionDelegate implements Node {
     }
   }
 
-
-
   private ScheduledExecutorService logExecutor = Executors.newSingleThreadScheduledExecutor();
 
   //public
