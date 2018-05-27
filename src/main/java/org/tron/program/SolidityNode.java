c4074eaf9d68a393cc64e1b19038e6e1bb568874
zhaohong <zhaohong229@gmail.com>
2018-05-27 17:57:56 +0800

    votesum should > 0

--- src/main/java/org/tron/program/SolidityNode.java
+++ src/main/java/org/tron/program/SolidityNode.java
@@ -67,6 +67,7 @@ public class SolidityNode {
       try {
         Thread.sleep(5000);
       } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
         e.printStackTrace();
       }
     }
