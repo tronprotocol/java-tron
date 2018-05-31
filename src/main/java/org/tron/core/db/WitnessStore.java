a5fab20391d9bb3baec51a36e4a4bca32b96ce5c
Eternity Yarr <savraz@gmail.com>
2018-05-31 18:47:34 +0300

    fix of null check

--- src/main/java/org/tron/core/db/WitnessStore.java
+++ src/main/java/org/tron/core/db/WitnessStore.java
@@ -33,13 +33,13 @@ public class WitnessStore extends TronStoreWithRevoking<WitnessCapsule> {
   @Override
   public boolean has(byte[] key) {
     byte[] account = dbSource.getData(key);
-    if (account == null) {
+    if (account != null) {
       // For debugging
       String readableWitnessAddress = StringUtil.createReadableString(account);
       List<String> allReadableWitnessAddress =
           StringUtil.getAddressStringListFromByteArray(dbSource.allKeys());
       logger.warn(
-          "address is {},witness is {},allWitness : ",
+          "address is {},witness is {},allWitness : {}",
           key,
           readableWitnessAddress,
           allReadableWitnessAddress);
