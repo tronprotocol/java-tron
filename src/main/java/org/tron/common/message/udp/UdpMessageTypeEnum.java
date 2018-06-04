cf16c664e31595a0703b7743156c63f9eaeacba7
wubin01 <wb_bupt@163.com>
2018-06-04 20:48:31 +0800

    mdf udp msg type

--- src/main/java/org/tron/common/message/udp/UdpMessageTypeEnum.java
+++ src/main/java/org/tron/common/message/udp/UdpMessageTypeEnum.java
@@ -5,19 +5,19 @@ import java.util.Map;
 
 public enum  UdpMessageTypeEnum {
 
-  DISCOVER_PING(0x01),
+  DISCOVER_PING((byte) 0x01),
 
-  DISCOVER_PONG(0x02),
+  DISCOVER_PONG((byte) 0x02),
 
-  DISCOVER_FIND_PEER(0x03),
+  DISCOVER_FIND_PEER((byte) 0x03),
 
-  DISCOVER_PEERS(0x04),
+  DISCOVER_PEERS((byte) 0x04),
 
-  LAST(0xFF);
+  LAST((byte) 0xFF);
 
-  private final int type;
+  private final byte type;
 
-  private static final Map<Integer, UdpMessageTypeEnum> intToTypeMap = new HashMap<>();
+  private static final Map<Byte, UdpMessageTypeEnum> intToTypeMap = new HashMap<>();
 
   static {
     for (UdpMessageTypeEnum value : values()) {
@@ -25,11 +25,11 @@ public enum  UdpMessageTypeEnum {
     }
   }
 
-  UdpMessageTypeEnum(int type) {
+  UdpMessageTypeEnum(byte type) {
     this.type = type;
   }
 
-  public static UdpMessageTypeEnum fromInt(int i) {
+  public static UdpMessageTypeEnum fromByte(byte i) {
     return intToTypeMap.get(i);
   }
 }
