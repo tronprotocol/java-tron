f953eb04917f319c948951e122f897ddbcfc28f6
zergweak <zhang_anwen@163.com>
2018-06-07 21:00:23 +0800

    change Hash.sha256 to Sha256Hash.hash

--- src/main/java/org/tron/core/Wallet.java
+++ src/main/java/org/tron/core/Wallet.java
@@ -140,8 +140,8 @@ public class Wallet {
   }
 
   public static String encode58Check(byte[] input) {
-    byte[] hash0 = Sha256Hash.of(input).getBytes();
-    byte[] hash1 = Sha256Hash.of(hash0).getBytes();
+    byte[] hash0 = Sha256Hash.hash(input);
+    byte[] hash1 = Sha256Hash.hash(hash0);
     byte[] inputCheck = new byte[input.length + 4];
     System.arraycopy(input, 0, inputCheck, 0, input.length);
     System.arraycopy(hash1, 0, inputCheck, input.length, 4);
@@ -155,8 +155,8 @@ public class Wallet {
     }
     byte[] decodeData = new byte[decodeCheck.length - 4];
     System.arraycopy(decodeCheck, 0, decodeData, 0, decodeData.length);
-    byte[] hash0 = Sha256Hash.of(decodeData).getBytes();
-    byte[] hash1 = Sha256Hash.of(hash0).getBytes();
+    byte[] hash0 = Sha256Hash.hash(decodeData);
+    byte[] hash1 = Sha256Hash.hash(hash0);
     if (hash1[0] == decodeCheck[decodeData.length] &&
         hash1[1] == decodeCheck[decodeData.length + 1] &&
         hash1[2] == decodeCheck[decodeData.length + 2] &&
