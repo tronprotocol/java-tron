bd6fc30447949848cb60e5572a875fdb9edfe1ce
Matt Yue <yrp1990@gmail.com>
2018-06-07 20:27:56 +0800

    fix a bad transaction make witness don't product a block

--- src/main/java/org/tron/core/db/KhaosDatabase.java
+++ src/main/java/org/tron/core/db/KhaosDatabase.java
@@ -10,7 +10,6 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 import java.util.Objects;
-import java.util.function.ToLongFunction;
 import java.util.stream.Stream;
 import javafx.util.Pair;
 import lombok.Getter;
