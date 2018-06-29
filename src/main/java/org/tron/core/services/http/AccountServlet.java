d596250808673c1d35236d91673463c5cafd9e71
renchengchang <rencc@sina.com>
2018-06-29 18:45:20 +0800

    account http

--- src/main/java/org/tron/core/services/http/AccountServlet.java
+++ src/main/java/org/tron/core/services/http/AccountServlet.java
@@ -7,8 +7,12 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.tron.core.Wallet;
+import org.tron.core.capsule.AccountCapsule;
+import org.tron.core.db.BandwidthProcessor;
+import org.tron.core.db.Manager;
 import org.tron.protos.Protocol.Account;
 import org.tron.protos.Protocol.Account.Builder;
 
@@ -16,14 +20,21 @@ import org.tron.protos.Protocol.Account.Builder;
 @Component
 public class AccountServlet extends HttpServlet {
 
+  @Autowired
+  private Wallet wallet;
+
+  @Autowired
+  private Manager dbManager;
+
   protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
     String account = request.getReader().lines().collect(Collectors.joining(System.lineSeparator()));
     Builder build = Account.newBuilder();
     JsonFormat.merge(account, build);
-
-    response.getWriter().println("account");
-    response.getWriter().println(Wallet.encode58Check(build.build().getAddress().toByteArray()));
-
+    Account reply = wallet.getAccount(build.build());
+    AccountCapsule accountCapsule = new AccountCapsule(reply);
+    BandwidthProcessor processor = new BandwidthProcessor(dbManager);
+    processor.updateUsage(accountCapsule);
+    response.getWriter().println(JsonFormat.printToString(accountCapsule.getInstance()));
   }
 
   protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
