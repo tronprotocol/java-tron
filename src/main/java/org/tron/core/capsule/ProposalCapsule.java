32569ff52f67b2ed29e981496ed6018deee00351
nanfengpo <lsxxxxxx@126.com>
2018-07-05 19:44:59 +0800

    add ProposalCapsule

---
+++ src/main/java/org/tron/core/capsule/ProposalCapsule.java
@@ -0,0 +1,118 @@
+package org.tron.core.capsule;
+
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+import java.util.List;
+import lombok.extern.slf4j.Slf4j;
+import org.tron.common.utils.ByteArray;
+import org.tron.protos.Protocol.ChainParameter;
+import org.tron.protos.Protocol.Proposal;
+
+@Slf4j
+public class ProposalCapsule implements ProtoCapsule<Proposal> {
+
+  private Proposal proposal;
+
+  public ProposalCapsule(final Proposal Proposal) {
+    this.proposal = Proposal;
+  }
+
+  public ProposalCapsule(final byte[] data) {
+    try {
+      this.proposal = Proposal.parseFrom(data);
+    } catch (InvalidProtocolBufferException e) {
+      logger.debug(e.getMessage(), e);
+    }
+  }
+
+  public ProposalCapsule(ByteString address, final long id) {
+    this.proposal = Proposal.newBuilder()
+        .setProposerAddress(address)
+        .setProposalId(id)
+        .build();
+  }
+
+  public long getID() {
+    return this.proposal.getProposalId();
+  }
+
+  public void setID(long id) {
+    this.proposal = this.proposal.toBuilder()
+        .setProposalId(id)
+        .build();
+  }
+
+  public ByteString getProposalAddress() {
+    return this.proposal.getProposerAddress();
+  }
+
+  public void setProposalAddress(ByteString address) {
+    this.proposal = this.proposal.toBuilder()
+        .setProposerAddress(address)
+        .build();
+  }
+
+  public List<ChainParameter> getParameters() {
+    return this.proposal.getParametersList();
+  }
+
+  public void setParameters(List<ChainParameter> parameters) {
+    this.proposal = this.proposal.toBuilder()
+        .addAllParameters(parameters)
+        .build();
+  }
+
+  public long getExpirationTime() {
+    return this.proposal.getExpirationTime();
+  }
+
+  public void setExpirationTime(long time) {
+    this.proposal = this.proposal.toBuilder()
+        .setExpirationTime(time)
+        .build();
+  }
+
+  public List<ByteString> getApprovals() {
+    return this.proposal.getApprovalsList();
+  }
+
+  public void removeApproval(ByteString address) {
+    List<ByteString> approvals = getApprovals();
+    approvals.remove(address);
+    this.proposal = this.proposal.toBuilder()
+        .addAllApprovals(approvals)
+        .build();
+  }
+
+  public void addApproval(ByteString committeeAddress) {
+    this.proposal = this.proposal.toBuilder()
+        .addApprovals(committeeAddress)
+        .build();
+  }
+
+  public boolean getIsApproved() {
+    return this.proposal.getIsApproved();
+  }
+
+  public void setIsApproved(boolean isApproved) {
+    this.proposal = this.proposal.toBuilder()
+        .setIsApproved(isApproved)
+        .build();
+  }
+
+
+  public byte[] createDbKey() {
+    return ByteArray.fromLong(getID());
+  }
+
+  @Override
+  public byte[] getData() {
+    return this.proposal.toByteArray();
+  }
+
+  @Override
+  public Proposal getInstance() {
+    return this.proposal;
+  }
+
+}
