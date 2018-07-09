d031dcb5e6a33bcfb3ad90b949269750cbeb3ba5
nanfengpo <lsxxxxxx@126.com>
2018-07-09 15:22:50 +0800

    minor change

--- src/main/java/org/tron/core/db/ProposalStore.java
+++ src/main/java/org/tron/core/db/ProposalStore.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.tron.core.capsule.ProposalCapsule;
-import org.tron.core.exception.BadItemException;
 import org.tron.core.exception.ItemNotFoundException;
 
 @Component
