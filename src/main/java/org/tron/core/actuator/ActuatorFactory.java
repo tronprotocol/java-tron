cbc28e6a955ee662ba391c1de3970e6e88a24a4f
sean-liu55 <liushaohua9017@163.com>
2018-07-10 18:15:36 +0800

    add ChainParameters

--- src/main/java/org/tron/core/actuator/ActuatorFactory.java
+++ src/main/java/org/tron/core/actuator/ActuatorFactory.java
@@ -73,6 +73,12 @@ public class ActuatorFactory {
         return new WithdrawBalanceActuator(contract.getParameter(), manager);
       case UpdateAssetContract:
         return new UpdateAssetActuator(contract.getParameter(), manager);
+      case ProposalCreateContract:
+        return new ProposalCreateActuator(contract.getParameter(), manager);
+      case ProposalApproveContract:
+        return new ProposalApproveActuator(contract.getParameter(), manager);
+      case ProposalDeleteContract:
+        return new ProposalDeleteActuator(contract.getParameter(), manager);
       default:
 
     }
