662711f7fabcdbea86bb1b2562c6b06e637cb97b
nanfengpo <lsxxxxxx@126.com>
2018-07-10 11:51:11 +0800

    fix: proposalControllerTest

--- src/test/java/org/tron/core/witness/ProposalControllerTest.java
+++ src/test/java/org/tron/core/witness/ProposalControllerTest.java
@@ -151,9 +151,9 @@ public class ProposalControllerTest {
     dbManager.getProposalStore().put(proposalCapsule4.createDbKey(), proposalCapsule4);
     dbManager.getProposalStore().put(proposalCapsule5.createDbKey(), proposalCapsule5);
 
-    long currentMaintenanceTime = 10000L;
+    dbManager.getDynamicPropertiesStore().updateNextMaintenanceTime(10000L);
 
-    proposalController.processProposals(currentMaintenanceTime);
+    proposalController.processProposals();
 
     try {
       proposalCapsule3 = dbManager.getProposalStore().get(proposalCapsule3.createDbKey());
