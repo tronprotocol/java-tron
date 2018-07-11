9bca448df62ad34b6c238bf895d395966d2faefd
Marcus <zhaohong229@gmail.com>
2018-07-11 17:12:28 +0800

    should not set the result.

--- src/main/java/org/tron/core/capsule/TransactionCapsule.java
+++ src/main/java/org/tron/core/capsule/TransactionCapsule.java
@@ -135,12 +135,12 @@ public class TransactionCapsule implements ProtoCapsule<Transaction> {
   }
 
   public void resetResult() {
-    //this.transaction = this.getInstance().toBuilder().clearRet().build();
+    this.transaction = this.getInstance().toBuilder().clearRet().build();
   }
 
   public void setResult(TransactionResultCapsule transactionResultCapsule) {
-    this.transaction = this.getInstance().toBuilder().addRet(transactionResultCapsule.getInstance())
-        .build();
+    //this.transaction = this.getInstance().toBuilder().addRet(transactionResultCapsule.getInstance())
+    //    .build();
   }
 
   public void setReference(long blockNum, byte[] blockHash) {
