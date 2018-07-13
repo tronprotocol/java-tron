3ca2291234db2ef6fd009eb8a35e84c7a99fd591
sun haoyu <shydesky@gmail.com>
2018-07-13 17:39:59 +0800

    shy ppt before

---
+++ src/main/java/org/tron/core/net/node/AdvBlockDisorder.java
@@ -0,0 +1,87 @@
+package org.tron.core.net.node;
+
+import org.tron.common.utils.Sha256Hash;
+import org.tron.core.capsule.BlockCapsule;
+import org.tron.core.net.peer.PeerConnection;
+
+import java.util.HashMap;
+
+/**
+ * @program: java-tron
+ * @description: This class is for Mapping Util using.
+ * The member variable map is used for store the block's parent hash(key)
+ * and the PeerAndBlockCapsule(value).
+ * When we handle a block unsuccessfully because of UnLinkedBlockException, we
+ * add the parent hash and the PeerAndBlockCapsule to the map.
+ * When we handle a block successfully,we can search whether his child block
+ * exists in the map, and we can handle this child simultaneously.
+ * @author: shydesky@gmail.com
+ * @create: 2018-07-13
+ **/
+public class AdvBlockDisorder {
+    private HashMap<Sha256Hash, PeerAndBlockCapsule> map;
+
+    public AdvBlockDisorder(){
+      init();
+    }
+
+    public void init(){
+        map = new HashMap<>();
+    }
+
+    public HashMap<Sha256Hash, PeerAndBlockCapsule> getMap(){
+        return map;
+    }
+
+    public void add(Sha256Hash hash, PeerConnection peer, BlockCapsule block){
+        PeerAndBlockCapsule peerAndBlockCapsule = new PeerAndBlockCapsule(peer, block);
+        if(!peerAndBlockCapsule.isEmpty()){
+            map.put(hash, peerAndBlockCapsule);
+        }
+    }
+
+    public void remove(Sha256Hash hash){
+        map.remove(hash);
+    }
+
+    public PeerAndBlockCapsule get(Sha256Hash hash){
+        return map.get(hash);
+    }
+
+    public void clear(){
+        this.map.clear();
+    }
+
+    public PeerConnection getPeer(Sha256Hash hash){
+        return get(hash) != null ? get(hash).getPeer(): null;
+    }
+
+    public BlockCapsule getBlockCapsule(Sha256Hash hash){
+        return get(hash) != null ? get(hash).getBlockCapsule(): null;
+    }
+
+    class PeerAndBlockCapsule{
+        private PeerConnection peer;
+        private BlockCapsule blockCapsule;
+
+        private PeerAndBlockCapsule(PeerConnection peer, BlockCapsule blockCapsule){
+            this.peer = peer;
+            this.blockCapsule = blockCapsule;
+        }
+
+        private boolean isEmpty(){
+            return (this.blockCapsule == null || this.peer == null);
+        }
+
+        public BlockCapsule getBlockCapsule() {
+            return blockCapsule;
+        }
+
+        public PeerConnection getPeer() {
+            return peer;
+        }
+    }
+}
+
+
+
---
+++ src/test/java/org/tron/core/net/node/AdvBlockDisorderTest.java
@@ -0,0 +1,90 @@
+package org.tron.core.net.node;
+
+import com.google.protobuf.ByteString;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Before;
+import org.junit.Test;
+import org.testng.Assert;
+import org.tron.common.utils.ByteArray;
+import org.tron.common.utils.Sha256Hash;
+import org.tron.core.Constant;
+import org.tron.core.capsule.BlockCapsule;
+import org.tron.core.capsule.utils.BlockUtil;
+import org.tron.core.config.args.Args;
+import org.tron.core.net.peer.PeerConnection;
+import org.tron.protos.Protocol;
+
+@Slf4j
+public class AdvBlockDisorderTest {
+
+    AdvBlockDisorder advBlockDisorder = new AdvBlockDisorder();
+
+    @Test
+    public void testAdd(){
+        PeerConnection peer1 = new PeerConnection();
+        PeerConnection peer2 = new PeerConnection();
+
+        BlockCapsule block0 = BlockUtil.newGenesisBlockCapsule();
+        BlockCapsule block1 = new BlockCapsule(Protocol.Block.newBuilder().setBlockHeader(
+                Protocol.BlockHeader.newBuilder().setRawData(Protocol.BlockHeader.raw.newBuilder().setNumber(1).setParentHash(ByteString.copyFrom(
+                        ByteArray
+                                .fromHexString("1304f784e4e7bae517bcab94c3e0c9214fb4ac7ff9d7d5a937d1f40031f87b81")))
+                )).build());
+        BlockCapsule block2 = new BlockCapsule(Protocol.Block.newBuilder().setBlockHeader(
+                Protocol.BlockHeader.newBuilder().setRawData(Protocol.BlockHeader.raw.newBuilder().setNumber(2).setParentHash(ByteString.copyFrom(
+                        ByteArray
+                                .fromHexString("2304f784e4e7bae517bcab94c3e0c9214fb4ac7ff9d7d5a937d1f40031f87b81")))
+                )).build());
+        advBlockDisorder.add(block0.getParentHash(), peer1, block0);
+        advBlockDisorder.add(block1.getParentHash(), peer1,block1);
+        advBlockDisorder.add(block2.getParentHash(), peer2,block2);
+
+        Assert.assertEquals(advBlockDisorder.getPeer(block0.getParentHash()), peer1);
+        Assert.assertEquals(advBlockDisorder.getPeer(block1.getParentHash()), peer1);
+        Assert.assertEquals(advBlockDisorder.getPeer(block2.getParentHash()),peer2);
+
+        Assert.assertEquals(advBlockDisorder.getBlockCapsule(block0.getParentHash()), block0);
+        Assert.assertEquals(advBlockDisorder.getBlockCapsule(block1.getParentHash()), block1);
+        Assert.assertEquals(advBlockDisorder.getBlockCapsule(block2.getParentHash()), block2);
+
+        advBlockDisorder.clear();
+        advBlockDisorder.add(block0.getParentHash(), peer1, block0);
+        advBlockDisorder.add(block0.getParentHash(), peer1, block0);
+        Assert.assertEquals(advBlockDisorder.getMap().size(), 1);
+    }
+
+    @Test
+    public void testRemove(){
+        PeerConnection peer = new PeerConnection();
+        BlockCapsule block = BlockUtil.newGenesisBlockCapsule();
+        advBlockDisorder.add(block.getParentHash(), peer, block);
+        advBlockDisorder.remove(block.getParentHash());
+        Assert.assertEquals(advBlockDisorder.getMap().size(), 0);
+        advBlockDisorder.add(block.getParentHash(), peer, block);
+        advBlockDisorder.remove(Sha256Hash.of("1234567".getBytes()));
+        Assert.assertEquals(advBlockDisorder.getMap().size(), 1);
+
+    }
+
+    @Test
+    public void testGetBlockCapsule(){
+        PeerConnection peer = new PeerConnection();
+        BlockCapsule block = BlockUtil.newGenesisBlockCapsule();
+        advBlockDisorder.add(block.getParentHash(), peer, block);
+        Assert.assertEquals(advBlockDisorder.getBlockCapsule(block.getParentHash()), block);
+    }
+
+    @Test
+    public void testGetPeer(){
+        PeerConnection peer = new PeerConnection();
+        BlockCapsule block = BlockUtil.newGenesisBlockCapsule();
+        advBlockDisorder.add(block.getParentHash(), peer,block);
+        Assert.assertEquals(advBlockDisorder.getPeer(block.getParentHash()), peer);
+    }
+
+    @Before
+    public void initConfiguration() {
+        Args.setParam(new String[]{}, Constant.TEST_CONF);
+    }
+
+}
