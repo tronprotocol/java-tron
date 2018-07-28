ae0075bd6d433f6bfb2ecbb74e5f380ee819dbc8
olivier <olenheim@hotmail.com>
2018-07-28 19:21:07 +0800

    add trx trace to pay bill

--- src/main/java/org/tron/core/db/TransactionTrace.java
+++ src/main/java/org/tron/core/db/TransactionTrace.java
@@ -1,5 +1,27 @@
 package org.tron.core.db;
 
+import org.tron.core.capsule.ReceiptCapsule;
+import org.tron.core.capsule.TransactionCapsule;
+
 public class TransactionTrace {
+  private TransactionCapsule trx;
+
+  private ReceiptCapsule receipt;
+
+  public TransactionTrace(TransactionCapsule trx) {
+    this.trx = trx;
+  }
+
+  public void init() {
+    //TODO: check
+  }
+
+  public void exec() {
+    //TODO:create vm and exec.
+  }
+
+  public void finalize() {
+    //TODO: pay bill
+  }
 
 }
