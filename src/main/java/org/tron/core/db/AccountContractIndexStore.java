5501ede5adc36404f13a98e05318d214a1ded69b
Matt Yue <yrp1990@gmail.com>
2018-07-30 10:17:54 +0800

    resolve confict

--- src/main/java/org/tron/core/db/AccountContractIndexStore.java
+++ src/main/java/org/tron/core/db/AccountContractIndexStore.java
@@ -17,7 +17,7 @@ public class AccountContractIndexStore extends TronStoreWithRevoking<BytesCapsul
 
   @Override
   public BytesCapsule get(byte[] key) {
-    byte[] value = dbSource.getData(key);
+    byte[] value = revokingDB.getUnchecked(key);
     if (Objects.nonNull(value)) {
       return new BytesCapsule(value);
     }
@@ -30,7 +30,7 @@ public class AccountContractIndexStore extends TronStoreWithRevoking<BytesCapsul
 
   @Override
   public boolean has(byte[] key) {
-    byte[] value = dbSource.getData(key);
+    byte[] value = revokingDB.getUnchecked(key);
     if (ArrayUtils.isEmpty(value)) {
       return false;
     }
