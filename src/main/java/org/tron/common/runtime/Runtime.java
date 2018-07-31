def1e3e0e0131a6be2b1ce13b07d2b5e1b0f6d50
tjchern <tjchern@pku.edu.cn>
2018-07-31 15:58:01 +0800

    add check before vm

--- src/main/java/org/tron/common/runtime/Runtime.java
+++ src/main/java/org/tron/common/runtime/Runtime.java
@@ -356,7 +356,7 @@ public class Runtime {
       AccountCapsule creator = this.deposit
           .getAccount(newSmartContract.getOriginAddress().toByteArray());
       long thisTxCPULimitInUs;
-      long maxCpuInUsByCreator = contract.getMaxCpuUsage();
+      long maxCpuInUsByCreator = trx.getRawData().getMaxCpuUsage();
       long accountCPULimitInUs = getAccountCPULimitInUs(creator, contract.getLimitInTrx(),
           maxCpuInUsByCreator);
       if (executerType == ET_NORMAL_TYPE) {
@@ -426,7 +426,7 @@ public class Runtime {
 
       // todo use default value for cpu max and storage max
       long thisTxCPULimitInUs;
-      long maxCpuInUsBySender = contract.getMaxCpuUsage();
+      long maxCpuInUsBySender = trx.getRawData().getMaxCpuUsage();
       long accountCPULimitInUs = getAccountCPULimitInUs(creator, sender, contract,
           maxCpuInUsBySender);
       if (executerType == ET_NORMAL_TYPE) {
