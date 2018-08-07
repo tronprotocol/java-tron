69f4acc4bd75f8a91fa39e869d65722986b31681
sean-liu55 <liushaohua9017@163.com>
2018-08-07 18:40:27 +0800

    fix conflict in unitTest

--- src/test/java/org/tron/core/db/ManagerTest.java
+++ src/test/java/org/tron/core/db/ManagerTest.java
@@ -310,7 +310,7 @@ public class ManagerTest {
 
     BlockCapsule blockCapsule1 =
         createTestBlockCapsule(
-            1533529947843L + 3000,
+            1533529947843L + 3001,
             num + 1,
             dbManager.getDynamicPropertiesStore().getLatestBlockHeaderHash().getByteString(),
             addressToProvateKeys);
@@ -347,7 +347,7 @@ public class ManagerTest {
     }
 
     BlockCapsule blockCapsule3 =
-        createTestBlockCapsule(
+        createTestBlockCapsule(1533529947843L + 9000,
             dbManager.getDynamicPropertiesStore().getLatestBlockHeaderNumber() + 1,
             dbManager.getDynamicPropertiesStore().getLatestBlockHeaderHash().getByteString(), addressToProvateKeys);
     logger.info("******block3:" + blockCapsule3);
@@ -359,7 +359,7 @@ public class ManagerTest {
         dbManager.getBlockStore().get(dbManager.getDynamicPropertiesStore().getLatestBlockHeaderHash().getBytes()).getBlockId());
 
     BlockCapsule blockCapsule4 =
-        createTestBlockCapsule(
+        createTestBlockCapsule(1533529947843L + 12000,
             dbManager.getDynamicPropertiesStore().getLatestBlockHeaderNumber() + 1,
             blockCapsule3.getBlockId().getByteString(), addressToProvateKeys);
     logger.info("******block4:" + blockCapsule4);
