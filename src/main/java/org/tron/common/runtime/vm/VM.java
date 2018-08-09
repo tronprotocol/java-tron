3d4b74488984ad42375edb041dd07b7393dec98d
ashu <ithinker1991@gmail.com>
2018-08-09 19:33:11 +0800

    refactor

--- src/main/java/org/tron/common/runtime/vm/VM.java
+++ src/main/java/org/tron/common/runtime/vm/VM.java
@@ -294,7 +294,7 @@ public class VM {
 
       // DEBUG System.out.println(" OP IS " + op.name() + " GASCOST IS " + gasCost + " NUM IS " + op.asInt());
       program.spendGas(gasCost, op.name());
-//      program.checkCPULimit(op.name());
+      program.checkCPULimit(op.name());
       // logger.info("after opName: {}, {}", op.name(), System.nanoTime() / 1000 - lastTime);
 
       // Execute operation
