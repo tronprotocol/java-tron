e737910d445a263534b478e4daeb6df76c8346b3
ashu <ithinker1991@gmail.com>
2018-08-09 19:43:20 +0800

    add contractstat

---
+++ src/main/java/org/tron/common/runtime/vm/program/ContractState.java
@@ -0,0 +1,257 @@
+/*
+ * Copyright (c) [2016] [ <ether.camp> ]
+ * This file is part of the ethereumJ library.
+ *
+ * The ethereumJ library is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The ethereumJ library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with the ethereumJ library. If not, see <http://www.gnu.org/licenses/>.
+ */
+package org.tron.common.runtime.vm.program;
+
+import org.tron.common.runtime.vm.DataWord;
+import org.tron.common.runtime.vm.program.invoke.ProgramInvoke;
+import org.tron.common.runtime.vm.program.listener.ProgramListener;
+import org.tron.common.runtime.vm.program.listener.ProgramListenerAware;
+import org.tron.common.storage.Deposit;
+import org.tron.common.storage.Key;
+import org.tron.common.storage.Value;
+import org.tron.core.capsule.AccountCapsule;
+import org.tron.core.capsule.BlockCapsule;
+import org.tron.core.capsule.BytesCapsule;
+import org.tron.core.capsule.ContractCapsule;
+import org.tron.core.capsule.TransactionCapsule;
+import org.tron.core.db.Manager;
+import org.tron.protos.Protocol;
+import org.tron.protos.Protocol.AccountType;
+
+public class ContractState implements Deposit, ProgramListenerAware {
+
+  private Deposit deposit;
+  private final DataWord address;  // contract address
+  private ProgramListener programListener;
+
+  public ContractState(ProgramInvoke programInvoke) {
+    this.address = programInvoke.getOwnerAddress(); // contract address
+    this.deposit = programInvoke.getDeposit();
+  }
+
+  @Override
+  public Manager getDbManager() {
+    return deposit.getDbManager();
+  }
+
+  @Override
+  public void setProgramListener(ProgramListener listener) {
+    this.programListener = listener;
+  }
+
+  @Override
+  public AccountCapsule createAccount(byte[] addr, Protocol.AccountType type) {
+    return deposit.createAccount(addr, type);
+  }
+
+  @Override
+  public AccountCapsule createAccount(byte[] address, String accountName, AccountType type) {
+    return deposit.createAccount(address, accountName, type);
+  }
+
+
+  @Override
+  public AccountCapsule getAccount(byte[] addr) {
+    return deposit.getAccount(addr);
+  }
+
+  @Override
+  public BytesCapsule getContractByNormalAccount(byte[] address) {
+    return deposit.getContractByNormalAccount(address);
+  }
+
+  @Override
+  public void createContractByNormalAccountIndex(byte[] address,
+      BytesCapsule contractAddress) {
+    deposit.createContractByNormalAccountIndex(address, contractAddress);
+  }
+
+  @Override
+  public void createContract(byte[] codeHash, ContractCapsule contractCapsule) {
+    deposit.createContract(codeHash, contractCapsule);
+  }
+
+  @Override
+  public ContractCapsule getContract(byte[] codeHash) {
+    return deposit.getContract(codeHash);
+  }
+
+  @Override
+  public void saveCode(byte[] addr, byte[] code) {
+    deposit.saveCode(addr, code);
+  }
+
+  @Override
+  public byte[] getCode(byte[] addr) {
+    return deposit.getCode(addr);
+  }
+
+    /*
+    @Override
+    public byte[] getCodeHash(byte[] addr) {
+        return deposit.getCodeHash(addr);
+    }
+    */
+
+  @Override
+  public void addStorageValue(byte[] addr, DataWord key, DataWord value) {
+    if (canListenTrace(addr)) {
+      programListener.onStoragePut(key, value);
+    }
+    deposit.addStorageValue(addr, key, value);
+  }
+
+  private boolean canListenTrace(byte[] address) {
+    return (programListener != null) && this.address.equals(new DataWord(address));
+  }
+
+  @Override
+  public DataWord getStorageValue(byte[] addr, DataWord key) {
+    return deposit.getStorageValue(addr, key);
+  }
+
+  @Override
+  public long getBalance(byte[] addr) {
+    return deposit.getBalance(addr);
+  }
+
+  @Override
+  public long addBalance(byte[] addr, long value) {
+    return deposit.addBalance(addr, value);
+  }
+
+  @Override
+  public Deposit newDepositChild() {
+    return deposit.newDepositChild();
+  }
+
+  @Override
+  public Deposit newDepositNext() {
+    return deposit.newDepositNext();
+  }
+
+  @Override
+  public void flush() {
+    deposit.flush();
+  }
+
+  @Override
+  public void commit() {
+    deposit.commit();
+  }
+
+//  @Override
+//  public StorageCapsule getContractState(byte[] address) {
+//    return deposit.getContractState(address);
+//  }
+
+  @Override
+  public Storage getStorage(byte[] address) {
+    return deposit.getStorage(address);
+  }
+
+  @Override
+  public void putAccount(Key key, Value value) {
+    deposit.putAccount(key, value);
+  }
+
+  @Override
+  public void putTransaction(Key key, Value value) {
+    deposit.putTransaction(key, value);
+  }
+
+  @Override
+  public void putBlock(Key key, Value value) {
+    deposit.putBlock(key, value);
+  }
+
+  @Override
+  public void putWitness(Key key, Value value) {
+    deposit.putWitness(key, value);
+  }
+
+  @Override
+  public void putCode(Key key, Value value) {
+    deposit.putCode(key, value);
+  }
+
+  @Override
+  public void putContract(Key key, Value value) {
+    deposit.putContract(key, value);
+  }
+
+  @Override
+  public void putContractByNormalAccountIndex(Key key, Value value) {
+    deposit.putContractByNormalAccountIndex(key, value);
+  }
+
+  @Override
+  public void putStorage(Key key, Storage cache) {
+    deposit.putStorage(key, cache);
+  }
+
+  @Override
+  public void putVotes(Key key, Value value) {
+    deposit.putVotes(key, value);
+  }
+
+  @Override
+  public void setParent(Deposit deposit) {
+    this.deposit.setParent(deposit);
+  }
+
+  @Override
+  public void setPrevDeposit(Deposit deposit) {
+    this.deposit.setPrevDeposit(deposit);
+  }
+
+  @Override
+  public void setNextDeposit(Deposit deposit) {
+    this.deposit.setNextDeposit(deposit);
+  }
+
+  @Override
+  public TransactionCapsule getTransaction(byte[] trxHash) {
+    return this.deposit.getTransaction(trxHash);
+  }
+
+  @Override
+  // Do nothing
+  public void syncCacheFromAccountStore(byte[] address) {
+  }
+
+  @Override
+  // Do nothing
+  public void syncCacheFromVotesStore(byte[] address) {
+  }
+
+  @Override
+  public BlockCapsule getBlock(byte[] blockHash) {
+    return this.deposit.getBlock(blockHash);
+  }
+
+  @Override
+  public long computeAfterRunStorageSize() {
+    return this.deposit.computeAfterRunStorageSize();
+  }
+
+  @Override
+  public long getBeforeRunStorageSize() {
+    return this.deposit.getBeforeRunStorageSize();
+  }
+}
