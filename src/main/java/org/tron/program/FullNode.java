719eb99e21433a07ddfc8727749b0b7c3e1c3752
zhaohong <zhaohong229@gmail.com>
2018-08-10 14:28:48 +0800

    fix for google checker.

--- src/main/java/org/tron/program/FullNode.java
+++ src/main/java/org/tron/program/FullNode.java
@@ -36,19 +36,21 @@ public class FullNode {
 
     DefaultListableBeanFactory beanFactory = new DefaultListableBeanFactory();
     beanFactory.setAllowCircularReferences(false);
-    AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(beanFactory);
+    AnnotationConfigApplicationContext context =
+        new AnnotationConfigApplicationContext(beanFactory);
     context.register(DefaultConfig.class);
     context.refresh();
     Application appT = ApplicationFactory.create(context);
     shutdown(appT);
-    //appT.init(cfgArgs);
 
+    // grpc api server
     RpcApiService rpcApiService = context.getBean(RpcApiService.class);
     appT.addService(rpcApiService);
     if (cfgArgs.isWitness()) {
       appT.addService(new WitnessService(appT, context));
     }
-    //http
+
+    // http api server
     FullNodeHttpApiService httpApiService = context.getBean(FullNodeHttpApiService.class);
     appT.addService(httpApiService);
 
