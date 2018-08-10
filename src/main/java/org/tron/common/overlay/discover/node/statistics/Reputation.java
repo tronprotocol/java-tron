bf0f252ccbc233facbe9694c69f6e531957bb852
liangzhiyan <zy.liang.5@163.com>
2018-08-10 15:50:32 +0800

    add the tcp reason

--- src/main/java/org/tron/common/overlay/discover/node/statistics/Reputation.java
+++ src/main/java/org/tron/common/overlay/discover/node/statistics/Reputation.java
@@ -72,6 +72,8 @@ public class Reputation {
       int reput = baseScore;
       reput += t.p2pHandShake.getTotalCount() > 0 ? 10 : 0;
       reput += t.tcpFlow.getTotalCount() / 10240;
+      reput += t.messageStatistics.p2pOutPing.getTotalCount() == t.messageStatistics.p2pInPong
+          .getTotalCount() ? 10 : 0;
       return reput;
     }
   }
