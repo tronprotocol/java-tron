2f971990d32e5c300ba2b7b328f330202d5133bb
liangzhiyan <zy.liang.5@163.com>
2018-08-16 18:41:04 +0800

    fix test

--- src/test/java/org/tron/core/net/node/HandleBlockMessageTest.java
+++ src/test/java/org/tron/core/net/node/HandleBlockMessageTest.java
@@ -93,7 +93,7 @@ public class HandleBlockMessageTest {
 
         peer.getSyncBlockRequested().put(blockMessage.getBlockId(), System.currentTimeMillis());
         node.onMessage(peer, blockMessageOther);
-        Assert.assertEquals(peer.getSyncBlockRequested().isEmpty(), false);
+        Assert.assertEquals(peer.getSyncBlockRequested().isEmpty(), true);
     }
 
     //根据父块生成一个区块
