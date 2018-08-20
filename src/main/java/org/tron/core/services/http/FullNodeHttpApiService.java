a87785713bfb283e5c2e4cd5a67fb7a4eddd65ff
huzhenyuan <huzhenyuan@github.com>
2018-08-20 20:01:06 +0800

    allow cors

--- src/main/java/org/tron/core/services/http/FullNodeHttpApiService.java
+++ src/main/java/org/tron/core/services/http/FullNodeHttpApiService.java
@@ -1,9 +1,13 @@
 package org.tron.core.services.http;
 
+import java.util.EnumSet;
+import javax.servlet.DispatcherType;
 import lombok.extern.slf4j.Slf4j;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.FilterHolder;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
+import org.eclipse.jetty.servlets.CrossOriginFilter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.tron.common.application.Service;
@@ -114,6 +118,12 @@ public class FullNodeHttpApiService implements Service {
       server = new Server(port);
       ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
       context.setContextPath("/wallet/");
+      FilterHolder cors = context.addFilter(CrossOriginFilter.class,"/*", EnumSet.of(DispatcherType.REQUEST));
+      cors.setInitParameter(CrossOriginFilter.ALLOWED_ORIGINS_PARAM, "*");
+      cors.setInitParameter(CrossOriginFilter.ACCESS_CONTROL_ALLOW_ORIGIN_HEADER, "*");
+      cors.setInitParameter(CrossOriginFilter.ALLOWED_METHODS_PARAM, "GET,POST,HEAD");
+      cors.setInitParameter(CrossOriginFilter.ALLOWED_HEADERS_PARAM, "X-Requested-With,Content-Type,Accept,Origin");
+
       server.setHandler(context);
       context.addServlet(new ServletHolder(accountServlet), "/getaccount");
       context.addServlet(new ServletHolder(transferServlet), "/createtransaction");
