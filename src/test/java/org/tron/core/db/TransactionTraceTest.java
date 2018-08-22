e505a24461f67199b48aebfc8c4ae3d25c21ddd3
sean-liu55 <liushaohua9017@163.com>
2018-08-22 18:23:26 +0800

    fix unit test

--- src/test/java/org/tron/core/db/TransactionTraceTest.java
+++ src/test/java/org/tron/core/db/TransactionTraceTest.java
@@ -152,7 +152,7 @@ public class TransactionTraceTest {
       trace.exec(runtime);
       trace.pay();
       Assert.assertEquals(0, trace.getReceipt().getEnergyUsage());
-      Assert.assertEquals(49503930, trace.getReceipt().getEnergyFee());
+      Assert.assertEquals(6034503930L, trace.getReceipt().getEnergyFee());
       // Assert.assertEquals(deployStorageDelta, trace.getReceipt().getStorageDelta());
       // Assert.assertEquals(494800000, trace.getReceipt().getStorageFee());
       accountCapsule = dbManager.getAccountStore().get(accountCapsule.getAddress().toByteArray());
@@ -189,7 +189,7 @@ public class TransactionTraceTest {
       trace.exec(runtime);
       trace.pay();
       Assert.assertEquals(50000, trace.getReceipt().getEnergyUsage());
-      Assert.assertEquals(6033531930L, trace.getReceipt().getEnergyFee());
+      Assert.assertEquals(6033003930L, trace.getReceipt().getEnergyFee());
       Assert.assertEquals(6034503930L,
           trace.getReceipt().getEnergyUsage() * 30 + trace.getReceipt().getEnergyFee());
       // Assert.assertEquals(deployStorageDelta, trace.getReceipt().getStorageDelta());
