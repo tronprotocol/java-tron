33957d1d017cd7377d6240d251a84ec0ce312087
wangzihe <wangzihe@tron.network>
2018-08-25 16:23:51 +0800

    Add create exchange test case

---
+++ src/test/java/stest/tron/wallet/exchange/WalletExchange001.java
@@ -0,0 +1,118 @@
+package stest.tron.wallet.exchange;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.tron.api.GrpcAPI.AccountResourceMessage;
+import org.tron.api.WalletGrpc;
+import org.tron.common.crypto.ECKey;
+import org.tron.common.utils.ByteArray;
+import org.tron.common.utils.Utils;
+import org.tron.core.Wallet;
+import org.tron.core.db.api.pojo.AssetIssue;
+import org.tron.protos.Protocol.Account;
+import org.tron.protos.Protocol.SmartContract;
+import stest.tron.wallet.common.client.Configuration;
+import stest.tron.wallet.common.client.Parameter.CommonConstant;
+import stest.tron.wallet.common.client.utils.PublicMethed;
+
+@Slf4j
+public class WalletExchange001 {
+
+  private final String testKey002 =
+      "FC8BF0238748587B9617EB6D15D47A66C0E07C1A1959033CF249C6532DC29FE6";
+  private final byte[] fromAddress = PublicMethed.getFinalAddress(testKey002);
+
+  private ManagedChannel channelFull = null;
+  private WalletGrpc.WalletBlockingStub blockingStubFull = null;
+  private String fullnode = Configuration.getByPath("testng.conf")
+      .getStringList("fullnode.ip.list").get(0);
+
+  private static final long now = System.currentTimeMillis();
+  private static String name1 = "exchange001_1_" + Long.toString(now);
+  private static String name2 = "exchange001_2_" + Long.toString(now);
+  private static final long totalSupply = 1000000001L;
+  String description = "just-test";
+  String url = "https://github.com/tronprotocol/wallet-cli/";
+
+  ECKey ecKey1 = new ECKey(Utils.getRandom());
+  byte[] exchange001Address = ecKey1.getAddress();
+  String exchange001Key = ByteArray.toHexString(ecKey1.getPrivKeyBytes());
+
+  ECKey ecKey2 = new ECKey(Utils.getRandom());
+  byte[] secondExchange001Address = ecKey2.getAddress();
+  String secondExchange001Key = ByteArray.toHexString(ecKey2.getPrivKeyBytes());
+  Long secondTransferAssetToFirstAccountNum = 100000000L;
+
+  @BeforeSuite
+  public void beforeSuite() {
+    Wallet wallet = new Wallet();
+    Wallet.setAddressPreFixByte(CommonConstant.ADD_PRE_FIX_BYTE_MAINNET);
+  }
+
+  @BeforeClass(enabled = true)
+  public void beforeClass() {
+    PublicMethed.printAddress(exchange001Key);
+    PublicMethed.printAddress(secondExchange001Key);
+    channelFull = ManagedChannelBuilder.forTarget(fullnode)
+        .usePlaintext(true)
+        .build();
+    blockingStubFull = WalletGrpc.newBlockingStub(channelFull);
+    Assert.assertTrue(PublicMethed.sendcoin(exchange001Address,10240000000L,fromAddress,
+        testKey002,blockingStubFull));
+    Assert.assertTrue(PublicMethed.sendcoin(secondExchange001Address,10240000000L,fromAddress,
+        testKey002,blockingStubFull));
+  }
+
+  @Test(enabled = true)
+  public void testCreateUsedAsset() {
+    Assert.assertTrue(PublicMethed.freezeBalance(exchange001Address, 1000000L,
+        3,exchange001Key,blockingStubFull));
+    Assert.assertTrue(PublicMethed.freezeBalance(secondExchange001Address, 1000000L,
+        3,secondExchange001Key,blockingStubFull));
+    Long start = System.currentTimeMillis() + 5000L;
+    Long end = System.currentTimeMillis() + 5000000L;
+    Assert.assertTrue(PublicMethed.createAssetIssue(exchange001Address, name1, totalSupply, 1,
+        1, start, end, 1, description, url, 10000L,10000L,
+        1L,1L,exchange001Key,blockingStubFull));
+    Assert.assertTrue(PublicMethed.createAssetIssue(secondExchange001Address, name2, totalSupply, 1,
+        1, start, end, 1, description, url, 10000L,10000L,
+        1L,1L,secondExchange001Key,blockingStubFull));
+  }
+
+  @Test(enabled = true)
+  public void createExchange() {
+    Account firstAccount = PublicMethed.queryAccount(exchange001Address, blockingStubFull);
+    Long token1BeforeBalance = 0L;
+    for (String name : firstAccount.getAssetMap().keySet()) {
+      token1BeforeBalance = firstAccount.getAssetMap().get(name);
+    }
+    Assert.assertTrue(PublicMethed.transferAsset(exchange001Address,name2.getBytes(),
+        secondTransferAssetToFirstAccountNum, secondExchange001Address,
+        secondExchange001Key,blockingStubFull));
+    Long token2BeforeBalance = secondTransferAssetToFirstAccountNum;
+
+    logger.info("name1 is " + name1);
+    logger.info("name2 is " + name2);
+    logger.info("first balance is " + Long.toString(token1BeforeBalance));
+    logger.info("second balance is " + token2BeforeBalance.toString());
+    //CreateExchange
+    Assert.assertTrue(PublicMethed.exchangeCreate(name1.getBytes(),100L,
+        name2.getBytes(),1000L,exchange001Address,exchange001Key,blockingStubFull));
+  }
+
+  @AfterClass
+  public void shutdown() throws InterruptedException {
+    if (channelFull != null) {
+      channelFull.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+  }
+}
+
+
