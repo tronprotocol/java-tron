371316e86220949105b59d2c08949b131af91f6f
taihaofu <taihao.fu@gmail.com>
2018-08-25 19:41:38 +0800

    fix ripempd length

--- src/main/java/org/tron/common/runtime/vm/PrecompiledContracts.java
+++ src/main/java/org/tron/common/runtime/vm/PrecompiledContracts.java
@@ -315,12 +315,12 @@ public class PrecompiledContracts {
 
     @Override
     public Pair<Boolean, byte[]> execute(byte[] data) {
-      byte[] target = new byte[160];
+      byte[] target = new byte[20];
       if (data == null) {
         data = EMPTY_BYTE_ARRAY;
       }
       byte[] orig = Sha256Hash.hash(data);
-      System.arraycopy(orig, 0, target, 0, 160);
+      System.arraycopy(orig, 0, target, 0, 20);
       return Pair.of(true, Sha256Hash.hash(target));
     }
   }
