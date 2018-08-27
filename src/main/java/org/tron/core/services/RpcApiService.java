b4ff7b30e416f7535ff0e4482a6ef7a9b0240b34
huzhenyuan <huzhenyuan@qq.com>
2018-08-27 11:50:03 +0800

    fix bug for setResult

--- src/main/java/org/tron/core/services/RpcApiService.java
+++ src/main/java/org/tron/core/services/RpcApiService.java
@@ -1312,6 +1312,7 @@ public class RpcApiService implements Service {
         trxExtBuilder.setTransaction(trx);
         trxExtBuilder.setTxid(trxCap.getTransactionId().getByteString());
         retBuilder.setResult(true).setCode(response_code.SUCCESS);
+        trxExtBuilder.setResult(retBuilder);
       } catch (ContractValidateException e) {
         retBuilder.setResult(false).setCode(response_code.CONTRACT_VALIDATE_ERROR)
             .setMessage(ByteString.copyFromUtf8("contract validate error : " + e.getMessage()));
