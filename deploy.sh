9f4e9f9707e9f2d4ae0695397b291b5d024df919
pangpangfeng <7577288@qq.com>
2018-09-06 12:09:35 +0800

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [[ $TRAVIS_BRANCH == 'develop' ]]
+if [[ $TRAVIS_BRANCH == 'deploy_test' ]]
     ssh tron@47.93.9.236 -p 22008 sh /home/tron/workspace/deploy_all.sh
     ./gradlew stest
 else
