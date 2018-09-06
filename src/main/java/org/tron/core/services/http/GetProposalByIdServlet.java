866b9fdca146d2b40ce22b94b1461b5ab996830e
zergweak <zhang_anwen@163.com>
2018-09-06 11:26:39 +0800

    change input name to id

--- src/main/java/org/tron/core/services/http/GetProposalByIdServlet.java
+++ src/main/java/org/tron/core/services/http/GetProposalByIdServlet.java
@@ -1,5 +1,6 @@
 package org.tron.core.services.http;
 
+import com.alibaba.fastjson.JSONObject;
 import com.google.protobuf.ByteString;
 import java.io.IOException;
 import java.util.stream.Collectors;
@@ -9,7 +10,6 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.tron.api.GrpcAPI.BytesMessage;
 import org.tron.common.utils.ByteArray;
 import org.tron.core.Wallet;
 import org.tron.protos.Protocol.Proposal;
@@ -23,8 +23,9 @@ public class GetProposalByIdServlet extends HttpServlet {
 
   protected void doGet(HttpServletRequest request, HttpServletResponse response) {
     try {
-      String input = request.getParameter("value");
-      Proposal reply = wallet.getProposalById(ByteString.copyFrom(ByteArray.fromHexString(input)));
+      String input = request.getParameter("id");
+      long id = new Long(input);
+      Proposal reply = wallet.getProposalById(ByteString.copyFrom(ByteArray.fromLong(id)));
       if (reply != null) {
         response.getWriter().println(JsonFormat.printToString(reply));
       } else {
@@ -44,9 +45,9 @@ public class GetProposalByIdServlet extends HttpServlet {
     try {
       String input = request.getReader().lines()
           .collect(Collectors.joining(System.lineSeparator()));
-      BytesMessage.Builder build = BytesMessage.newBuilder();
-      JsonFormat.merge(input, build);
-      Proposal reply = wallet.getProposalById(build.getValue());
+      JSONObject jsonObject = JSONObject.parseObject(input);
+      long id = jsonObject.getLong("id");
+      Proposal reply = wallet.getProposalById(ByteString.copyFrom(ByteArray.fromLong(id)));
       if (reply != null) {
         response.getWriter().println(JsonFormat.printToString(reply));
       } else {
