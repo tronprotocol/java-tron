0e4a1cf0ee08b93f8d1612bbfee7331ae9278113
pangpangfeng <7577288@qq.com>
2018-09-07 15:15:52 +0800

    deploy test

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,7 @@ if [ "$TRAVIS_BRANCH" = "deploy_test" ];then
   echo "init env"
   ssh tron@47.93.9.236 -p 22008 sh /home/tron/workspace/deploy_all.sh
   echo "stest start"
-  ./gradlew stest
+  ./gradlew stest | tee stest.log
   echo "stest end"
 
   echo $?
