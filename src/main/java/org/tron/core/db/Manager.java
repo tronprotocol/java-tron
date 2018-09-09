801f52194ab0918db3794e1fc64ac91ab248e44c
sun haoyu <shydesky@gmail.com>
2018-09-09 20:22:53 +0800

    close some store

--- src/main/java/org/tron/core/db/Manager.java
+++ src/main/java/org/tron/core/db/Manager.java
@@ -1475,6 +1475,12 @@ public class Manager {
     closeOneStore(codeStore);
     closeOneStore(contractStore);
     closeOneStore(storageRowStore);
+    closeOneStore(exchangeStore);
+    closeOneStore(peersStore);
+    closeOneStore(proposalStore);
+    closeOneStore(recentBlockStore);
+    closeOneStore(transactionHistoryStore);
+    closeOneStore(votesStore);
     System.err.println("******** end to close db ********");
   }
 
