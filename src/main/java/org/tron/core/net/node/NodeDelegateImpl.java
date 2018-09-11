028f255f85a772076a62adda14038c51250e9877
wubin01 <wb_bupt@163.com>
2018-09-11 15:28:25 +0800

    mdf code style

--- src/main/java/org/tron/core/net/node/NodeDelegateImpl.java
+++ src/main/java/org/tron/core/net/node/NodeDelegateImpl.java
@@ -64,7 +64,6 @@ public class NodeDelegateImpl implements NodeDelegate {
       throw new BadBlockException("block size over limit");
     }
 
-    // TODO timestamp should be consistent.
     long gap = block.getTimeStamp() - System.currentTimeMillis();
     if (gap >= BLOCK_PRODUCED_INTERVAL) {
       throw new BadBlockException("block time error");
@@ -182,8 +181,7 @@ public class NodeDelegateImpl implements NodeDelegate {
   @Override
   public LinkedList<BlockId> getLostBlockIds(List<BlockId> blockChainSummary)
       throws StoreException {
-    //todo: return the remain block count.
-    //todo: return the blocks it should be have.
+
     if (dbManager.getHeadBlockNum() == 0) {
       return new LinkedList<>();
     }
@@ -198,7 +196,7 @@ public class NodeDelegateImpl implements NodeDelegate {
         && blockChainSummary.get(0).getNum() == 0) {
       return new LinkedList(Arrays.asList(dbManager.getGenesisBlockId()));
     } else {
-      //todo: find a block we all know between the summary and my db.
+
       Collections.reverse(blockChainSummary);
       unForkedBlockId = blockChainSummary.stream()
           .filter(blockId -> containBlockInMainChain(blockId))
@@ -206,10 +204,8 @@ public class NodeDelegateImpl implements NodeDelegate {
       if (unForkedBlockId == null) {
         return new LinkedList<>();
       }
-      //todo: can not find any same block form peer's summary and my db.
     }
 
-    //todo: limit the count of block to send peer by one time.
     long unForkedBlockIdNum = unForkedBlockId.getNum();
     long len = Longs
         .min(dbManager.getHeadBlockNum(), unForkedBlockIdNum + NodeConstant.SYNC_FETCH_BATCH_NUM);
@@ -302,6 +298,7 @@ public class NodeDelegateImpl implements NodeDelegate {
         } catch (BadItemException e) {
           logger.debug(e.getMessage());
         } catch (ItemNotFoundException e) {
+
           logger.debug(e.getMessage());
         } catch (Exception e) {
           logger.error("new BlockMessage fail", e);
@@ -328,7 +325,6 @@ public class NodeDelegateImpl implements NodeDelegate {
       logger.info("Sync Block Completed !!!");
     }
     dbManager.setSyncMode(unSyncNum == 0);
-    //TODO: notify cli know how many block we need to sync
   }
 
   @Override
@@ -372,7 +368,6 @@ public class NodeDelegateImpl implements NodeDelegate {
     if (type.equals(MessageTypes.BLOCK)) {
       return dbManager.containBlock(hash);
     } else if (type.equals(MessageTypes.TRX)) {
-      //TODO: check it
       return dbManager.getTransactionStore().has(hash.getBytes());
     }
     return false;
@@ -380,20 +375,9 @@ public class NodeDelegateImpl implements NodeDelegate {
 
   @Override
   public BlockCapsule getGenesisBlock() {
-    //TODO return a genesisBlock
     return dbManager.getGenesisBlock();
   }
 
-  //  @Override
-//  public long getLatestSolidifiedBlockNum() {
-//    return dbManager.getDynamicPropertiesStore().getLatestSolidifiedBlockNum();
-//  }
-//
-//  @Override
-//  public long getSyncBeginNumber() {
-//    return dbManager.getSyncBeginNumber();
-//  }
-//
   @Override
   public boolean canChainRevoke(long num) {
     return num >= dbManager.getSyncBeginNumber();
