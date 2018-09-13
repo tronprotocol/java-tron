9152a39b60170e78cbadd0a7df6e3c0c47571d0e
wubin01 <wb_bupt@163.com>
2018-09-13 15:46:49 +0800

    mdf trx cache size

--- src/main/java/org/tron/core/net/node/NodeImpl.java
+++ src/main/java/org/tron/core/net/node/NodeImpl.java
@@ -84,7 +84,7 @@ public class NodeImpl extends PeerConnectionDelegate implements Node {
   private MessageCount trxCount = new MessageCount();
 
   private Cache<Sha256Hash, TransactionMessage> TrxCache = CacheBuilder.newBuilder()
-      .maximumSize(100_000).expireAfterWrite(1, TimeUnit.HOURS).initialCapacity(100_000)
+      .maximumSize(50_000).expireAfterWrite(1, TimeUnit.HOURS).initialCapacity(50_000)
       .recordStats().build();
 
   private Cache<Sha256Hash, BlockMessage> BlockCache = CacheBuilder.newBuilder()
