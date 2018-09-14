8fb8f1ec33ad18d1e95f4cb9c180e0eaa2149d3b
Matt Yue <yrp1990@gmail.com>
2018-09-14 15:55:34 +0800

    add timeResult in popTrxs

--- src/main/java/org/tron/core/db/PendingManager.java
+++ src/main/java/org/tron/core/db/PendingManager.java
@@ -40,7 +40,10 @@ public class PendingManager implements AutoCloseable {
 
     for (TransactionCapsule tx : dbManager.getPoppedTransactions()) {
       try {
+        if (tx.getTrxTrace() != null &&
+            tx.getTrxTrace().getTimeResultType().equals(TimeResultType.NORMAL)) {
           dbManager.getRepushTransactions().put(tx);
+        }
       } catch (InterruptedException e) {
         logger.error(e.getMessage());
         Thread.currentThread().interrupt();
