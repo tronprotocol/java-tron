79aa39ead6db4863a32406bcf3cb989358888270
wubin01 <wb_bupt@163.com>
2018-09-14 14:37:20 +0800

    mdf adv block handle

--- src/main/java/org/tron/core/net/node/NodeImpl.java
+++ src/main/java/org/tron/core/net/node/NodeImpl.java
@@ -719,36 +719,38 @@ public class NodeImpl extends PeerConnectionDelegate implements Node {
   }
 
   private void processAdvBlock(PeerConnection peer, BlockCapsule block) {
-    if (!freshBlockId.contains(block.getBlockId())) {
-      try {
-        LinkedList<Sha256Hash> trxIds = null;
-        trxIds = del.handleBlock(block, false);
-        freshBlockId.offer(block.getBlockId());
+    synchronized (freshBlockId){
+      if (!freshBlockId.contains(block.getBlockId())) {
+        try {
+          LinkedList<Sha256Hash> trxIds = null;
+          trxIds = del.handleBlock(block, false);
+          freshBlockId.offer(block.getBlockId());
 
-        trxIds.forEach(trxId -> advObjToFetch.remove(trxId));
+          trxIds.forEach(trxId -> advObjToFetch.remove(trxId));
 
-        getActivePeer().stream()
-            .filter(p -> p.getAdvObjSpreadToUs().containsKey(block.getBlockId()))
-            .forEach(p -> updateBlockWeBothHave(p, block));
+          getActivePeer().stream()
+              .filter(p -> p.getAdvObjSpreadToUs().containsKey(block.getBlockId()))
+              .forEach(p -> updateBlockWeBothHave(p, block));
 
-        broadcast(new BlockMessage(block));
+          broadcast(new BlockMessage(block));
 
-      } catch (BadBlockException e) {
-        logger.error("We get a bad block {}, from {}, reason is {} ",
-            block.getBlockId().getString(), peer.getNode().getHost(), e.getMessage());
-        disconnectPeer(peer, ReasonCode.BAD_BLOCK);
-      } catch (UnLinkedBlockException e) {
-        logger.error("We get a unlinked block {}, from {}, head is {}",
-            block.getBlockId().getString(), peer.getNode().getHost(),
-            del.getHeadBlockId().getString());
-        startSyncWithPeer(peer);
-      } catch (NonCommonBlockException e) {
-        logger.error(
-            "We get a block {} that do not have the most recent common ancestor with the main chain, from {}, reason is {} ",
-            block.getBlockId().getString(), peer.getNode().getHost(), e.getMessage());
-        disconnectPeer(peer, ReasonCode.FORKED);
-      } catch (InterruptedException e) {
-        Thread.currentThread().interrupt();
+        } catch (BadBlockException e) {
+          logger.error("We get a bad block {}, from {}, reason is {} ",
+              block.getBlockId().getString(), peer.getNode().getHost(), e.getMessage());
+          disconnectPeer(peer, ReasonCode.BAD_BLOCK);
+        } catch (UnLinkedBlockException e) {
+          logger.error("We get a unlinked block {}, from {}, head is {}",
+              block.getBlockId().getString(), peer.getNode().getHost(),
+              del.getHeadBlockId().getString());
+          startSyncWithPeer(peer);
+        } catch (NonCommonBlockException e) {
+          logger.error(
+              "We get a block {} that do not have the most recent common ancestor with the main chain, from {}, reason is {} ",
+              block.getBlockId().getString(), peer.getNode().getHost(), e.getMessage());
+          disconnectPeer(peer, ReasonCode.FORKED);
+        } catch (InterruptedException e) {
+          Thread.currentThread().interrupt();
+        }
       }
     }
   }
