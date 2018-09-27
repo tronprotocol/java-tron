27a030a11462734aa31598a4cf80296b890cad49
zergweak <zhang_anwen@163.com>
2018-09-27 15:22:57 +0800

    add zksnark verify code

---
+++ src/main/java/org/tron/common/crypto/zksnark/Proof.java
@@ -0,0 +1,13 @@
+package org.tron.common.crypto.zksnark;
+
+public class Proof {
+
+  G1Point A;
+  G1Point A_p;
+  G2Point B;
+  G1Point B_p;
+  G1Point C;
+  G1Point C_p;
+  G1Point K;
+  G1Point H;
+}
---
+++ src/main/java/org/tron/common/crypto/zksnark/VerifyingKey.java
@@ -0,0 +1,13 @@
+package org.tron.common.crypto.zksnark;
+
+public class VerifyingKey {
+
+  G2Point A;
+  G1Point B;
+  G2Point C;
+  G2Point gamma;
+  G1Point gammaBeta1;
+  G2Point gammaBeta2;
+  G2Point Z;
+  G1Point[] IC;
+}
