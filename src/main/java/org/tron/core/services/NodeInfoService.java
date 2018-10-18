4eaf09f9691d2d863ad51a10def0cb4a7cc173d8
lvs007 <zy.liang.5@163.com>
2018-10-18 15:25:39 +0800

    fix sonar

--- src/main/java/org/tron/core/services/NodeInfoService.java
+++ src/main/java/org/tron/core/services/NodeInfoService.java
@@ -8,6 +8,7 @@ import java.lang.management.RuntimeMXBean;
 import java.lang.management.ThreadInfo;
 import java.lang.management.ThreadMXBean;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.lang3.ArrayUtils;
@@ -97,7 +98,7 @@ public class NodeInfoService {
         deadLockThreadInfo.setBlockTime(deadlockInfo.getBlockedTime());
         deadLockThreadInfo.setWaitTime(deadlockInfo.getWaitedTime());
         deadLockThreadInfo.setState(deadlockInfo.getThreadState().name());
-        deadLockThreadInfo.setStackTrace(deadlockInfo.getStackTrace().toString());
+        deadLockThreadInfo.setStackTrace(Arrays.toString(deadlockInfo.getStackTrace()));
         deadLockThreadInfoList.add(deadLockThreadInfo);
       }
       machineInfo.setDeadLockThreadInfoList(deadLockThreadInfoList);
