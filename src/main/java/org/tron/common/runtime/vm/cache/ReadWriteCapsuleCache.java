27acde5bdc6220e4e41bcf4e5fb27dcf62bfc62b
ashu <ithinker1991@gmail.com>
2018-10-24 11:02:46 +0800

    add ReadWriteCache and WriteCache

--- src/main/java/org/tron/common/runtime/vm/cache/ReadWriteCapsuleCache.java
+++ src/main/java/org/tron/common/runtime/vm/cache/ReadWriteCapsuleCache.java
@@ -1,66 +1,67 @@
 package org.tron.common.runtime.vm.cache;
 
+
 import org.tron.common.utils.ByteArrayMap;
 import org.tron.core.capsule.ProtoCapsule;
 import org.tron.core.db.TronStoreWithRevoking;
 import org.tron.core.exception.BadItemException;
 import org.tron.core.exception.ItemNotFoundException;
 
+public class ReadWriteCapsuleCache<V extends ProtoCapsule> implements CachedSource<byte[], V> {
+  private ByteArrayMap<V>  writeCapsuleCache;
+  private ByteArrayMap<V> readCapsuleCache;
+  private TronStoreWithRevoking<V> store;
 
-public class ReadWriteCapsuleCache<V extends ProtoCapsule> implements CachedSource<byte[], V>{
-    private ByteArrayMap<V> cache;
-    private TronStoreWithRevoking<V> store;
+  public ReadWriteCapsuleCache(TronStoreWithRevoking<V> store) {
+    this.store = store;
+    writeCapsuleCache = new ByteArrayMap<>();
+    readCapsuleCache = new ByteArrayMap<>();
+  }
 
-    public ReadWriteCapsuleCache(ByteArrayMap<V> cache, TronStoreWithRevoking<V> store) {
-        this.cache = cache;
-        this.store = store;
+  @Override
+  public void delete(byte[] key) {
+    writeCapsuleCache.put(key, null);
+  }
+
+  @Override
+  public void put(byte[] key, V value) {
+    writeCapsuleCache.put(key, value);
+  }
+
+  @Override
+  public boolean containsKey(byte[] key) {
+    return writeCapsuleCache.containsKey(key) || readCapsuleCache.containsKey(key);
+  }
+
+  @Override
+  public V get(byte[] key) {
+    if(writeCapsuleCache.containsKey(key)) {
+      return writeCapsuleCache.get(key);
     }
 
-    @Override
-    public void delete(byte[] key) {
-        cache.put(key, null);
+    if (readCapsuleCache.containsKey(key)) {
+      return readCapsuleCache.get(key);
+    } else {
+      V value;
+      try {
+        value = this.store.get(key);
+      } catch (ItemNotFoundException | BadItemException e) {
+        value = null;
+      }
+      // Ensure each key should be visit once, though value is null
+      readCapsuleCache.put(key, value);
     }
+    return readCapsuleCache.get(key);
+  }
 
-    @Override
-    public void put(byte[] key, V value) {
-        cache.put(key, value);
-    }
-
-    @Override
-    public boolean containsKey(byte[] key) {
-        if (cache.containsKey(key)) {
-            return true;
-        }
-        V v;
-        try {
-            v = store.get(key);
-            cache.put(key, v);
-        } catch (ItemNotFoundException | BadItemException e) {
-            v = null;
-        }
-        return v == null;
-    }
-
-    @Override
-    public V get(byte[] key) {
-        if (cache.containsKey(key)) {
-            return cache.get(key);
-        } else {
-            V v;
-            try {
-                v = store.get(key);
-            } catch (ItemNotFoundException | BadItemException e) {
-                v = null;
-            }
-            // cached null
-            cache.put(key, v);
-            return v;
-        }
-    }
-
-    @Override
-    public void commit() {
-
-    }
+  @Override
+  public void commit() {
+    writeCapsuleCache.forEach((key, value) -> {
+      if (value == null) {
+        this.store.delete(key);
+      } else {
+        this.store.put(key, value);
+      }
+    });
+  }
 }
-
--- src/main/java/org/tron/common/runtime/vm/cache/WriteCapsuleCache.java
+++ src/main/java/org/tron/common/runtime/vm/cache/WriteCapsuleCache.java
@@ -3,35 +3,46 @@ package org.tron.common.runtime.vm.cache;
 import org.tron.common.utils.ByteArrayMap;
 import org.tron.core.capsule.ProtoCapsule;
 
-public class WriteCapsuleCache<V extends ProtoCapsule> implements CachedSource<byte[], V>{
-    CachedSource<byte[], V> backingSource;
-    ByteArrayMap<V> writeCache;
+public class WriteCapsuleCache<V extends ProtoCapsule> implements CachedSource<byte[], V> {
+  private CachedSource<byte[], V> backingSource;
+  private ByteArrayMap<V> writeCache;
 
-    @Override
-    public void delete(byte[] key) {
-        writeCache.put(key, null);
+  public WriteCapsuleCache(CachedSource<byte[], V> backingSource) {
+    this.backingSource = backingSource;
+    this.writeCache = new ByteArrayMap<>();
+  }
+
+  @Override
+  public void delete(byte[] key) {
+    writeCache.put(key, null);
+  }
+
+  @Override
+  public void put(byte[] key, V value) {
+    writeCache.put(key, value);
+  }
+
+  @Override
+  public boolean containsKey(byte[] key) {
+    return writeCache.containsKey(key) || backingSource.containsKey(key);
+  }
+
+  @Override
+  public V get(byte[] key) {
+    if (writeCache.containsKey(key)) {
+      return writeCache.get(key);
     }
+    return backingSource.get(key);
+  }
 
-    @Override
-    public void put(byte[] key, V value) {
-        writeCache.put(key, value);
-    }
-
-    @Override
-    public boolean containsKey(byte[] key) {
-        return writeCache.containsKey(key) || backingSource.containsKey(key);
-    }
-
-    @Override
-    public V get(byte[] key) {
-        if (writeCache.containsKey(key)) {
-            return writeCache.get(key);
-        }
-        return backingSource.get(key);
-    }
-
-    @Override
-    public void commit() {
-
-    }
+  @Override
+  public void commit() {
+    writeCache.forEach((key, value) -> {
+      if (value == null) {
+        this.backingSource.delete(key);
+      } else {
+        this.backingSource.put(key, value);
+      }
+    });
+  }
 }
