16f32f71c67e434bdef10cd8e9695c8dc7577cda
nanfengpo <lsxxxxxx@126.com>
2018-11-06 23:40:44 +0800

    add comittee parameter ALLOW_ADAPTIVE_RESOURCE

--- src/main/java/org/tron/core/actuator/ProposalCreateActuator.java
+++ src/main/java/org/tron/core/actuator/ProposalCreateActuator.java
@@ -211,6 +211,13 @@ public class ProposalCreateActuator extends AbstractActuator {
         }
         break;
       }
+      case (17): {
+        if (entry.getValue() != 1) {
+          throw new ContractValidateException(
+              "This value[ALLOW_ADAPTIVE_RESOURCE] is only allowed to be 1");
+        }
+        break;
+      }
       default:
         break;
     }
