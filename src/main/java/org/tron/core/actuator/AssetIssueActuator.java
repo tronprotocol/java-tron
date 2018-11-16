a48cec2271b8f0b15dd7d94aa36fc0ca56d52fa3
zergweak <zhang_anwen@163.com>
2018-11-16 11:28:36 +0800

    set asset issue name

--- src/main/java/org/tron/core/actuator/AssetIssueActuator.java
+++ src/main/java/org/tron/core/actuator/AssetIssueActuator.java
@@ -91,9 +91,9 @@ public class AssetIssueActuator extends AbstractActuator {
       }
 
       if (dbManager.getDynamicPropertiesStore().getAllowSameTokenName() == 0) {
-        accountCapsule.setAssetIssuedName(assetIssueCapsule.createDbKey());
         accountCapsule.addAsset(assetIssueCapsule.createDbKey(), remainSupply);
       }
+      accountCapsule.setAssetIssuedName(assetIssueCapsule.createDbKey());
       accountCapsule.setAssetIssuedID(assetIssueCapsule.createDbV2Key());
       accountCapsule.addAssetV2(assetIssueCapsule.createDbV2Key(), remainSupply);
       accountCapsule.setInstance(accountCapsule.getInstance().toBuilder()
