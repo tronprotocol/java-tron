e041911bd9de519e4567309082f3a798b959021e
Kamil Zielinski <me@kamilzielin.ski>
2018-12-01 13:28:22 +0100

    Moved static class to the bottom

--- src/main/java/org/tron/core/capsule/BlockCapsule.java
+++ src/main/java/org/tron/core/capsule/BlockCapsule.java
@@ -42,79 +42,6 @@ import org.tron.protos.Protocol.Transaction;
 @Slf4j
 public class BlockCapsule implements ProtoCapsule<Block> {
 
-  public static class BlockId extends Sha256Hash {
-
-    @Override
-    public boolean equals(Object o) {
-      if (this == o) {
-        return true;
-      }
-      if (o == null || (getClass() != o.getClass() && !(o instanceof Sha256Hash))) {
-        return false;
-      }
-      return Arrays.equals(getBytes(), ((Sha256Hash) o).getBytes());
-    }
-
-    public String getString() {
-      return "Num:" + num + ",ID:" + super.toString();
-    }
-
-    @Override
-    public String toString() {
-      return super.toString();
-    }
-
-    @Override
-    public int hashCode() {
-      return super.hashCode();
-    }
-
-    @Override
-    public int compareTo(Sha256Hash other) {
-      if (other.getClass().equals(BlockId.class)) {
-        long otherNum = ((BlockId) other).getNum();
-        return Long.compare(num, otherNum);
-      }
-      return super.compareTo(other);
-    }
-
-    private long num;
-
-    public BlockId() {
-      super(Sha256Hash.ZERO_HASH.getBytes());
-      num = 0;
-    }
-
-    public BlockId(Sha256Hash blockId) {
-      super(blockId.getBytes());
-      byte[] blockNum = new byte[8];
-      System.arraycopy(blockId.getBytes(), 0, blockNum, 0, 8);
-      num = Longs.fromByteArray(blockNum);
-    }
-
-    /**
-     * Use {@link #wrap(byte[])} instead.
-     */
-    public BlockId(Sha256Hash hash, long num) {
-      super(num, hash);
-      this.num = num;
-    }
-
-    public BlockId(byte[] hash, long num) {
-      super(num, hash);
-      this.num = num;
-    }
-
-    public BlockId(ByteString hash, long num) {
-      super(num, hash.toByteArray());
-      this.num = num;
-    }
-
-    public long getNum() {
-      return num;
-    }
-  }
-
   private BlockId blockId = new BlockId(Sha256Hash.ZERO_HASH, 0);
 
   private Block block;
@@ -323,5 +250,78 @@ public class BlockCapsule implements ProtoCapsule<Block> {
     }
     toStringBuff.append("]");
     return toStringBuff.toString();
+  }
+
+  public static class BlockId extends Sha256Hash {
+
+    private long num;
+
+    public BlockId() {
+      super(Sha256Hash.ZERO_HASH.getBytes());
+      num = 0;
+    }
+
+    public BlockId(Sha256Hash blockId) {
+      super(blockId.getBytes());
+      byte[] blockNum = new byte[8];
+      System.arraycopy(blockId.getBytes(), 0, blockNum, 0, 8);
+      num = Longs.fromByteArray(blockNum);
+    }
+
+    /**
+     * Use {@link #wrap(byte[])} instead.
+     */
+    public BlockId(Sha256Hash hash, long num) {
+      super(num, hash);
+      this.num = num;
+    }
+
+    public BlockId(byte[] hash, long num) {
+      super(num, hash);
+      this.num = num;
+    }
+
+    public BlockId(ByteString hash, long num) {
+      super(num, hash.toByteArray());
+      this.num = num;
+    }
+
+    public long getNum() {
+      return num;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+      if (this == o) {
+        return true;
+      }
+      if (o == null || (getClass() != o.getClass() && !(o instanceof Sha256Hash))) {
+        return false;
+      }
+      return Arrays.equals(getBytes(), ((Sha256Hash) o).getBytes());
+    }
+
+    public String getString() {
+      return "Num:" + num + ",ID:" + super.toString();
+    }
+
+    @Override
+    public String toString() {
+      return super.toString();
+    }
+
+    @Override
+    public int hashCode() {
+      return super.hashCode();
+    }
+
+    @Override
+    public int compareTo(Sha256Hash other) {
+      if (other.getClass().equals(BlockId.class)) {
+        long otherNum = ((BlockId) other).getNum();
+        return Long.compare(num, otherNum);
+      }
+      return super.compareTo(other);
+    }
   }
 }
