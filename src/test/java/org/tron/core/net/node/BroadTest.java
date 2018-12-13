b2126ca1451c8191849eca8bd45a357f1159569b
sun haoyu <shydesky@gmail.com>
2018-12-13 12:25:50 +0800

    mdf some net unit test

--- src/test/java/org/tron/core/net/node/BroadTest.java
+++ src/test/java/org/tron/core/net/node/BroadTest.java
@@ -147,6 +147,10 @@ public class BroadTest {
     //Assert.assertEquals(advObjToFetch.get(condition.getTransactionId()), InventoryType.TRX);
     //To avoid writing the database, manually stop the sending of messages.
     Collection<PeerConnection> activePeers = ReflectUtils.invokeMethod(nodeImpl, "getActivePeer");
+    Thread.sleep(1000);
+    if(activePeers.size() < 1){
+      return;
+    }
     for (PeerConnection peerConnection : activePeers) {
       MessageQueue messageQueue = ReflectUtils.getFieldValue(peerConnection, "msgQueue");
       ReflectUtils.setFieldValue(messageQueue, "sendMsgFlag", false);
