8b7ad0aa4cc467d73e5ed5afdf9a81773af8ee90
wubin1 <wubin1@tron.network>
2018-12-20 16:35:22 +0800

    add some unit test

--- src/main/java/org/tron/core/db2/core/AbstractSnapshot.java
+++ src/main/java/org/tron/core/db2/core/AbstractSnapshot.java
@@ -7,7 +7,7 @@ import org.tron.core.db2.common.DB;
 
 public abstract class AbstractSnapshot<K, V> implements Snapshot {
   @Getter
-  protected volatile DB<K, V> db;
+  protected DB<K, V> db;
   @Getter
   @Setter
   protected Snapshot previous;
