f208e91b591567aca1b51e36778e251df1939abf
jeancky <jeancky@163.com>
2018-12-24 16:21:59 +0800

    commit

--- src/main/java/org/tron/common/runtime/vm/event/ContractEventListener.java
+++ src/main/java/org/tron/common/runtime/vm/event/ContractEventListener.java
@@ -3,6 +3,10 @@ package org.tron.common.runtime.vm.event;
 
 public interface ContractEventListener {
 
+  /**
+   * on Event callback
+   * @param event
+   */
   void onEvent(ContractEvent event);
 
 }
