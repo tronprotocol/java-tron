bd469a0dbcd790c39fdfbc844d5741eb205d79a9
jiangyy <jiangyy0824@163.com>
2018-12-24 15:59:29 +0800

    remove useless import

---
+++ src/main/java/org/tron/common/logsfilter/trigger/BlockLogTrigger.java
@@ -0,0 +1,43 @@
+package org.tron.common.logsfilter.trigger;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.List;
+
+public class BlockLogTrigger {
+    @Getter
+    @Setter
+    private long timeStamp;
+
+    @Getter
+    @Setter
+    private long blockNumber;
+
+    @Getter
+    @Setter
+    private String blockHash;
+
+    @Getter
+    @Setter
+    private long transactionSize;
+
+    @Getter
+    @Setter
+    private List<String> transactionList;
+
+    @Override
+    public String toString(){
+        return new StringBuilder().append("timestamp: ")
+                .append(timeStamp)
+                .append(", blockNumber: ")
+                .append(blockNumber)
+                .append(", blockhash: ")
+                .append(blockHash)
+                .append(", transaction size: ")
+                .append(transactionSize)
+                .append(", transaction list: ")
+                .append(transactionList).toString();
+    }
+
+}
