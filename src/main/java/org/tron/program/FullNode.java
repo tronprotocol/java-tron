6d47c2408fd9ac3ab7953bc066f0c5f9265fd5a0
Matt <yrp1990@gmail.com>
2018-12-28 17:49:48 +0800

    cache some txs

--- src/main/java/org/tron/program/FullNode.java
+++ src/main/java/org/tron/program/FullNode.java
@@ -3,7 +3,21 @@ package org.tron.program;
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
+import com.google.common.collect.Maps;
+import com.google.common.collect.Streams;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
+import com.google.common.util.concurrent.ListeningExecutorService;
+import com.google.common.util.concurrent.MoreExecutors;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.LongStream;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -12,8 +26,15 @@ import org.tron.common.application.Application;
 import org.tron.common.application.ApplicationFactory;
 import org.tron.common.application.TronApplicationContext;
 import org.tron.core.Constant;
+import org.tron.core.capsule.BlockCapsule;
 import org.tron.core.config.DefaultConfig;
 import org.tron.core.config.args.Args;
+import org.tron.core.db.DynamicPropertiesStore;
+import org.tron.core.db.Manager;
+import org.tron.core.db.RecentBlockStore;
+import org.tron.core.db2.common.Key;
+import org.tron.core.exception.BadItemException;
+import org.tron.core.exception.ItemNotFoundException;
 import org.tron.core.services.RpcApiService;
 import org.tron.core.services.WitnessService;
 import org.tron.core.services.http.FullNodeHttpApiService;
@@ -103,5 +124,47 @@ public class FullNode {
   public static void shutdown(final Application app) {
     logger.info("********register application shutdown hook********");
     Runtime.getRuntime().addShutdownHook(new Thread(app::shutdown));
+  }
+
+  public static void cache(TronApplicationContext context) {
+    long start = System.currentTimeMillis();
+    long headNum = context.getBean(DynamicPropertiesStore.class).getLatestBlockHeaderNumber();
+    long recentBlockCount = Streams.stream(context.getBean(RecentBlockStore.class).getRevokingDB()).count();
+    Manager manager = context.getBean(Manager.class);
+    Map<Key, Long> trxIds = new ConcurrentHashMap<>();
+    ListeningExecutorService service = MoreExecutors.listeningDecorator(Executors.newFixedThreadPool(50));
+    List<ListenableFuture<?>> futures = new ArrayList<>();
+    AtomicLong atomicLong = new AtomicLong(0);
+    LongStream.rangeClosed(headNum - recentBlockCount + 1, headNum).forEach(
+        blockNum -> futures.add(service.submit(() -> {
+          try {
+            atomicLong.incrementAndGet();
+            BlockCapsule blockCapsule = manager.getBlockByNum(blockNum);
+            blockCapsule.getTransactions().stream()
+                .map(tc -> tc.getTransactionId().getBytes())
+                .map(bytes -> Maps.immutableEntry(Key.of(bytes), blockNum))
+                .forEach(e -> trxIds.put(e.getKey(), e.getValue()));
+          } catch (ItemNotFoundException e) {
+            e.printStackTrace();
+          } catch (BadItemException e) {
+            e.printStackTrace();
+          }
+        })));
+    ListenableFuture<?> future = Futures.allAsList(futures);
+    try {
+      future.get();
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    } catch (ExecutionException e) {
+      e.printStackTrace();
+    }
+    System.out.println("trxids:" + trxIds.size()
+        + ", block count:" + atomicLong.get()
+        + ", cost:" + (System.currentTimeMillis() - start)
+        + ", block count2:" + trxIds.values().stream()
+        .distinct().count()
+    );
+    trxIds.values().forEach(System.out::println);
+    System.exit(0);
   }
 }
