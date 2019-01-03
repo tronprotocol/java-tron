56b1949e90bfd4436b0eb59d4f027ec8c3f51cb9
Matt <yrp1990@gmail.com>
2019-01-03 16:43:44 +0800

    add Flusher

---
+++ src/main/java/org/tron/core/db/TransactionCache.java
@@ -0,0 +1,28 @@
+package org.tron.core.db;
+
+import java.util.List;
+import java.util.Objects;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ArrayUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.tron.common.utils.ByteArray;
+import org.tron.common.utils.Sha256Hash;
+import org.tron.core.capsule.BlockCapsule;
+import org.tron.core.capsule.BytesCapsule;
+import org.tron.core.capsule.TransactionCapsule;
+import org.tron.core.db.KhaosDatabase.KhaosBlock;
+import org.tron.core.db2.common.TxCacheDB;
+import org.tron.core.exception.BadItemException;
+import org.tron.core.exception.StoreException;
+
+@Slf4j
+@Component
+public class TransactionCache extends TronStoreWithRevoking<BytesCapsule> {
+
+  @Autowired
+  private TransactionCache(@Value("trans-cache") String dbName) {
+    super(dbName, TxCacheDB.class);
+  }
+}
