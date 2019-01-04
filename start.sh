324d1c06743f4ffbd44fe0760bdf99a486858ca8
sun haoyu <shydesky@gmail.com>
2019-01-04 15:32:39 +0800

    modify start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,19 @@
-#!/bin/bash
-kill -9 `cat /home/tron/pid.txt`
-nohup  java -jar /home/tron/java-tron/java-tron.jar -p $LOCAL_WITNESS_PRIVATE_KEY --witness -c /home/tron/config.conf > /home/tron/tron-shell.log 2>&1 & echo $! >/home/tron/pid.txt
+count=1
+while [ $count -le 60 ]; do
+  pid=`ps -ef |grep FullNode.jar |grep -v grep |awk '{print $2}'`
+  if [ -n "$pid" ]; then
+    kill -15 $pid
+    echo "kill -15 java-tron, counter $count"
+    sleep 1
+  else
+    echo "java-tron killed"
+    break
+  fi
+done
+
+total=`cat /proc/meminfo  |grep MemTotal |awk -F ' ' '{print $2}'`
+xmx=`echo "$total/1024/1024*0.8" | bc |awk -F. '{print $1"g"}'`
+logtime=`date +%Y-%m-%d_%H-%M-%S`
+nohup java -Xmx$xmx -jar build/libs/FullNode.jar -c config.conf  >> start.log 2>&1 &
+pid=`ps -ef |grep FullNode.jar |grep -v grep |awk '{print $2}'`
+echo "start java-tron with pid $pid on $HOSTNAME"
