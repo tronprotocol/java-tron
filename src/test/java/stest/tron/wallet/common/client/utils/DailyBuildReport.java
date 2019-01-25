918673c7943e4afa3b9dc23b6df5f9cd6f38ad2b
wangzihe <wangzihe@tron.network>
2019-01-25 17:06:34 +0800

    Modify deploy.sh

--- src/test/java/stest/tron/wallet/common/client/utils/DailyBuildReport.java
+++ src/test/java/stest/tron/wallet/common/client/utils/DailyBuildReport.java
@@ -35,7 +35,6 @@ public class DailyBuildReport extends TestListenerAdapter {
     passedDescriptionList.append(result.getMethod().getRealClass() + ": "
         + result.getMethod().getDescription() + "\n");
     passedNum++;
-    result.getMethod()
   }
 
   @Override
