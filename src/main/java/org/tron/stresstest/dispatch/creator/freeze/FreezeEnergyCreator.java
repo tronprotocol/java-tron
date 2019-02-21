66351b88b0706b941244b9ce4e8772b135597e94
HuangZhengSi <huangzhengsi@tron.network>
2019-02-21 13:03:59 +0800

    update

--- src/main/java/org/tron/stresstest/dispatch/creator/freeze/FreezeEnergyCreator.java
+++ src/main/java/org/tron/stresstest/dispatch/creator/freeze/FreezeEnergyCreator.java
@@ -17,7 +17,7 @@ public class FreezeEnergyCreator extends AbstractTransactionCreator implements G
 
   private String ownerAddress = commonOwnerAddress;
   private long frozenBalance = 1000000L;
-  private long frozenDuration = 0L;
+  private long frozenDuration = 3L;
   private int resourceCode = 1;
   private String delegateAddress = "TDZdB4ogHSgU1CGrun8WXaMb2QDDkvAKQm";
   private String privateKey = commonOwnerPrivateKey;
