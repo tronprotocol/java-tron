0a1ffbc610f17adc1023d74fe6adb1b8a5dfd4d0
wangzihe <wangzihe@tron.network>
2019-02-21 11:34:08 +0800

    Add HttpMethed for stest http test case

--- src/test/java/stest/tron/wallet/common/client/utils/HttpMethed.java
+++ src/test/java/stest/tron/wallet/common/client/utils/HttpMethed.java
@@ -45,6 +45,30 @@ public class HttpMethed {
     return response;
   }
 
+  public static HttpResponse getAccountNet(String httpNode, byte[] queryAddress) {
+    httpClient.getParams().setParameter(CoreConnectionPNames.CONNECTION_TIMEOUT, 2000);
+    httpClient.getParams().setParameter(CoreConnectionPNames.SO_TIMEOUT, 2000);
+    try {
+      String getAccountUrl = "http://" + httpNode + "/wallet/getaccountnet";
+      httppost = new HttpPost(getAccountUrl);
+      httppost.setHeader("Content-type", "application/json; charset=utf-8");
+      httppost.setHeader("Connection", "Close");
+      JsonObject userBaseObj2 = new JsonObject();
+      userBaseObj2.addProperty("address", ByteArray.toHexString(queryAddress));
+      StringEntity entity = new StringEntity(userBaseObj2.toString(), Charset.forName("UTF-8"));
+      entity.setContentEncoding("UTF-8");
+      entity.setContentType("application/json");
+      httppost.setEntity(entity);
+      response = httpClient.execute(httppost);
+    } catch (Exception e) {
+      e.printStackTrace();
+      httppost.releaseConnection();
+      return null;
+    }
+    httppost.releaseConnection();
+    return response;
+  }
+
   /**
    * constructor.
    */
--- src/test/java/stest/tron/wallet/dailybuild/http/HttpTestAccount001.java
+++ src/test/java/stest/tron/wallet/dailybuild/http/HttpTestAccount001.java
@@ -46,6 +46,35 @@ public class HttpTestAccount001 {
     Assert.assertTrue(responseContent.size() > 3);
   }
 
+  /**
+   * constructor.
+   */
+  @Test(enabled = true, description = "Get accountNet by http")
+  public void getAccountNet() {
+    response = HttpMethed.getAccountNet(httpnode,fromAddress);
+    try {
+      entity = response.getEntity();
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+    logger.info("code is " + response.getStatusLine().getStatusCode());
+    Assert.assertEquals(response.getStatusLine().getStatusCode(),200);
+    responseContent = HttpMethed.parseResponseContent(response);
+
+    for (String str:responseContent.keySet()) {
+      if (str.equals("freeNetLimit")) {
+        Assert.assertEquals(responseContent.get(str),5000);
+      }
+      if (str.equals("TotalNetLimit")) {
+        Assert.assertEquals(responseContent.get(str),43200000000L);
+      }
+      logger.info(str + ":" + responseContent.get(str));
+    }
+    Assert.assertTrue(responseContent.size() >= 4);
+  }
+
+
+
 }
 
 
