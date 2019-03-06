e783303a34a67e0fea51d50173cf871880ea3568
HuangZhengSi <huangzhengsi@tron.network>
2019-03-06 11:34:27 +0800

    update

--- runcontinue.sh
+++ runcontinue.sh
@@ -4,3 +4,9 @@ curl http://60.205.215.34/Daily_Build_Task_Report
 
 PassFlag=`curl http://60.205.215.34/Daily_Build_Task_Report | grep "Failed: 0"`
 echo "PassFlag is: $PassFlag"
+
+if [$PassFlag==""]; then
+    exit 0
+else
+    exit 1
+fi
