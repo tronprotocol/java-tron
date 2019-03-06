68ec00337fa1d059c6df3921e3c413de1e8c79ab
HuangZhengSi <huangzhengsi@tron.network>
2019-03-06 12:16:31 +0800

    update

--- DownloadLinks.sh
+++ DownloadLinks.sh
@@ -1,16 +1,16 @@
 #DATE= $(date "+%Y-%m-%d")
 #date =`date +"%Y-%m-%d" -d "-1day"`
 
-PassFlag=`curl http://60.205.215.34/Daily_Build_Task_Report | grep "Failed: 0"`
+PassFlag=`curl -s http://60.205.215.34/Daily_Build_Task_Report | grep "Failed: 0"`
 
 if [$PassFlag == ""]; then
-    echo "Stest Failed"
+    echo "Daily Build Stest Fail"
     echo "To view Daily Rplay and Stress Test logs please visit website below on browsers"
     echo "--- http://60.205.215.34/latestReplayLog"
     echo "--- http://60.205.215.34/latestStressLog"
 
 else
-    echo "Stest Pass"
+    echo "Daily Build Stest Pass"
     echo "Build on `date +"%Y-%m-%d"` 3:00:00 (CST), UTC +8"
     echo "Please visit following website to download java-tron.jar on browsers"
     echo "--- http://60.205.215.34/Daily_Build/java-tron.jar"
