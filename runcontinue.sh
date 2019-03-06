35c610a44bbb4a9470c440e61c6cdf1281ba85a7
HuangZhengSi <huangzhengsi@tron.network>
2019-03-06 10:30:26 +0800

    update

--- runcontinue.sh
+++ runcontinue.sh
@@ -1,5 +1,6 @@
-DATE= expr`$(date "+%Y-%m-%d_%H:%M:%S")`
-echo "Build on `$DATE` (CST), UTC +8"
+#DATE= $(date "+%Y-%m-%d")
+DATE=`date +"%Y-%m-%d %H:%M:%S" -d "-1day"`
+echo "Build on `$DATE`  (CST), UTC +8"
 echo "Please visit following website to download java-tron.jar on browsers"
 echo "--- http://60.205.215.34/Daily_Build/java-tron.jar"
 echo "To view Daily Rplay and Stress Test logs please visit website below on browsers"
