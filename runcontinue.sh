e27aecb5d1ebd2a1afaf8da34ad3e6fb83f30b3b
HuangZhengSi <huangzhengsi@tron.network>
2019-03-06 11:30:12 +0800

    update

--- runcontinue.sh
+++ runcontinue.sh
@@ -2,5 +2,5 @@
 #date =`date +"%Y-%m-%d" -d "-1day"`
 curl http://60.205.215.34/Daily_Build_Task_Report
 
-PassFlag=`curl http://60.205.215.34/Daily_Build_Task_Report | grep "Failed: 0"`
+PassFlag=`curl http://60.205.215.34/Daily_Build_Task_Report | grep "Failed: 0" | grep "Failed: 0"`
 echo "$PassFlag"
