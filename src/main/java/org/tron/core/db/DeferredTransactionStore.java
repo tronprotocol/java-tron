a2d231a7cf6cc9e1af99e08ac87f0c27a98baf38
wubin1 <wubin1@tron.network>
2019-03-06 17:49:27 +0800

    add max pending length

--- src/main/java/org/tron/core/db/DeferredTransactionStore.java
+++ src/main/java/org/tron/core/db/DeferredTransactionStore.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.tron.core.capsule.DeferredTransactionCapsule;
+import static org.tron.core.config.Parameter.NodeConstant.MAX_TRANSACTION_PENDING;
 import java.util.List;
 
 @Slf4j(topic = "DB")
@@ -27,7 +28,7 @@ public class DeferredTransactionStore extends TronStoreWithRevoking<DeferredTran
     }
 
     public List<DeferredTransactionCapsule> getScheduledTransactions (long time){
-        return revokingDB.getValuesPrevious(Longs.toByteArray(time), Long.MAX_VALUE).stream()
+        return revokingDB.getValuesPrevious(Longs.toByteArray(time), MAX_TRANSACTION_PENDING).stream()
             .filter(Objects::nonNull)
             .map(DeferredTransactionCapsule::new)
             .collect(Collectors.toList());
