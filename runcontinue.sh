f1ee36ad79c846b11611ae301174982bdaad0688
HuangZhengSi <huangzhengsi@tron.network>
2019-03-06 11:07:10 +0800

    update

--- runcontinue.sh
+++ runcontinue.sh
@@ -1,5 +1,6 @@
 #DATE= $(date "+%Y-%m-%d")
 #date =`date +"%Y-%m-%d" -d "-1day"`
+curl http://60.205.215.34/Daily_Build_Task_Report
 echo "Build on `date +"%Y-%m-%d"` 3:00:00 (CST), UTC +8"
 echo "Please visit following website to download java-tron.jar on browsers"
 echo "--- http://60.205.215.34/Daily_Build/java-tron.jar"
