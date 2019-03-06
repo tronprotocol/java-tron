c03b505877054ee8c20360b08c5431886bcb020f
HuangZhengSi <huangzhengsi@tron.network>
2019-03-06 12:11:08 +0800

    update

--- DownloadLinks.sh
+++ DownloadLinks.sh
@@ -1,12 +1,25 @@
 #DATE= $(date "+%Y-%m-%d")
 #date =`date +"%Y-%m-%d" -d "-1day"`
-echo "Build on `date +"%Y-%m-%d"` 3:00:00 (CST), UTC +8"
-echo "Please visit following website to download java-tron.jar on browsers"
-echo "--- http://60.205.215.34/Daily_Build/java-tron.jar"
-echo "To view Daily Rplay and Stress Test logs please visit website below on browsers"
-echo "--- http://60.205.215.34/latestReplayLog"
-echo "--- http://60.205.215.34/latestStressLog"
-echo "The following compressed package is provided for user to set up Fullnode. Please use Linux OS to Download"
-echo "--- curl -# -O http://60.205.215.34/Daily_Build/java-tron.tar.gz"
-echo "To unzip file use the command below"
-echo "--- tar -xzvf java-tron.tar.gz"
+
+PassFlag=`curl http://60.205.215.34/Daily_Build_Task_Report | grep "Failed: 0"`
+
+if [$PassFlag == ""]; then
+    echo "Stest Failed"
+    echo "To view Daily Rplay and Stress Test logs please visit website below on browsers"
+    echo "--- http://60.205.215.34/latestReplayLog"
+    echo "--- http://60.205.215.34/latestStressLog"
+
+else
+    echo "Stest Pass"
+    echo "Build on `date +"%Y-%m-%d"` 3:00:00 (CST), UTC +8"
+    echo "Please visit following website to download java-tron.jar on browsers"
+    echo "--- http://60.205.215.34/Daily_Build/java-tron.jar"
+    echo "To view Daily Rplay and Stress Test logs please visit website below on browsers"
+    echo "--- http://60.205.215.34/latestReplayLog"
+    echo "--- http://60.205.215.34/latestStressLog"
+    echo "The following compressed package is provided for user to set up Fullnode. Please use Linux OS to Download"
+    echo "--- curl -# -O http://60.205.215.34/Daily_Build/java-tron.tar.gz"
+    echo "To unzip file use the command below"
+    echo "--- tar -xzvf java-tron.tar.gz"
+fi
+
--- runcontinue.sh
+++ runcontinue.sh
@@ -1,12 +1,3 @@
 
 curl http://60.205.215.34/Daily_Build_Task_Report
 
-PassFlag=`curl http://60.205.215.34/Daily_Build_Task_Report | grep "Failed: 0"`
-
-if [$PassFlag == ""]; then
-    echo "Stest Failed"
-    exit 1
-else
-    echo "Stest Pass"
-    exit 0
-fi
