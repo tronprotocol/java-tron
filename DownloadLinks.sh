70901186eb4b99f3dd5bd5f373cfc516f1bc7d2e
HuangZhengSi <huangzhengsi@tron.network>
2019-03-06 12:19:43 +0800

    update

--- DownloadLinks.sh
+++ DownloadLinks.sh
@@ -5,7 +5,7 @@ PassFlag=`curl -s http://60.205.215.34/Daily_Build_Task_Report | grep "Failed: 0
 
 if [$PassFlag == ""]; then
     echo "Daily Build Stest Fail"
-    echo "To view Daily Rplay and Stress Test logs please visit website below on browsers"
+    echo "To view Daily Replay and Stress Test logs please visit website below on browsers"
     echo "--- http://60.205.215.34/latestReplayLog"
     echo "--- http://60.205.215.34/latestStressLog"
 
@@ -14,7 +14,7 @@ else
     echo "Build on `date +"%Y-%m-%d"` 3:00:00 (CST), UTC +8"
     echo "Please visit following website to download java-tron.jar on browsers"
     echo "--- http://60.205.215.34/Daily_Build/java-tron.jar"
-    echo "To view Daily Rplay and Stress Test logs please visit website below on browsers"
+    echo "To view Daily Replay and Stress Test logs please visit website below on browsers"
     echo "--- http://60.205.215.34/latestReplayLog"
     echo "--- http://60.205.215.34/latestStressLog"
     echo "The following compressed package is provided for user to set up Fullnode. Please use Linux OS to Download"
