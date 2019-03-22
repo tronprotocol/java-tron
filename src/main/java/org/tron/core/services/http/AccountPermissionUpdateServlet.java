bb7cfe16148ffc0ad25afc3ce977f68f6eb1ca7f
wubin1 <wubin1@tron.network>
2019-03-22 12:20:14 +0800

    revert AccountPermissionUpdateContract

--- src/main/java/org/tron/core/services/http/AccountPermissionUpdateServlet.java
+++ src/main/java/org/tron/core/services/http/AccountPermissionUpdateServlet.java
@@ -1,6 +1,5 @@
 package org.tron.core.services.http;
 
-import com.alibaba.fastjson.JSONObject;
 import java.io.IOException;
 import java.util.stream.Collectors;
 import javax.servlet.http.HttpServlet;
@@ -9,11 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.tron.core.Constant;
 import org.tron.core.Wallet;
-import org.tron.core.capsule.utils.TransactionUtil;
 import org.tron.protos.Contract.AccountPermissionUpdateContract;
-import org.tron.protos.Protocol.DeferredStage;
 import org.tron.protos.Protocol.Transaction;
 import org.tron.protos.Protocol.Transaction.Contract.ContractType;
 
@@ -36,17 +32,10 @@ public class AccountPermissionUpdateServlet extends HttpServlet {
       Util.checkBodySize(contract);
       AccountPermissionUpdateContract.Builder build = AccountPermissionUpdateContract.newBuilder();
       JsonFormat.merge(contract, build);
+
       Transaction tx = wallet
           .createTransactionCapsule(build.build(), ContractType.AccountPermissionUpdateContract)
           .getInstance();
-
-      JSONObject input = JSONObject.parseObject(contract);
-
-      if (input.containsKey(Constant.DELAY_SECONDS)) {
-        long delaySeconds = input.getLong(Constant.DELAY_SECONDS);
-        tx = TransactionUtil.setTransactionDelaySeconds(tx, delaySeconds);
-      }
-
       response.getWriter().println(Util.printTransaction(tx));
     } catch (Exception e) {
       logger.debug("Exception: {}", e.getMessage());
