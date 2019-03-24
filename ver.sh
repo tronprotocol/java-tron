c1fb74b2e644b2a9f98adbd63e0e9af408f78320
Matt <yrp1990@gmail.com>
2019-03-24 19:52:31 +0800

    auto update version

--- ver.sh
+++ ver.sh
@@ -5,7 +5,7 @@ currentBranch=`git rev-parse --abbrev-ref HEAD`
 if [ "$currentBranch"x != "master"x ]
 then
   echo The current branch is not master. Please checkout into master.
-  #exit 1
+  exit 1
 fi
 
 git pull origin master
