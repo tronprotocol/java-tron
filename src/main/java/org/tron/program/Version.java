fafc4674cb5604c4cfe76435b132ceaf51bb67a2
Matt <yrp1990@gmail.com>
2019-03-24 19:45:58 +0800

    update a new version. version name:Odyssey-v3.5.0.1-4-g789710111,version code:9597

--- src/main/java/org/tron/program/Version.java
+++ src/main/java/org/tron/program/Version.java
@@ -5,9 +5,9 @@ import lombok.Getter;
 public class Version {
   private static final String version = "3.5";
     @Getter
-    private String versionName = "";
+    private String versionName = "Odyssey-v3.5.0.1-4-g789710111";
     @Getter
-    private String versionCode = "";
+    private String versionCode = "9597";
 
     public static String getVersion() {
       return version;
