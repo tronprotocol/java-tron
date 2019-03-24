25bdad0bbcdb91f3f87a4de658c05df4446b8586
Matt <yrp1990@gmail.com>
2019-03-24 19:30:59 +0800

    auto update version

--- src/main/java/org/tron/program/Version.java
+++ src/main/java/org/tron/program/Version.java
@@ -1,10 +1,16 @@
 package org.tron.program;
 
+import lombok.Getter;
+
 public class Version {
-
   private static final String version = "3.5";
+    @Getter
+    private String versionName = "";
+    @Getter
+    private String versionCode = "";
 
-  public static String getVersion() {
-    return version;
-  }
+    public static String getVersion() {
+      return version;
+    }
 }
+
