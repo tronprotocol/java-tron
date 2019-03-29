72143bebe43a8c15a73809f12f70fc98815951af
wangming <wm18840987261@163.com>
2019-03-29 19:00:04 +0800

    change solidity pragma

--- src/test/resources/soliditycode/contractTrcToken049.sol
+++ src/test/resources/soliditycode/contractTrcToken049.sol
@@ -3,7 +3,7 @@
  contract tokenTest{
      constructor() public payable{}
      // positive case
-     function TransferTokenTo(address toAddress, trcToken id,uint256 amount) public payable{
+     function TransferTokenTo(address payable toAddress, trcToken id,uint256 amount) public payable{
          //trcToken id = 0x74657374546f6b656e;
          toAddress.transferToken(amount,id);
      }
