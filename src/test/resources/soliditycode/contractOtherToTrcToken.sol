dddb22fd7285b2610ea3f62a6cb37d90d6a81153
HuangZhengSi <huangzhengsi@tron.network>
2019-04-01 14:56:32 +0800

    modify solidity file

--- src/test/resources/soliditycode/contractOtherToTrcToken.sol
+++ src/test/resources/soliditycode/contractOtherToTrcToken.sol
@@ -4,37 +4,37 @@ contract ConvertType {
 
 constructor() payable public{}
 
-function() payable public{}
+function() payable external{}
 
-function stringToTrctoken(address toAddress, string tokenStr, uint256 tokenValue) public {
+function stringToTrctoken(address toAddress, string memory tokenStr, uint256 tokenValue) public {
 // trcToken t = trcToken(tokenStr); // ERROR
 // toAddress.transferToken(tokenValue, tokenStr); // ERROR
 }
 
-function uint256ToTrctoken(address toAddress, uint256 tokenInt, uint256 tokenValue) public {
+function uint256ToTrctoken(address payable toAddress, uint256 tokenInt, uint256 tokenValue)  public {
 trcToken t = trcToken(tokenInt); // OK
 toAddress.transferToken(tokenValue, t); // OK
 toAddress.transferToken(tokenValue, tokenInt); // OK
 }
 
-function addressToTrctoken(address toAddress, address adr, uint256 tokenValue) public {
+function addressToTrctoken(address payable toAddress, address adr, uint256 tokenValue) public {
 trcToken t = trcToken(adr); // OK
 toAddress.transferToken(tokenValue, t); // OK
 // toAddress.transferToken(tokenValue, adr); // ERROR
 }
 
-function bytesToTrctoken(address toAddress, bytes b, uint256 tokenValue) public {
+function bytesToTrctoken(address payable toAddress, bytes memory b, uint256 tokenValue) public {
 // trcToken t = trcToken(b); // ERROR
 // toAddress.transferToken(tokenValue, b); // ERROR
 }
 
-function bytes32ToTrctoken(address toAddress, bytes32 b32, uint256 tokenValue) public {
+function bytes32ToTrctoken(address payable toAddress, bytes32 b32, uint256 tokenValue) public {
 trcToken t = trcToken(b32); // OK
 toAddress.transferToken(tokenValue, t); // OK
 // toAddress.transferToken(tokenValue, b32); // ERROR
 }
 
-function arrayToTrctoken(address toAddress, uint256[] arr, uint256 tokenValue) public {
+function arrayToTrctoken(address payable toAddress, uint256 arr, uint256 tokenValue) public {
 trcToken t = trcToken(arr); // ERROR
 toAddress.transferToken(tokenValue, arr); // ERROR
 }
--- src/test/resources/soliditycode/contractToMathedFeed.sol
+++ src/test/resources/soliditycode/contractToMathedFeed.sol
@@ -2,18 +2,18 @@
 
 contract ToMathedFeed {
     uint public i=1;
-    function ToMathed(uint value) {
+    function ToMathed (uint value) public {
          i=value;
     }
 }
 
 contract ToMathedUseINContract {
-    function ToMathedIUseNR(address a,uint256 n) returns(bool){
+    function ToMathedIUseNR(address a,uint256 n) public returns(bool,bytes memory){
         address payContract=a;
-        return payContract.call(bytes4(keccak256("ToMathedNot(uint256)")),n);
+        return payContract.call(abi.encode(bytes4(keccak256("ToMathedNot(uint256)")),n));
     }
-    function ToMathedIUseNRE(address a,uint256 value) returns(bool){
+    function ToMathedIUseNRE(address a,uint256 value) public returns(bool,bytes memory){
         address payContract=a;
-        return payContract.call(bytes4(keccak256("ToMathed(uint256)")),value);
+        return payContract.call(abi.encode(bytes4(keccak256("ToMathed(uint256)")),value));
     }
 }
