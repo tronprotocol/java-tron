805cafb56e0eb96aaf054eb6d5fc398e4ca9392a
dannaguo <guoxiaodan_guo@163.com>
2019-04-01 14:32:36 +0800

    modify parts of solidity source codes

--- src/test/resources/soliditycode/contractTrcToken037.sol
+++ src/test/resources/soliditycode/contractTrcToken037.sol
@@ -1,7 +1,7 @@
 //pragma solidity ^0.4.24;
 
 contract transferTrc10 {
-    function receive(address rec) public payable {
+    function receive(address payable rec) public payable {
         uint256 aamount=address(this).tokenBalance(msg.tokenid);
         uint256 bamount=rec.tokenBalance(msg.tokenid);
         require(msg.tokenvalue==aamount);
@@ -9,12 +9,14 @@ contract transferTrc10 {
         rec.transferToken(aamount,msg.tokenid);
         require(0==address(this).tokenBalance(msg.tokenid));
         require(bamount+aamount==rec.tokenBalance(msg.tokenid));
-        require(rec.call(bytes4(keccak256("checkTrc10(uint256,trcToken,uint256)")),bamount+aamount,msg.tokenid,0));
+        //require(rec.call(abi.encode(bytes4(keccak256("checkTrc10(uint256,trcToken,uint256)")),bamount+aamount,msg.tokenid,0)));
+        (bool suc, bytes memory data) = rec.call(abi.encode(bytes4(keccak256("checkTrc10(uint256,trcToken,uint256)")),bamount+aamount,msg.tokenid,0));
+        require(suc);
     }
 }
 
 contract receiveTrc10 {
-    function() public payable {
+    function() payable external {
     }
     function checkTrc10(uint256 amount,trcToken tid,uint256 meamount) public{
         require(amount==address(this).tokenBalance(tid));
