9f64bc553d483fd1f69e0eb59608dfbfe7ef16ae
dannaguo <guoxiaodan_guo@163.com>
2019-04-08 19:30:44 +0800

    modify two solidity files

--- src/test/resources/soliditycode/contractTrcTokenToOther.sol
+++ src/test/resources/soliditycode/contractTrcTokenToOther.sol
@@ -6,12 +6,12 @@ constructor() payable public{}
 
 function() payable external{}
 
-// function trcTokenOnStorage(trcToken storage token) internal { // ERROR Storage location can only be given for array or struct types
-// }
+//function trcTokenOnStorage(trcToken storage token) internal { // ERROR: Data location can only be specified for array, struct or mapping types, but "storage" was given.
+//}
 
-function trcTokenToString(trcToken token) public pure returns(string memory r){
-// string s = token; // ERROR
-// string s2 = string(token); // ERROR
+function trcTokenToString(trcToken token) public pure returns(string memory s){
+// s = token; // ERROR
+// s = string(token); // ERROR
 }
 
 function trcTokenToUint256(trcToken token) public pure returns(uint256 r){
@@ -21,24 +21,24 @@ r = u2;
 }
 
 function trcTokenToAddress(trcToken token) public pure returns(address r){
-// address a = token; // ERROR
+//r = token; // ERROR
 token = 0x1234567812345678123456781234567812345678123456781234567812345678;
 address a2 = address(token); // OK
 r = a2;
 }
 
 function trcTokenToBytes(trcToken token) public pure returns(bytes memory r){
-// bytes b = token; // ERROR
-// bytes b2 = bytes(token); // ERROR
+//r = token; // ERROR
+// r = bytes(token); // ERROR
 }
 
 function trcTokenToBytes32(trcToken token) public pure returns(bytes32 r){
-// bytes32 b = token; // ERROR
+// r = token; // ERROR
 bytes32 b2 = bytes32(token); // OK
 r = b2;
 }
 
 function trcTokenToArray(trcToken token) public pure returns(uint[] memory r){
-// uint[] a = token; // ERROR
+//r = token; // ERROR
 }
 }
