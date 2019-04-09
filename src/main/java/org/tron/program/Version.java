12110d6c747c828f3f38e132a00d086b037e72e7
Matt <yrp1990@gmail.com>
2019-04-09 17:34:22 +0800

    update a new version. version name:Odyssey-v3.5.0.1-318-gc6864f595,version code:9911

--- src/main/java/org/tron/program/Version.java
+++ src/main/java/org/tron/program/Version.java
@@ -2,8 +2,8 @@ package org.tron.program;
 
 public class Version {
   private static final String version = "3.5.1";
-  public static final String versionName = "";
-  public static final String versionCode = "";
+  public static final String versionName = "Odyssey-v3.5.0.1-318-gc6864f595";
+  public static final String versionCode = "9911";
 
   public static String getVersion() {
     return version;
