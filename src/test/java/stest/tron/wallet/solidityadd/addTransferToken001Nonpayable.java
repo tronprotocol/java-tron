bdc6f05c52f4fa99981228de225b050cc33dbdb0
HuangZhengSi <huangzhengsi@tron.network>
2019-04-10 11:20:24 +0800

    modify solidityadd file

--- src/test/java/stest/tron/wallet/solidityadd/addTransferToken001Nonpayable.java
+++ src/test/java/stest/tron/wallet/solidityadd/addTransferToken001Nonpayable.java
@@ -94,7 +94,7 @@ public class addTransferToken001Nonpayable {
   }
 
   @Test(enabled = true, description = "Support function type")
-  public void test1Grammar001() {
+  public void test1Nonpayable() {
     PublicMethed.waitProduceNextBlock(blockingStubFull);
     Assert.assertTrue(PublicMethed
             .sendcoin(contractExcAddress, 100000000000L, testNetAccountAddress,
@@ -179,6 +179,109 @@ public class addTransferToken001Nonpayable {
     Assert.assertTrue(beforecontractAssetCount == AftercontractAssetCount);
     Assert.assertTrue(beforeAddressAssetCount == AfterAddressAssetCount);
     Assert.assertTrue(infoById.get().getResultValue() == 1);
+  }
+
+  @Test(enabled = true, description = "Support function type")
+  public void test2Payable() {
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    Assert.assertTrue(PublicMethed
+            .sendcoin(contractExcAddress, 100000000000L, testNetAccountAddress,
+                    testNetAccountKey, blockingStubFull));
+    Assert.assertTrue(PublicMethed
+            .sendcoin(toAddress, 100000000000L, testNetAccountAddress,
+                    testNetAccountKey, blockingStubFull));
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+
+    long start = System.currentTimeMillis() + 2000;
+    long end = System.currentTimeMillis() + 1000000000;
+    //Create a new AssetIssue success.
+    Assert.assertTrue(PublicMethed.createAssetIssue(contractExcAddress, tokenName, TotalSupply, 1,
+            10000, start, end, 1, description, url, 100000L, 100000L,
+            1L, 1L, contractExcKey, blockingStubFull));
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    Protocol.Account getAssetIdFromThisAccount = PublicMethed
+            .queryAccount(contractExcAddress, blockingStubFull);
+    assetAccountId = getAssetIdFromThisAccount.getAssetIssuedID();
+
+    String filePath = "src/test/resources/soliditycode/addTransferToken001payable.sol";
+    String contractName = "IllegalDecorate";
+    HashMap retMap = PublicMethed.getBycodeAbi(filePath, contractName);
+
+    String code = retMap.get("byteCode").toString();
+    String abi = retMap.get("abI").toString();
+    logger.info("code:" + code);
+    logger.info("abi:" + abi);
+    contractAddress = PublicMethed.deployContract(contractName, abi, code, "", maxFeeLimit,
+            0L, 100, null, contractExcKey,
+            contractExcAddress, blockingStubFull);
+    Protocol.Account info;
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    Assert.assertTrue(PublicMethed
+            .sendcoin(contractAddress, 100000000000L, testNetAccountAddress,
+                    testNetAccountKey, blockingStubFull));
+    Assert.assertTrue(PublicMethed.transferAsset(contractAddress,
+            assetAccountId.toByteArray(), 100L, contractExcAddress,
+            contractExcKey, blockingStubFull));
+    GrpcAPI.AccountResourceMessage resourceInfo =
+            PublicMethed.getAccountResource(contractExcAddress, blockingStubFull);
+    info = PublicMethed.queryAccount(contractExcKey, blockingStubFull);
+    Long beforeBalance = info.getBalance();
+    Long beforeEnergyUsed = resourceInfo.getEnergyUsed();
+    Long beforeNetUsed = resourceInfo.getNetUsed();
+    Long beforeFreeNetUsed = resourceInfo.getFreeNetUsed();
+    logger.info("beforeBalance:" + beforeBalance);
+    logger.info("beforeEnergyUsed:" + beforeEnergyUsed);
+    logger.info("beforeNetUsed:" + beforeNetUsed);
+    logger.info("beforeFreeNetUsed:" + beforeFreeNetUsed);
+    String txid = "";
+    Long tokenvalue = 10L;
+    Long Tokenvalue = 5L;
+    String tokenid = assetAccountId.toStringUtf8();
+    Long beforecontractAssetCount = PublicMethed.getAssetIssueValue(contractAddress,
+            assetAccountId, blockingStubFull);
+    Long beforeAddressAssetCount = PublicMethed.getAssetIssueValue(toAddress,
+            assetAccountId, blockingStubFull);
+    Long beforecontractExcAddress =  PublicMethed.getAssetIssueValue(contractExcAddress,
+            assetAccountId, blockingStubFull);
+    logger.info("tokenId: {}", tokenid);
+
+    String para = "\"" + Base58.encode58Check(toAddress)
+            + "\",\"" + tokenid + "\" ,\"" + tokenvalue + "\"";
+    txid = PublicMethed.triggerContract(contractAddress,
+            "transferTokenWithOutPayable(address,trcToken,uint256)", para, false,0,maxFeeLimit,assetAccountId.toStringUtf8(),Tokenvalue, contractExcAddress, contractExcKey, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull1);
+    Optional<Protocol.TransactionInfo> infoById = null;
+    infoById = PublicMethed.getTransactionInfoById(txid, blockingStubFull);
+    Long fee = infoById.get().getFee();
+    Long netUsed = infoById.get().getReceipt().getNetUsage();
+    Long energyUsed = infoById.get().getReceipt().getEnergyUsageTotal();
+    Long netFee = infoById.get().getReceipt().getNetFee();
+    logger.info("netUsed:" + netUsed);
+    logger.info("energyUsed:" + energyUsed);
+    Long AftercontractAssetCount = PublicMethed.getAssetIssueValue(contractAddress,
+            assetAccountId, blockingStubFull);
+    Long AfterAddressAssetCount = PublicMethed.getAssetIssueValue(toAddress,
+            assetAccountId, blockingStubFull);
+    Long AftercontractExcAddress =  PublicMethed.getAssetIssueValue(contractExcAddress,
+            assetAccountId, blockingStubFull);
+
+    logger.info("beforecontractAssetCount:"+beforecontractAssetCount);
+    logger.info("AftercontractAssetCount:"+AftercontractAssetCount);
+    logger.info("beforeAddressAssetCount:"+beforeAddressAssetCount);
+    logger.info("AfterAddressAssetCount:"+AfterAddressAssetCount);
+    logger.info("beforecontractExcAddress:"+beforecontractExcAddress);
+    logger.info("AftercontractExcAddress:"+AftercontractExcAddress);
+
+    Assert.assertTrue(beforecontractAssetCount == AftercontractAssetCount + tokenvalue - Tokenvalue);
+    Assert.assertTrue(beforeAddressAssetCount == AfterAddressAssetCount - tokenvalue );
+    Assert.assertTrue(beforecontractExcAddress == AftercontractExcAddress + Tokenvalue );
+
+//    Assert.assertTrue(beforecontractAssetCount == AftercontractAssetCount + tokenvalue);
+//    Assert.assertTrue(beforeAddressAssetCount == AfterAddressAssetCount - tokenvalue);
+//    Assert.assertTrue(beforecontractAssetCount == AftercontractAssetCount);
+//    Assert.assertTrue(beforeAddressAssetCount == AfterAddressAssetCount);
+//    Assert.assertTrue(infoById.get().getResultValue() == 1);
   }
 
 
