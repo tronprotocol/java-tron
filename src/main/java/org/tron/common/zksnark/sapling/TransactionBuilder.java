18685b6ff58eab07e432ede57df5f08bdf801a55
renchenchang <dzhrcc@gmail.com>
2019-04-11 17:18:02 +0800

    remame ReceiverDescriptionCapsule

--- src/main/java/org/tron/common/zksnark/sapling/TransactionBuilder.java
+++ src/main/java/org/tron/common/zksnark/sapling/TransactionBuilder.java
@@ -12,7 +12,7 @@ import org.tron.common.zksnark.sapling.note.BaseNotePlaintext.NotePlaintext;
 import org.tron.common.zksnark.sapling.note.BaseNotePlaintext.SaplingNotePlaintextEncryptionResult;
 import org.tron.common.zksnark.sapling.note.SaplingNoteEncryption;
 import org.tron.common.zksnark.sapling.note.SaplingOutgoingPlaintext;
-import org.tron.common.zksnark.sapling.transaction.OutputDescriptionCapsule;
+import org.tron.common.zksnark.sapling.transaction.ReceiverDescriptionCapsule;
 import org.tron.common.zksnark.sapling.transaction.SpendDescriptionCapsule;
 import org.tron.protos.Contract.ShieldedTransferContract;
 
@@ -148,7 +148,7 @@ public class TransactionBuilder {
         throw new RuntimeException("Output proof failed");
       }
 
-      OutputDescriptionCapsule odesc = new OutputDescriptionCapsule();
+      ReceiverDescriptionCapsule odesc = new ReceiverDescriptionCapsule();
       odesc.setValueCommitment(cv);
       odesc.setNoteCommitment(cm);
       odesc.setEpk(encryptor.epk);
