0400083bb6fd85796a15ee35deee6f2677283a48
olivier <olenheim@hotmail.com>
2019-04-12 17:23:19 +0800

    add zk signautre verifier

---
+++ src/main/java/org/tron/common/zksnark/sapling/SignatureVerifier.java
@@ -0,0 +1,16 @@
+package org.tron.common.zksnark.sapling;
+
+public class SignatureVerifier {
+
+  protected Librustzcash zcash;
+
+  protected Libsodium crypto;
+
+  protected TransactionBuilder shieldedTrx;
+
+  public boolean verify() {
+    return false;
+  }
+
+
+}
