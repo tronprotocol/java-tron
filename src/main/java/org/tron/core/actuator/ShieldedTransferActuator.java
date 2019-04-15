c88fb4801e0a23386e531aa0d3895c8838e5aba3
Matt <yrp1990@gmail.com>
2019-04-15 18:27:52 +0800

    add shieldedtransfercontract validate

--- src/main/java/org/tron/core/actuator/ShieldedTransferActuator.java
+++ src/main/java/org/tron/core/actuator/ShieldedTransferActuator.java
@@ -3,12 +3,17 @@ package org.tron.core.actuator;
 import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
 import com.google.protobuf.InvalidProtocolBufferException;
+import lombok.extern.slf4j.Slf4j;
 import org.tron.core.capsule.TransactionResultCapsule;
 import org.tron.core.db.Manager;
 import org.tron.core.exception.ContractExeException;
 import org.tron.core.exception.ContractValidateException;
+import org.tron.protos.Contract;
+import org.tron.protos.Contract.ShieldedTransferContract;
+import org.tron.protos.Protocol.Transaction.Result.code;
 
 
+@Slf4j(topic = "actuator")
 public class ShieldedTransferActuator extends AbstractActuator {
 
   ShieldedTransferActuator(Any contract, Manager dbManager) {
@@ -22,6 +27,27 @@ public class ShieldedTransferActuator extends AbstractActuator {
 
   @Override
   public boolean validate() throws ContractValidateException {
+    if (this.contract == null) {
+      throw new ContractValidateException("No contract!");
+    }
+    if (this.dbManager == null) {
+      throw new ContractValidateException("No dbManager!");
+    }
+    if (!this.contract.is(ShieldedTransferContract.class)) {
+      throw new ContractValidateException(
+          "contract type error,expected type [ShieldedTransferContract],real type[" + contract
+              .getClass() + "]");
+    }
+
+    ShieldedTransferContract shieldedTransferContract;
+    try {
+      shieldedTransferContract = contract.unpack(ShieldedTransferContract.class);
+    } catch (InvalidProtocolBufferException e) {
+      logger.debug(e.getMessage(), e);
+      throw new ContractValidateException(e.getMessage());
+    }
+
+
     return false;
   }
 
