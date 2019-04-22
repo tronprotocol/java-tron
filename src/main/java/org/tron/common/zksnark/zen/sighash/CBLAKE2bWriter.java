8980956f78a39dfbae8a8b23e2a5fc958949a998
renchenchang <dzhrcc@gmail.com>
2019-04-22 14:30:28 +0800

    CBLAKE2bWriter

---
+++ src/main/java/org/tron/common/zksnark/zen/sighash/CBLAKE2bWriter.java
@@ -0,0 +1,40 @@
+package org.tron.common.zksnark.zen.sighash;
+
+import org.tron.common.zksnark.zen.Libsodium;
+import org.tron.common.zksnark.zen.Libsodium.ILibsodium.crypto_generichash_blake2b_state;
+
+
+public class CBLAKE2bWriter {
+
+  private crypto_generichash_blake2b_state.ByReference state;
+  public int type;
+  public int version;
+
+  public CBLAKE2bWriter(int type, int version, byte[] personal) {
+    this.type = type;
+    this.type = version;
+
+    assert Libsodium
+        .cryptoGenerichashBlake2bInitSaltPersonal(state, null, 0, 32,
+            null, personal) == 0;
+  }
+
+  public int getType() {
+    return type;
+  }
+
+  public int getVersion() {
+    return type;
+  }
+
+  public CBLAKE2bWriter write(byte[] pch, int size) {
+    Libsodium.cryptoGenerichashBlake2bUpdate(state, pch, size);
+    return this;
+  }
+
+  public byte[] getHash() {
+    byte[] result = new byte[256];
+    Libsodium.cryptoGenerichashBlake2bFinal(state, result, 32);
+    return result;
+  }
+}
