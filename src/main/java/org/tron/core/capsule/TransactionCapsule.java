7f574e9f4b609fe8df23c4478e91f6b9efc39ebf
sean-liu55 <liushaohua9017@163.com>
2019-05-24 17:08:00 +0800

    revert hash method

--- src/main/java/org/tron/core/capsule/TransactionCapsule.java
+++ src/main/java/org/tron/core/capsule/TransactionCapsule.java
@@ -258,11 +258,7 @@ public class TransactionCapsule implements ProtoCapsule<Transaction> {
   }
 
   private Sha256Hash getRawHash() {
-    if(this.isShieldedTransferTransaction()){
-      return Sha256Hash.wrap(TransactionCapsule.getShieldTransactionHashIgnoreTypeException(this));
-    }else {
-      return Sha256Hash.of(this.transaction.getRawData().toByteArray());
-    }
+    return Sha256Hash.of(this.transaction.getRawData().toByteArray());
   }
 
   public void sign(byte[] privateKey) {
