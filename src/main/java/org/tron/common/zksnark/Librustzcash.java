570b3656071a778ba6e890bd6eef5410a4c6d074
wenpinghou <skipjack8@live.com>
2019-05-24 21:04:14 +0800

    add annotations of some APIs

--- src/main/java/org/tron/common/zksnark/Librustzcash.java
+++ src/main/java/org/tron/common/zksnark/Librustzcash.java
@@ -146,6 +146,11 @@ public class Librustzcash {
     return INSTANCE.librustzcash_zip32_xfvk_address(xfvk, j, j_ret, addr_ret);
   }
 
+  /*
+  * ak: 32 bytes
+  * nk: 32 bytes
+  * ivk: 32 bytes
+  */
   public static void librustzcashCrhIvk(byte[] ak, byte[] nk, byte[] ivk) {
     if (!(ak.length == 32 && nk.length == 32 && ivk.length == 32)) {
       throw new RuntimeException("librustzcash_crh_ivk invalid array size");
@@ -153,6 +158,11 @@ public class Librustzcash {
     INSTANCE.librustzcash_crh_ivk(ak, nk, ivk);
   }
 
+  /*
+  * p: 32 bytes
+  * sk: 32 bytes
+  * result: 32 bytes
+  */
   public static boolean librustzcashSaplingKaAgree(byte[] p, byte[] sk, byte[] result) {
     if (!(p.length == 32 && sk.length == 32 && result.length == 32)) {
       throw new RuntimeException("librustzcash_sapling_ka_agree invalid array size");
@@ -160,6 +170,13 @@ public class Librustzcash {
     return INSTANCE.librustzcash_sapling_ka_agree(p, sk, result);
   }
 
+  /*
+  * d: diversifier, 11 bytes
+  * pk_d: 32 bytes
+  * value: 64 bits integer
+  * r: rcm,  32 bytes
+  * cm: note commitment, 32 bytes
+  */
   public static boolean librustzcashSaplingComputeCm(byte[] d, byte[] pk_d, long value, byte[] r,
       byte[] cm) {
     if (!(d.length == 11 && pk_d.length == 32 && r.length == 32 && cm.length == 32)) {
@@ -168,6 +185,15 @@ public class Librustzcash {
     return INSTANCE.librustzcash_sapling_compute_cm(d, pk_d, value, r, cm);
   }
 
+  /*
+  * d: diversifier, 11 bytes
+  * pk_d, 32 bytes
+  * value: 64 bits integer
+  * r: rcm,  32 bytes
+  * ak: 32 bytes
+  * nk: 32 bytes
+  * result: nullifier, 32 bytes
+  */
   public static boolean librustzcashSaplingComputeNf(byte[] d, byte[] pk_d, long value_, byte[] r,
       byte[] ak, byte[] nk, long position, byte[] result) {
     if (!(d.length == 11 && pk_d.length == 32 && r.length == 32
@@ -179,6 +205,10 @@ public class Librustzcash {
   }
 
   // void librustzcash_ask_to_ak(const unsigned char *ask, unsigned char *result);
+  /*
+  * ask: 32 bytes
+  @ return 32 bytes
+  */
   public static byte[] librustzcashAskToAk(byte[] ask) {
     if (!(ask.length == 32)) {
       throw new RuntimeException("librustzcash_ask_to_ak invalid array size");
@@ -189,6 +219,10 @@ public class Librustzcash {
   }
 
   // void librustzcash_nsk_to_nk(const unsigned char *nsk, unsigned char *result);
+  /*
+  * nsk: 32 bytes
+  @ return  32 bytes
+  */
   public static byte[] librustzcashNskToNk(byte[] nsk) {
     if (!(nsk.length == 32)) {
       throw new RuntimeException("librustzcash_nsk_to_nk invalid array size");
@@ -198,6 +232,9 @@ public class Librustzcash {
     return nk;
   }
 
+  /*
+  * r: return random numbe,  32 bytes
+  */
   public static byte[] librustzcashSaplingGenerateR(byte[] r) {
     if (!(r.length == 32)) {
       throw new RuntimeException("librustzcash_sapling_generate_r invalid array size");
@@ -206,6 +243,11 @@ public class Librustzcash {
     return r;
   }
 
+  /*
+  * diversifier: d, 11 bytes
+  * esk: 32 bytes
+  * result: return 32 bytes
+  */
   public static boolean librustzcashSaplingKaDerivepublic(byte[] diversifier, byte[] esk,
       byte[] result) {
     if (!(diversifier.length == 11 && esk.length == 32 && result.length == 32)) {
@@ -218,6 +260,10 @@ public class Librustzcash {
     return INSTANCE.librustzcash_sapling_proving_ctx_init();
   }
 
+  /*
+  * check validity of d
+  * d: 11 bytes
+  */
   public static boolean librustzcashCheckDiversifier(byte[] d) {
     if (!(d.length == 11)) {
       throw new RuntimeException("librustzcash_check_diversifier invalid array size");
@@ -225,6 +271,20 @@ public class Librustzcash {
     return INSTANCE.librustzcash_check_diversifier(d);
   }
 
+  /*
+  * calculate spend proof
+  * ak: 32 bytes
+  * nsk: 32 bytes
+  * d: 11 bytes
+  * r: rcm, 32 bytes
+  * alpha: 32 bytes
+  * value: 64 bits integer
+  * anchor: 32 bytes
+  * voucherPath: (1 + 33 * 32 + 8) bytes,  voucherPath[0]=0x20, voucherPath[1+i*33]=0x20,i=0,1,...31.
+  * cv: value commitments, 32 bytes
+  * rk: spendAuthSig.PublicKey 32 bytes
+  * zkproof: spend proof, 192 bytes
+  */
   public static boolean librustzcashSaplingSpendProof(Pointer ctx, byte[] ak, byte[] nsk, byte[] d,
       byte[] r, byte[] alpha, long value, byte[] anchor, byte[] voucherPath, byte[] cv, byte[] rk,
       byte[] zkproof) {
@@ -239,6 +299,15 @@ public class Librustzcash {
             rk, zkproof);
   }
 
+  /*
+  * esk: 32 bytes
+  * d: 11 bytes
+  * pk_d: 32 bytes
+  * r: rcm, 32 bytes
+  * value: 64 bits integer
+  * cv: value commitment, 32 bytes
+  * zkproof: receive proof, 192 bytes
+  */
   public static boolean librustzcashSaplingOutputProof(Pointer ctx, byte[] esk, byte[] d,
       byte[] pk_d, byte[] r, long value, byte[] cv, byte[] zkproof) {
     if (!(esk.length == 32 && d.length == 11 && pk_d.length == 32 && r.length == 32
@@ -248,6 +317,12 @@ public class Librustzcash {
     return INSTANCE.librustzcash_sapling_output_proof(ctx, esk, d, pk_d, r, value, cv, zkproof);
   }
 
+  /*
+  * ask: 32 bytes
+  * alpha: random number, 32 bytes
+  * sigHash: 32 bytes
+  * result: spendAuthSig, 64 bytes
+  */
   public static boolean librustzcashSaplingSpendSig(byte[] ask, byte[] alpha, byte[] sigHash,
       byte[] result) {
     if (!(ask.length == 32 && alpha.length == 32 && sigHash.length == 32 && result.length == 64)) {
@@ -256,6 +331,11 @@ public class Librustzcash {
     return INSTANCE.librustzcash_sapling_spend_sig(ask, alpha, sigHash, result);
   }
 
+  /*
+  * valueBalance: 64 bits integer
+  * sigHash: 32 bytes
+  * result: binding signature, 64 bytes
+  */
   public static boolean librustzcashSaplingBindingSig(
       Pointer ctx, long valueBalance, byte[] sighash, byte[] result) {
     if (!(sighash.length == 32 && result.length == 64)) {
@@ -264,6 +344,11 @@ public class Librustzcash {
     return INSTANCE.librustzcash_sapling_binding_sig(ctx, valueBalance, sighash, result);
   }
 
+  /*
+  * convert value to 32-byte scalar
+  * value: 64 bytes
+  * data: return, 32 bytes
+  */
   public static void librustzcashToScalar(byte[] value, byte[] data) {
     if (!(value.length == 64 && data.length == 32)) {
       throw new RuntimeException("librustzcash_to_scalar invalid array size");
@@ -279,6 +364,15 @@ public class Librustzcash {
     return INSTANCE.librustzcash_sapling_verification_ctx_init();
   }
 
+  /*
+  * cv: value commitments, 32 bytes
+  * anchor: 32 bytes
+  * nullifier: 32 bytes
+  * rk: spendAuthSig.PublicKey 32 bytes
+  * zkproof: spend proof, 192 bytes
+  * spendAuthSig: 64 bytes
+  * sigHash: 32 bytes
+  */
   public static boolean librustzcashSaplingCheckSpend(Pointer ctx, byte[] cv, byte[] anchor,
       byte[] nullifier, byte[] rk, byte[] zkproof, byte[] spendAuthSig, byte[] sighashValue) {
     if (!(cv.length == 32 && anchor.length == 32 && nullifier.length == 32 && rk.length == 32
@@ -290,6 +384,12 @@ public class Librustzcash {
             sighashValue);
   }
 
+  /*
+  * cv: value commitments, 32 bytes
+  * cm: note commitment, 32 bytes
+  * ephemeralKey: 32 bytes
+  * zkproof: 192 bytes
+  */
   public static boolean librustzcashSaplingCheckOutput(Pointer ctx, byte[] cv, byte[] cm,
       byte[] ephemeralKey, byte[] zkproof) {
     if (!(cv.length == 32 && cm.length == 32 && ephemeralKey.length == 32
@@ -299,6 +399,10 @@ public class Librustzcash {
     return INSTANCE.librustzcash_sapling_check_output(ctx, cv, cm, ephemeralKey, zkproof);
   }
 
+  /*
+  * bindingSig: 64 bytes
+  * sighashValue: 32 bytes
+  */
   public static boolean librustzcashSaplingFinalCheck(Pointer ctx, long valueBalance,
       byte[] bindingSig, byte[] sighashValue) {
     if (!(bindingSig.length == 64 && sighashValue.length == 32)) {
@@ -311,6 +415,11 @@ public class Librustzcash {
     INSTANCE.librustzcash_sapling_verification_ctx_free(ctx);
   }
 
+  /*
+  * ivk: 32 bytes
+  * d: 1 bytes
+  * pk_d: 32 bytes
+  */
   public static boolean librustzcashIvkToPkd(byte[] ivk, byte[] d, byte[] pk_d) {
     if (!(ivk.length == 32 && d.length == 11 && pk_d.length == 32)) {
       throw new RuntimeException("librustzcash_ivk_to_pkd invalid array size");
@@ -318,6 +427,11 @@ public class Librustzcash {
     return INSTANCE.librustzcash_ivk_to_pkd(ivk, d, pk_d);
   }
 
+  /*
+  * a: 32 bytes
+  * b: 32 bytes
+  * result: 32 bytes
+  */
   public static void librustzcashMerkleHash(int depth, byte[] a, byte[] b, byte[] result) {
     if (!(a.length == 32 && b.length == 32 && result.length == 32)) {
       throw new RuntimeException("librustzcash_merkle_hash invalid array size");
@@ -325,6 +439,9 @@ public class Librustzcash {
     INSTANCE.librustzcash_merkle_hash(depth, a, b, result);
   }
 
+  /*
+  * result: uncommitted value, 32 bytes
+   */
   public static void librustzcash_tree_uncommitted(byte[] result) {
     if (!(result.length == 32)) {
       throw new RuntimeException("librustzcash_tree_uncommitted invalid array size");
