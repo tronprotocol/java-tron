8434febff2e86d07419bfbb26bdcec1c75933099
wangming <wm18840987261@163.com>
2019-05-29 17:11:48 +0800

    add create2 case 019-020

---
+++ src/test/java/stest/tron/wallet/dailybuild/tvmnewcommand/create2/Create2Test020.java
@@ -0,0 +1,317 @@
+package stest.tron.wallet.dailybuild.tvmnewcommand.create2;
+
+import static org.hamcrest.core.StringContains.containsString;
+
+import com.google.protobuf.ByteString;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.tron.api.GrpcAPI.AccountResourceMessage;
+import org.tron.api.WalletGrpc;
+import org.tron.common.crypto.ECKey;
+import org.tron.common.utils.ByteArray;
+import org.tron.common.utils.Utils;
+import org.tron.core.Wallet;
+import org.tron.protos.Protocol.SmartContract;
+import org.tron.protos.Protocol.TransactionInfo;
+import stest.tron.wallet.common.client.Configuration;
+import stest.tron.wallet.common.client.Parameter.CommonConstant;
+import stest.tron.wallet.common.client.WalletClient;
+import stest.tron.wallet.common.client.utils.Base58;
+import stest.tron.wallet.common.client.utils.PublicMethed;
+
+@Slf4j
+public class Create2Test020 {
+
+  private final String testKey002 = Configuration.getByPath("testng.conf")
+      .getString("foundationAccount.key2");
+  private final byte[] fromAddress = PublicMethed.getFinalAddress(testKey002);
+
+  private ManagedChannel channelFull = null;
+  private WalletGrpc.WalletBlockingStub blockingStubFull = null;
+  private String fullnode = Configuration.getByPath("testng.conf")
+      .getStringList("fullnode.ip.list").get(1);
+  private long maxFeeLimit = Configuration.getByPath("testng.conf")
+      .getLong("defaultParameter.maxFeeLimit");
+
+  private byte[] factoryContractAddress = null;
+  private byte[] testContractAddress = null;
+
+  private ECKey ecKey1 = new ECKey(Utils.getRandom());
+  private byte[] dev001Address = ecKey1.getAddress();
+  private String dev001Key = ByteArray.toHexString(ecKey1.getPrivKeyBytes());
+
+  private ECKey ecKey2 = new ECKey(Utils.getRandom());
+  private byte[] user001Address = ecKey2.getAddress();
+  private String user001Key = ByteArray.toHexString(ecKey2.getPrivKeyBytes());
+
+  @BeforeSuite
+  public void beforeSuite() {
+    Wallet wallet = new Wallet();
+    Wallet.setAddressPreFixByte(CommonConstant.ADD_PRE_FIX_BYTE_MAINNET);
+  }
+
+  /**
+   * constructor.
+   */
+  @BeforeClass(enabled = true)
+  public void beforeClass() {
+
+    channelFull = ManagedChannelBuilder.forTarget(fullnode)
+        .usePlaintext(true)
+        .build();
+    blockingStubFull = WalletGrpc.newBlockingStub(channelFull);
+
+    PublicMethed.printAddress(dev001Key);
+    PublicMethed.printAddress(user001Key);
+  }
+
+
+  public byte[] subByte(byte[] b, int off, int length) {
+    byte[] b1 = new byte[length];
+    System.arraycopy(b, off, b1, 0, length);
+    return b1;
+
+  }
+
+  @Test(enabled = true, description = "Deploy factory contract")
+  public void test01DeployFactoryContract() {
+    Assert.assertTrue(PublicMethed.sendcoin(dev001Address, 10000_000_000L, fromAddress,
+        testKey002, blockingStubFull));
+    Assert.assertTrue(PublicMethed.sendcoin(user001Address, 10000_000_000L, fromAddress,
+        testKey002, blockingStubFull));
+    Assert.assertTrue(PublicMethed.freezeBalanceForReceiver(fromAddress,
+        PublicMethed.getFreezeBalanceCount(dev001Address, dev001Key, 170000L,
+            blockingStubFull), 0, 1,
+        ByteString.copyFrom(dev001Address), testKey002, blockingStubFull));
+
+    Assert.assertTrue(PublicMethed.freezeBalanceForReceiver(fromAddress, 10_000_000L,
+        0, 0, ByteString.copyFrom(dev001Address), testKey002, blockingStubFull));
+
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+
+    //before deploy, check account resource
+    AccountResourceMessage accountResource = PublicMethed.getAccountResource(dev001Address,
+        blockingStubFull);
+    long energyLimit = accountResource.getEnergyLimit();
+    long energyUsage = accountResource.getEnergyUsed();
+    long balanceBefore = PublicMethed.queryAccount(dev001Key, blockingStubFull).getBalance();
+    logger.info("before energyLimit is " + Long.toString(energyLimit));
+    logger.info("before energyUsage is " + Long.toString(energyUsage));
+    logger.info("before balanceBefore is " + Long.toString(balanceBefore));
+
+    String filePath = "./src/test/resources/soliditycode/suicide002.sol";
+    String contractName = "Factory";
+    HashMap retMap = PublicMethed.getBycodeAbi(filePath, contractName);
+
+    String code = retMap.get("byteCode").toString();
+    String abi = retMap.get("abI").toString();
+
+    final String transferTokenTxid = PublicMethed
+        .deployContractAndGetTransactionInfoById(contractName, abi, code, "",
+            maxFeeLimit, 0L, 0, 10000,
+            "0", 0, null, dev001Key,
+            dev001Address, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+
+    accountResource = PublicMethed.getAccountResource(dev001Address, blockingStubFull);
+    energyLimit = accountResource.getEnergyLimit();
+    energyUsage = accountResource.getEnergyUsed();
+    long balanceAfter = PublicMethed.queryAccount(dev001Key, blockingStubFull).getBalance();
+
+    logger.info("after energyLimit is " + Long.toString(energyLimit));
+    logger.info("after energyUsage is " + Long.toString(energyUsage));
+    logger.info("after balanceAfter is " + Long.toString(balanceAfter));
+
+    Optional<TransactionInfo> infoById = PublicMethed
+        .getTransactionInfoById(transferTokenTxid, blockingStubFull);
+
+    if (infoById.get().getResultValue() != 0) {
+      Assert.fail("deploy transaction failed with message: " + infoById.get().getResMessage());
+    }
+
+    TransactionInfo transactionInfo = infoById.get();
+    logger.info("EnergyUsageTotal: " + transactionInfo.getReceipt().getEnergyUsageTotal());
+    logger.info("NetUsage: " + transactionInfo.getReceipt().getNetUsage());
+
+    factoryContractAddress = infoById.get().getContractAddress().toByteArray();
+    SmartContract smartContract = PublicMethed.getContract(factoryContractAddress,
+        blockingStubFull);
+    Assert.assertNotNull(smartContract.getAbi());
+  }
+
+  @Test(enabled = true, description = "function create2 ")
+  public void test02TriggerTestContract() {
+    Assert.assertTrue(PublicMethed.freezeBalanceForReceiver(fromAddress,
+        PublicMethed.getFreezeBalanceCount(user001Address, user001Key, 50000L,
+            blockingStubFull), 0, 1,
+        ByteString.copyFrom(user001Address), testKey002, blockingStubFull));
+
+    AccountResourceMessage accountResource = PublicMethed.getAccountResource(dev001Address,
+        blockingStubFull);
+    long devEnergyLimitBefore = accountResource.getEnergyLimit();
+    long devEnergyUsageBefore = accountResource.getEnergyUsed();
+    long devBalanceBefore = PublicMethed.queryAccount(dev001Address, blockingStubFull).getBalance();
+
+    logger.info("before trigger, devEnergyLimitBefore is " + Long.toString(devEnergyLimitBefore));
+    logger.info("before trigger, devEnergyUsageBefore is " + Long.toString(devEnergyUsageBefore));
+    logger.info("before trigger, devBalanceBefore is " + Long.toString(devBalanceBefore));
+
+    accountResource = PublicMethed.getAccountResource(user001Address, blockingStubFull);
+    long userEnergyLimitBefore = accountResource.getEnergyLimit();
+    long userEnergyUsageBefore = accountResource.getEnergyUsed();
+    long userBalanceBefore = PublicMethed.queryAccount(user001Address, blockingStubFull)
+        .getBalance();
+
+    logger.info("before trigger, userEnergyLimitBefore is " + Long.toString(userEnergyLimitBefore));
+    logger.info("before trigger, userEnergyUsageBefore is " + Long.toString(userEnergyUsageBefore));
+    logger.info("before trigger, userBalanceBefore is " + Long.toString(userBalanceBefore));
+
+    Long callValue = Long.valueOf(0);
+
+    String filePath = "./src/test/resources/soliditycode/suicide002.sol";
+    String contractName = "TestConstract";
+    HashMap retMap = PublicMethed.getBycodeAbi(filePath, contractName);
+
+    String testContractCode = retMap.get("byteCode").toString();
+    Long salt = 1L;
+
+    String param = "\"" + testContractCode + "\"," + salt;
+
+    String triggerTxid = PublicMethed.triggerContract(factoryContractAddress,
+        "deploy(bytes,uint256)", param, false, callValue,
+        1000000000L, "0", 0, user001Address, user001Key,
+        blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+
+    accountResource = PublicMethed.getAccountResource(dev001Address, blockingStubFull);
+    long devEnergyLimitAfter = accountResource.getEnergyLimit();
+    long devEnergyUsageAfter = accountResource.getEnergyUsed();
+    long devBalanceAfter = PublicMethed.queryAccount(dev001Address, blockingStubFull).getBalance();
+
+    logger.info("after trigger, devEnergyLimitAfter is " + Long.toString(devEnergyLimitAfter));
+    logger.info("after trigger, devEnergyUsageAfter is " + Long.toString(devEnergyUsageAfter));
+    logger.info("after trigger, devBalanceAfter is " + Long.toString(devBalanceAfter));
+
+    accountResource = PublicMethed.getAccountResource(user001Address, blockingStubFull);
+    long userEnergyLimitAfter = accountResource.getEnergyLimit();
+    long userEnergyUsageAfter = accountResource.getEnergyUsed();
+    long userBalanceAfter = PublicMethed.queryAccount(user001Address, blockingStubFull)
+        .getBalance();
+
+    logger.info("after trigger, userEnergyLimitAfter is " + Long.toString(userEnergyLimitAfter));
+    logger.info("after trigger, userEnergyUsageAfter is " + Long.toString(userEnergyUsageAfter));
+    logger.info("after trigger, userBalanceAfter is " + Long.toString(userBalanceAfter));
+
+    Optional<TransactionInfo> infoById = PublicMethed
+        .getTransactionInfoById(triggerTxid, blockingStubFull);
+
+    byte[] a = infoById.get().getContractResult(0).toByteArray();
+    byte[] b = subByte(a, 11, 1);
+    byte[] c = subByte(a, 0, 11);
+    byte[] e = "41".getBytes();
+    byte[] d = subByte(a, 12, 20);
+
+    logger.info("a:" + ByteArray.toHexString(a));
+
+    logger.info("b:" + ByteArray.toHexString(b));
+    logger.info("c:" + ByteArray.toHexString(c));
+
+    logger.info("d:" + ByteArray.toHexString(d));
+
+    logger.info("41" + ByteArray.toHexString(d));
+    String exceptedResult = "41" + ByteArray.toHexString(d);
+    String realResult = ByteArray.toHexString(b);
+    Assert.assertEquals(realResult, "00");
+    Assert.assertNotEquals(realResult, "41");
+
+
+    String addressFinal = Base58.encode58Check(ByteArray.fromHexString(exceptedResult));
+    logger.info("B Address : " + addressFinal);
+
+    Assert.assertEquals(infoById.get().getResult().toString(),"SUCESS");
+    Assert.assertEquals(infoById.get().getResultValue(),0);
+
+
+    triggerTxid = PublicMethed.triggerContract(factoryContractAddress,
+        "deploy2(bytes,uint256)", param, false, callValue,
+        1000000000L, "0", 0, user001Address, user001Key,
+        blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    infoById = PublicMethed
+        .getTransactionInfoById(triggerTxid, blockingStubFull);
+
+    byte[] a1 = infoById.get().getContractResult(0).toByteArray();
+    byte[] b1 = subByte(a, 11, 1);
+    byte[] c1 = subByte(a, 0, 11);
+    byte[] e1 = "41".getBytes();
+    byte[] d1 = subByte(a, 12, 20);
+
+    logger.info("a:" + ByteArray.toHexString(a));
+
+    logger.info("b:" + ByteArray.toHexString(b));
+    logger.info("c:" + ByteArray.toHexString(c));
+
+    logger.info("d:" + ByteArray.toHexString(d));
+
+    logger.info("41" + ByteArray.toHexString(d));
+    exceptedResult = "41" + ByteArray.toHexString(d);
+    realResult = ByteArray.toHexString(b);
+    Assert.assertEquals(realResult, "00");
+    Assert.assertNotEquals(realResult, "41");
+
+
+    addressFinal = Base58.encode58Check(ByteArray.fromHexString(exceptedResult));
+    logger.info("B Address : " + addressFinal);
+    accountResource = PublicMethed.getAccountResource(dev001Address, blockingStubFull);
+    devEnergyLimitAfter = accountResource.getEnergyLimit();
+    devEnergyUsageAfter = accountResource.getEnergyUsed();
+    devBalanceAfter = PublicMethed.queryAccount(dev001Address, blockingStubFull).getBalance();
+
+    logger.info("after trigger, devEnergyLimitAfter is " + Long.toString(devEnergyLimitAfter));
+    logger.info("after trigger, devEnergyUsageAfter is " + Long.toString(devEnergyUsageAfter));
+    logger.info("after trigger, devBalanceAfter is " + Long.toString(devBalanceAfter));
+
+    accountResource = PublicMethed.getAccountResource(user001Address, blockingStubFull);
+    userEnergyLimitAfter = accountResource.getEnergyLimit();
+    userEnergyUsageAfter = accountResource.getEnergyUsed();
+    userBalanceAfter = PublicMethed.queryAccount(user001Address, blockingStubFull)
+        .getBalance();
+
+    logger.info("after trigger, userEnergyLimitAfter is " + Long.toString(userEnergyLimitAfter));
+    logger.info("after trigger, userEnergyUsageAfter is " + Long.toString(userEnergyUsageAfter));
+    logger.info("after trigger, userBalanceAfter is " + Long.toString(userBalanceAfter));
+    TransactionInfo transactionInfo = infoById.get();
+    logger.info("EnergyUsageTotal: " + transactionInfo.getReceipt().getEnergyUsageTotal());
+    logger.info("NetUsage: " + transactionInfo.getReceipt().getNetUsage());
+
+    Assert.assertEquals(infoById.get().getResultValue(),1);
+    Assert.assertEquals(infoById.get().getResult().toString(),"FAILED");
+    Assert.assertThat(ByteArray.toStr(infoById.get().getResMessage().toByteArray()),
+        containsString("Not enough energy for 'SWAP1' operation executing: "));
+
+
+
+  }
+
+  /**
+   * constructor.
+   */
+  @AfterClass
+  public void shutdown() throws InterruptedException {
+    if (channelFull != null) {
+      channelFull.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+  }
+}
+
+
