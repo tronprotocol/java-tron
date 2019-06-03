85d1464000f29b316e2fb52dda93b858a03e2e0e
ashu <ithinker1991@gmail.com>
2019-06-03 20:27:46 +0800

    support mac sed

--- ver.sh
+++ ver.sh
@@ -25,7 +25,7 @@ fi
 
 
 versionPath="src/main/java/org/tron/program/Version.java"
-sed -i "s/versionName.*$/versionName = \"$versionName\";/g;s/versionCode.*$/versionCode = \"$versionCode\";/g" $versionPath
+sed -i -e "s/versionName.*$/versionName = \"$versionName\";/g;s/versionCode.*$/versionCode = \"$versionCode\";/g" $versionPath
 git add $versionPath
 git commit -m "update a new version. version name:$versionName,version code:$versionCode"
 git push origin $versionBranch
