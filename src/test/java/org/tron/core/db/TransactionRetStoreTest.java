589f02d920dd50590983b34a628c7dffb28806ea
wubin1 <wubin1@tron.network>
2019-06-10 11:56:54 +0800

    add unit test for transaction ret

--- src/test/java/org/tron/core/db/TransactionRetStoreTest.java
+++ src/test/java/org/tron/core/db/TransactionRetStoreTest.java
@@ -70,7 +70,6 @@ public class TransactionRetStoreTest {
 
   @Test
   public void get() throws BadItemException {
-    //test get and has Method
     TransactionInfoCapsule resultCapsule = transactionRetStore.getTransactionInfo(transactionId);
     Assert.assertNotNull("get transaction ret store", resultCapsule);
   }
