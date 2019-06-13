721fc814c4b78a8b394dcbf4ae7468faf00dd859
wangming <wm18840987261@163.com>
2019-06-13 14:19:37 +0800

    add solFile

---
+++ src/test/resources/soliditycode/TvmOldCommand001.sol
@@ -0,0 +1,11 @@
+//pragma solidity ^0.4.0;
+
+contract binaryRightContract{
+    function binaryMoveR(int i)public returns (int z) {
+        return z = 5 >> i;
+    }
+    function binaryLiftR(int i)public returns (int z) {
+        return z = 5 << i;
+    }
+
+}
