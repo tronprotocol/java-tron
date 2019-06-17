328b9965412024a39a398546a3822f28336d9a58
wubin1 <wubin1@tron.network>
2019-06-17 14:33:56 +0800

    fix start bash

--- start.sh
+++ start.sh
@@ -1,3 +1,47 @@
 #!/bin/bash
-kill -9 `cat /home/tron/pid.txt`
-nohup  java -jar /home/tron/java-tron/java-tron.jar -p $LOCAL_WITNESS_PRIVATE_KEY --witness -c /home/tron/config.conf > /home/tron/tron-shell.log 2>&1 & echo $! >/home/tron/pid.txt
+APP=$1
+APP=${APP:-"FullNode"}
+START_OPT=`echo ${@:2}`
+JAR_NAME="$APP.jar"
+MAX_STOP_TIME=60
+
+checkpid() {
+ pid=`ps -ef | grep $JAR_NAME |grep -v grep | awk '{print $2}'`
+ return $pid
+}
+
+stopService() {
+  count=1
+  while [ $count -le $MAX_STOP_TIME ]; do
+    checkpid
+    if [ $pid ]; then
+       kill -15 $pid
+       sleep 1
+    else
+       echo "java-tron stop"
+       return
+    fi
+    count=$[$count+1]
+    if [ $count -eq $MAX_STOP_TIME ]; then
+      kill -9 $pid
+      sleep 1
+    fi
+  done
+}
+
+startService() {
+ echo `date` >> start.log
+ total=`cat /proc/meminfo  |grep MemTotal |awk -F ' ' '{print $2}'`
+ xmx=`echo "$total/1024/1024*0.8" | bc |awk -F. '{print $1"g"}'`
+ xms=`echo "$total/1024/1024*0.8" | bc |awk -F. '{print $1"g"}'`
+ logtime=`date +%Y-%m-%d_%H-%M-%S`
+ nohup java -Xmx$xmx -Xms$xms  -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -Xloggc:./gc.log\
+ -XX:+PrintGCDateStamps -XX:+CMSParallelRemarkEnabled -XX:ReservedCodeCacheSize=256m\
+ -XX:+CMSScavengeBeforeRemark -jar $JAR_NAME $START_OPT -c config.conf  >> start.log 2>&1 &
+
+ pid=`ps -ef |grep $JAR_NAME |grep -v grep |awk '{print $2}'`
+ echo "start java-tron with pid $pid on $HOSTNAME"
+}
+
+stopService
+startService
