138c88487c6a38e037d194fd44339e210f060a09
wubin01 <wb_bupt@163.com>
2019-06-24 15:02:45 +0800

    modify nodes cfg

--- src/main/java/org/tron/program/FullNode.java
+++ src/main/java/org/tron/program/FullNode.java
@@ -1,11 +1,9 @@
 package org.tron.program;
 
-import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.LoggerContext;
 import ch.qos.logback.classic.joran.JoranConfigurator;
 import java.io.File;
 import lombok.extern.slf4j.Slf4j;
-import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.tron.common.application.Application;
