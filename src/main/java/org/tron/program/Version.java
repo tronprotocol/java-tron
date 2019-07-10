9dd80301313857d7a11a834b855495a4d17f6d29
wubin01 <wb_bupt@163.com>
2019-07-10 14:44:26 +0800

    update a new version. version name:Odyssey-v3.6.0-127-g0845ba722,version code:10942

--- src/main/java/org/tron/program/Version.java
+++ src/main/java/org/tron/program/Version.java
@@ -3,8 +3,8 @@ package org.tron.program;
 public class Version {
 
   private static final String version = "3.6.1";
-  public static final String versionName = "Odyssey-v3.5.1-890-gd39973cbb";
-  public static final String versionCode = "10803";
+  public static final String versionName = "Odyssey-v3.6.0-127-g0845ba722";
+  public static final String versionCode = "10942";
 
   public static String getVersion() {
     return version;
