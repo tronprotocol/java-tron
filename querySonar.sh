e99c2e247bf34ba24b5f43162bc88d88d9c38c1d
wangming <wm18840987261@163.com>
2019-07-17 14:15:58 +0800

    modify querySonar.sh

--- querySonar.sh
+++ querySonar.sh
@@ -2,7 +2,7 @@ echo $BUILDKITE_BRANCH
 SonarStatus_Url="https://sonarcloud.io/api/qualitygates/project_status?projectKey=java-tron&branch="$BUILDKITE_BRANCH
 Status=`curl -s $SonarStatus_Url | jq '.projectStatus.status'`
 echo $Status
-if [ "$Status"=="ERROR" ]; then
+if [ "$Status"x = "ERROR"x ]; then
     echo "Sonar Check Failed"
     echo "Please visit https://sonarcloud.io/dashboard?branch="$BUILDKITE_BRANCH"&id=java-tron for more details"
     exit 1
