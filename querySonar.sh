d5aec3a61409f540c7725d134f679971c53ba191
wangming <wm18840987261@163.com>
2019-07-17 14:10:06 +0800

    modify querySonar.sh

--- querySonar.sh
+++ querySonar.sh
@@ -1,10 +1,10 @@
-echo $BUILDKITE_PIPELINE_DEFAULT_BRANCH
-SonarStatus_Url="https://sonarcloud.io/api/qualitygates/project_status?projectKey=java-tron&branch="$BUILDKITE_PIPELINE_DEFAULT_BRANCH
+echo $BUILDKITE_BRANCH
+SonarStatus_Url="https://sonarcloud.io/api/qualitygates/project_status?projectKey=java-tron&branch="$BUILDKITE_BRANCH
 Status=`curl -s $SonarStatus_Url | jq '.projectStatus.status'`
 echo $Status
 if [ "$Status"=="ERROR" ]; then
     echo "Sonar Check Failed"
-    echo "Please visit https://sonarcloud.io/dashboard?branch="$BUILDKITE_PIPELINE_DEFAULT_BRANCH"&id=java-tron for more details"
+    echo "Please visit https://sonarcloud.io/dashboard?branch="$BUILDKITE_BRANCH"&id=java-tron for more details"
     exit 1
 else
     echo "Sonar Check Pass"
