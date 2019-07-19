1dc1268d47e52de4adaab058bf97b7588e1494ae
sun haoyu <shydesky@gmail.com>
2019-07-19 13:57:52 +0800

    add unit test

--- src/main/java/org/tron/core/services/ratelimiter/RuntimeData.java
+++ src/main/java/org/tron/core/services/ratelimiter/RuntimeData.java
@@ -4,17 +4,28 @@ package org.tron.core.services.ratelimiter;
 import io.grpc.Grpc;
 import io.grpc.ServerCall;
 import javax.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class RuntimeData {
 
 
-  private String address;
+  private String address = "";
 
   public RuntimeData(Object o) {
     if (o instanceof HttpServletRequest) {
       address = ((HttpServletRequest) o).getRemoteAddr();
     } else if (o instanceof ServerCall) {
-      address = ((ServerCall) o).getAttributes().get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR).toString();
+      try {
+        address = ((ServerCall) o).getAttributes().get(Grpc.TRANSPORT_ATTR_REMOTE_ADDR).toString();
+      } catch (NullPointerException npe) {
+        logger.warn("the address get from the runtime data is a null value unexpected.");
+      }
+    }
+
+    if (address == null) {
+      logger.warn("assign the address with an empty string.");
+      address = "";
     }
   }
 
