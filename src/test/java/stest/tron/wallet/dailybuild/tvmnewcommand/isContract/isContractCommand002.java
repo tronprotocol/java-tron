37de92ef759487e3952193c2963ba825bea0487a
kaizhu@tron.network <kaizhu@tron.network>
2019-07-29 15:49:58 +0800

    Add strict check address test Add new command isContract test

---
+++ src/test/java/stest/tron/wallet/dailybuild/tvmnewcommand/isContract/isContractCommand002.java
@@ -0,0 +1,171 @@
+package stest.tron.wallet.dailybuild.tvmnewcommand.isContract;
+
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.util.HashMap;
+import java.util.Optional;
+import java.util.concurrent.TimeUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.tron.api.WalletGrpc;
+import org.tron.api.WalletSolidityGrpc;
+import org.tron.common.crypto.ECKey;
+import org.tron.common.utils.ByteArray;
+import org.tron.common.utils.Utils;
+import org.tron.core.Wallet;
+import org.tron.protos.Protocol;
+import stest.tron.wallet.common.client.Configuration;
+import stest.tron.wallet.common.client.Parameter;
+import stest.tron.wallet.common.client.utils.Base58;
+import stest.tron.wallet.common.client.utils.PublicMethed;
+
+
+@Slf4j
+public class isContractCommand002 {
+
+  private final String testNetAccountKey = Configuration.getByPath("testng.conf")
+      .getString("foundationAccount.key2");
+  private final byte[] testNetAccountAddress = PublicMethed.getFinalAddress(testNetAccountKey);
+  private Long maxFeeLimit = Configuration.getByPath("testng.conf")
+      .getLong("defaultParameter.maxFeeLimit");
+  private ManagedChannel channelSolidity = null;
+
+  private ManagedChannel channelFull = null;
+  private WalletGrpc.WalletBlockingStub blockingStubFull = null;
+
+  private ManagedChannel channelFull1 = null;
+  private WalletGrpc.WalletBlockingStub blockingStubFull1 = null;
+
+
+  private WalletSolidityGrpc.WalletSolidityBlockingStub blockingStubSolidity = null;
+
+  private String fullnode = Configuration.getByPath("testng.conf")
+      .getStringList("fullnode.ip.list").get(0);
+  private String fullnode1 = Configuration.getByPath("testng.conf")
+      .getStringList("fullnode.ip.list").get(1);
+
+  private String soliditynode = Configuration.getByPath("testng.conf")
+      .getStringList("solidityNode.ip.list").get(0);
+  byte[] contractAddress = null;
+  byte[] selfdestructContractAddress = null;
+  byte[] emptyAddress = null;
+
+  ECKey ecKey1 = new ECKey(Utils.getRandom());
+  byte[] contractExcAddress = ecKey1.getAddress();
+  String contractExcKey = ByteArray.toHexString(ecKey1.getPrivKeyBytes());
+
+  byte[] selfdestructContractExcAddress = ecKey1.getAddress();
+  String selfdestructContractKey = ByteArray.toHexString(ecKey1.getPrivKeyBytes());
+
+
+  @BeforeSuite
+  public void beforeSuite() {
+    Wallet wallet = new Wallet();
+    Wallet.setAddressPreFixByte(Parameter.CommonConstant.ADD_PRE_FIX_BYTE_MAINNET);
+  }
+
+  /**
+   * constructor.
+   */
+
+  @BeforeClass(enabled = true)
+  public void beforeClass() {
+    PublicMethed.printAddress(contractExcKey);
+    PublicMethed.printAddress(selfdestructContractKey);
+    channelFull = ManagedChannelBuilder.forTarget(fullnode)
+        .usePlaintext(true)
+        .build();
+    blockingStubFull = WalletGrpc.newBlockingStub(channelFull);
+    channelFull1 = ManagedChannelBuilder.forTarget(fullnode1)
+        .usePlaintext(true)
+        .build();
+    blockingStubFull1 = WalletGrpc.newBlockingStub(channelFull1);
+
+    channelSolidity = ManagedChannelBuilder.forTarget(soliditynode)
+        .usePlaintext(true)
+        .build();
+    blockingStubSolidity = WalletSolidityGrpc.newBlockingStub(channelSolidity);
+  }
+
+
+  @Test(enabled = true, description = "selfdestruct Contract test isContract Command")
+  public void testselfdestructContract() {
+    Assert.assertTrue(PublicMethed
+        .sendcoin(contractExcAddress, 10000000000L, testNetAccountAddress, testNetAccountKey,
+            blockingStubFull));
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    String filePath = "src/test/resources/soliditycode/TvmIsContract.sol";
+    String contractName = "testIsContract";
+    HashMap retMap = PublicMethed.getBycodeAbi(filePath, contractName);
+    String code = retMap.get("byteCode").toString();
+    String abi = retMap.get("abI").toString();
+    contractAddress = PublicMethed.deployContract(contractName, abi, code, "", maxFeeLimit,
+        0L, 100, null, contractExcKey,
+        contractExcAddress, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    String txid = "";
+    String num = "\"" + Base58.encode58Check(contractAddress) + "\"";
+    txid = PublicMethed.triggerContract(contractAddress,
+        "testIsContractCommand(address)", num, false,
+        0, maxFeeLimit, contractExcAddress, contractExcKey, blockingStubFull);
+    Optional<Protocol.TransactionInfo> infoById = null;
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    infoById = PublicMethed.getTransactionInfoById(txid, blockingStubFull);
+    Assert.assertEquals(0, infoById.get().getResultValue());
+    System.out.println(infoById);
+    Assert.assertTrue(PublicMethed
+        .sendcoin(selfdestructContractExcAddress, 10000000000L, testNetAccountAddress,
+            testNetAccountKey,
+            blockingStubFull));
+    selfdestructContractAddress = PublicMethed
+        .deployContract(contractName, abi, code, "", maxFeeLimit,
+            0L, 100, null, selfdestructContractKey,
+            selfdestructContractExcAddress, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    txid = PublicMethed.triggerContract(selfdestructContractAddress,
+        "testIsContractCommand(address)", num, false,
+        0, maxFeeLimit, selfdestructContractExcAddress, selfdestructContractKey, blockingStubFull);
+    Optional<Protocol.TransactionInfo> infoById1 = null;
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    infoById1 = PublicMethed.getTransactionInfoById(txid, blockingStubFull);
+    Assert.assertEquals(0, ByteArray.toInt(infoById.get().getContractResult(0).toByteArray()));
+    System.out.println(infoById1);
+    String txid1 = "";
+    txid1 = PublicMethed.triggerContract(contractAddress,
+        "selfdestructContract(address)", num, false,
+        0, maxFeeLimit, contractExcAddress, contractExcKey, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    infoById1 = PublicMethed.getTransactionInfoById(txid1, blockingStubFull);
+    Assert.assertEquals(0, infoById1.get().getResultValue());
+    txid1 = PublicMethed.triggerContract(selfdestructContractAddress,
+        "testIsContractCommand(address)", num, false,
+        0, maxFeeLimit, selfdestructContractExcAddress, selfdestructContractKey, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    infoById1 = PublicMethed.getTransactionInfoById(txid1, blockingStubFull);
+    Assert.assertEquals(1, ByteArray.toInt(infoById1.get().getContractResult(0).toByteArray()));
+    System.out.println(infoById);
+  }
+
+
+  /**
+   * constructor.
+   */
+  @AfterClass
+  public void shutdown() throws InterruptedException {
+    if (channelFull != null) {
+      channelFull.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+    if (channelFull1 != null) {
+      channelFull1.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+    if (channelSolidity != null) {
+      channelSolidity.shutdown().awaitTermination(5, TimeUnit.SECONDS);
+    }
+  }
+
+
+}
