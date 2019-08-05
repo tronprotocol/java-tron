c0a8c8f16fd88c552ebb407152301fb70839df1a
kaizhu@tron.network <kaizhu@tron.network>
2019-08-05 12:29:23 +0800

    add empty address hex test

--- src/test/java/stest/tron/wallet/dailybuild/tvmnewcommand/multiValidateSignContract/multiValidateSignContract004.java
+++ src/test/java/stest/tron/wallet/dailybuild/tvmnewcommand/multiValidateSignContract/multiValidateSignContract004.java
@@ -60,7 +60,6 @@ public class multiValidateSignContract004 {
   ECKey ecKey1 = new ECKey(Utils.getRandom());
   byte[] contractExcAddress = ecKey1.getAddress();
   String contractExcKey = ByteArray.toHexString(ecKey1.getPrivKeyBytes());
-
   private String parametersString(List<Object>parameters){
     String[] inputArr = new String[parameters.size()];
     int i = 0;
@@ -158,9 +157,67 @@ public class multiValidateSignContract004 {
         .assertEquals(1, ByteArray.toInt(transactionExtention.getConstantResult(0).toByteArray()));
   }
 
+  @Test(enabled = true, description = "incorrect hex test multivalidatesign")
+  public void test02multivalidatesign() {
+    String txid = PublicMethed
+        .sendcoinGetTransactionId(contractExcAddress, 10000000000L, testNetAccountAddress,
+            testNetAccountKey,
+            blockingStubFull);
+    System.out.println(txid);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    String filePath = "src/test/resources/soliditycode/multivalidatesign.sol";
+    String contractName = "Demo";
+    HashMap retMap = PublicMethed.getBycodeAbi(filePath, contractName);
+    String code = retMap.get("byteCode").toString();
+    String abi = retMap.get("abI").toString();
+    contractAddress = PublicMethed.deployContract(contractName, abi, code, "", maxFeeLimit,
+        0L, 100, null, contractExcKey,
+        contractExcAddress, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    List<Object> signatures = new ArrayList<>();
+    List<Object> addresses = new ArrayList<>();
+    String input = "7d889f42b4a56ebe78264631a3b4daf21019e1170cce71929fb396761cdf532e000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000001c00000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000004000000000000000000000000000000000000000000000000000000000000000c00000000000000000000000000000000000000000000000000000000000000041ad7ca8100cf0ce028b83ac719c8458655a6605317abfd071b91f5cc14d53e87a299fe0cdf6a8567074e9be3944affba33b1e15d14b7cb9003ec2c87cb1a56405000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000417ce31e565fb99451f87db65e75f46672e8a8f7b29e6589e60fd11e076550d0a66d0b05e4b4d7d40bd34140f13dc3632d3ce0f25e4cf75840238b6fe2346c94fa010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000020000000000000000000000410d6b1de9e84c1d7a9a5b43d93dbe4a5aae79b1890000000000000000000000123456";
+    String method = "testArray(bytes32,bytes[],address[])";
+    TransactionExtention transactionExtention = PublicMethed
+        .triggerConstantContractForExtention(contractAddress,
+            "testArray(bytes32,bytes[],address[])", input, true,
+            0, 0, "0", 0, contractExcAddress, contractExcKey, blockingStubFull);
+    logger.info(transactionExtention.toString());
+    Assert
+        .assertEquals(0, ByteArray.toInt(transactionExtention.getConstantResult(0).toByteArray()));
+  }
 
-
-
+  @Test(enabled = true, description = "empty address hex test multivalidatesign")
+  public void test03multivalidatesign() {
+    String txid = PublicMethed
+        .sendcoinGetTransactionId(contractExcAddress, 10000000000L, testNetAccountAddress,
+            testNetAccountKey,
+            blockingStubFull);
+    System.out.println(txid);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    String filePath = "src/test/resources/soliditycode/multivalidatesign.sol";
+    String contractName = "Demo";
+    HashMap retMap = PublicMethed.getBycodeAbi(filePath, contractName);
+    String code = retMap.get("byteCode").toString();
+    String abi = retMap.get("abI").toString();
+    contractAddress = PublicMethed.deployContract(contractName, abi, code, "", maxFeeLimit,
+        0L, 100, null, contractExcKey,
+        contractExcAddress, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    List<Object> signatures = new ArrayList<>();
+    List<Object> addresses = new ArrayList<>();
+    String input = "da586d881362c0c38eb31b556ce0f7c2837a3ebb60080e8e665a6b92c7541837b95064ba000000000000000000000000000000000000000000000000000000000000006000000000000000000000000000000000000000000000000000000000000001200000000000000000000000000000000000000000000000000000000000000001000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000412d34d149ddf924b3ef806d7bc9b658ea28141962238a1b3dd351b777e28e84ce0794cf92de31d527476b4240a6d496089a2ae0c312816bbb20858ee607ab446a010000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000000";
+    String method = "testArray(bytes32,bytes[],address[])";
+    TransactionExtention transactionExtention = PublicMethed
+        .triggerConstantContractForExtention(contractAddress,
+            "testArray(bytes32,bytes[],address[])", input, true,
+            0, 0, "0", 0, contractExcAddress, contractExcKey, blockingStubFull);
+    logger.info(transactionExtention.toString());
+    Assert
+        .assertEquals(0, ByteArray.toInt(transactionExtention.getConstantResult(0).toByteArray()));
+  }
 
 
   /**
