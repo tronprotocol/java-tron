ef144b19b8d6f37120d7a772382993edb610f8e3
wangming <wm18840987261@163.com>
2019-08-16 15:02:51 +0800

    modify Gateway deployer account

--- src/test/java/stest/tron/wallet/other/deployMainGateway.java
+++ src/test/java/stest/tron/wallet/other/deployMainGateway.java
@@ -9,33 +9,27 @@ import java.util.Optional;
 import java.util.concurrent.TimeUnit;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
-import org.spongycastle.util.encoders.Hex;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 import org.tron.api.WalletGrpc;
 import org.tron.api.WalletSolidityGrpc;
-import org.tron.common.crypto.ECKey;
-import org.tron.common.utils.ByteArray;
-import org.tron.common.utils.Utils;
 import org.tron.core.Wallet;
 import org.tron.protos.Protocol.Account;
-import org.tron.protos.Protocol.SmartContract;
 import org.tron.protos.Protocol.TransactionInfo;
 import stest.tron.wallet.common.client.Configuration;
 import stest.tron.wallet.common.client.Parameter.CommonConstant;
 import stest.tron.wallet.common.client.WalletClient;
-import stest.tron.wallet.common.client.utils.AbiUtil;
 import stest.tron.wallet.common.client.utils.Base58;
 import stest.tron.wallet.common.client.utils.PublicMethed;
-import stest.tron.wallet.common.client.utils.PublicMethedForMutiSign;
 
 @Slf4j
 public class deployMainGateway {
 
-  private final String testDepositTrx = "324a2052e491e99026442d81df4d2777292840c1b3949e20696c49096c6bacb7";
-  private final byte[] testDepositAddress = PublicMethed.getFinalAddress(testDepositTrx);
+  private final String oracleKey =
+      "324a2052e491e99026442d81df4d2777292840c1b3949e20696c49096c6bacb7";
+  private final byte[] oracleAddress = PublicMethed.getFinalAddress(oracleKey);
   private Long maxFeeLimit = Configuration.getByPath("testng.conf")
       .getLong("defaultParameter.maxFeeLimit");
   private final String foundationKey001 = Configuration.getByPath("testng.conf").
@@ -44,6 +38,9 @@ public class deployMainGateway {
   private final String foundationKey002 = Configuration.getByPath("testng.conf").
       getString("foundationAccount.key2");
   private final byte[] foundationAddress002 = PublicMethed.getFinalAddress(foundationKey002);
+  private final String foundationKey003 = Configuration.getByPath("testng.conf")
+      .getString("foundationAccount.key3");
+  private final byte[] foundationAddress003 = PublicMethed.getFinalAddress(foundationKey003);
 
 
   private static final long now = System.currentTimeMillis();
@@ -72,10 +69,6 @@ public class deployMainGateway {
   private String fullnode = Configuration.getByPath("testng.conf")
       .getStringList("fullnode.ip.list").get(0);
 
-  ECKey ecKey1 = new ECKey(Utils.getRandom());
-  byte[] depositAddress = ecKey1.getAddress();
-  String testKeyFordeposit = ByteArray.toHexString(ecKey1.getPrivKeyBytes());
-
   @BeforeSuite
   public void beforeSuite() {
     Wallet wallet = new Wallet();
@@ -98,14 +91,9 @@ public class deployMainGateway {
   @Test(enabled = true, description = "deploy Main Chain Gateway")
   public void deplyMainChainGateway() {
 
-    PublicMethed.printAddress(testKeyFordeposit);
+    PublicMethed.printAddress(oracleKey);
 
-    Assert.assertTrue(PublicMethed
-        .sendcoin(depositAddress, 10000000_000_000L, testDepositAddress, testDepositTrx,
-            blockingStubFull));
-    PublicMethed.waitProduceNextBlock(blockingStubFull);
-
-    Account accountOralce = PublicMethed.queryAccount(depositAddress, blockingStubFull);
+    Account accountOralce = PublicMethed.queryAccount(oracleAddress, blockingStubFull);
     long OralceBalance = accountOralce.getBalance();
     logger.info("OralceBalance: " + OralceBalance);
 
@@ -114,16 +102,15 @@ public class deployMainGateway {
         .getString("code.code_MainGateway");
     String abi = Configuration.getByPath("testng.conf")
         .getString("abi.abi_MainGateway");
-    String parame = "\"" + Base58.encode58Check(testDepositAddress) + "\"";
+    String parame = "\"" + Base58.encode58Check(oracleAddress) + "\"";
     String mainChainGatewayAddress = "3QJmnh";
 
     int tryCount = 0;
     while (tryCount++ < 3) {
       String deployTxid = PublicMethed
           .deployContractAndGetTransactionInfoById(contractName, abi, code, "",
-              maxFeeLimit,
-              0L, 100, null, testKeyFordeposit, depositAddress
-              , blockingStubFull);
+              maxFeeLimit, 0L, 100, null,
+              foundationKey003, foundationAddress003, blockingStubFull);
       PublicMethed.waitProduceNextBlock(blockingStubFull);
 
       Optional<TransactionInfo> infoById = PublicMethed
@@ -133,9 +120,9 @@ public class deployMainGateway {
 
       if((!mainChainGatewayAddress.equals("3QJmnh"))&& !mainChainGatewayAddress.isEmpty()){
         String triggerTxid1 = PublicMethed
-            .triggerContract(
-                WalletClient.decodeFromBase58Check(mainChainGatewayAddress),"addOracle(address)",parame,false,0L, maxFeeLimit,
-                depositAddress, testKeyFordeposit, blockingStubFull);
+            .triggerContract(WalletClient.decodeFromBase58Check(mainChainGatewayAddress),
+                "addOracle(address)",parame,false,0L, maxFeeLimit,
+                foundationAddress003, foundationKey003, blockingStubFull);
         PublicMethed.waitProduceNextBlock(blockingStubFull);
         Optional<TransactionInfo> infoById1 = PublicMethed
             .getTransactionInfoById(triggerTxid1, blockingStubFull);
@@ -160,7 +147,7 @@ public class deployMainGateway {
 
   }
 
-  @Test(enabled = true, description = "deploy Main Chain Gateway")
+  @Test(enabled = true, description = "create Token Foundation")
   public void createTokenFoundation(){
     logger.info("foundationAccount 001 : ");
     PublicMethed.printAddress(foundationKey001);
@@ -169,14 +156,16 @@ public class deployMainGateway {
 
     long start = System.currentTimeMillis() + 2000;
     long end = System.currentTimeMillis() + 1000000000;
-    Assert.assertTrue(PublicMethed.createAssetIssue(foundationAddress001,"testAssetIssue_001",TotalSupply,
-        1,1, start,end,1,description,url,maxFeeLimit,1000L,
+    Assert.assertTrue(PublicMethed.createAssetIssue(
+        foundationAddress001,"testAssetIssue_001",TotalSupply, 1,1,
+        start,end,1,description,url,maxFeeLimit,1000L,
         1L,1L,foundationKey001,blockingStubFull));
 
     start = System.currentTimeMillis() + 2000;
     end = System.currentTimeMillis() + 1000000000;
-    Assert.assertTrue(PublicMethed.createAssetIssue(foundationAddress002,"testAssetIssue_002",TotalSupply,
-        1,1, start,end,1,description,url,maxFeeLimit,1000L,
+    Assert.assertTrue(PublicMethed.createAssetIssue(
+        foundationAddress002,"testAssetIssue_002",TotalSupply, 1,1,
+        start,end,1,description,url,maxFeeLimit,1000L,
         1L,1L,foundationKey002,blockingStubFull));
 
     for(int keycount=0;keycount<AssertAccount.length;keycount++){
@@ -190,9 +179,10 @@ public class deployMainGateway {
       String AssertName = "testAssetIssue_00" + keycount;
       start = System.currentTimeMillis() + 2000;
       end = System.currentTimeMillis() + 1000000000;
-      Assert.assertTrue(PublicMethed.createAssetIssue(AssertAddress,AssertName,TotalSupply,
-          1,1, start,end,1,description,url,maxFeeLimit,1000L,
-          1L,1L,AssertKey,blockingStubFull));
+      Assert.assertTrue(PublicMethed.createAssetIssue(
+          AssertAddress,AssertName,TotalSupply, 1,1, start,end,1,
+          description,url,maxFeeLimit,1000L, 1L,1L,
+          AssertKey,blockingStubFull));
       try {
         Thread.sleep(1000);
       }catch (Exception e){
