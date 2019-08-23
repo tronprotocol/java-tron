d0f786c02f4a67fa4d8f5088e22a1e1504f3c519
wubin1 <wubin1@tron.network>
2019-08-23 15:06:41 +0800

    fix bug

--- chainbase/src/main/java/org/tron/core/config/args/Parameter.java
+++ chainbase/src/main/java/org/tron/core/config/args/Parameter.java
@@ -8,6 +8,7 @@ public class Parameter {
     public static final long TRANSFER_FEE = 0; // free
     public static final int BLOCK_PRODUCED_INTERVAL = 3000; //ms,produce block period, must be divisible by 60. millisecond
     public static final int MAX_VOTE_NUMBER = 30;
+    public static final int MAX_ACTIVE_WITNESS_NUM = 27;
   }
   public enum ChainParameters {
     MAINTENANCE_TIME_INTERVAL, //ms  ,0
