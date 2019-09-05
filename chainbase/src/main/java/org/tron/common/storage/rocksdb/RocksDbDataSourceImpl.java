6f42e003882b149bf0a150a26ef5abd86657efcd
wubin1 <wubin1@tron.network>
2019-09-05 12:27:30 +0800

    fix bug

--- chainbase/src/main/java/org/tron/common/storage/rocksdb/RocksDbDataSourceImpl.java
+++ chainbase/src/main/java/org/tron/common/storage/rocksdb/RocksDbDataSourceImpl.java
@@ -26,6 +26,7 @@ import org.rocksdb.RocksIterator;
 import org.rocksdb.Statistics;
 import org.rocksdb.WriteBatch;
 import org.rocksdb.WriteOptions;
+import org.tron.common.utils.DBConfig;
 import org.tron.common.utils.FileUtil;
 import org.tron.common.utils.PropUtil;
 import org.tron.core.db.common.DbSourceInter;
@@ -49,14 +50,14 @@ public class RocksDbDataSourceImpl implements DbSourceInter<byte[]>,
 
   public RocksDbDataSourceImpl(String parentPath, String name, RocksDbSettings settings) {
     this.dataBaseName = name;
-    this.parentPath = parentPath;
+    this.parentPath = Paths.get(parentPath, DBConfig.getDbDirectory()).toString();
     RocksDbSettings.setSettings(settings);
     initDB();
   }
 
   public RocksDbDataSourceImpl(String parentPath, String name) {
     this.dataBaseName = name;
-    this.parentPath = parentPath;
+    this.parentPath = Paths.get(parentPath, DBConfig.getDbDirectory()).toString();
   }
 
   public Path getDbPath() {
@@ -221,6 +222,7 @@ public class RocksDbDataSourceImpl implements DbSourceInter<byte[]>,
         try {
           logger.debug("Opening database");
           final Path dbPath = getDbPath();
+
           if (!Files.isSymbolicLink(dbPath.getParent())) {
             Files.createDirectories(dbPath.getParent());
           }
