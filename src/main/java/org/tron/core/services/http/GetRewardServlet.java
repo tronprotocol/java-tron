d162089f27db6fb915930355236ed1468bf53be8
lvs007 <zy.liang.5@163.com>
2019-09-23 12:22:20 +0800

    query address suport 41 start address

--- src/main/java/org/tron/core/services/http/GetRewardServlet.java
+++ src/main/java/org/tron/core/services/http/GetRewardServlet.java
@@ -6,8 +6,10 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
+import org.spongycastle.util.encoders.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.tron.core.Constant;
 import org.tron.core.Wallet;
 import org.tron.core.db.Manager;
 
@@ -45,7 +47,11 @@ public class GetRewardServlet extends HttpServlet {
     byte[] address = null;
     String addressStr = request.getParameter("address");
     if (StringUtils.isNotBlank(addressStr)) {
-      address = Wallet.decodeFromBase58Check(addressStr);
+      if (StringUtils.startsWith(addressStr, Constant.ADD_PRE_FIX_STRING_MAINNET)) {
+        address = Hex.decode(addressStr);
+      } else {
+        address = Wallet.decodeFromBase58Check(addressStr);
+      }
     }
     return address;
   }
