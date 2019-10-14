247e0991f1af0967530071a9ebf36596bb59e20d
wubin01 <wb_bupt@163.com>
2019-10-14 19:06:46 +0800

    modify solid node start logic

--- framework/src/main/java/org/tron/program/SolidityNode.java
+++ framework/src/main/java/org/tron/program/SolidityNode.java
@@ -20,6 +20,7 @@ import org.tron.core.capsule.BlockCapsule;
 import org.tron.core.config.DefaultConfig;
 import org.tron.core.config.args.Args;
 import org.tron.core.db.Manager;
+import org.tron.core.net.TronNetService;
 import org.tron.core.services.RpcApiService;
 import org.tron.core.services.http.solidity.SolidityNodeHttpApiService;
 import org.tron.protos.Protocol.Block;
@@ -89,10 +90,10 @@ public class SolidityNode {
     //Disable peer discovery for solidity node
     DiscoverServer discoverServer = context.getBean(DiscoverServer.class);
     discoverServer.close();
-    ChannelManager channelManager = context.getBean(ChannelManager.class);
-    channelManager.close();
     NodeManager nodeManager = context.getBean(NodeManager.class);
     nodeManager.close();
+    TronNetService tronNetService = context.getBean(TronNetService.class);
+    tronNetService.stop();
 
     SolidityNode node = new SolidityNode(appT.getDbManager());
     node.start();
