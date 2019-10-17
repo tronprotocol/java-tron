72fcbed39a49253158b83b47e730ca1c64e1c5a9
ayoayo <546431096@qq.com>
2019-10-17 17:22:27 +0800

    Change the code format

--- consensus/src/main/java/org/tron/consensus/base/Param.java
+++ consensus/src/main/java/org/tron/consensus/base/Param.java
@@ -8,41 +8,21 @@ import org.tron.core.config.args.GenesisBlock;
 
 public class Param {
 
-  @Getter
-  @Setter
-  private boolean enable;
-  @Getter
-  @Setter
-  private boolean needSyncCheck;
-  @Getter
-  @Setter
-  private int minParticipationRate;
-  @Getter
-  @Setter
-  private int blockProduceTimeoutPercent;
-  @Getter
-  @Setter
-  private GenesisBlock genesisBlock;
-  @Getter
-  @Setter
-  private List<Miner> miners;
-  @Getter
-  @Setter
-  private BlockHandle blockHandle;
+  @Getter @Setter private boolean enable;
+  @Getter @Setter private boolean needSyncCheck;
+  @Getter @Setter private int minParticipationRate;
+  @Getter @Setter private int blockProduceTimeoutPercent;
+  @Getter @Setter private GenesisBlock genesisBlock;
+  @Getter @Setter private List<Miner> miners;
+  @Getter @Setter private BlockHandle blockHandle;
 
   public class Miner {
 
-    @Getter
-    @Setter
-    private byte[] privateKey;
+    @Getter @Setter private byte[] privateKey;
 
-    @Getter
-    @Setter
-    private ByteString privateKeyAddress;
+    @Getter @Setter private ByteString privateKeyAddress;
 
-    @Getter
-    @Setter
-    private ByteString witnessAddress;
+    @Getter @Setter private ByteString witnessAddress;
 
     public Miner(byte[] privateKey, ByteString privateKeyAddress, ByteString witnessAddress) {
       this.privateKey = privateKey;
