fee487cd3fbd706427e784e8600b283841e42703
lvs007 <zy.liang.5@163.com>
2019-10-22 11:57:11 +0800

    if sr commit no complete try once

--- consensus/src/main/java/org/tron/consensus/pbft/PbftMessageHandle.java
+++ consensus/src/main/java/org/tron/consensus/pbft/PbftMessageHandle.java
@@ -1,10 +1,11 @@
 package org.tron.consensus.pbft;
 
+import static org.tron.core.net.message.MessageTypes.PBFT_SR_MSG;
+
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
-import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
 import com.google.common.util.concurrent.AtomicLongMap;
@@ -63,6 +64,8 @@ public class PbftMessageHandle {
             }
           });
 
+  private PbftBaseMessage srPbftBaseMessage;
+
   private Timer timer;
 
   @Autowired
@@ -103,6 +106,9 @@ public class PbftMessageHandle {
     }
     PbftBaseMessage paMessage = message.buildPrePareMessage();
     forwardMessage(paMessage);
+    if (message.getType() == PBFT_SR_MSG) {
+      srPbftBaseMessage = message;
+    }
   }
 
   public void onPrepare(PbftBaseMessage message) {
@@ -158,6 +164,7 @@ public class PbftMessageHandle {
       //commit,
       if (!isSyncing()) {
         pbftMessageAction.action(message, dataSignCache.getUnchecked(message.getDataKey()));
+        srPbftBaseMessage = null;
       }
     }
   }
@@ -252,13 +259,21 @@ public class PbftMessageHandle {
     });
     doneMsg.remove(no);
     timeOuts.remove(no);
+    //just try once
+    if (srPbftBaseMessage != null && StringUtils.equals(no, srPbftBaseMessage.getNo())) {
+      try {
+        Thread.sleep(100);
+      } catch (Exception e) {
+      }
+      onPrePrepare(srPbftBaseMessage);
+      srPbftBaseMessage = null;
+    }
   }
 
   /**
    * Detect timeout
    */
   private void checkTimer() {
-    List<String> remo = Lists.newArrayList();
     for (Entry<String, Long> item : timeOuts.entrySet()) {
       if (System.currentTimeMillis() - item.getValue() > TIME_OUT) {
         //If the timeout has not been agreed, the vote will be invalid.
