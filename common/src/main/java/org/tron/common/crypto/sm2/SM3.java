ec5fefa2a8ea95feb6aedcea59d3c112a7627548
zhenping <federico.zhen@tron.network>
2019-11-18 11:07:01 +0800

    optimize the SM3 code

--- common/src/main/java/org/tron/common/crypto/sm2/SM3.java
+++ common/src/main/java/org/tron/common/crypto/sm2/SM3.java
@@ -5,15 +5,8 @@ import org.spongycastle.util.encoders.Hex;
 
 public class SM3 {
     public static byte[] hash(String message) {
-        SM3Digest digest = new SM3Digest();
         byte[] msg = Hex.decode(message);
-        digest.update(msg,0,msg.length);
-
-        byte[] eHash = new byte[digest.getDigestSize()];
-
-        digest.doFinal(eHash, 0);
-
-        return eHash;
+        return hash(msg);
     }
 
     public static byte[] hash(byte[] message) {
