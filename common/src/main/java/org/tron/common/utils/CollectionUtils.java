9f472b719c7035d7c2d7f3606b6235452f820e8d
M-blockcoder <mao514556@163.com>
2019-11-28 16:03:14 +0800

    remove unusefull code and optimized code

--- common/src/main/java/org/tron/common/utils/CollectionUtils.java
+++ common/src/main/java/org/tron/common/utils/CollectionUtils.java
@@ -11,22 +11,6 @@ import java.util.function.Predicate;
 
 public class CollectionUtils {
 
-  public static <K, V> List<V> collectList(Collection<K> items, Function<K, V> collector) {
-    List<V> collected = new ArrayList<>(items.size());
-    for (K item : items) {
-      collected.add(collector.apply(item));
-    }
-    return collected;
-  }
-
-  public static <K, V> Set<V> collectSet(Collection<K> items, Function<K, V> collector) {
-    Set<V> collected = new HashSet<>();
-    for (K item : items) {
-      collected.add(collector.apply(item));
-    }
-    return collected;
-  }
-
   public static <T> List<T> truncate(List<T> items, int limit) {
     if (limit > items.size()) {
       return new ArrayList<>(items);
@@ -64,25 +48,5 @@ public class CollectionUtils {
       }
     }
     return truncated;
-  }
-
-  public static <T> List<T> selectList(Collection<T> items, Predicate<T> predicate) {
-    List<T> selected = new ArrayList<>();
-    for (T item : items) {
-      if (predicate.test(item)) {
-        selected.add(item);
-      }
-    }
-    return selected;
-  }
-
-  public static <T> Set<T> selectSet(Collection<T> items, Predicate<T> predicate) {
-    Set<T> selected = new HashSet<>();
-    for (T item : items) {
-      if (predicate.test(item)) {
-        selected.add(item);
-      }
-    }
-    return selected;
   }
 }
