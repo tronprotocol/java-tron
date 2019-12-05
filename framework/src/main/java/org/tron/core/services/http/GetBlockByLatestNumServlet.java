31086153bce6d3c985dabce4597f2b3bf2b6996b
Parachuteuk <parachuteuk@outlook.com>
2019-12-05 14:44:00 +0800

    check style fix

--- framework/src/main/java/org/tron/core/services/http/GetBlockByLatestNumServlet.java
+++ framework/src/main/java/org/tron/core/services/http/GetBlockByLatestNumServlet.java
@@ -23,7 +23,7 @@ public class GetBlockByLatestNumServlet extends RateLimiterServlet {
     try {
       boolean visible = Util.getVisible(request);
       long getNum = Long.parseLong(request.getParameter("num"));
-      writeResponse(getNum, visible, response);
+      writeRes(getNum, visible, response);
     } catch (Exception e) {
       Util.processError(e, response);
     }
@@ -38,13 +38,13 @@ public class GetBlockByLatestNumServlet extends RateLimiterServlet {
       NumberMessage.Builder build = NumberMessage.newBuilder();
       JsonFormat.merge(input, build, visible);
       long getNum = build.getNum();
-      writeResponse(getNum, visible, response);
+      writeRes(getNum, visible, response);
     } catch (Exception e) {
       Util.processError(e, response);
     }
   }
 
-  private void writeResponse(long getNum, boolean visible, HttpServletResponse response)
+  private void writeRes(long getNum, boolean visible, HttpServletResponse response)
           throws Exception {
     if (getNum > 0 && getNum < BLOCK_LIMIT_NUM) {
       BlockList reply = wallet.getBlockByLatestNum(getNum);
