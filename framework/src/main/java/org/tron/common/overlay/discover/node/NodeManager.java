6ccae5d9033c8991c91074219b8ae9f0207c8cb3
Sakary Jia <sakary.jia@tron.network>
2019-12-11 12:36:06 +0800

    add nodemanager test
    modify NodeManager.trimTable()
    
    split original test function to small ones

--- framework/src/main/java/org/tron/common/overlay/discover/node/NodeManager.java
+++ framework/src/main/java/org/tron/common/overlay/discover/node/NodeManager.java
@@ -167,7 +167,7 @@ public class NodeManager implements EventHandler {
     if (nodeHandlerMap.size() > NODES_TRIM_THRESHOLD) {
       nodeHandlerMap.values().forEach(handler -> {
         if (!handler.getNode().isConnectible(Args.getInstance().getNodeP2pVersion())) {
-          nodeHandlerMap.remove(handler);
+          nodeHandlerMap.values().remove(handler);
         }
       });
     }
