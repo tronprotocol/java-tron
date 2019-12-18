57b245a1e2ba329b03dcabdbcf45ec3c10de9cd5
Wenhua Zhang <zhangwenhua@tron.network>
2019-12-18 15:48:45 +0800

    feat(market_api): add api for get price by pair

--- framework/src/main/java/org/tron/core/db/Manager.java
+++ framework/src/main/java/org/tron/core/db/Manager.java
@@ -127,6 +127,7 @@ import org.tron.core.store.ExchangeV2Store;
 import org.tron.core.store.IncrementalMerkleTreeStore;
 import org.tron.core.store.MarketAccountStore;
 import org.tron.core.store.MarketOrderStore;
+import org.tron.core.store.MarketPairToPriceStore;
 import org.tron.core.store.NullifierStore;
 import org.tron.core.store.ProposalStore;
 import org.tron.core.store.StorageRowStore;
@@ -224,6 +225,9 @@ public class Manager {
   @Autowired
   @Getter
   private MarketOrderStore marketOrderStore;
+  @Autowired
+  @Getter
+  private MarketPairToPriceStore marketPairToPriceStore;
   // for network
   @Autowired
   private PeersStore peersStore;
@@ -1697,6 +1701,7 @@ public class Manager {
     closeOneStore(transactionRetStore);
     closeOneStore(marketAccountStore);
     closeOneStore(marketOrderStore);
+    closeOneStore(marketPairToPriceStore);
     logger.info("******** end to close db ********");
   }
 
