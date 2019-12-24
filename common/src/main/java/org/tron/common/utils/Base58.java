b910da4ad9aa6194ebcbe82e273aab09540a62ea
451PCBCOM <52361477+451PCBCOM@users.noreply.github.com>
2019-12-24 18:41:21 +0100

    Update Base58.java
    Delete unused import.

--- common/src/main/java/org/tron/common/utils/Base58.java
+++ common/src/main/java/org/tron/common/utils/Base58.java
@@ -1,7 +1,6 @@
 package org.tron.common.utils;
 
 import java.io.UnsupportedEncodingException;
-import java.math.BigInteger;
 
 public class Base58 {
 
@@ -69,7 +68,6 @@ public class Base58 {
     // Transform the String to a base58 byte sequence
     for (int i = 0; i < input.length(); ++i) {
       char c = input.charAt(i);
-
       int digit58 = -1;
       if (c >= 0 && c < 128) {
         digit58 = INDEXES[c];
@@ -77,7 +75,6 @@ public class Base58 {
       if (digit58 < 0) {
         throw new IllegalArgumentException("Illegal character " + c + " at " + i);
       }
-
       input58[i] = (byte) digit58;
     }
     // Count leading zeroes
@@ -88,14 +85,12 @@ public class Base58 {
     // The encoding
     byte[] temp = new byte[input.length()];
     int j = temp.length;
-
     int startAt = zeroCount;
     while (startAt < input58.length) {
       byte mod = divmod256(input58, startAt);
       if (input58[startAt] == 0) {
         ++startAt;
       }
-
       temp[--j] = mod;
     }
     // Do no add extra leading zeroes, move j to first non null byte.
@@ -114,12 +109,9 @@ public class Base58 {
     for (int i = startAt; i < number.length; i++) {
       int digit256 = (int) number[i] & 0xFF;
       int temp = remainder * 256 + digit256;
-
       number[i] = (byte) (temp / 58);
-
       remainder = temp % 58;
     }
-
     return (byte) remainder;
   }
 
@@ -131,9 +123,7 @@ public class Base58 {
     for (int i = startAt; i < number58.length; i++) {
       int digit58 = (int) number58[i] & 0xFF;
       int temp = remainder * 58 + digit58;
-
       number58[i] = (byte) (temp / 256);
-
       remainder = temp % 256;
     }
 
@@ -143,8 +133,6 @@ public class Base58 {
   private static byte[] copyOfRange(byte[] source, int from, int to) {
     byte[] range = new byte[to - from];
     System.arraycopy(source, from, range, 0, range.length);
-
     return range;
   }
-
 }
