6d838e4cc60f6ac9fa0ea9d9b1f9b0dac1118ac0
451PCBCOM <52361477+451PCBCOM@users.noreply.github.com>
2019-12-24 15:06:46 +0100

    Update DecodeUtil.java
    Fix grammatical mistakes and code style.

--- common/src/main/java/org/tron/common/utils/DecodeUtil.java
+++ common/src/main/java/org/tron/common/utils/DecodeUtil.java
@@ -2,7 +2,6 @@ package org.tron.common.utils;
 
 import static java.util.Arrays.copyOfRange;
 import static org.tron.common.utils.Hash.sha3;
-
 import java.util.Arrays;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ArrayUtils;
@@ -11,12 +10,9 @@ import org.tron.core.Constant;
 
 @Slf4j(topic = "Commons")
 public class DecodeUtil {
-
   public static final int ADDRESS_SIZE = 42;
   public static byte addressPreFixByte = Constant.ADD_PRE_FIX_BYTE_MAINNET;
-
   public static String addressPreFixString = Constant.ADD_PRE_FIX_STRING_MAINNET;
-
   public static boolean addressValid(byte[] address) {
     if (ArrayUtils.isEmpty(address)) {
       logger.warn("Warning: Address is empty !!");
@@ -24,14 +20,11 @@ public class DecodeUtil {
     }
     if (address.length != ADDRESS_SIZE / 2) {
       logger.warn(
-          "Warning: Address length need " + ADDRESS_SIZE + " but " + address.length
-              + " !!");
+          "Warning: Address length needs to be " + ADDRESS_SIZE + ", but it is " + address.length + " !!");
       return false;
     }
-
     if (address[0] != addressPreFixByte) {
-      logger.warn("Warning: Address need prefix with " + addressPreFixByte + " but "
-          + address[0] + " !!");
+      logger.warn("Warning: Address needs to be prefix with " + addressPreFixByte + ", but it is " + address[0] + " !!");
       return false;
     }
     return true;
@@ -47,6 +40,7 @@ public class DecodeUtil {
    * @param input - data
    * @return - add_pre_fix + 20 right bytes of the hash keccak of the data
    */
+  
   public static byte[] sha3omit12(byte[] input) {
     byte[] hash = sha3(input);
     byte[] address = copyOfRange(hash, 11, hash.length);
