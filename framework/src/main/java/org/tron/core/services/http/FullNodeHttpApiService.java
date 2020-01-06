8cf09ac60e6141b5f18c98451a20e576af611f40
wubinTron <wubin1@tron.network>
2020-01-06 12:24:36 +0800

    add switch for http api and solidity api

--- framework/src/main/java/org/tron/core/services/http/FullNodeHttpApiService.java
+++ framework/src/main/java/org/tron/core/services/http/FullNodeHttpApiService.java
@@ -409,6 +409,7 @@ public class FullNodeHttpApiService implements Service {
       if (maxHttpConnectNumber > 0) {
         server.addBean(new ConnectionLimit(maxHttpConnectNumber, server));
       }
+
       server.start();
     } catch (Exception e) {
       logger.debug("IOException: {}", e.getMessage());
