c25fc742936a3e542bd52367a073debf429a487c
gossens <gossens@163.com>
2020-01-14 20:15:24 +0800

    solve checkstyle

--- framework/src/test/java/stest/tron/wallet/common/client/utils/AbiUtil.java
+++ framework/src/test/java/stest/tron/wallet/common/client/utils/AbiUtil.java
@@ -8,9 +8,9 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import org.apache.commons.lang3.StringUtils;
 import org.spongycastle.util.encoders.Hex;
+import org.tron.common.crypto.Hash;
 import org.tron.common.utils.ByteUtil;
 import org.tron.common.utils.Commons;
-import org.tron.common.crypto.Hash;
 
 public class AbiUtil {
 
