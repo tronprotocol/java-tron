afdb20ff193d4c9d602b1702ab744116cee21fde
Parachuteuk <parachuteuk@outlook.com>
2020-01-17 11:04:50 +0800

    no message

--- framework/src/main/java/org/tron/core/services/http/GetBrokerageServlet.java
+++ framework/src/main/java/org/tron/core/services/http/GetBrokerageServlet.java
@@ -7,7 +7,6 @@ import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang3.StringUtils;
 import org.spongycastle.util.encoders.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
