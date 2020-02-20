1854d6ef46547db86d963d40f19856d88054394d
shulinkang <ksl2945@gmail.com>
2020-02-20 19:00:03 +0800

    change exception description

--- chainbase/src/main/java/org/tron/common/utils/WalletUtil.java
+++ chainbase/src/main/java/org/tron/common/utils/WalletUtil.java
@@ -22,7 +22,7 @@ public class WalletUtil {
       throws PermissionException {
     ByteString operations = permission.getOperations();
     if (operations.size() != 32) {
-      throw new PermissionException("operations size must be 32");
+      throw new PermissionException("operations size must 32");
     }
     int contractType = contract.getTypeValue();
     boolean b = (operations.byteAt(contractType / 8) & (1 << (contractType % 8))) != 0;
