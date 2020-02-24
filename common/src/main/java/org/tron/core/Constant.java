ce2f94d1202f3f2d3b8a527e1c3f3b96ce1428ec
ZaraLang <57788351+ZaraLang@users.noreply.github.com>
2020-02-24 11:36:49 +0800

    Update Constant.java

--- common/src/main/java/org/tron/core/Constant.java
+++ common/src/main/java/org/tron/core/Constant.java
@@ -30,7 +30,7 @@ public class Constant {
   public static final long MAXIMUM_TIME_UNTIL_EXPIRATION = 24 * 60 * 60 * 1_000L; //one day
   public static final long TRANSACTION_DEFAULT_EXPIRATION_TIME = 60 * 1_000L; //60 seconds
   // config for smart contract
-  public static final long SUN_PER_ENERGY = 100; // 1 us = 100 DROP = 100 * 10^-6 TRX
+  public static final long SUN_PER_ENERGY = 100; // 1 us = 100 SUN = 100 * 10^-6 TRX
   public static final long ENERGY_LIMIT_IN_CONSTANT_TX = 3_000_000L; // ref: 1 us = 1 energy
   public static final long MAX_RESULT_SIZE_IN_TX = 64; // max 8 * 8 items in result
   public static final long PB_DEFAULT_ENERGY_LIMIT = 0L;
