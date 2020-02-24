765288af8b4681066c0bbe5ce2b6161afcee3c0a
ZaraLang <57788351+ZaraLang@users.noreply.github.com>
2020-02-24 11:38:51 +0800

    Update VMConstant.java

--- actuator/src/main/java/org/tron/core/vm/VMConstant.java
+++ actuator/src/main/java/org/tron/core/vm/VMConstant.java
@@ -10,7 +10,7 @@ public class VMConstant {
   // Numbers
   public static final int ONE_HUNDRED = 100;
   public static final int ONE_THOUSAND = 1000;
-  public static final long SUN_PER_ENERGY = 100; // 1 us = 100 DROP = 100 * 10^-6 TRX
+  public static final long SUN_PER_ENERGY = 100; // 1 us = 100 SUN = 100 * 10^-6 TRX
   public static final long ENERGY_LIMIT_IN_CONSTANT_TX = 3_000_000L; // ref: 1 us = 1 energy
 
 
