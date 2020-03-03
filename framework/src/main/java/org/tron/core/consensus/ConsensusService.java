fb1059079cc69dc0c63ff29955934e0631259925
wubin01 <wb_bupt@163.com>
2020-03-03 13:18:46 +0800

    support mul witness

--- framework/src/main/java/org/tron/core/consensus/ConsensusService.java
+++ framework/src/main/java/org/tron/core/consensus/ConsensusService.java
@@ -1,21 +1,17 @@
 package org.tron.core.consensus;
 
+import static org.tron.common.utils.ByteArray.fromHexString;
+
 import com.google.protobuf.ByteString;
-import com.sun.org.apache.xpath.internal.Arg;
 import java.util.ArrayList;
 import java.util.List;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.codec.binary.Hex;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-import org.tron.common.crypto.ECKey;
 import org.tron.common.crypto.SignUtils;
-import org.tron.common.utils.ByteArray;
-import org.tron.common.utils.DBConfig;
 import org.tron.consensus.Consensus;
 import org.tron.consensus.base.Param;
 import org.tron.consensus.base.Param.Miner;
-import org.tron.core.capsule.WitnessCapsule;
 import org.tron.core.config.args.Args;
 import org.tron.core.store.WitnessStore;
 
@@ -42,20 +38,31 @@ public class ConsensusService {
     param.setBlockProduceTimeoutPercent(Args.getInstance().getBlockProducedTimeOut());
     param.setNeedSyncCheck(args.isNeedSyncCheck());
     List<Miner> miners = new ArrayList<>();
-    byte[] privateKey = ByteArray
-        .fromHexString(Args.getInstance().getLocalWitnesses().getPrivateKey());
-    byte[] privateKeyAddress = SignUtils.fromPrivate(privateKey,
-        Args.getInstance().isECKeyCryptoEngine()).getAddress();
-    byte[] witnessAddress = Args.getInstance().getLocalWitnesses().getWitnessAccountAddress(
-        DBConfig.isECKeyCryptoEngine());
-    WitnessCapsule witnessCapsule = witnessStore.get(witnessAddress);
-    if (null == witnessCapsule) {
-      logger.warn("Witness {} is not in witnessStore.", Hex.encodeHexString(witnessAddress));
-    } else {
+
+    List<String> privateKeys = Args.getInstance().getLocalWitnesses().getPrivateKeys();
+    for (String key : privateKeys) {
+      byte[] privateKey = fromHexString(key);
+      byte[] privateKeyAddress = SignUtils
+          .fromPrivate(privateKey, Args.getInstance().isECKeyCryptoEngine()).getAddress();
       Miner miner = param.new Miner(privateKey, ByteString.copyFrom(privateKeyAddress),
-          ByteString.copyFrom(witnessAddress));
+          ByteString.copyFrom(privateKeyAddress));
       miners.add(miner);
     }
+
+//    byte[] privateKey =
+//        fromHexString(Args.getInstance().getLocalWitnesses().getPrivateKey());
+//    byte[] privateKeyAddress = SignUtils.fromPrivate(privateKey,
+//        Args.getInstance().isECKeyCryptoEngine()).getAddress();
+//    byte[] witnessAddress = Args.getInstance().getLocalWitnesses().getWitnessAccountAddress(
+//        DBConfig.isECKeyCryptoEngine());
+//    WitnessCapsule witnessCapsule = witnessStore.get(witnessAddress);
+//    if (null == witnessCapsule) {
+//      logger.warn("Witness {} is not in witnessStore.", Hex.encodeHexString(witnessAddress));
+//    } else {
+//      Miner miner = param.new Miner(privateKey, ByteString.copyFrom(privateKeyAddress),
+//          ByteString.copyFrom(witnessAddress));
+//      miners.add(miner);
+//    }
     param.setMiners(miners);
     param.setBlockHandle(blockHandle);
     consensus.start(param);
