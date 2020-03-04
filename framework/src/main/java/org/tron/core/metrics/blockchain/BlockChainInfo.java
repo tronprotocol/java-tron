5f6554cb0da2220b4ba32c1a2dc71337893a8ff7
Bill <xing@tron.network>
2020-03-03 18:56:08 -0800

    fix naming

--- framework/src/main/java/org/tron/core/metrics/blockchain/BlockChainInfo.java
+++ framework/src/main/java/org/tron/core/metrics/blockchain/BlockChainInfo.java
@@ -86,7 +86,7 @@ public class BlockChainInfo {
     this.transactionCacheSize = transactionCacheSize;
   }
 
-  public RateInfo getMissedTransactionCount() {
+  public RateInfo getMissedTransaction() {
     return missedTransaction;
   }
 
