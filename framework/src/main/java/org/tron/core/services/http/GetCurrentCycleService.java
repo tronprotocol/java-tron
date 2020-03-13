357e7015ba0d81cfbb549e35bc73a32d366c4d7f
wubinTron <wubin1@tron.network>
2020-03-13 18:41:50 +0800

    add sr pay

--- framework/src/main/java/org/tron/core/services/http/GetCurrentCycleService.java
+++ framework/src/main/java/org/tron/core/services/http/GetCurrentCycleService.java
@@ -4,9 +4,13 @@ import com.alibaba.fastjson.JSONObject;
 import java.util.stream.Collectors;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.tron.core.db.Manager;
 
+@Component
+@Slf4j(topic = "API")
 public class GetCurrentCycleService extends RateLimiterServlet {
   @Autowired
   private Manager manager;
