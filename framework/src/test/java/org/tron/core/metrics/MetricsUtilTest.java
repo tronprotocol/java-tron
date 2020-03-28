3fd61411e000d22f66f8d004cd67596bf739053b
kaiserlinda <kaiserlinda@163.com>
2020-03-28 14:47:03 +0800

    modify metriceutil test

--- framework/src/test/java/org/tron/core/metrics/MetricsUtilTest.java
+++ framework/src/test/java/org/tron/core/metrics/MetricsUtilTest.java
@@ -14,20 +14,20 @@ public class MetricsUtilTest {
 
   @Test
   public void testMeterMark() {
-    MetricsUtil.meterMark(MetricsKey.BLOCKCHAIN_FAIL_FORK_COUNT);
-    Assert.assertEquals(1, MetricsUtil.getMeter(MetricsKey.BLOCKCHAIN_FAIL_FORK_COUNT).getCount());
+    MetricsUtil.meterMark(MetricsKey.BLOCKCHAIN_FORK_COUNT);
+    Assert.assertEquals(1, MetricsUtil.getMeter(MetricsKey.BLOCKCHAIN_FORK_COUNT).getCount());
   }
 
   @Test
   public void testMeterMark2() {
-    MetricsUtil.meterMark(MetricsKey.BLOCKCHAIN_FAIL_FORK_COUNT, 2);
-    Assert.assertEquals(2, MetricsUtil.getMeter(MetricsKey.BLOCKCHAIN_FAIL_FORK_COUNT).getCount());
+    MetricsUtil.meterMark(MetricsKey.NET_API_QPS, 2);
+    Assert.assertEquals(2, MetricsUtil.getMeter(MetricsKey.NET_API_QPS).getCount());
   }
 
   @Test
   public void testHistogramUpdate() {
-    MetricsUtil.histogramUpdate(MetricsKey.BLOCKCHAIN_FAIL_FORK_COUNT, 2);
+    MetricsUtil.histogramUpdate(MetricsKey.NET_API_FAIL_QPS, 2);
     Assert.assertEquals(2,
-        MetricsUtil.getHistogram(MetricsKey.BLOCKCHAIN_FAIL_FORK_COUNT).getCount());
+        MetricsUtil.getHistogram(MetricsKey.NET_API_FAIL_QPS).getCount());
   }
 }
