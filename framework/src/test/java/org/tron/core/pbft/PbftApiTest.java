7bcad43c4219abbe1638663f3a53df58b6efc074
Matt <yrp1990@gmail.com>
2020-04-16 17:28:05 +0800

    pbft api unittest

---
+++ framework/src/test/java/org/tron/core/pbft/PbftApiTest.java
@@ -0,0 +1,144 @@
+package org.tron.core.pbft;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.google.common.collect.Maps;
+import com.google.protobuf.ByteString;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections4.CollectionUtils;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
+import org.eclipse.jetty.server.Response;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.tron.common.application.TronApplicationContext;
+import org.tron.common.crypto.ECKey;
+import org.tron.common.utils.ByteArray;
+import org.tron.common.utils.FileUtil;
+import org.tron.common.utils.Sha256Hash;
+import org.tron.common.utils.Utils;
+import org.tron.consensus.dpos.DposSlot;
+import org.tron.core.ChainBaseManager;
+import org.tron.core.Constant;
+import org.tron.core.capsule.AccountCapsule;
+import org.tron.core.capsule.BlockCapsule;
+import org.tron.core.capsule.TransactionCapsule;
+import org.tron.core.capsule.WitnessCapsule;
+import org.tron.core.config.DefaultConfig;
+import org.tron.core.config.args.Args;
+import org.tron.core.consensus.ConsensusService;
+import org.tron.core.db.BlockGenerate;
+import org.tron.core.db.CommonDataBase;
+import org.tron.core.db.KhaosDatabase;
+import org.tron.core.db.Manager;
+import org.tron.core.db2.core.Chainbase;
+import org.tron.core.db2.core.ISession;
+import org.tron.core.exception.AccountResourceInsufficientException;
+import org.tron.core.exception.BadBlockException;
+import org.tron.core.exception.BadItemException;
+import org.tron.core.exception.BadNumberBlockException;
+import org.tron.core.exception.ContractExeException;
+import org.tron.core.exception.ContractValidateException;
+import org.tron.core.exception.DupTransactionException;
+import org.tron.core.exception.HeaderNotFound;
+import org.tron.core.exception.ItemNotFoundException;
+import org.tron.core.exception.NonCommonBlockException;
+import org.tron.core.exception.ReceiptCheckErrException;
+import org.tron.core.exception.TaposException;
+import org.tron.core.exception.TooBigTransactionException;
+import org.tron.core.exception.TooBigTransactionResultException;
+import org.tron.core.exception.TransactionExpirationException;
+import org.tron.core.exception.UnLinkedBlockException;
+import org.tron.core.exception.VMIllegalException;
+import org.tron.core.exception.ValidateScheduleException;
+import org.tron.core.exception.ValidateSignatureException;
+import org.tron.core.exception.ZksnarkException;
+import org.tron.core.services.interfaceOnPBFT.http.GetNowBlockOnPBFTServlet;
+import org.tron.core.services.interfaceOnPBFT.http.PBFT.HttpApiOnPBFTService;
+import org.tron.core.store.DynamicPropertiesStore;
+import org.tron.protos.Protocol.Account;
+import org.tron.protos.Protocol.Block;
+import org.tron.protos.Protocol.Transaction.Contract.ContractType;
+import org.tron.protos.contract.BalanceContract.TransferContract;
+import stest.tron.wallet.dailybuild.http.HttpTestZenToken002;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+@Slf4j
+public class PbftApiTest extends BlockGenerate {
+
+  private static Manager dbManager;
+  private static TronApplicationContext context;
+  private static String dbPath = "output_pbftAPI_test";
+
+  @Before
+  public void init() {
+    Args.setParam(new String[]{"-d", dbPath, "-w"}, Constant.TEST_CONF);
+    context = new TronApplicationContext(DefaultConfig.class);
+    dbManager = context.getBean(Manager.class);
+    setManager(dbManager);
+  }
+
+  @After
+  public void removeDb() {
+    Args.clearParam();
+    context.destroy();
+    FileUtil.deleteDir(new File(dbPath));
+  }
+
+  @Test
+  public void getNowBlock() throws IOException, InterruptedException, HeaderNotFound {
+    ChainBaseManager chainBaseManager = dbManager.getChainBaseManager();
+    DynamicPropertiesStore dynamicPropertiesStore = chainBaseManager.getDynamicPropertiesStore();
+    CommonDataBase commonDataBase = chainBaseManager.getCommonDataBase();
+
+    for (int i = 1; i <= 10; i++) {
+      try (ISession tmpSession = dbManager.getRevokingStore().buildSession()) {
+        BlockCapsule blockCapsule = createTestBlockCapsule(dbManager.getHeadBlockTimeStamp() + 3000L,
+            dbManager.getHeadBlockNum() + 1, dynamicPropertiesStore.getLatestBlockHeaderHash());
+        dynamicPropertiesStore.saveLatestBlockHeaderNumber(blockCapsule.getNum());
+        dynamicPropertiesStore.saveLatestBlockHeaderTimestamp(blockCapsule.getTimeStamp());
+        dbManager.getBlockStore().put(blockCapsule.getBlockId().getBytes(), blockCapsule);
+        tmpSession.commit();
+      }
+    }
+
+    Assert.assertTrue(dynamicPropertiesStore.getLatestBlockHeaderNumber() >= 10);
+    commonDataBase.saveLatestPbftBlockNum(6);
+    HttpApiOnPBFTService httpApiOnPBFTService = context
+        .getBean(HttpApiOnPBFTService.class);
+    httpApiOnPBFTService.start();
+    CloseableHttpResponse response = null;
+    try (CloseableHttpClient httpClient = HttpClients.createDefault()) {
+      HttpGet httpGet = new HttpGet("http://127.0.0.1:8092/walletpbft/getnowblock");
+      response = httpClient.execute(httpGet);
+      String responseString = EntityUtils.toString(response.getEntity());
+      JSONObject jsonObject = JSON.parseObject(responseString);
+      long num = jsonObject.getJSONObject("block_header").getJSONObject("raw_data").getLongValue("number");
+      Assert.assertEquals(commonDataBase.getLatestPbftBlockNum(), num);
+      response.close();
+    }
+    httpApiOnPBFTService.stop();
+  }
+
+  private BlockCapsule createTestBlockCapsule(long time, long number, Sha256Hash hash) {
+    ECKey ecKey = new ECKey(Utils.getRandom());
+    ByteString address = ByteString.copyFrom(ecKey.getAddress());
+
+    BlockCapsule blockCapsule = new BlockCapsule(number, hash, time, address);
+    blockCapsule.generatedByMyself = true;
+    blockCapsule.setMerkleRoot();
+    return blockCapsule;
+  }
+}
