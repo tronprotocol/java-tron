af9acdea6a85d9b3f2699a459eac344c74644a2e
sean-liu55 <liushaohua9017@163.com>
2020-04-27 17:41:07 +0800

    add test for pairKeyIsEqual method

--- framework/src/test/java/org/tron/common/utils/DBKeyComparatorTest.java
+++ framework/src/test/java/org/tron/common/utils/DBKeyComparatorTest.java
@@ -1,6 +1,7 @@
 package org.tron.common.utils;
 
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Test;
 import org.tron.core.capsule.utils.MarketUtils;
 
@@ -25,7 +26,29 @@ public class DBKeyComparatorTest {
         1000L,
         2001L
     );
-    System.out.println(comparator.compare(pairPriceKey1, pairPriceKey2));
-    System.exit(0);
+    Assert.assertEquals(-1,comparator.compare(pairPriceKey1, pairPriceKey2));
   }
+
+
+  @Test
+  public void pairKeyIsEqual() {
+
+    byte[] pairPriceKey1 = MarketUtils.createPairPriceKey(
+        ByteArray.fromString("100"),
+        ByteArray.fromString("200"),
+        1000L,
+        2000L
+    );
+    byte[] pairPriceKey2 = MarketUtils.createPairPriceKey(
+        ByteArray.fromString("10"),
+        ByteArray.fromString("200"),
+        1000L,
+        2001L
+    );
+
+    Assert.assertEquals(false,MarketUtils.pairKeyIsEqual(pairPriceKey1,pairPriceKey2));
+  }
+
+
+
 }
