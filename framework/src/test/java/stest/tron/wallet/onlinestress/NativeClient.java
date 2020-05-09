1aee7bc93b5b7e89692c5137a719c8c82c8734dd
pangpangfeng <7577288@qq.com>
2020-05-09 16:40:09 +0800

    Modify socket client

--- framework/src/test/java/stest/tron/wallet/onlinestress/NativeClient.java
+++ framework/src/test/java/stest/tron/wallet/onlinestress/NativeClient.java
@@ -4,7 +4,11 @@ import org.zeromq.ZMQ;
 import zmq.ZMQ.Event;
 
 public class NativeClient {
-  public static void main (String args[]) {
+
+  /**
+   * constructor.
+   */
+  public static void main(String[] args) {
     ZMQ.Context context = ZMQ.context(1);
     ZMQ.Socket req = context.socket(ZMQ.SUB);
     req.subscribe("blockTrigger");
@@ -14,14 +18,14 @@ public class NativeClient {
     req.subscribe("contractEventTrigger");
     req.subscribe("solidityLogTrigger");
     req.subscribe("solidityEventTrigger");
-    req.monitor("inproc://reqmoniter", ZMQ.EVENT_CONNECTED | ZMQ.EVENT_DISCONNECTED);  //这段代码会创建一个pair类型的socket，专门来接收当前socket发生的事件
-    final ZMQ.Socket moniter = context.socket(ZMQ.PAIR);   //这里创建一个pair类型的socket，用于与上面建立的moniter建立连接
-    moniter.connect("inproc://reqmoniter");  //连接当前socket的监听
-    new Thread(new Runnable(){
+    req.monitor("inproc://reqmoniter", ZMQ.EVENT_CONNECTED | ZMQ.EVENT_DISCONNECTED);
+    final ZMQ.Socket moniter = context.socket(ZMQ.PAIR);
+    moniter.connect("inproc://reqmoniter");
+    new Thread(new Runnable() {
       public void run() {
         // TODO Auto-generated method stub
         while (true) {
-          Event event = Event.read(moniter.base());  //从当前moniter里面读取event
+          Event event = Event.read(moniter.base());
           System.out.println(event.event +  "  " + event.addr);
         }
       }
@@ -30,8 +34,9 @@ public class NativeClient {
     req.setReceiveTimeOut(10000);
     while (true) {
       byte[] message = req.recv();
-      if (message != null)
+      if (message != null) {
         System.out.println("receive : " + new String(message));
+      }
     }
   }
 }
