e15d75c2fcb565b9801010b3ffd727156e96a127
Heng Zhang <zhangheng1536@gmail.com>
2020-05-26 18:17:30 +0800

    Implement delete reset transaction ret and fix ret to contract result

--- chainbase/src/main/java/org/tron/core/capsule/TransactionCapsule.java
+++ chainbase/src/main/java/org/tron/core/capsule/TransactionCapsule.java
@@ -726,31 +726,30 @@ public class TransactionCapsule implements ProtoCapsule<Transaction> {
   }
 
   public void setResultCode(contractResult code) {
-    Result ret = Result.newBuilder().setContractRet(code).build();
+    Result ret;
     if (this.transaction.getRetCount() > 0) {
       ret = this.transaction.getRet(0).toBuilder().setContractRet(code).build();
 
       this.transaction = transaction.toBuilder().setRet(0, ret).build();
       return;
     }
+    ret = Result.newBuilder().setContractRet(code).build();
     this.transaction = transaction.toBuilder().addRet(ret).build();
   }
 
   public void setResultWithRet(TransactionContext context) {
     contractResult code = context.getProgramResult().getResultCode();
-    Result.Builder ret = Result.newBuilder().setContractRet(code);
 
-    if (code.equals(contractResult.SUCCESS)) {
-      ret.setRet(Result.code.SUCESS);
-    } else {
-      ret.setRet(Result.code.FAILED);
-    }
+    Result ret;
     if (this.transaction.getRetCount() > 0) {
-      this.transaction = transaction.toBuilder().setRet(0, ret.build()).build();
+      ret = this.transaction.getRet(0).toBuilder().setContractRet(code).setRet(Result.code.SUCESS)
+          .build();
+
+      this.transaction = transaction.toBuilder().setRet(0, ret).build();
       return;
     }
-    this.transaction = transaction.toBuilder().addRet(ret.build()).build();
-
+    ret = Result.newBuilder().setContractRet(code).setRet(Result.code.SUCESS).build();
+    this.transaction = transaction.toBuilder().addRet(ret).build();
   }
 
   public contractResult getContractRet() {
--- framework/src/main/java/org/tron/core/net/TronNetDelegate.java
+++ framework/src/main/java/org/tron/core/net/TronNetDelegate.java
@@ -242,7 +242,6 @@ public class TronNetDelegate {
   public void pushTransaction(TransactionCapsule trx) throws P2pException {
     try {
       trx.setTime(System.currentTimeMillis());
-      trx.resetResult();
       dbManager.pushTransaction(trx);
     } catch (ContractSizeNotEqualToOneException
         | VMIllegalException e) {
