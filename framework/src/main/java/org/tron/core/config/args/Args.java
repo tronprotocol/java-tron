bfa77634dcf3baf15c269e2a5ddcfdcc5e87f34e
BentoCrypto <62317740+BentoCrypto@users.noreply.github.com>
2020-06-08 18:10:23 +0800

    Update Args.java

--- framework/src/main/java/org/tron/core/config/args/Args.java
+++ framework/src/main/java/org/tron/core/config/args/Args.java
@@ -613,7 +613,7 @@ public class Args extends CommonParameter {
     PARAMETER.minEffectiveConnection = config.hasPath(Constant.NODE_RPC_MIN_EFFECTIVE_CONNECTION)
         ? config.getInt(Constant.NODE_RPC_MIN_EFFECTIVE_CONNECTION) : 1;
 
-    PARAMETER.blockNumForEneryLimit = config.hasPath(Constant.ENERGY_LIMIT_BLOCK_NUM)
+    PARAMETER.blockNumForEnergyLimit = config.hasPath(Constant.ENERGY_LIMIT_BLOCK_NUM)
         ? config.getInt(Constant.ENERGY_LIMIT_BLOCK_NUM) : 4727890L;
 
     PARAMETER.vmTrace =
