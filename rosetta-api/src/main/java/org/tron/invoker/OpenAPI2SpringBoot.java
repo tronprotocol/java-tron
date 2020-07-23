39c6a133ec2604c9d5f0bbddec327e3786d60229
Matt <yrp1990@gmail.com>
2020-07-24 03:01:31 +0800

    sync block bootstrap

--- rosetta-api/src/main/java/org/tron/invoker/OpenAPI2SpringBoot.java
+++ rosetta-api/src/main/java/org/tron/invoker/OpenAPI2SpringBoot.java
@@ -1,19 +1,38 @@
 package org.tron.invoker;
 
 import com.fasterxml.jackson.databind.Module;
+import lombok.extern.slf4j.Slf4j;
 import org.openapitools.jackson.nullable.JsonNullableModule;
+import org.springframework.beans.factory.support.DefaultListableBeanFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.ExitCodeGenerator;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.tron.common.application.Application;
+import org.tron.common.application.ApplicationFactory;
+import org.tron.common.application.TronApplicationContext;
+import org.tron.common.parameter.CommonParameter;
+import org.tron.core.Constant;
+import org.tron.core.config.DefaultConfig;
+import org.tron.core.config.args.Args;
+import org.tron.core.services.RpcApiService;
+import org.tron.core.services.http.FullNodeHttpApiService;
+import org.tron.core.services.interfaceOnPBFT.RpcApiServiceOnPBFT;
+import org.tron.core.services.interfaceOnPBFT.http.PBFT.HttpApiOnPBFTService;
+import org.tron.core.services.interfaceOnSolidity.RpcApiServiceOnSolidity;
+import org.tron.core.services.interfaceOnSolidity.http.solidity.HttpApiOnSolidityService;
+import org.tron.program.FullNode;
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"org.tron.invoker", "org.tron.api" , "org.tron.config"})
-public class OpenAPI2SpringBoot implements CommandLineRunner {
+@ComponentScan(basePackages = {"org.tron"})
+@Slf4j(topic = "app")
+public class OpenAPI2SpringBoot extends FullNode implements CommandLineRunner {
 
     @Override
     public void run(String... arg0) throws Exception {
@@ -22,8 +41,33 @@ public class OpenAPI2SpringBoot implements CommandLineRunner {
         }
     }
 
-    public static void main(String[] args) throws Exception {
-        new SpringApplication(OpenAPI2SpringBoot.class).run(args);
+    public static void main(String[] args) {
+        logger.info("Rosetta Api running.");
+
+        Args.setParam(args, Constant.TESTNET_CONF);
+        CommonParameter parameter = Args.getInstance();
+
+        load(parameter.getLogbackPath());
+
+        if (parameter.isHelp()) {
+            logger.info("Here is the help message.");
+            return;
+        }
+
+        if (Args.getInstance().isDebug()) {
+            logger.info("in debug mode, it won't check energy time");
+        } else {
+            logger.info("not in debug mode, it will check energy time");
+        }
+
+        ConfigurableApplicationContext context = new SpringApplication(OpenAPI2SpringBoot.class).run(args);
+
+        Application appT = ApplicationFactory.create(context);
+        shutdown(appT);
+
+        appT.initServices(parameter);
+        appT.startServices();
+        appT.startup();
     }
 
     static class ExitException extends RuntimeException implements ExitCodeGenerator {
