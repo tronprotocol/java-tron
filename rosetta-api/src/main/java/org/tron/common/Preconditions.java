91402626ff827d65ba2469294da0d837a43dce01
Matt <Matt>
2020-07-27 20:53:30 +0800

    implement /construction/derive

---
+++ rosetta-api/src/main/java/org/tron/common/Preconditions.java
@@ -0,0 +1,10 @@
+package org.tron.common;
+
+import org.tron.model.NetworkIdentifier;
+
+public class Preconditions {
+
+  public boolean checkNetworkIdentifier(NetworkIdentifier networkIdentifier) {
+    return true;
+  }
+}
