aff357d95835904214000b87fafb26a4950c5cef
Matt <Matt>
2020-08-12 22:50:48 +0800

    record balance for every block

--- chainbase/src/main/java/org/tron/core/store/AccountTraceStore.java
+++ chainbase/src/main/java/org/tron/core/store/AccountTraceStore.java
@@ -53,8 +53,8 @@ public class AccountTraceStore extends TronStoreWithRevoking<AccountTraceCapsule
   }
 
   public void recordBalanceWithBlock(byte[] address, long number, long balance) {
-    Pair<Long, Long> pair = getPrevBalance(address, number);
-    logger.info("===== {} {} {} {} {}", StringUtil.encode58Check(address), pair.getLeft(), pair.getRight(), number, balance);
+//    Pair<Long, Long> pair = getPrevBalance(address, number);
+//    logger.info("===== {} {} {} {} {}", StringUtil.encode58Check(address), pair.getLeft(), pair.getRight(), number, balance);
     byte[] key = Bytes.concat(address, Longs.toByteArray(xor(number)));
     put(key, new AccountTraceCapsule(balance));
   }
