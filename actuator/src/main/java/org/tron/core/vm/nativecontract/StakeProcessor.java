888598a6c96697992ce955e9deb73f90ecac5520
xiangdepeng <xiangdepeng@outlook.com>
2020-08-13 03:07:32 +0800

    move freeze and vote into stakeProcessor, move unfreeze into unstake

--- actuator/src/main/java/org/tron/core/vm/nativecontract/StakeProcessor.java
+++ actuator/src/main/java/org/tron/core/vm/nativecontract/StakeProcessor.java
@@ -1,21 +1,27 @@
 package org.tron.core.vm.nativecontract;
 
+import com.google.common.math.LongMath;
 import com.google.protobuf.ByteString;
 import lombok.extern.slf4j.Slf4j;
+import org.tron.common.parameter.CommonParameter;
+import org.tron.common.utils.ByteArray;
 import org.tron.common.utils.DecodeUtil;
 import org.tron.common.utils.StringUtil;
 import org.tron.core.actuator.ActuatorConstant;
 import org.tron.core.capsule.AccountCapsule;
+import org.tron.core.capsule.VotesCapsule;
 import org.tron.core.exception.ContractExeException;
 import org.tron.core.exception.ContractValidateException;
-import org.tron.core.vm.nativecontract.param.SampleFreezeBalanceParam;
+import org.tron.core.store.DynamicPropertiesStore;
+import org.tron.core.store.WitnessStore;
 import org.tron.core.vm.nativecontract.param.StakeParam;
-import org.tron.core.vm.nativecontract.param.SampleVoteWitnessParam;
 import org.tron.core.vm.repository.Repository;
 import org.tron.protos.Protocol;
 import org.tron.protos.contract.Common;
 
+import static org.tron.core.config.Parameter.ChainConstant.FROZEN_PERIOD;
 import static org.tron.core.config.Parameter.ChainConstant.TRX_PRECISION;
+import static org.tron.core.vm.nativecontract.ContractProcessorConstant.*;
 
 
 @Slf4j(topic = "Processor")
@@ -47,29 +53,21 @@ public class StakeProcessor{
         long freezeBalance = stakeParam.getStakeAmount() - tronPower;
         // if need freeze balance
         if(freezeBalance > 0) {
-            SampleFreezeBalanceParam freezeBalanceParam;
-            freezeBalanceParam = new SampleFreezeBalanceParam();
-            freezeBalanceParam.setFrozenBalance(freezeBalance);
-            freezeBalanceParam.setFrozenDuration(3);
-            freezeBalanceParam.setOwnerAddress(stakeParam.getOwnerAddress());
-            freezeBalanceParam.setResource(Common.ResourceCode.BANDWIDTH);
-            if(!validateFreeze(freezeBalanceParam, repository)){
+            if(!validateFreeze(stakeParam.getOwnerAddress(), 3L, freezeBalance, Common.ResourceCode.BANDWIDTH, repository)){
                 return false;
             }
-            if(!executeFreeze(freezeBalanceParam, repository)){
+            if(!executeFreeze(stakeParam.getOwnerAddress(), 3L, freezeBalance, Common.ResourceCode.BANDWIDTH, repository)){
                 return false;
             }
         }
         long voteCount = stakeParam.getStakeAmount() / TRX_PRECISION;
-        SampleVoteWitnessParam voteWitnessParam = new SampleVoteWitnessParam();
-        voteWitnessParam.setOwnerAddress(stakeParam.getOwnerAddress());
-        voteWitnessParam.setVote(Protocol.Vote.newBuilder()
+        Protocol.Vote vote = Protocol.Vote.newBuilder()
                 .setVoteAddress(ByteString.copyFrom(stakeParam.getSrAddress()))
-                .setVoteCount(voteCount).build());
-        if(!validateVote(voteWitnessParam, repository)){
+                .setVoteCount(voteCount).build();
+        if(!validateVote(stakeParam.getOwnerAddress(), vote, repository)){
             return false;
         }
-        return executeVote(voteWitnessParam, repository);
+        return executeVote(stakeParam.getOwnerAddress(), vote, repository);
     }
 
     private boolean selfValidate(Object contract, Repository repository) throws ContractValidateException {
@@ -101,23 +99,182 @@ public class StakeProcessor{
         return true;
     }
 
-    private boolean validateFreeze(SampleFreezeBalanceParam freezeBalanceParam, Repository repository) throws ContractValidateException {
-        SampleFreezeBalanceProcessor freezeBalanceProcessor = SampleFreezeBalanceProcessor.getInstance();
-        return freezeBalanceProcessor.validate(freezeBalanceParam, repository);
+    private boolean validateFreeze(byte[] ownerAddress, long frozenDuration, long frozenBalance, Common.ResourceCode resource, Repository repository) throws ContractValidateException {
+        if (repository == null) {
+            throw new ContractValidateException(ActuatorConstant.STORE_NOT_EXIST);
+        }
+
+        DynamicPropertiesStore dynamicStore = repository.getDynamicPropertiesStore();
+
+        if (!DecodeUtil.addressValid(ownerAddress)) {
+            throw new ContractValidateException("Invalid address");
+        }
+
+        AccountCapsule accountCapsule = repository.getAccount(ownerAddress);
+        if (accountCapsule == null) {
+            String readableOwnerAddress = StringUtil.createReadableString(ownerAddress);
+            throw new ContractValidateException(
+                    "Account[" + readableOwnerAddress + "] not exists");
+        }
+
+        if (frozenBalance <= 0) {
+            throw new ContractValidateException("frozenBalance must be positive");
+        }
+        if (frozenBalance < TRX_PRECISION) {
+            throw new ContractValidateException("frozenBalance must be more than 1TRX");
+        }
+
+        int frozenCount = accountCapsule.getFrozenCount();
+        if (!(frozenCount == 0 || frozenCount == 1)) {
+            throw new ContractValidateException("frozenCount must be 0 or 1");
+        }
+        if (frozenBalance > accountCapsule.getBalance()) {
+            throw new ContractValidateException("frozenBalance must be less than accountBalance");
+        }
+
+        long minFrozenTime = dynamicStore.getMinFrozenTime();
+        long maxFrozenTime = dynamicStore.getMaxFrozenTime();
+
+        boolean needCheckFrozeTime = CommonParameter.getInstance()
+                .getCheckFrozenTime() == 1;//for test
+        if (needCheckFrozeTime && !(frozenDuration >= minFrozenTime
+                && frozenDuration <= maxFrozenTime)) {
+            throw new ContractValidateException(
+                    "frozenDuration must be less than " + maxFrozenTime + " days "
+                            + "and more than " + minFrozenTime + " days");
+        }
+
+        switch (resource) {
+            case BANDWIDTH:
+                break;
+            case ENERGY:
+                break;
+            default:
+                throw new ContractValidateException(
+                        "ResourceCode error,valid ResourceCode[BANDWIDTH、ENERGY]");
+        }
+
+        return true;
     }
 
-    private boolean validateVote(SampleVoteWitnessParam voteWitnessParam, Repository repository) throws ContractValidateException {
-        SampleVoteWitnessProcessor voteWitnessProcessor = SampleVoteWitnessProcessor.getInstance();
-        return voteWitnessProcessor.validate(voteWitnessParam, repository);
+    private boolean validateVote(byte[] ownerAddress, Protocol.Vote vote, Repository repository) throws ContractValidateException {
+        if (repository == null) {
+            throw new ContractValidateException(ContractProcessorConstant.STORE_NOT_EXIST);
+        }
+        if (!DecodeUtil.addressValid(ownerAddress)) {
+            throw new ContractValidateException("Invalid address");
+        }
+        AccountCapsule accountCapsule = repository.getAccount(ownerAddress);
+        String readableOwnerAddress = StringUtil.createReadableString(ownerAddress);
+        if (accountCapsule == null) {
+            throw new ContractValidateException(
+                    ACCOUNT_EXCEPTION_STR + readableOwnerAddress + NOT_EXIST_STR);
+        }
+
+        WitnessStore witnessStore = repository.getWitnessStore();
+        try {
+            Long sum = 0L;
+            byte[] witnessCandidate = vote.getVoteAddress().toByteArray();
+            if (!DecodeUtil.addressValid(witnessCandidate)) {
+                throw new ContractValidateException("Invalid vote address!");
+            }
+            if (vote.getVoteCount() <= 0) {
+                throw new ContractValidateException("vote count must be greater than 0");
+            }
+            String readableWitnessAddress = StringUtil.createReadableString(vote.getVoteAddress());
+            if (repository.getAccount(witnessCandidate) == null) {
+                throw new ContractValidateException(
+                        ACCOUNT_EXCEPTION_STR + readableWitnessAddress + NOT_EXIST_STR);
+            }
+            if (!witnessStore.has(witnessCandidate)) {
+                throw new ContractValidateException(
+                        WITNESS_EXCEPTION_STR + readableWitnessAddress + NOT_EXIST_STR);
+            }
+            sum = vote.getVoteCount();
+
+            long tronPower = accountCapsule.getTronPower();
+
+            // trx -> drop. The vote count is based on TRX
+            sum = LongMath
+                    .checkedMultiply(sum, TRX_PRECISION);
+            if (sum > tronPower) {
+                throw new ContractValidateException(
+                        "The total number of votes[" + sum + "] is greater than the tronPower[" + tronPower
+                                + "]");
+            }
+        } catch (ArithmeticException e) {
+            logger.debug(e.getMessage(), e);
+            throw new ContractValidateException(e.getMessage());
+        }
+
+        return true;
     }
 
-    private boolean executeFreeze(SampleFreezeBalanceParam freezeBalanceParam, Repository repository) throws ContractExeException{
-        SampleFreezeBalanceProcessor freezeBalanceProcessor = SampleFreezeBalanceProcessor.getInstance();
-        return freezeBalanceProcessor.execute(freezeBalanceParam, repository);
+    private boolean executeFreeze(byte[] ownerAddress, long frozenDuration, long frozenBalance, Common.ResourceCode resource, Repository repository) throws ContractExeException{
+        DynamicPropertiesStore dynamicStore = repository.getDynamicPropertiesStore();
+        AccountCapsule accountCapsule = repository.getAccount(ownerAddress);
+
+        long now = dynamicStore.getLatestBlockHeaderTimestamp();
+        long duration = frozenDuration * FROZEN_PERIOD;
+
+        long newBalance = accountCapsule.getBalance() - frozenBalance;
+
+        long expireTime = now + duration;
+
+        switch (resource) {
+            case BANDWIDTH:
+
+                long newFrozenBalanceForBandwidth =
+                        frozenBalance + accountCapsule.getFrozenBalance();
+                accountCapsule.setFrozenForBandwidth(newFrozenBalanceForBandwidth, expireTime);
+                repository
+                        .addTotalNetWeight(frozenBalance / TRX_PRECISION);
+                break;
+            case ENERGY:
+
+                long newFrozenBalanceForEnergy =
+                        frozenBalance + accountCapsule.getAccountResource()
+                                .getFrozenBalanceForEnergy()
+                                .getFrozenBalance();
+                accountCapsule.setFrozenForEnergy(newFrozenBalanceForEnergy, expireTime);
+                repository
+                        .addTotalEnergyWeight(frozenBalance / TRX_PRECISION);
+                break;
+            default:
+                logger.debug("Resource Code Error.");
+        }
+
+        accountCapsule.setBalance(newBalance);
+        repository.updateAccount(accountCapsule.createDbKey(), accountCapsule);
+
+        return true;
     }
 
-    public boolean executeVote(SampleVoteWitnessParam voteWitnessParam, Repository repository) throws ContractExeException {
-        SampleVoteWitnessProcessor voteWitnessProcessor = SampleVoteWitnessProcessor.getInstance();
-        return voteWitnessProcessor.execute(voteWitnessParam, repository);
+    public boolean executeVote(byte[] ownerAddress, Protocol.Vote vote, Repository repository) throws ContractExeException {
+        AccountCapsule accountCapsule = repository.getAccount(ownerAddress);
+        VotesCapsule votesCapsule;
+
+        ContractService contractService = ContractService.getInstance();
+        contractService.withdrawReward(ownerAddress, repository);
+
+        if (repository.getVotesCapsule(ownerAddress) == null) {
+            votesCapsule = new VotesCapsule(ByteString.copyFrom(ownerAddress),
+                    accountCapsule.getVotesList());
+        } else {
+            votesCapsule = repository.getVotesCapsule(ownerAddress);
+        }
+
+        accountCapsule.clearVotes();
+        votesCapsule.clearNewVotes();
+
+        logger.debug("countVoteAccount, address[{}]",
+                ByteArray.toHexString(vote.getVoteAddress().toByteArray()));
+
+        votesCapsule.addNewVotes(vote.getVoteAddress(), vote.getVoteCount());
+        accountCapsule.addVotes(vote.getVoteAddress(), vote.getVoteCount());
+
+        repository.putAccountValue(accountCapsule.createDbKey(), accountCapsule);
+        repository.updateVotesCapsule(ownerAddress, votesCapsule);
+        return true;
     }
 }
--- actuator/src/main/java/org/tron/core/vm/nativecontract/UnstakeProcessor.java
+++ actuator/src/main/java/org/tron/core/vm/nativecontract/UnstakeProcessor.java
@@ -1,14 +1,25 @@
 package org.tron.core.vm.nativecontract;
 
+import com.google.common.collect.Lists;
+import com.google.protobuf.ByteString;
 import lombok.extern.slf4j.Slf4j;
 import org.tron.common.utils.DecodeUtil;
+import org.tron.common.utils.StringUtil;
 import org.tron.core.actuator.ActuatorConstant;
+import org.tron.core.capsule.AccountCapsule;
+import org.tron.core.capsule.VotesCapsule;
 import org.tron.core.exception.ContractExeException;
 import org.tron.core.exception.ContractValidateException;
-import org.tron.core.vm.nativecontract.param.SampleUnfreezeBalanceParam;
+import org.tron.core.store.DynamicPropertiesStore;
 import org.tron.core.vm.nativecontract.param.UnstakeParam;
 import org.tron.core.vm.repository.Repository;
+import org.tron.protos.Protocol;
 import org.tron.protos.contract.Common;
+
+import java.util.Iterator;
+import java.util.List;
+
+import static org.tron.core.config.Parameter.ChainConstant.TRX_PRECISION;
 
 @Slf4j(topic = "Processor")
 public class UnstakeProcessor implements IContractProcessor {
@@ -33,12 +44,74 @@ public class UnstakeProcessor implements IContractProcessor {
     public boolean execute(Object contract, Repository repository) throws ContractExeException {
         UnstakeParam unstakeParam = (UnstakeParam)contract;
         byte[] ownerAddress = unstakeParam.getOwnerAddress();
+        Common.ResourceCode resource = Common.ResourceCode.BANDWIDTH;
 
-        SampleUnfreezeBalanceProcessor unfreezeBalanceProcessor = SampleUnfreezeBalanceProcessor.getInstance();
-        SampleUnfreezeBalanceParam unfreezeBalanceParam = new SampleUnfreezeBalanceParam();
-        unfreezeBalanceParam.setOwnerAddress(ownerAddress);
-        unfreezeBalanceParam.setResource(Common.ResourceCode.BANDWIDTH);
-        unfreezeBalanceProcessor.execute(unfreezeBalanceParam, repository);
+        DynamicPropertiesStore dynamicStore = repository.getDynamicPropertiesStore();
+
+        ContractService contractService = ContractService.getInstance();
+        contractService.withdrawReward(ownerAddress, repository);
+
+        AccountCapsule accountCapsule = repository.getAccount(ownerAddress);
+        long oldBalance = accountCapsule.getBalance();
+
+        long unfreezeBalance = 0L;
+
+        switch (resource) {
+            case BANDWIDTH:
+                List<Protocol.Account.Frozen> frozenList = Lists.newArrayList();
+                frozenList.addAll(accountCapsule.getFrozenList());
+                Iterator<Protocol.Account.Frozen> iterator = frozenList.iterator();
+                long now = dynamicStore.getLatestBlockHeaderTimestamp();
+                while (iterator.hasNext()) {
+                    Protocol.Account.Frozen next = iterator.next();
+                    if (next.getExpireTime() <= now) {
+                        unfreezeBalance += next.getFrozenBalance();
+                        iterator.remove();
+                    }
+                }
+
+                accountCapsule.setInstance(accountCapsule.getInstance().toBuilder()
+                        .setBalance(oldBalance + unfreezeBalance)
+                        .clearFrozen().addAllFrozen(frozenList).build());
+                break;
+            case ENERGY:
+                unfreezeBalance = accountCapsule.getAccountResource().getFrozenBalanceForEnergy()
+                        .getFrozenBalance();
+
+                Protocol.Account.AccountResource newAccountResource = accountCapsule.getAccountResource().toBuilder()
+                        .clearFrozenBalanceForEnergy().build();
+                accountCapsule.setInstance(accountCapsule.getInstance().toBuilder()
+                        .setBalance(oldBalance + unfreezeBalance)
+                        .setAccountResource(newAccountResource).build());
+                break;
+            default:
+                break;
+        }
+        switch (resource) {
+            case BANDWIDTH:
+                dynamicStore
+                        .addTotalNetWeight(-unfreezeBalance / TRX_PRECISION);
+                break;
+            case ENERGY:
+                dynamicStore
+                        .addTotalEnergyWeight(-unfreezeBalance / TRX_PRECISION);
+                break;
+            default:
+                break;
+        }
+
+        VotesCapsule votesCapsule = repository.getVotesCapsule(ownerAddress);
+        if (votesCapsule == null) {
+            votesCapsule = new VotesCapsule(ByteString.copyFrom(ownerAddress),
+                    accountCapsule.getVotesList());
+        }
+        accountCapsule.clearVotes();
+        votesCapsule.clearNewVotes();
+
+        repository.updateAccount(ownerAddress, accountCapsule);
+
+        repository.updateVotesCapsule(ownerAddress, votesCapsule);
+
         return true;
     }
 
@@ -62,11 +135,47 @@ public class UnstakeProcessor implements IContractProcessor {
         if (!DecodeUtil.addressValid(ownerAddress)) {
             throw new ContractValidateException("Invalid address");
         }
-        SampleUnfreezeBalanceProcessor unfreezeBalanceProcessor = SampleUnfreezeBalanceProcessor.getInstance();
-        SampleUnfreezeBalanceParam unfreezeBalanceParam = new SampleUnfreezeBalanceParam();
-        unfreezeBalanceParam.setOwnerAddress(ownerAddress);
-        unfreezeBalanceParam.setResource(Common.ResourceCode.BANDWIDTH);
-        return unfreezeBalanceProcessor.validate(unfreezeBalanceParam, repository);
+
+        DynamicPropertiesStore dynamicStore = repository.getDynamicPropertiesStore();
+        Common.ResourceCode resource = Common.ResourceCode.BANDWIDTH;
+
+        AccountCapsule accountCapsule = repository.getAccount(ownerAddress);
+        if (accountCapsule == null) {
+            String readableOwnerAddress = StringUtil.createReadableString(ownerAddress);
+            throw new ContractValidateException(
+                    "Account[" + readableOwnerAddress + "] does not exist");
+        }
+        long now = dynamicStore.getLatestBlockHeaderTimestamp();
+
+        switch (resource) {
+            case BANDWIDTH:
+                if (accountCapsule.getFrozenCount() <= 0) {
+                    throw new ContractValidateException("no frozenBalance(BANDWIDTH)");
+                }
+
+                long allowedUnfreezeCount = accountCapsule.getFrozenList().stream()
+                        .filter(frozen -> frozen.getExpireTime() <= now).count();
+                if (allowedUnfreezeCount <= 0) {
+                    throw new ContractValidateException("It's not time to unfreeze(BANDWIDTH).");
+                }
+                break;
+            case ENERGY:
+                Protocol.Account.Frozen frozenBalanceForEnergy = accountCapsule.getAccountResource()
+                        .getFrozenBalanceForEnergy();
+                if (frozenBalanceForEnergy.getFrozenBalance() <= 0) {
+                    throw new ContractValidateException("no frozenBalance(Energy)");
+                }
+                if (frozenBalanceForEnergy.getExpireTime() > now) {
+                    throw new ContractValidateException("It's not time to unfreeze(Energy).");
+                }
+
+                break;
+            default:
+                throw new ContractValidateException(
+                        "ResourceCode error.valid ResourceCode[BANDWIDTH、Energy]");
+        }
+
+        return true;
     }
 
 }
