d20d87228fa7b0483b9822b186de20bb13ef0910
AlexeyAkhunov-star <60053018+AlexeyAkhunov-star@users.noreply.github.com>
2020-09-08 14:41:37 +0800

    Update FullNode.java

--- framework/src/main/java/org/tron/program/FullNode.java
+++ framework/src/main/java/org/tron/program/FullNode.java
@@ -23,7 +23,7 @@ import org.tron.core.services.interfaceOnSolidity.http.solidity.HttpApiOnSolidit
 @Slf4j(topic = "app")
 public class FullNode {
   
-  private int dbVersion = 2;
+  public static int dbVersion = 2;
 
   public static void load(String path) {
     try {
