2ee8d04602f59c3b91d632a849226b72319bb687
Tronkai <kerwin.zhu@tron.network>
2020-09-09 16:08:14 +0800

    add stake test cases

---
+++ framework/src/test/java/stest/tron/wallet/dailybuild/tvmnewcommand/tvmStake/StakeTest001.java
@@ -0,0 +1,162 @@
+package stest.tron.wallet.dailybuild.tvmnewcommand.tvmStake;
+
+import com.google.protobuf.ByteString;
+import io.grpc.ManagedChannel;
+import io.grpc.ManagedChannelBuilder;
+import java.util.HashMap;
+import java.util.Optional;
+import org.testng.Assert;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.tron.api.GrpcAPI.TransactionExtention;
+import org.tron.api.WalletGrpc;
+import org.tron.common.crypto.ECKey;
+import org.tron.common.utils.ByteArray;
+import org.tron.common.utils.Utils;
+import org.tron.core.Wallet;
+import org.tron.protos.Protocol.Transaction;
+import org.tron.protos.Protocol.TransactionInfo;
+import stest.tron.wallet.common.client.Configuration;
+import stest.tron.wallet.common.client.Parameter;
+import stest.tron.wallet.common.client.utils.Base58;
+import stest.tron.wallet.common.client.utils.PublicMethed;
+import zmq.socket.pubsub.Pub;
+
+public class StakeTest001 {
+  private String testFoundationKey = Configuration.getByPath("testng.conf")
+      .getString("foundationAccount.key2");
+  private byte[] testFoundationAddress = PublicMethed.getFinalAddress(testFoundationKey);
+  private String testWitnessKey = Configuration.getByPath("testng.conf")
+      .getString("witness.key1");
+  private byte[] testWitnessAddress = PublicMethed.getFinalAddress(testWitnessKey);
+
+
+  private Long maxFeeLimit = Configuration.getByPath("testng.conf")
+      .getLong("defaultParameter.maxFeeLimit");
+  private String fullnode = Configuration.getByPath("testng.conf").getStringList("fullnode.ip.list")
+      .get(0);
+  private ManagedChannel channelFull = null;
+  private WalletGrpc.WalletBlockingStub blockingStubFull = null;
+
+  ECKey ecKey1 = new ECKey(Utils.getRandom());
+  byte[] testAddress001 = ecKey1.getAddress();
+  String testKey001 = ByteArray.toHexString(ecKey1.getPrivKeyBytes());
+  private byte[] contractAddress;
+
+  @BeforeSuite
+  public void beforeSuite() {
+    Wallet wallet = new Wallet();
+    Wallet.setAddressPreFixByte(Parameter.CommonConstant.ADD_PRE_FIX_BYTE_MAINNET);
+  }
+
+  /**
+   * constructor.
+   */
+
+  @BeforeClass(enabled = true)
+  public void beforeClass() {
+    PublicMethed.printAddress(testKey001);
+    channelFull = ManagedChannelBuilder.forTarget(fullnode).usePlaintext(true).build();
+    blockingStubFull = WalletGrpc.newBlockingStub(channelFull);
+
+    PublicMethed
+        .sendcoin(testAddress001, 1000_000_000L, testFoundationAddress, testFoundationKey,
+            blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    String filePath = "src/test/resources/soliditycode/testStakeSuicide.sol";
+    String contractName = "testStakeSuicide";
+    HashMap retMap = PublicMethed.getBycodeAbi(filePath, contractName);
+    String code = retMap.get("byteCode").toString();
+    String abi = retMap.get("abI").toString();
+    contractAddress = PublicMethed
+        .deployContract(contractName, abi, code, "", maxFeeLimit, 0L, 100, null, testKey001,
+            testAddress001, blockingStubFull);
+
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+  }
+
+  @Test(enabled = true, description = "")
+  void tvmStakeTest001() {
+    String methodStr = "Stake(address,uint256)";
+    String argsStr = "\"" + Base58.encode58Check(testWitnessAddress) + "\","  + 1000000 ;
+    String txid  = PublicMethed
+        .triggerContract(contractAddress, methodStr, argsStr,
+            false, 0, maxFeeLimit,
+            testAddress001, testKey001, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    Optional<TransactionInfo> info =  PublicMethed.getTransactionInfoById(txid,blockingStubFull);
+    int contractResult = ByteArray.toInt(info.get().getContractResult(0).toByteArray());
+
+    Assert.assertEquals(contractResult,1);
+
+  }
+
+  @Test(enabled = true, description = "no witness ")
+  void tvmStakeTest002(){
+    //account address
+    String methodStr = "Stake(address,uint256)";
+    String argsStr = "\"" + Base58.encode58Check(testAddress001) + "\","  + 1000000 ;
+    String txid  = PublicMethed
+        .triggerContract(contractAddress, methodStr, argsStr,
+            false, 0, maxFeeLimit,
+            testAddress001, testKey001, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    Optional<TransactionInfo> info =  PublicMethed.getTransactionInfoById(txid,blockingStubFull);
+    int contractResult = ByteArray.toInt(info.get().getContractResult(0).toByteArray());
+    Assert.assertEquals(contractResult,0);
+
+    //contract address
+    methodStr = "Stake(address,uint256)";
+    argsStr = "\"" + Base58.encode58Check(contractAddress) + "\","  + 1000000 ;
+    txid  = PublicMethed
+        .triggerContract(contractAddress, methodStr, argsStr,
+            false, 0, maxFeeLimit,
+            testAddress001, testKey001, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    info =  PublicMethed.getTransactionInfoById(txid,blockingStubFull);
+    contractResult = ByteArray.toInt(info.get().getContractResult(0).toByteArray());
+    Assert.assertEquals(contractResult,0);
+
+
+  }
+
+
+  @Test(enabled = true, description = ">balance")
+  void tvmStakeTest003() {
+    String methodStr = "Stake(address,uint256)";
+    String argsStr = "\"" + Base58.encode58Check(testWitnessAddress) + "\","  + Long.MAX_VALUE ;
+    String txid  = PublicMethed
+        .triggerContract(contractAddress, methodStr, argsStr,
+            false, 0, maxFeeLimit,
+            testAddress001, testKey001, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    Optional<TransactionInfo> info =  PublicMethed.getTransactionInfoById(txid,blockingStubFull);
+    int contractResult = ByteArray.toInt(info.get().getContractResult(0).toByteArray());
+
+    Assert.assertEquals(contractResult,0);
+
+  }
+
+  @Test(enabled = true, description = "other address frozenBalance")
+  void tvmStakeTest004() {
+
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+
+    String methodStr = "Stake(address,uint256)";
+    String argsStr = "\"" + Base58.encode58Check(testWitnessAddress) + "\","  + 1000000 ;
+    String txid  = PublicMethed
+        .triggerContract(contractAddress, methodStr, argsStr,
+            false, 0, maxFeeLimit,
+            testAddress001, testKey001, blockingStubFull);
+    PublicMethed.waitProduceNextBlock(blockingStubFull);
+    Optional<TransactionInfo> info =  PublicMethed.getTransactionInfoById(txid,blockingStubFull);
+    int contractResult = ByteArray.toInt(info.get().getContractResult(0).toByteArray());
+
+    Assert.assertEquals(contractResult,1);
+
+  }
+
+
+
+}
