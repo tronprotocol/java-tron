b2df97d9759cc79599da04ea87a75e214b8e720d
Matt <Matt>
2020-09-27 17:09:04 +0800

    fix db

--- chainbase/src/main/java/org/tron/core/store/AccountTraceStore.java
+++ chainbase/src/main/java/org/tron/core/store/AccountTraceStore.java
@@ -54,7 +54,7 @@ public class AccountTraceStore extends TronStoreWithRevoking<AccountTraceCapsule
 
   public void recordBalanceWithBlock(byte[] address, long number, long balance) {
 //    Pair<Long, Long> pair = getPrevBalance(address, number);
-    logger.info("recordBalanceWithBlock===== address:{} number:{} balance:{}", StringUtil.encode58Check(address), number, balance);
+//    logger.info("recordBalanceWithBlock===== address:{} number:{} balance:{}", StringUtil.encode58Check(address), number, balance);
     byte[] key = Bytes.concat(address, Longs.toByteArray(xor(number)));
     put(key, new AccountTraceCapsule(balance));
   }
