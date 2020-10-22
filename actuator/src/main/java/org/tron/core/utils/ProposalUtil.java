bedd983b47d863201a1c3d050524600388b6297e
xiangdepeng <xiangdepeng@outlook.com>
2020-10-22 15:34:58 +0800

    delete ALLOW_TVM_STAKE proposal

--- actuator/src/main/java/org/tron/core/utils/ProposalUtil.java
+++ actuator/src/main/java/org/tron/core/utils/ProposalUtil.java
@@ -339,17 +339,17 @@ public class ProposalUtil {
         }
         break;
       }
-      case ALLOW_TVM_STAKE: {
-          if (!forkController.pass(ForkBlockVersionEnum.VERSION_4_1)) {
-          throw new ContractValidateException(
-              "Bad chain parameter id [ALLOW_TVM_STAKE]");
-        }
-        if (value != 1 && value != 0) {
-          throw new ContractValidateException(
-              "This value[ALLOW_TVM_STAKE] is only allowed to be 1 or 0");
-        }
-        break;
-      }
+//      case ALLOW_TVM_STAKE: {
+//          if (!forkController.pass(ForkBlockVersionEnum.VERSION_4_1)) {
+//          throw new ContractValidateException(
+//              "Bad chain parameter id [ALLOW_TVM_STAKE]");
+//        }
+//        if (value != 1 && value != 0) {
+//          throw new ContractValidateException(
+//              "This value[ALLOW_TVM_STAKE] is only allowed to be 1 or 0");
+//        }
+//        break;
+//      }
       case ALLOW_TVM_ASSET_ISSUE: {
         if (!forkController.pass(ForkBlockVersionEnum.VERSION_4_1)) {
           throw new ContractValidateException(
@@ -446,7 +446,7 @@ public class ProposalUtil {
     ALLOW_PBFT(40),// 1,40
     ALLOW_TVM_ISTANBUL(41),//1, {0,1}
     ALLOW_TVM_ASSET_ISSUE(42), // 0, 1
-    ALLOW_TVM_STAKE(43), // 0, 1
+    // ALLOW_TVM_STAKE(43), // 0, 1
     ALLOW_MARKET_TRANSACTION(44), // {0, 1}
     MARKET_SELL_FEE(45), // 0 [0,10_000_000_000]
     MARKET_CANCEL_FEE(46); // 0 [0,10_000_000_000]
