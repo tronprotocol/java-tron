2530646a30b76e2f4374019d8505bbdeeca18a33
ray.wu <wgq.quan@gmail.com>
2020-11-02 08:39:00 +0800

    update a new version. version name:GreatVoyage-v4.0.1-1-g6cd6ed5fb,version code:13213

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -2,9 +2,9 @@ package org.tron.program;
 
 public class Version {
 
-  public static final String versionName = "GreatVoyage-v4.0.0-8-g67170b635";
-  public static final String versionCode = "13210";
-  private static final String version = "4.0.1";
+  public static final String versionName = "GreatVoyage-v4.0.1-1-g6cd6ed5fb";
+  public static final String versionCode = "13213";
+  private static final String version = "4.1.0";
 
   public static String getVersion() {
     return version;
