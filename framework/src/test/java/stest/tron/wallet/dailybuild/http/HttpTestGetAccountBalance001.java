acadc19b4b3a25cf623e8ae7458804b15995e0f9
pangpangfeng <7577288@qq.com>
2020-12-09 15:01:58 +0800

    Add get account balance test case

---
+++ framework/src/test/java/stest/tron/wallet/dailybuild/http/HttpTestGetAccountBalance001.java
@@ -0,0 +1,103 @@
+package stest.tron.wallet.dailybuild.http;
+
+import com.alibaba.fastjson.JSONObject;
+import java.util.HashMap;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.http.HttpResponse;
+import org.junit.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+import org.tron.common.crypto.ECKey;
+import org.tron.common.utils.ByteArray;
+import org.tron.common.utils.Utils;
+import stest.tron.wallet.common.client.Configuration;
+import stest.tron.wallet.common.client.utils.HttpMethed;
+import stest.tron.wallet.common.client.utils.PublicMethed;
+
+@Slf4j
+public class HttpTestGetAccountBalance001 {
+
+  private final String testKey002 = Configuration.getByPath("testng.conf")
+      .getString("foundationAccount.key1");
+  private final byte[] fromAddress = PublicMethed.getFinalAddress(testKey002);
+  private JSONObject responseContent;
+  private HttpResponse response;
+  private String httpnode = Configuration.getByPath("testng.conf").getStringList("httpnode.ip.list")
+      .get(0);
+  private String httpSoliditynode = Configuration.getByPath("testng.conf")
+      .getStringList("httpnode.ip.list").get(2);
+  private String httpPbftNode = Configuration.getByPath("testng.conf")
+      .getStringList("httpnode.ip.list").get(4);
+  ECKey ecKey2 = new ECKey(Utils.getRandom());
+  byte[] assetOwnerAddress = ecKey2.getAddress();
+  String assetOwnerKey = ByteArray.toHexString(ecKey2.getPrivKeyBytes());
+  Long amount = 2048000000L;
+  String contractAddress;
+  String txid;
+
+  /**
+   * constructor.
+   */
+  @BeforeClass(enabled = true, description = "Deploy smart contract by http")
+  public void test01DeployContract() {
+    PublicMethed.printAddress(assetOwnerKey);
+    HttpMethed.waitToProduceOneBlock(httpnode);
+    response = HttpMethed.sendCoin(httpnode, fromAddress, assetOwnerAddress, amount, testKey002);
+    Assert.assertTrue(HttpMethed.verificationResult(response));
+    HttpMethed.waitToProduceOneBlock(httpnode);
+
+    response = HttpMethed.getAccount(httpnode, assetOwnerAddress);
+    responseContent = HttpMethed.parseResponseContent(response);
+    HttpMethed.printJsonContent(responseContent);
+
+    String filePath = "./src/test/resources/soliditycode//contractScenario004.sol";
+    String contractName = "TronToken";
+    HashMap retMap = PublicMethed.getBycodeAbi(filePath, contractName);
+
+    String code = retMap.get("byteCode").toString();
+    String abi = retMap.get("abI").toString();
+
+    txid = HttpMethed
+        .deployContractGetTxid(httpnode, contractName, abi, code, 1000000L, 1000000000L, 100,
+            11111111111111L, 0L, 0, 0L, assetOwnerAddress, assetOwnerKey);
+
+    HttpMethed.waitToProduceOneBlock(httpnode);
+    logger.info(txid);
+    response = HttpMethed.getTransactionById(httpnode, txid);
+    responseContent = HttpMethed.parseResponseContent(response);
+    HttpMethed.printJsonContent(responseContent);
+    Assert.assertTrue(!responseContent.getString("contract_address").isEmpty());
+    contractAddress = responseContent.getString("contract_address");
+
+    response = HttpMethed.getTransactionInfoById(httpnode, txid);
+    responseContent = HttpMethed.parseResponseContent(response);
+    String receiptString = responseContent.getString("receipt");
+    Assert
+        .assertEquals(HttpMethed.parseStringContent(receiptString).getString("result"), "SUCCESS");
+  }
+
+
+
+  /**
+   * constructor.
+   */
+  @Test(enabled = true, description = "Get account balance by http")
+  public void getAccountBalance() {
+    response = HttpMethed.getAccountBalance(httpnode, assetOwnerAddress,1L,"aaa");
+    logger.info("code is " + response.getStatusLine().getStatusCode());
+    Assert.assertEquals(response.getStatusLine().getStatusCode(), 200);
+    responseContent = HttpMethed.parseResponseContent(response);
+    HttpMethed.printJsonContent(responseContent);
+    Assert.assertTrue(responseContent.size() > 3);
+  }
+
+  /**
+   * constructor.
+   */
+
+  @AfterClass
+  public void shutdown() throws InterruptedException {
+    HttpMethed.disConnect();
+  }
+}
