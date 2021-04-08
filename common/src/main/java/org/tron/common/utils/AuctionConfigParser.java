1b0688871baf1dddbf2afa3e09845087f9b43262
xq-lu <583591aa!!>
2021-04-08 15:13:12 +0800

    code  format

--- common/src/main/java/org/tron/common/utils/AuctionConfigParser.java
+++ common/src/main/java/org/tron/common/utils/AuctionConfigParser.java
@@ -3,51 +3,52 @@ package org.tron.common.utils;
 import org.tron.protos.contract.CrossChain;
 
 public class AuctionConfigParser {
-    private static Integer ROUND_LENGH = 2;//round
+	private static Integer ROUND_LENGH = 2;//round
 
-    private static Integer SLOT_COUNT_LENGH = 2;//slot count for every round
+	private static Integer SLOT_COUNT_LENGH = 2;//slot count for every round
 
-    private static Integer END_TIME_LENGH = 10;//auction end time
+	private static Integer END_TIME_LENGH = 10;//auction end time
 
-    private static Integer DURATION_LENGH = 3;//slot duration
+	private static Integer DURATION_LENGH = 3;//slot duration
 
 
-    public static CrossChain.AuctionRoundContract parseAuctionConfig(Long auctionConfig) {
-        try {
-            String auctionConfigStr = String.valueOf(auctionConfig);
-            int configLength = auctionConfigStr.length();
-            if (!(configLength == 16 || configLength == 17)) {
-                throw new RuntimeException("invalid auction config");
-            }
-            //  eg:01 08 1617162101 023 or 11 08 1617162101 023
-            Integer duration = Integer.valueOf(auctionConfigStr.substring(configLength - 3, configLength));
-            Long endTime = Long.valueOf(auctionConfigStr.substring(configLength - 13, configLength - 3));
-            Integer slotCount = Integer.valueOf(auctionConfigStr.substring(configLength - 15, configLength - 13));
-            Integer round = Integer.valueOf(auctionConfigStr.substring(0, configLength - 15));
-            CrossChain.AuctionRoundContract.Builder builder = CrossChain.AuctionRoundContract.newBuilder()
-                    .setDuration(duration)
-                    .setRound(round)
-                    .setEndTime(endTime)
-                    .setSlotCount(slotCount);
-            return builder.build();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-    }
+	public static CrossChain.AuctionRoundContract parseAuctionConfig(Long auctionConfig) {
+		try {
+			String auctionConfigStr = String.valueOf(auctionConfig);
+			int configLength = auctionConfigStr.length();
+			if (!(configLength == 16 || configLength == 17)) {
+				throw new RuntimeException("invalid auction config");
+			}
+			//  eg:01 08 1617162101 023 or 11 08 1617162101 023
+			Integer duration = Integer.valueOf(auctionConfigStr.substring(configLength - 3, configLength));
+			Long endTime = Long.valueOf(auctionConfigStr.substring(configLength - 13, configLength - 3));
+			Integer slotCount = Integer.valueOf(auctionConfigStr.substring(configLength - 15, configLength - 13));
+			Integer round = Integer.valueOf(auctionConfigStr.substring(0, configLength - 15));
+			CrossChain.AuctionRoundContract.Builder builder = CrossChain.AuctionRoundContract.newBuilder()
+					.setDuration(duration)
+					.setRound(round)
+					.setEndTime(endTime)
+					.setSlotCount(slotCount);
+			return builder.build();
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
 
-    public static Long getAuctionEndTime(long value) {
-        return parseAuctionConfig(value).getEndTime();
-    }
+	public static Long getAuctionEndTime(long value) {
+		return parseAuctionConfig(value).getEndTime();
+	}
 
-    public static Integer getAuctionRound(long value) {
-        return parseAuctionConfig(value).getRound();
-    }
+	public static Integer getAuctionRound(long value) {
+		return parseAuctionConfig(value).getRound();
+	}
 
-    public static Long getAuctionDuration(long value) {
-        return parseAuctionConfig(value).getDuration();
-    }
-    public static Integer getSlotCount(long value) {
-        return parseAuctionConfig(value).getSlotCount();
-    }
+	public static Long getAuctionDuration(long value) {
+		return parseAuctionConfig(value).getDuration();
+	}
+
+	public static Integer getSlotCount(long value) {
+		return parseAuctionConfig(value).getSlotCount();
+	}
 }
