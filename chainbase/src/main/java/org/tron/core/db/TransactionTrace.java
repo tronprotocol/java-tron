68777c977c51989a1660accf2c714460f8b5e0ff
Asuka <yanghang8612@163.com>
2021-04-23 14:15:15 +0800

    style: remove unused field and method

--- chainbase/src/main/java/org/tron/core/db/TransactionTrace.java
+++ chainbase/src/main/java/org/tron/core/db/TransactionTrace.java
@@ -64,10 +64,6 @@ public class TransactionTrace {
 
   private ForkController forkController;
 
-  private VotesStore votesStore;
-
-  private DelegationStore delegationStore;
-
   @Getter
   private TransactionContext transactionContext;
   @Getter
@@ -103,9 +99,6 @@ public class TransactionTrace {
     this.runtime = runtime;
     this.forkController = new ForkController();
     forkController.init(storeFactory.getChainBaseManager());
-
-    this.votesStore = storeFactory.getChainBaseManager().getVotesStore();
-    this.delegationStore = storeFactory.getChainBaseManager().getDelegationStore();
   }
 
   public TransactionCapsule getTrx() {
@@ -319,13 +312,6 @@ public class TransactionTrace {
     }
     return address;
   }
-
-  public void deleteDelegationByAddress(byte[] address){
-    delegationStore.delete(address); //begin Cycle
-    delegationStore.delete(("lastWithdraw-" + Hex.toHexString(address)).getBytes()); //last Withdraw cycle
-    delegationStore.delete(("end-" + Hex.toHexString(address)).getBytes()); //end cycle
-  }
-
 
   public enum TimeResultType {
     NORMAL,
