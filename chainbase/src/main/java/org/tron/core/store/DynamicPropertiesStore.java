2189bdd13721c82b354fd63edaac463551ba2512
liukai <forfreeday@163.com>
2021-04-26 16:30:04 +0800

    add account asset optimization proposal

--- chainbase/src/main/java/org/tron/core/store/DynamicPropertiesStore.java
+++ chainbase/src/main/java/org/tron/core/store/DynamicPropertiesStore.java
@@ -765,7 +765,6 @@ public class DynamicPropertiesStore extends TronStoreWithRevoking<BytesCapsule> 
 
     try {
       this.getAllowAccountAssetOptimization();
-      this.setAllowAccountAssetOptimization(1);
     } catch (IllegalArgumentException e) {
       this.setAllowAccountAssetOptimization(0L);
     }
