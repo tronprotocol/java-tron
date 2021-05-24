0a3bff964a82c0ef9d18041c43e1b02ffa1cd9c0
jiangyuanshu <317787106@qq.com>
2021-05-24 15:59:43 +0800

    add jsonrpc

---
+++ framework/src/main/java/org/tron/core/services/jsonrpc/JsonRpcServer.java
@@ -0,0 +1,27 @@
+package org.tron.core.services.jsonrpc;
+
+import org.tron.common.application.Service;
+import org.tron.common.parameter.CommonParameter;
+
+public class JsonRpcServer implements Service {
+
+
+  @Override
+  public void init() {
+
+  }
+
+  @Override
+  public void init(CommonParameter parameter) {
+
+  }
+
+  @Override
+  public void start() {
+
+  }
+
+  @Override
+  public void stop() {
+  }
+}
