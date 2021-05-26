2cccc0d1f7f654285afc713ed6955874c5167661
Wenhua Zhang <zhangwenhua@tron.network>
2021-05-26 15:24:52 +0800

    feat: add web3_sha3 and web3_clientVersion

--- framework/src/main/java/org/tron/core/services/jsonrpc/TestService.java
+++ framework/src/main/java/org/tron/core/services/jsonrpc/TestService.java
@@ -2,4 +2,7 @@ package org.tron.core.services.jsonrpc;
 
 public interface TestService {
   int getInt(int code);
+
+  String web3_clientVersion();
+  String web3_sha3(String data) throws Exception;
 }
--- framework/src/main/java/org/tron/core/services/jsonrpc/TestServiceImpl.java
+++ framework/src/main/java/org/tron/core/services/jsonrpc/TestServiceImpl.java
@@ -1,7 +1,33 @@
 package org.tron.core.services.jsonrpc;
 
+import java.util.Arrays;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import org.tron.common.crypto.Hash;
+import org.tron.common.utils.ByteArray;
+import org.tron.program.Version;
+
 public class TestServiceImpl implements TestService {
   public int getInt(int code) {
     return code;
+  }
+
+  public String web3_clientVersion() {
+    Pattern shortVersion = Pattern.compile("(\\d\\.\\d).*");
+    Matcher matcher = shortVersion.matcher(System.getProperty("java.version"));
+    matcher.matches();
+
+    return Arrays.asList(
+        "TRON", "v" + Version.getVersion(),
+        System.getProperty("os.name"),
+        "Java" + matcher.group(1),
+        Version.VERSION_NAME).stream()
+        .collect(Collectors.joining("/"));
+  }
+
+  public String web3_sha3(String data) {
+    byte[] result = Hash.sha3(ByteArray.fromHexString(data));
+    return ByteArray.toJsonHex(result);
   }
 }
