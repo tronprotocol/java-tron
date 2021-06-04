4c780561f429d4bba5042d6230e79644ddf99daa
Asuka <yanghang8612@163.com>
2021-06-04 15:06:09 +0800

    bug: fix abi not stored

--- actuator/src/main/java/org/tron/core/vm/repository/RepositoryImpl.java
+++ actuator/src/main/java/org/tron/core/vm/repository/RepositoryImpl.java
@@ -788,7 +788,7 @@ public class RepositoryImpl implements Repository {
           deposit.putContract(key, value);
         } else {
           ContractCapsule contractCapsule = value.getContract();
-          if (value.getType().isCreate()) {
+          if (!abiStore.has(key.getData())) {
             abiStore.put(key.getData(), new AbiCapsule(contractCapsule));
           }
           getContractStore().put(key.getData(), contractCapsule);
@@ -952,4 +952,5 @@ public class RepositoryImpl implements Repository {
         .orElseThrow(
             () -> new IllegalArgumentException("not found TOTAL_ENERGY_WEIGHT"));
   }
+
 }
