7983a3a7eef43b75482cad665670605fb28f0c8c
halibobo1205 <halibobo1205@gmail.com>
2021-06-10 18:28:04 +0800

    corn shutdown

--- framework/src/main/java/org/tron/core/db/Manager.java
+++ framework/src/main/java/org/tron/core/db/Manager.java
@@ -19,6 +19,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Date;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
@@ -217,6 +218,8 @@ public class Manager {
   // the capacity is equal to Integer.MAX_VALUE default
   private BlockingQueue<TransactionCapsule> rePushTransactions;
   private BlockingQueue<TriggerCapsule> triggerCapsuleQueue;
+
+  private volatile long latestSolidityNumShutDown = -1;
 
   /**
    * Cycle thread to rePush Transactions
@@ -808,9 +811,17 @@ public class Manager {
     updateFork(block);
     if (System.currentTimeMillis() - block.getTimeStamp() >= 60_000) {
       revokingStore.setMaxFlushCount(SnapshotManager.DEFAULT_MAX_FLUSH_COUNT);
+      if (Args.getCronExpression() != null) {
+        if (Args.getCronExpression().getNextValidTimeAfter(
+            new Date(block.getTimeStamp() - SnapshotManager.DEFAULT_MAX_FLUSH_COUNT * 1000 * 3))
+                .compareTo(new Date(block.getTimeStamp())) <= 0) {
+          revokingStore.setMaxFlushCount(SnapshotManager.DEFAULT_MIN_FLUSH_COUNT);
+        }
+      }
     } else {
       revokingStore.setMaxFlushCount(SnapshotManager.DEFAULT_MIN_FLUSH_COUNT);
     }
+
   }
 
   private void switchFork(BlockCapsule newHead)
@@ -929,6 +940,16 @@ public class Manager {
       BadNumberBlockException, BadBlockException, NonCommonBlockException,
       ReceiptCheckErrException, VMIllegalException, ZksnarkException {
     long start = System.currentTimeMillis();
+
+    if (latestSolidityNumShutDown == -1) {
+      if (Args.getCronExpression() != null) {
+        if (Args.getCronExpression().isSatisfiedBy(new Date(block.getTimeStamp()))) {
+          latestSolidityNumShutDown = block.getNum();
+        }
+      } else {
+        latestSolidityNumShutDown = -2;
+      }
+    }
     try (PendingManager pm = new PendingManager(this)) {
 
       if (!block.generatedByMyself) {
@@ -1063,6 +1084,16 @@ public class Manager {
         block.getNum(),
         System.currentTimeMillis() - start,
         block.getTransactions().size());
+
+    if (latestSolidityNumShutDown > 0
+        && latestSolidityNumShutDown == getDynamicPropertiesStore().getLatestSolidifiedBlockNum()) {
+
+      logger.info("begin shutdown, currentBlockNum:{}, solidifiedBlockNum:{}",
+          block.getNum(),
+          getDynamicPropertiesStore().getLatestSolidifiedBlockNum());
+      System.exit(0);
+    }
+
   }
 
   public void updateDynamicProperties(BlockCapsule block) {
@@ -1197,6 +1228,7 @@ public class Manager {
     }
     //set the sort order
     trxCap.setOrder(transactionInfo.getFee());
+    trxCap.setTrxTrace(null);
     return transactionInfo.getInstance();
   }
 
