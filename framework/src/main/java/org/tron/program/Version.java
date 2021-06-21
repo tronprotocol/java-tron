1f3301903c118f8a9617ff673edb8edbabe08468
zhang0125 <jell7@163.com>
2021-06-22 00:36:06 +0800

    update a new version. version name:GreatVoyage-v4.2.1-62-g0ba5c5c77,version code:15584

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -2,8 +2,8 @@ package org.tron.program;
 
 public class Version {
 
-  public static final String VERSION_NAME = "GreatVoyage-v4.2.1-56-gaff237ef7";
-  public static final String VERSION_CODE = "15578";
+  public static final String VERSION_NAME = "GreatVoyage-v4.2.1-62-g0ba5c5c77";
+  public static final String VERSION_CODE = "15584";
   private static final String VERSION = "4.2.2";
 
   public static String getVersion() {
