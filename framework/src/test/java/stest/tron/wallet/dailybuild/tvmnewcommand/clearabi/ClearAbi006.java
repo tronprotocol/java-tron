19f1bc1b4e4876776440ee94dea5fcc1d0b6877b
wangqqqqq <wangqianqian@tron.network>
2021-06-23 17:24:00 +0800

    Add release_4.2.2 test case

--- framework/src/test/java/stest/tron/wallet/dailybuild/tvmnewcommand/clearabi/ClearAbi006.java
+++ framework/src/test/java/stest/tron/wallet/dailybuild/tvmnewcommand/clearabi/ClearAbi006.java
@@ -78,7 +78,7 @@ public class ClearAbi006 {
     blockingStubSolidity = WalletSolidityGrpc.newBlockingStub(channelSolidity);
   }
 
-  @Test(enabled = false, description = "Clear a uninitialized account")
+  @Test(enabled = true, description = "Clear a uninitialized account")
   public void testClearAbi() {
     Assert.assertTrue(PublicMethed
         .sendcoin(contractExcAddress, 10000000000L, testNetAccountAddress, testNetAccountKey,
