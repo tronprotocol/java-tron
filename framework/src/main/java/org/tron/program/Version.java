a537578c8e9b47d57be0d7c985f0c2c1ff82ac3d
liukai <forfreeday@163.com>
2021-06-25 12:09:41 +0800

    modify version modify processTransaction

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
   public static final String VERSION_NAME = "GreatVoyage-v4.2.1-62-g0ba5c5c77";
   public static final String VERSION_CODE = "15584";
-  private static final String VERSION = "4.2.2";
+  private static final String VERSION = "4.2.2.1";
 
   public static String getVersion() {
     return VERSION;
