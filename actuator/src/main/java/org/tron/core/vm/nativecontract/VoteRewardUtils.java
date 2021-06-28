f4e21fafb00ba1b2389aebb34f6962ecd1fb3ba2
Asuka <yanghang8612@163.com>
2021-06-28 10:31:07 +0800

    refactor: rename ContractService

--- actuator/src/main/java/org/tron/core/vm/nativecontract/VoteRewardUtils.java
+++ actuator/src/main/java/org/tron/core/vm/nativecontract/VoteRewardUtils.java
@@ -8,29 +8,13 @@ import org.tron.core.store.DelegationStore;
 import org.tron.core.vm.repository.Repository;
 import org.tron.protos.Protocol;
 
-@Slf4j(topic = "contractService")
-public class ContractService {
-  private ContractService() {
+@Slf4j(topic = "VoteRewardUtils")
+public class VoteRewardUtils {
+
+  private VoteRewardUtils() {
   }
 
-  public static ContractService getInstance() {
-    return ContractService.Singleton.INSTANCE.getInstance();
-  }
-
-  private enum Singleton {
-    INSTANCE;
-    private ContractService instance;
-
-    Singleton() {
-      instance = new ContractService();
-    }
-
-    public ContractService getInstance() {
-      return instance;
-    }
-  }
-
-  public void withdrawReward(byte[] address, Repository repository) {
+  public static void withdrawReward(byte[] address, Repository repository) {
     if (!repository.getDynamicPropertiesStore().allowChangeDelegation()) {
       return;
     }
@@ -48,20 +32,17 @@ public class ContractService {
         return;
       }
     }
-    //withdraw the latest cycle reward
     if (beginCycle + 1 == endCycle && beginCycle < currentCycle) {
       AccountCapsule account = repository.getAccountVote(beginCycle, address);
       if (account != null) {
         reward = computeReward(beginCycle, account, repository);
         adjustAllowance(address, reward, repository);
         reward = 0;
-        logger.info("latest cycle reward {},{}", beginCycle, account.getVotesList());
       }
       beginCycle += 1;
     }
     endCycle = currentCycle;
     if (CollectionUtils.isEmpty(accountCapsule.getVotesList())) {
-      repository.updateRemark(address, endCycle);
       repository.updateBeginCycle(address, endCycle + 1);
       return;
     }
@@ -70,48 +51,13 @@ public class ContractService {
         reward += computeReward(cycle, accountCapsule, repository);
       }
       adjustAllowance(address, reward, repository);
-
     }
     repository.updateBeginCycle(address, endCycle);
     repository.updateEndCycle(address, endCycle + 1);
     repository.updateAccountVote(address, endCycle, accountCapsule);
-    logger.info("adjust {} allowance {}, now currentCycle {}, beginCycle {}, endCycle {}, "
-            + "account vote {},", Hex.toHexString(address), reward, currentCycle,
-        beginCycle, endCycle, accountCapsule.getVotesList());
   }
 
-  private long computeReward(long cycle, AccountCapsule accountCapsule, Repository repository) {
-    long reward = 0;
-
-    for (Protocol.Vote vote : accountCapsule.getVotesList()) {
-      byte[] srAddress = vote.getVoteAddress().toByteArray();
-      long totalReward = repository.getDelegationStore().getReward(cycle,
-          srAddress);
-      long totalVote = repository.getDelegationStore().getWitnessVote(cycle, srAddress);
-      if (totalVote == DelegationStore.REMARK || totalVote == 0) {
-        continue;
-      }
-      long userVote = vote.getVoteCount();
-      double voteRate = (double) userVote / totalVote;
-      reward += voteRate * totalReward;
-      logger.debug("computeReward {} {} {} {},{},{},{}", cycle,
-          Hex.toHexString(accountCapsule.getAddress().toByteArray()), Hex.toHexString(srAddress),
-          userVote, totalVote, totalReward, reward);
-    }
-    return reward;
-  }
-
-  public void adjustAllowance(byte[] address, long amount, Repository repository) {
-    if (amount <= 0) {
-      return;
-    }
-    AccountCapsule accountCapsule = repository.getAccount(address);
-    long allowance = accountCapsule.getAllowance();
-    accountCapsule.setAllowance(allowance + amount);
-    repository.putAccountValue(accountCapsule.createDbKey(), accountCapsule);
-  }
-
-  public long queryReward(byte[] address, Repository repository) {
+  public static long queryReward(byte[] address, Repository repository) {
     if (!repository.getDynamicPropertiesStore().allowChangeDelegation()) {
       return 0;
     }
@@ -144,5 +90,32 @@ public class ContractService {
       }
     }
     return reward + accountCapsule.getAllowance();
+  }
+
+  private static long computeReward(long cycle, AccountCapsule accountCapsule, Repository repository) {
+    long reward = 0;
+
+    for (Protocol.Vote vote : accountCapsule.getVotesList()) {
+      byte[] srAddress = vote.getVoteAddress().toByteArray();
+      long totalReward = repository.getDelegationStore().getReward(cycle, srAddress);
+      long totalVote = repository.getDelegationStore().getWitnessVote(cycle, srAddress);
+      if (totalVote == DelegationStore.REMARK || totalVote == 0) {
+        continue;
+      }
+      long userVote = vote.getVoteCount();
+      double voteRate = (double) userVote / totalVote;
+      reward += voteRate * totalReward;
+    }
+    return reward;
+  }
+
+  private static void adjustAllowance(byte[] address, long amount, Repository repository) {
+    if (amount <= 0) {
+      return;
+    }
+    AccountCapsule accountCapsule = repository.getAccount(address);
+    long allowance = accountCapsule.getAllowance();
+    accountCapsule.setAllowance(allowance + amount);
+    repository.putAccountValue(accountCapsule.createDbKey(), accountCapsule);
   }
 }
