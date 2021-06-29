317bdc3b173f78a18e65dabf564643169a6baff5
Asuka <yanghang8612@163.com>
2021-06-29 15:06:32 +0800

    feature: use new reward algorithm in VM reward compute utils

--- actuator/src/main/java/org/tron/core/vm/nativecontract/VoteRewardUtils.java
+++ actuator/src/main/java/org/tron/core/vm/nativecontract/VoteRewardUtils.java
@@ -2,11 +2,13 @@ package org.tron.core.vm.nativecontract;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.collections4.CollectionUtils;
-import org.spongycastle.util.encoders.Hex;
 import org.tron.core.capsule.AccountCapsule;
 import org.tron.core.store.DelegationStore;
+import org.tron.core.vm.config.VMConfig;
 import org.tron.core.vm.repository.Repository;
 import org.tron.protos.Protocol;
+
+import java.math.BigInteger;
 
 @Slf4j(topic = "VoteRewardUtils")
 public class VoteRewardUtils {
@@ -15,7 +17,7 @@ public class VoteRewardUtils {
   }
 
   public static void withdrawReward(byte[] address, Repository repository) {
-    if (!repository.getDynamicPropertiesStore().allowChangeDelegation()) {
+    if (!VMConfig.allowTvmVote()) {
       return;
     }
     AccountCapsule accountCapsule = repository.getAccount(address);
@@ -35,7 +37,7 @@ public class VoteRewardUtils {
     if (beginCycle + 1 == endCycle && beginCycle < currentCycle) {
       AccountCapsule account = repository.getAccountVote(beginCycle, address);
       if (account != null) {
-        reward = computeReward(beginCycle, account, repository);
+        reward = computeReward(beginCycle, endCycle, account, repository);
         adjustAllowance(address, reward, repository);
         reward = 0;
       }
@@ -47,9 +49,7 @@ public class VoteRewardUtils {
       return;
     }
     if (beginCycle < endCycle) {
-      for (long cycle = beginCycle; cycle < endCycle; cycle++) {
-        reward += computeReward(cycle, accountCapsule, repository);
-      }
+      reward += computeReward(beginCycle, endCycle, accountCapsule, repository);
       adjustAllowance(address, reward, repository);
     }
     repository.updateBeginCycle(address, endCycle);
@@ -58,7 +58,7 @@ public class VoteRewardUtils {
   }
 
   public static long queryReward(byte[] address, Repository repository) {
-    if (!repository.getDynamicPropertiesStore().allowChangeDelegation()) {
+    if (!VMConfig.allowTvmVote()) {
       return 0;
     }
     AccountCapsule accountCapsule = repository.getAccount(address);
@@ -76,7 +76,7 @@ public class VoteRewardUtils {
     if (beginCycle + 1 == endCycle && beginCycle < currentCycle) {
       AccountCapsule account = repository.getAccountVote(beginCycle, address);
       if (account != null) {
-        reward = computeReward(beginCycle, account, repository);
+        reward = computeReward(beginCycle, endCycle, account, repository);
       }
       beginCycle += 1;
     }
@@ -85,26 +85,29 @@ public class VoteRewardUtils {
       return reward + accountCapsule.getAllowance();
     }
     if (beginCycle < endCycle) {
-      for (long cycle = beginCycle; cycle < endCycle; cycle++) {
-        reward += computeReward(cycle, accountCapsule, repository);
-      }
+      reward += computeReward(beginCycle, endCycle, accountCapsule, repository);
     }
     return reward + accountCapsule.getAllowance();
   }
 
-  private static long computeReward(long cycle, AccountCapsule accountCapsule, Repository repository) {
-    long reward = 0;
+  private static long computeReward(long beginCycle, long endCycle,
+                                    AccountCapsule accountCapsule, Repository repository) {
+    if (beginCycle >= endCycle) {
+      return 0;
+    }
 
+    long reward = 0;
     for (Protocol.Vote vote : accountCapsule.getVotesList()) {
       byte[] srAddress = vote.getVoteAddress().toByteArray();
-      long totalReward = repository.getDelegationStore().getReward(cycle, srAddress);
-      long totalVote = repository.getDelegationStore().getWitnessVote(cycle, srAddress);
-      if (totalVote == DelegationStore.REMARK || totalVote == 0) {
+      BigInteger beginVi = repository.getDelegationStore().getWitnessVi(beginCycle - 1, srAddress);
+      BigInteger endVi = repository.getDelegationStore().getWitnessVi(endCycle - 1, srAddress);
+      BigInteger deltaVi = endVi.subtract(beginVi);
+      if (deltaVi.signum() <= 0) {
         continue;
       }
       long userVote = vote.getVoteCount();
-      double voteRate = (double) userVote / totalVote;
-      reward += voteRate * totalReward;
+      reward += deltaVi.multiply(BigInteger.valueOf(userVote))
+          .divide(DelegationStore.DECIMAL_OF_VI_REWARD).longValue();
     }
     return reward;
   }
