5b0bf8ef5885343871aa59dbc55d34081f2727bf
Wenhua Zhang <zhangwenhua@tron.network>
2021-07-14 15:39:27 +0800

    typo

--- framework/src/test/java/org/tron/common/crypto/BouncyCastleTest.java
+++ framework/src/test/java/org/tron/common/crypto/BouncyCastleTest.java
@@ -12,7 +12,7 @@ import org.junit.Test;
 import org.tron.common.crypto.sm2.SM2;
 import org.tron.common.utils.Sha256Hash;
 
-public class BCCompatibilityTest {
+public class BouncyCastleTest {
 
   private String privString = "c85ef7d79691fe79573b1a7064c19c1a9819ebdbd1faaab1a8ec92344438aaf4";
   private BigInteger privateKey = new BigInteger(privString, 16);
@@ -50,8 +50,8 @@ public class BCCompatibilityTest {
   @Test
   public void testECKeyAddress() {
     String spongyPubkey =
-        "040947751e3022ecf3016be03ec77ab0ce3c2662b4843898cb068d74f698ccc8ad75" +
-            "aa17564ae80a20bb044ee7a6d903e8e8df624b089c95d66a0570f051e5a05b";
+        "040947751e3022ecf3016be03ec77ab0ce3c2662b4843898cb068d74f698ccc8ad75"
+            + "aa17564ae80a20bb044ee7a6d903e8e8df624b089c95d66a0570f051e5a05b";
     String spongyAddress = "cd2a3d9f938e13cd947ec05abc7fe734df8dd826";
     ECKey key = ECKey.fromPrivate(privateKey);
     byte[] pubkey = key.getPubKey();
@@ -75,8 +75,8 @@ public class BCCompatibilityTest {
   @Test
   public void testECSpongySignature() throws SignatureException {
     String msg = "transaction raw data";
-    String spongySig = "GwYii3BGoQq3sdyWiGVv7bGCR5hJy62g+IF+1jPOSqHt" +
-        "IDfuKgowhiiK7ivcqk+T7qq/hlfIjaRe+t1drFDZ+Mo=";
+    String spongySig = "GwYii3BGoQq3sdyWiGVv7bGCR5hJy62g+IF+1jPOSqHt"
+        + "IDfuKgowhiiK7ivcqk+T7qq/hlfIjaRe+t1drFDZ+Mo=";
     String spongyAddress = "cd2a3d9f938e13cd947ec05abc7fe734df8dd826";
     byte[] hash = Sha256Hash.hash(true, msg.getBytes());
     byte[] address = SignUtils.signatureToAddress(hash, spongySig, true);
@@ -96,8 +96,8 @@ public class BCCompatibilityTest {
 
   @Test
   public void testSM2Address() {
-    String spongyPublickey = "04f9539070c135be6183cbff4539f8298755df5981022769cd16cfdcb917fa7e32" +
-        "4e83b50f0bdbb34acd6ccbb78d45a8a383403f26bbd03805a178c43407dfdeae";
+    String spongyPublickey = "04f9539070c135be6183cbff4539f8298755df5981022769cd16cfdcb917fa7e32"
+        + "4e83b50f0bdbb34acd6ccbb78d45a8a383403f26bbd03805a178c43407dfdeae";
     String spongyAddress = "7dc44d739a5226c0d3037bb7919f653eb2f938b9";
     SM2 key = SM2.fromPrivate(privateKey);
     assertEquals(spongyPublickey, Hex.toHexString(key.getPubKey()));
@@ -119,8 +119,8 @@ public class BCCompatibilityTest {
   @Test
   public void testSM2SpongySignature() throws SignatureException {
     String msg = "transaction raw data";
-    String spongySig = "HOoyvBLOJ+dKReQdAc6W/ffRi/KmVntco0+xgzmFItEExq/fHF" +
-        "veCe0GoCJUBdyHyUFjwn+a18ibtGJcHxnvLj0=";
+    String spongySig = "HOoyvBLOJ+dKReQdAc6W/ffRi/KmVntco0+xgzmFItEExq/fHF"
+        + "veCe0GoCJUBdyHyUFjwn+a18ibtGJcHxnvLj0=";
     String spongyAddress = "7dc44d739a5226c0d3037bb7919f653eb2f938b9";
     byte[] hash = Sha256Hash.hash(false, msg.getBytes());
     byte[] address = SignUtils.signatureToAddress(hash, spongySig, false);
