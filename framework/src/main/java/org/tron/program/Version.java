e1ce07fb53f4937a53be39de66eedd025e44f4ec
lvs007 <zy.liang.5@163.com>
2021-08-02 12:42:54 +0800

    modify the version

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
   public static final String VERSION_NAME = "GreatVoyage-v4.2.2-3-g6d7831aab";
   public static final String VERSION_CODE = "15589";
-  private static final String VERSION = "4.2.2.1";
+  private static final String VERSION = "4.3.0";
 
   public static String getVersion() {
     return VERSION;
