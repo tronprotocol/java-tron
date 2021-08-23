af26fbfdeeefd1402bbea7ed9d152a300e133428
wubin01 <wb_bupt@163.com>
2021-08-23 16:40:13 +0800

    solve sonar problem

--- framework/src/main/java/org/tron/core/config/args/Args.java
+++ framework/src/main/java/org/tron/core/config/args/Args.java
@@ -611,7 +611,7 @@ public class Args extends CommonParameter {
         ? config.getInt(Constant.NODE_RPC_MIN_EFFECTIVE_CONNECTION) : 1;
 
     PARAMETER.trxCacheEnable = config.hasPath(Constant.NODE_RPC_TRX_CACHE_ENABLE)
-            ? config.getBoolean(Constant.NODE_RPC_TRX_CACHE_ENABLE) : false;
+            && config.getBoolean(Constant.NODE_RPC_TRX_CACHE_ENABLE);
 
     PARAMETER.blockNumForEnergyLimit = config.hasPath(Constant.ENERGY_LIMIT_BLOCK_NUM)
         ? config.getInt(Constant.ENERGY_LIMIT_BLOCK_NUM) : 4727890L;
