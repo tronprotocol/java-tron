d1e1b6e0928e2a4c38fb01f2c6169a4030a6e494
Wenhua Zhang <zhangwenhua@tron.network>
2021-08-30 14:50:50 +0800

    typo: remove unused codes

--- framework/src/main/java/org/tron/core/services/jsonrpc/JsonRpcApiUtil.java
+++ framework/src/main/java/org/tron/core/services/jsonrpc/JsonRpcApiUtil.java
@@ -4,7 +4,6 @@ import com.google.common.base.Throwables;
 import com.google.common.primitives.Longs;
 import com.google.protobuf.Any;
 import com.google.protobuf.ByteString;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -16,14 +15,12 @@ import org.tron.common.crypto.Hash;
 import org.tron.common.parameter.CommonParameter;
 import org.tron.common.utils.ByteArray;
 import org.tron.common.utils.ByteUtil;
-import org.tron.common.utils.Commons;
 import org.tron.common.utils.DecodeUtil;
 import org.tron.common.utils.Sha256Hash;
 import org.tron.common.utils.StringUtil;
 import org.tron.core.Wallet;
 import org.tron.core.capsule.ContractCapsule;
 import org.tron.core.exception.JsonRpcInvalidParamsException;
-import org.tron.core.exception.TronException;
 import org.tron.protos.Protocol.Block;
 import org.tron.protos.Protocol.Transaction;
 import org.tron.protos.Protocol.Transaction.Contract.ContractType;
@@ -52,68 +49,6 @@ import org.tron.protos.contract.WitnessContract.VoteWitnessContract.Vote;
 
 @Slf4j(topic = "API")
 public class JsonRpcApiUtil {
-
-  // transform the Tron address to Ethereum Address
-  public static String tronToEthAddress(String tronAddress) {
-    byte[] tronBytes = Commons.decodeFromBase58Check(tronAddress);
-    byte[] ethBytes = new byte[20];
-    try {
-      if ((tronBytes.length != 21 && tronBytes[0] != Wallet.getAddressPreFixByte())) {
-        throw new TronException("invalid Tron address");
-      }
-      System.arraycopy(tronBytes, 1, ethBytes, 0, 20);
-      return toChecksumAddress(ByteArray.toHexString(ethBytes));
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
-
-  // transform the Ethereum address to Tron Address
-  public static String ethToTronAddress(String ethAddress) {
-    byte[] address = ByteArray.fromHexString(ethAddress);
-    byte[] tronAddress = new byte[21];
-    try {
-      if (address.length != 20) {
-        throw new TronException("invalid Ethereum address");
-      }
-      System.arraycopy(address, 0, tronAddress, 1, 20);
-      tronAddress[0] = Wallet.getAddressPreFixByte();
-      return StringUtil.encode58Check(tronAddress);
-    } catch (Exception e) {
-      e.printStackTrace();
-      return null;
-    }
-  }
-
-  //reference: https://github.com/ethereum/EIPs/blob/master/EIPS/eip-55.md
-  private static String toChecksumAddress(String address) throws TronException {
-    StringBuffer sb = new StringBuffer();
-    int nibble;
-
-    if (address.startsWith("0x")) {
-      address = address.substring(2);
-    }
-    String hashedAddress = ByteArray
-        .toHexString(Hash.sha3(address.getBytes(StandardCharsets.UTF_8)));
-    sb.append("0x");
-    for (int i = 0; i < address.length(); i++) {
-      if ("0123456789".contains(String.valueOf(address.charAt(i)))) {
-        sb.append(address.charAt(i));
-      } else if ("abcdef".contains(String.valueOf(address.charAt(i)))) {
-        nibble = Integer.parseInt(String.valueOf(hashedAddress.charAt(i)), 16);
-        if (nibble > 7) {
-          sb.append(String.valueOf(address.charAt(i)).toUpperCase());
-        } else {
-          sb.append(address.charAt(i));
-        }
-      } else {
-        throw new TronException("invalid hex character in address");
-      }
-    }
-    return sb.toString();
-  }
-
   public static byte[] convertToTronAddress(byte[] address) {
     byte[] newAddress = new byte[21];
     byte[] temp = new byte[] {Wallet.getAddressPreFixByte()};
