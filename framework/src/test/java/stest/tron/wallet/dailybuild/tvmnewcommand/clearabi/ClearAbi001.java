e91d6e0eceffe3b819c9653302fc24c9dfecc882
daxinzang1 <280474930@qq.com>
2021-08-30 17:55:45 +0800

    Stest assert adapt 4.4.0

--- framework/src/test/java/stest/tron/wallet/dailybuild/tvmnewcommand/clearabi/ClearAbi001.java
+++ framework/src/test/java/stest/tron/wallet/dailybuild/tvmnewcommand/clearabi/ClearAbi001.java
@@ -234,7 +234,7 @@ public class ClearAbi001 {
             containsString("CONTRACT_VALIDATE_ERROR"));
     Assert
         .assertThat(transactionExtention.getResult().getMessage().toStringUtf8(),
-            containsString("contract validate error : Contract not exists"));
+            containsString("Contract validate error : Contract not exists"));
   }
 
 
@@ -251,7 +251,7 @@ public class ClearAbi001 {
     Assert.assertThat(transactionExtention.getResult().getCode().toString(),
         containsString("CONTRACT_VALIDATE_ERROR"));
     Assert.assertThat(transactionExtention.getResult().getMessage().toStringUtf8(),
-        containsString("contract validate error : Contract not exists"));
+        containsString("Contract validate error : Contract not exists"));
 
   }
 
@@ -266,7 +266,7 @@ public class ClearAbi001 {
             containsString("CONTRACT_VALIDATE_ERROR"));
     Assert
         .assertThat(transactionExtention.getResult().getMessage().toStringUtf8(),
-            containsString("contract validate error : Contract not exists"));
+            containsString("Contract validate error : Contract not exists"));
     byte[] fakeAddress1 = "412B5D3405B2D26767C9C09886D53DEAFF6EB718AC111".getBytes();
 
     TransactionExtention transactionExtention1 = PublicMethed
@@ -277,7 +277,7 @@ public class ClearAbi001 {
             containsString("CONTRACT_VALIDATE_ERROR"));
     Assert
         .assertThat(transactionExtention1.getResult().getMessage().toStringUtf8(),
-            containsString("contract validate error : Contract not exists"));
+            containsString("Contract validate error : Contract not exists"));
 
 
   }
