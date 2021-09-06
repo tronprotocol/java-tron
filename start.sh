3ca2f340bf5428a648a8a61f292c856db3f2b4ec
liukai <forfreeday@163.com>
2021-09-06 16:15:58 +0800

    add shell options: 1. add stop option 2. add quick option

--- start.sh
+++ start.sh
@@ -4,12 +4,17 @@ FULL_NODE_DIR="FullNode"
 FULL_NODE_CONFIG="main_net_config.conf"
 FULL_NODE_SHELL="start.sh"
 JAR_NAME="FullNode.jar"
-FULL_START_OPT=$(echo ${@:2})
+FULL_START_OPT=$(echo ${@:1})
 
 # start service option
 MAX_STOP_TIME=60
 # modify this option to allow the minimum memory to be started, unit MB
-ALLOW_MIN_MEMORY=8000
+ALLOW_MIN_MEMORY=8192
+# JVM option
+MAX_DIRECT_MEMORY=3g
+JVM_MS=4g
+JVM_MX=4g
+
 SPECIFY_MEMORY=0
 RUN=false
 UPGRADE=false
@@ -20,84 +25,95 @@ REBUILD_DIR="$PWD/output-directory/database"
 REBUILD_MANIFEST_SIZE=0
 REBUILD_BATCH_SIZE=80000
 
+# download and upgrade
+DOWNLOAD=false
+RELEASE_URL='https://github.com/tronprotocol/java-tron/releases'
+QUICK_START=false
+
 getLatestReleaseVersion() {
   default_version='GreatVoyage-v4.3.0'
   full_node_version=`git ls-remote --tags git@github.com:tronprotocol/java-tron.git |grep GreatVoyage- | awk -F '/' 'END{print $3}'`
   if [[ -n $full_node_version ]]; then
-   echo "latest release version: $full_node_version"
-   return $full_node_version
+   echo $full_node_version
   else
-   echo "default version: " $default_version
-   return $default_version
+   echo $default_version
   fi
 }
 
 checkVersion() {
- local_full_node_version=''
- github_release_version=getLatestReleaseVersion
- if [[ $local_full_node_version -ne $github_release_version ]]; then
-  echo "local version: $local_full_node_version, remote latest version: $github_release_version"
-  return $github_release_version
+ github_release_version=$(`echo getLatestReleaseVersion`)
+ if [[ -n $github_release_version ]]; then
+  echo "info: github latest version: $github_release_version"
+  echo $github_release_version
  else
-   return $local_full_node_version
+   echo ''
  fi
 }
 
-upgrade(latest_version) {
+upgrade() {
+  latest_version=$(`echo getLatestReleaseVersion`)
+  echo "info: latest version: $latest_version"
   if [[ -n $latest_version ]]; then
-    //备份
-    old_jar=$PWD/$JAR_NAME
-    if [[ -f $old_jar ]];then
+    old_jar="$PWD/$JAR_NAME"
+    if [[ -f $old_jar ]]; then
+      echo "info: backup $old_jar"
       mv $PWD/$JAR_NAME $PWD/$JAR_NAME'_bak'
-    if
-
-    is_download=$(wget -q https://github.com/tronprotocol/java-tron/releases/download/$latest_version/$JAR_NAME)
-    if [[ $is_download == 0 ]]; then
-      echo "download version $latest_version success"
     fi
+    download $RELEASE_URL/download/$latest_version/$JAR_NAME $JAR_NAME
+    if [[ $? == 0 ]]; then
+      echo "info: download version $latest_version success"
+    fi
+  else
+    echo 'info: nothing to upgrade'
   fi
 }
 
+unsetGitProxy() {
+  echo 'info: unset git proxy'
+  git config --global --unset http.proxy
+}
+
 download() {
+  local url=$1
+  local file_name=$2
   if type wget >/dev/null 2>&1; then
-    full_node_version=getLatestReleaseVersion
-    if [ ! -d "$FULL_NODE_DIR" ]; then
-      echo "mkdir $FULL_NODE_DIR"
-      mkdir $FULL_NODE_DIR
-      cd $FULL_NODE_DIR
-    elif [ -d "$FULL_NODE_DIR" ]; then
-      cd $FULL_NODE_DIR
-    fi
-    echo 'execute download config'
-    config_file=$(wget -q https://raw.githubusercontent.com/tronprotocol/tron-deployment/master/$FULL_NODE_CONFIG)
-    sh_file=$(wget -q https://raw.githubusercontent.com/tronprotocol/java-tron/master/$FULL_NODE_SHELL)
-    full_node=$(wget -q https://github.com/tronprotocol/java-tron/releases/download/$full_node_version/$JAR_NAME)
-
-    if [[ $full_node == 0 ]]; then
-      echo "download $JAR_NAME success"
-    fi
-
-    if [[ $sh_file == 0 ]]; then
-      chmod u+rwx start.sh
-      echo "download $FULL_NODE_SHELL success"
-    fi
-
-    if [[ $config_file == 0 ]]; then
-      echo "download $FULL_NODE_CONFIG success"
-    fi
+    wget --no-check-certificate -q $url
+  elif type curl >/dev/null 2>&1; then
+    echo "curl -OLJ $url"
+    curl -OLJ $url
   else
-    echo 'no exists wget, make sure the system can use the "wget" command'
+    echo 'info: no exists wget or curl, make sure the system can use the "wget" or "curl" command'
   fi
+}
+
+quickStart() {
+  if [ ! -d $FULL_NODE_DIR ]; then
+    echo "info: mkdir $FULL_NODE_DIR"
+    mkdir $FULL_NODE_DIR
+    $(cp $0 $FULL_NODE_DIR)
+    cd $FULL_NODE_DIR
+  elif [ -d $FULL_NODE_DIR ]; then
+    cd $FULL_NODE_DIR
+  fi
+  full_node_version=$(`echo getLatestReleaseVersion`)
+  echo "info: check latest version: $full_node_version"
+  echo 'info: download config'
+  download https://raw.githubusercontent.com/tronprotocol/tron-deployment/master/$FULL_NODE_CONFIG $FULL_NODE_CONFIG
+  mv $FULL_NODE_CONFIG 'config.conf'
+
+  echo "info: download $full_node_version"
+#  unsetGitProxy
+  download $RELEASE_URL/download/$full_node_version/$JAR_NAME $JAR_NAME
 }
 
 cloneCode() {
   if type git >/dev/null 2>&1; then
     git_clone=$(git clone -b master git@github.com:tronprotocol/java-tron.git)
     if [[ git_clone == 0 ]]; then
-      echo 'git clone java-tron success'
+      echo 'info: git clone java-tron success'
     fi
   else
-    echo 'no exists git, make sure the system can use the "git" command'
+    echo 'info: no exists git, make sure the system can use the "git" command'
   fi
 }
 
@@ -123,7 +139,7 @@ stopService() {
       kill -15 $pid
       sleep 1
     else
-      echo "java-tron stop"
+      echo "info: java-tron stop"
       return
     fi
     count=$(($count + 1))
@@ -136,68 +152,105 @@ stopService() {
 }
 
 checkAllowMemory() {
-  if [[ $SPECIFY_MEMORY -gt 0 ]] &&
+  os=`uname`
+  totalMemory=`getTotalMemory`
+  totalMemory=`echo $totalMemory`
+  total=`expr $totalMemory / 1024`
+  if [[ $os == 'Darwin' ]]; then
+    return
+  fi
+
+  if [[ $total -lt $ALLOW_MIN_MEMORY ]]; then
+    echo "warn: the memory $total MB cannot be smaller than the minimum memory $ALLOW_MIN_MEMORY MB"
+    exit
+  elif [[ $SPECIFY_MEMORY -gt 0 ]] &&
    [[ $SPECIFY_MEMORY -lt $ALLOW_MIN_MEMORY ]]; then
     echo "warn: the specified memory $SPECIFY_MEMORY MB cannot be smaller than the minimum memory $ALLOW_MIN_MEMORY MB"
-    echo 'start abort'
+    echo 'warn: start abort'
     exit
   fi
 }
 
 setTCMalloc() {
-  libtcmalloc="/usr/lib64/libtcmalloc.so"
-  if [[ -f $libtcmalloc ]]; then
-    export LD_PRELOAD="$libtcmalloc"
-    export TCMALLOC_RELEASE_RATE=10
-  else
-    echo 'recommended for linux systems using tcmalloc as the default memory management tool'
+  os=`uname`
+  if [[ $os == 'Linux' ]] || [[ $os == 'linux' ]] ; then
+    lib_tc_malloc="/usr/lib64/libtcmalloc.so"
+    if [[ -f $lib_tc_malloc ]]; then
+      export LD_PRELOAD="$lib_tc_malloc"
+      export TCMALLOC_RELEASE_RATE=10
+    else
+      echo 'info: recommended for linux systems using tcmalloc as the default memory management tool'
+    fi
   fi
 }
 
+getTotalMemory() {
+  os=`uname`
+  if [[ $os == 'Linux' ]] || [[ $os == 'linux' ]] ; then
+    total=$(cat /proc/meminfo | grep MemTotal | awk -F ' ' '{print $2}')
+    echo $total
+    return
+  elif [[  $os == 'Darwin' ]]; then
+    total=$(sysctl -a | grep mem |grep hw.memsize |awk -F ' ' '{print $2}')
+    echo `expr $total / 1024`
+  fi
+}
+
+setJVMMemory() {
+  totalMemory=`getTotalMemory`
+  total=`echo $totalMemory`
+  os=`uname`
+  if [[ $os == 'Linux' ]] || [[ $os == 'linux' ]] ; then
+    MAX_DIRECT_MEMORY=$(echo "$total/1024/1024*0.1" | bc | awk -F. '{print $1"g"}')
+  elif [[ $os == 'Darwin' ]]; then
+    MAX_DIRECT_MEMORY='1g'
+  fi
+  JVM_MX=$(echo "$total/1024/1024*0.6" | bc | awk -F. '{print $1"g"}')
+  JVM_MS=$JVM_MX
+}
+
 startService() {
-  setTCMalloc
   echo $(date) >>start.log
-  total=$(cat /proc/meminfo | grep MemTotal | awk -F ' ' '{print $2}')
-  xmx=$(echo "$total/1024/1024*0.6" | bc | awk -F. '{print $1"g"}')
-  directmem=$(echo "$total/1024/1024*0.1" | bc | awk -F. '{print $1"g"}')
   logtime=$(date +%Y-%m-%d_%H-%M-%S)
-  nohup java -Xms$xmx -Xmx$xmx -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -Xloggc:./gc.log \
+  setTCMalloc
+  setJVMMemory
+  nohup java -Xms$JVM_MS -Xmx$JVM_MX -XX:+UseConcMarkSweepGC -XX:+PrintGCDetails -Xloggc:./gc.log \
     -XX:+PrintGCDateStamps -XX:+CMSParallelRemarkEnabled -XX:ReservedCodeCacheSize=256m -XX:+UseCodeCacheFlushing \
     -XX:MetaspaceSize=256m -XX:MaxMetaspaceSize=512m \
-    -XX:MaxDirectMemorySize=$directmem -XX:+HeapDumpOnOutOfMemoryError \
+    -XX:MaxDirectMemorySize=$MAX_DIRECT_MEMORY -XX:+HeapDumpOnOutOfMemoryError \
     -XX:NewRatio=2 -jar \
     $JAR_NAME $FULL_START_OPT -c config.conf >>start.log 2>&1 &
   pid=$(ps -ef | grep $JAR_NAME | grep -v grep | awk '{print $2}')
-  echo "start java-tron with pid $pid on $HOSTNAME"
+  echo "info: start java-tron with pid $pid on $HOSTNAME"
 }
 
 rebuildManifest() {
-  if [[ "$REBUILD_MANIFEST" = false ]]; then
-    echo 'disable rebuild manifest!'
+  if [[ $REBUILD_MANIFEST = false ]]; then
+    echo 'info: disable rebuild manifest!'
     return
   fi
 
   if [[ ! -d $REBUILD_DIR ]]; then
-    echo "$REBUILD_DIR not exists, skip rebuild manifest"
+    echo "info: database not exists, skip rebuild manifest"
     return
   fi
 
   ARCHIVE_JAR='ArchiveManifest.jar'
   if [[ -f $ARCHIVE_JAR ]]; then
-    echo 'execute rebuild manifest.'
+    echo 'info: execute rebuild manifest.'
     java -jar $ARCHIVE_JAR -d $REBUILD_DIR -m $REBUILD_MANIFEST_SIZE -b $REBUILD_BATCH_SIZE
   else
-    echo 'download the rebuild manifest plugin from the github'
-    download=$(wget -q https://github.com/tronprotocol/java-tron/releases/download/GreatVoyage-v4.3.0/$ARCHIVE_JAR)
+    echo 'info: download the rebuild manifest plugin from the github'
+    download $RELEASE_URL/download/GreatVoyage-v4.3.0/$ARCHIVE_JAR $ARCHIVE_JAR
     if [[ $download == 0 ]]; then
-      echo 'download success, rebuild manifest'
+      echo 'info: download success, rebuild manifest'
       java -jar $ARCHIVE_JAR $REBUILD_DIR -m $REBUILD_MANIFEST_SIZE -b $REBUILD_BATCH_SIZE
     fi
   fi
   if [[ $? == 0 ]]; then
-    echo 'rebuild manifest success'
+    echo 'info: rebuild manifest success'
   else
-    echo 'rebuild manifest fail, log in logs/archive.log'
+    echo 'info: rebuild manifest fail, log in logs/archive.log'
   fi
 }
 
@@ -223,8 +276,12 @@ while [ -n "$1" ]; do
     shift 2
     ;;
   --download)
-    download
-    exit
+    DOWNLOAD=true
+    shift 1
+    ;;
+  --quickstart)
+    QUICK_START=true
+    shift 1
     ;;
   --clone)
     cloneCode
@@ -254,6 +311,10 @@ while [ -n "$1" ]; do
     RUN=true
     shift 1
     ;;
+  --stop)
+    stopService
+    exit
+    ;;
   *)
     echo "warn: option $1 does not exist"
     exit
@@ -261,13 +322,31 @@ while [ -n "$1" ]; do
   esac
 done
 
-if [[ $UPGRADE = true ]]; then
-  latest_version=checkVersion
-  upgrade($latest_version)
+if [[ $QUICK_START == true ]]; then
+  quickStart
+  if [[ $? == 0 ]] ; then
+    if [[ $RUN == true ]]; then
+      cd $FULL_NODE_DIR
+      FULL_START_OPT=''
+      restart
+    fi
+  fi
+  exit
 fi
 
-if [[ $RUN = true ]]; then
+if [[ $UPGRADE == true ]]; then
+  upgrade
+  exit
+fi
+
+if [[ $RUN == true ]]; then
   restart
+  exit
+fi
+
+if [[ $DOWNLOAD == true ]]; then
+  latest=$(`echo getLatestReleaseVersion`)
+  download $RELEASE_URL/download/$latest/$JAR_NAME $latest
   exit
 fi
 
