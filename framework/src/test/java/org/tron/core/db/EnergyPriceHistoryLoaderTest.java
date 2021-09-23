bd0a3996b5efdd0442462511adbf6c79a9460cb8
Wenhua Zhang <zhangwenhua@tron.network>
2021-09-23 12:28:11 +0800

    test: ignore EnergyPriceHistoryLoaderTest

--- framework/src/test/java/org/tron/core/db/EnergyPriceHistoryLoaderTest.java
+++ framework/src/test/java/org/tron/core/db/EnergyPriceHistoryLoaderTest.java
@@ -15,6 +15,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.tron.common.application.TronApplicationContext;
 import org.tron.common.utils.FileUtil;
@@ -29,6 +30,7 @@ import org.tron.protos.Protocol.Proposal;
 import org.tron.protos.Protocol.Proposal.State;
 
 @Slf4j
+@Ignore
 public class EnergyPriceHistoryLoaderTest {
 
   private static ChainBaseManager chainBaseManager;
