62ff1cd171bc05a649421a379958eade24774505
liukai <forfreeday@163.com>
2021-10-19 21:11:20 +0800

    update a new version. version name:GreatVoyage-v4.4.0-5-g030ef17b4,version code:16341

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -2,8 +2,8 @@ package org.tron.program;
 
 public class Version {
 
-  public static final String VERSION_NAME = "GreatVoyage-v4.3.0-460-g973a4dc82";
-  public static final String VERSION_CODE = "16334";
+  public static final String VERSION_NAME = "GreatVoyage-v4.4.0-5-g030ef17b4";
+  public static final String VERSION_CODE = "16341";
   private static final String VERSION = "4.4.1";
 
   public static String getVersion() {
