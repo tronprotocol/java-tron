341a77311892262bed0884f1986171240ff99208
wubin01 <wb_bupt@163.com>
2022-03-21 12:20:02 +0800

    perf(net): solve sonar and check style problem

--- framework/src/test/java/org/tron/core/db/CommonStoreTest.java
+++ framework/src/test/java/org/tron/core/db/CommonStoreTest.java
@@ -1,5 +1,6 @@
 package org.tron.core.db;
 
+import java.io.File;
 import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.BeforeClass;
@@ -10,8 +11,6 @@ import org.tron.core.Constant;
 import org.tron.core.config.DefaultConfig;
 import org.tron.core.config.args.Args;
 
-import java.io.File;
-
 public class CommonStoreTest {
   private static String dbPath = "output_CommonStore_test";
   private static String dbDirectory = "db_CommonStore_test";
@@ -20,13 +19,11 @@ public class CommonStoreTest {
   private static CommonStore commonStore;
 
   static {
-    Args.setParam(
-            new String[]{
-                    "--output-directory", dbPath,
-                    "--storage-db-directory", dbDirectory,
-                    "--storage-index-directory", indexDirectory
-            },
-            Constant.TEST_CONF
+    Args.setParam(new String[] {
+        "--output-directory", dbPath,
+        "--storage-db-directory", dbDirectory,
+        "--storage-index-directory", indexDirectory},
+        Constant.TEST_CONF
     );
     context = new TronApplicationContext(DefaultConfig.class);
   }
