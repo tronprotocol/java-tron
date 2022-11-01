b6e44a511d97caee95ee39d474d75e321e0a6dbc
chaozhu <super.zhu@tron.network>
2022-10-31 19:12:32 -0700

    feat(freezeV2): new stake, add 3 interfaces

---
+++ framework/src/main/java/org/tron/core/services/interfaceOnSolidity/http/GetCanWithdrawUnfreezeAmountOnSolidityServlet.java
@@ -0,0 +1,30 @@
+package org.tron.core.services.interfaceOnSolidity.http;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.tron.core.Wallet;
+import org.tron.core.services.http.GetCanWithdrawUnfreezeAmountServlet;
+import org.tron.core.services.interfaceOnSolidity.WalletOnSolidity;
+
+@Component
+@Slf4j(topic = "API")
+public class GetCanWithdrawUnfreezeAmountOnSolidityServlet
+        extends GetCanWithdrawUnfreezeAmountServlet {
+
+  @Autowired
+  private Wallet wallet;
+
+  @Autowired
+  private WalletOnSolidity walletOnSolidity;
+
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) {
+    walletOnSolidity.futureGet(() -> super.doGet(request, response));
+  }
+
+  protected void doPost(HttpServletRequest request, HttpServletResponse response) {
+    walletOnSolidity.futureGet(() -> super.doPost(request, response));
+  }
+}
