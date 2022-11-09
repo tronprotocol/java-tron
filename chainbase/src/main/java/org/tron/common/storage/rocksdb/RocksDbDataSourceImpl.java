4f9a7e9826020203e6345691f09352f6577d3934
morgan <morgan.p@qq.com>
2022-11-09 14:32:16 +0800

    feat(db): print db internal log 1.print db internal log

--- chainbase/src/main/java/org/tron/common/storage/rocksdb/RocksDbDataSourceImpl.java
+++ chainbase/src/main/java/org/tron/common/storage/rocksdb/RocksDbDataSourceImpl.java
@@ -24,6 +24,8 @@ import org.rocksdb.BlockBasedTableConfig;
 import org.rocksdb.BloomFilter;
 import org.rocksdb.Checkpoint;
 import org.rocksdb.DirectComparator;
+import org.rocksdb.InfoLogLevel;
+import org.rocksdb.Logger;
 import org.rocksdb.Options;
 import org.rocksdb.ReadOptions;
 import org.rocksdb.RocksDB;
@@ -32,6 +34,7 @@ import org.rocksdb.RocksIterator;
 import org.rocksdb.Statistics;
 import org.rocksdb.WriteBatch;
 import org.rocksdb.WriteOptions;
+import org.slf4j.LoggerFactory;
 import org.tron.common.setting.RocksDbSettings;
 import org.tron.common.storage.WriteOptionsWrapper;
 import org.tron.common.storage.metric.DbStat;
@@ -57,6 +60,7 @@ public class RocksDbDataSourceImpl extends DbStat implements DbSourceInter<byte[
   private static final String KEY_ENGINE = "ENGINE";
   private static final String ROCKSDB = "ROCKSDB";
   private DirectComparator comparator;
+  private static final org.slf4j.Logger rocksDbLogger = LoggerFactory.getLogger(ROCKSDB);
 
   public RocksDbDataSourceImpl(String parentPath, String name, RocksDbSettings settings,
       DirectComparator comparator) {
@@ -227,6 +231,12 @@ public class RocksDbDataSourceImpl extends DbStat implements DbSourceInter<byte[
         if (comparator != null) {
           options.setComparator(comparator);
         }
+        options.setLogger(new Logger(options) {
+          @Override
+          protected void log(InfoLogLevel infoLogLevel, String logMsg) {
+            rocksDbLogger.info(logMsg);
+          }
+        });
 
         // table options
         final BlockBasedTableConfig tableCfg;
