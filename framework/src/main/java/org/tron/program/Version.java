37a09b2d5fddf0dfec82270ebe2823b6754fb059
liukai <forfreeday@163.com>
2022-11-18 11:51:51 +0800

    feat(version): update Version
    1. update to Version 4.6.0

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
   public static final String VERSION_NAME = "GreatVoyage-v4.5.1-125-ga9809b5bb";
   public static final String VERSION_CODE = "17071";
-  private static final String VERSION = "4.5.2";
+  private static final String VERSION = "4.6.0";
 
   public static String getVersion() {
     return VERSION;
