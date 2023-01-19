568b5e0bd71a1ca5e9d1c738f5d668c80eaabedb
forfreeday <forfreeday@163.com>
2023-01-19 11:34:16 +0800

    feature(version): update VERSION

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -2,8 +2,8 @@ package org.tron.program;
 
 public class Version {
 
-  public static final String VERSION_NAME = "GreatVoyage-v4.5.2-125-ga2906563d";
-  public static final String VERSION_CODE = "17198";
+  public static final String VERSION_NAME = "GreatVoyage-v4.6.0-385-g4445a420c";
+  public static final String VERSION_CODE = "17585";
   private static final String VERSION = "4.7.0";
 
   public static String getVersion() {
