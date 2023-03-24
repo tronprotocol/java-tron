dd56bf0b4c5f2966c9b42b3991e6411e8ec0d814
jiangyuanshu <317787106@qq.com>
2023-03-24 17:57:13 +0800

    fix(net): use get,add to replace set list in P2pConfig

--- framework/src/main/java/org/tron/core/net/TronNetService.java
+++ framework/src/main/java/org/tron/core/net/TronNetService.java
@@ -154,9 +154,9 @@ public class TronNetService {
       logger.debug("Seed InetSocketAddress: {}", inetSocketAddress);
     }
     P2pConfig config = new P2pConfig();
-    config.setSeedNodes(seeds);
-    config.setActiveNodes(parameter.getActiveNodes());
-    config.setTrustNodes(parameter.getPassiveNodes());
+    config.getSeedNodes().addAll(seeds);
+    config.getActiveNodes().addAll(parameter.getActiveNodes());
+    config.getTrustNodes().addAll(parameter.getPassiveNodes());
     config.getActiveNodes().forEach(n -> config.getTrustNodes().add(n.getAddress()));
     parameter.getFastForwardNodes().forEach(f -> config.getTrustNodes().add(f.getAddress()));
     int maxConnections = parameter.getMaxConnections();
