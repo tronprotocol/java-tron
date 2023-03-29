287aa7ae20cdbef07cd6b0406eafbb77039799d8
jiangyuanshu <317787106@qq.com>
2023-03-30 01:04:45 +0800

    fix(net): add test case for MessageStatistics

--- framework/src/test/java/org/tron/core/net/MessageTest.java
+++ framework/src/test/java/org/tron/core/net/MessageTest.java
@@ -2,9 +2,11 @@ package org.tron.core.net;
 
 import org.junit.Assert;
 import org.junit.Test;
+import org.tron.common.overlay.message.Message;
 import org.tron.core.exception.P2pException;
 import org.tron.core.net.message.MessageTypes;
 import org.tron.core.net.message.base.DisconnectMessage;
+import org.tron.core.net.service.statistics.MessageStatistics;
 import org.tron.protos.Protocol.ReasonCode;
 
 public class MessageTest {
@@ -33,6 +35,78 @@ public class MessageTest {
     }
     long endTime = System.currentTimeMillis();
     System.out.println("spend time : " + (endTime - startTime));
+  }
+
+  @Test
+  public void testMessageStatistics() {
+    MessageStatistics messageStatistics = new MessageStatistics();
+    Message message1 = new Message(MessageTypes.P2P_HELLO.asByte(), null) {
+      @Override
+      public Class<?> getAnswerMessage() {
+        return null;
+      }
+    };
+    Message message2 = new Message(MessageTypes.P2P_PING.asByte(), null) {
+      @Override
+      public Class<?> getAnswerMessage() {
+        return null;
+      }
+    };
+    Message message3 = new Message(MessageTypes.P2P_PONG.asByte(), null) {
+      @Override
+      public Class<?> getAnswerMessage() {
+        return null;
+      }
+    };
+    Message message4 = new Message(MessageTypes.P2P_DISCONNECT.asByte(), null) {
+      @Override
+      public Class<?> getAnswerMessage() {
+        return null;
+      }
+    };
+    Message message5 = new Message(MessageTypes.SYNC_BLOCK_CHAIN.asByte(), null) {
+      @Override
+      public Class<?> getAnswerMessage() {
+        return null;
+      }
+    };
+    Message message6 = new Message(MessageTypes.BLOCK_CHAIN_INVENTORY.asByte(), null) {
+      @Override
+      public Class<?> getAnswerMessage() {
+        return null;
+      }
+    };
+    Message message7 = new Message(MessageTypes.TRX.asByte(), null) {
+      @Override
+      public Class<?> getAnswerMessage() {
+        return null;
+      }
+    };
+    Message message8 = new Message(MessageTypes.BLOCK.asByte(), null) {
+      @Override
+      public Class<?> getAnswerMessage() {
+        return null;
+      }
+    };
+    messageStatistics.addTcpInMessage(message1);
+    messageStatistics.addTcpOutMessage(message1);
+    messageStatistics.addTcpInMessage(message2);
+    messageStatistics.addTcpOutMessage(message2);
+    messageStatistics.addTcpInMessage(message3);
+    messageStatistics.addTcpOutMessage(message3);
+    messageStatistics.addTcpInMessage(message4);
+    messageStatistics.addTcpOutMessage(message4);
+    messageStatistics.addTcpInMessage(message5);
+    messageStatistics.addTcpOutMessage(message5);
+    messageStatistics.addTcpInMessage(message6);
+    messageStatistics.addTcpOutMessage(message6);
+    messageStatistics.addTcpInMessage(message7);
+    messageStatistics.addTcpOutMessage(message7);
+    messageStatistics.addTcpInMessage(message8);
+    messageStatistics.addTcpOutMessage(message8);
+
+    Assert.assertEquals(8, messageStatistics.tronInMessage.getTotalCount());
+    Assert.assertEquals(8, messageStatistics.tronOutMessage.getTotalCount());
   }
 
 }
