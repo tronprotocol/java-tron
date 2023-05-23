c8b9d1a2c03058f93e2014b30f3351a2ba9fe782
chengtx01 <allen_cheng1@163.com>
2023-05-23 17:49:35 +0800

    fix(net):fix the issue of repeated request blocks

--- framework/src/main/java/org/tron/core/net/service/sync/SyncService.java
+++ framework/src/main/java/org/tron/core/net/service/sync/SyncService.java
@@ -225,7 +225,8 @@ public class SyncService {
             send.put(peer, new LinkedList<>());
           }
           for (BlockId blockId : peer.getSyncBlockToFetch()) {
-            if (requestBlockIds.getIfPresent(blockId) == null) {
+            if (requestBlockIds.getIfPresent(blockId) == null
+                && !peer.getSyncBlockInProcess().contains(blockId)) {
               requestBlockIds.put(blockId, peer);
               peer.getSyncBlockRequested().put(blockId, System.currentTimeMillis());
               send.get(peer).add(blockId);
@@ -266,6 +267,7 @@ public class SyncService {
           }
           if (msg.getBlockId().getNum() <= solidNum) {
             blockWaitToProcess.remove(msg);
+            peerConnection.getSyncBlockInProcess().remove(msg.getBlockId());
             return;
           }
           final boolean[] isFound = {false};
@@ -278,6 +280,7 @@ public class SyncService {
             blockWaitToProcess.remove(msg);
             isProcessed[0] = true;
             processSyncBlock(msg.getBlockCapsule(), peerConnection);
+            peerConnection.getSyncBlockInProcess().remove(msg.getBlockId());
           }
         }
       });
