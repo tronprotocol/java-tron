a5cb15a6d3738f504c8a3432f6fd1ed82a896a6c
Sergei panfilov <137243099+sergei17081985@users.noreply.github.com>
2023-07-01 21:34:30 +0600

    Revert "feat(merge): master merge int Nile"

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -2,8 +2,8 @@ package org.tron.program;
 
 public class Version {
 
-  public static final String VERSION_NAME = "GreatVoyage-v4.7.1.1-343-g959e0199de";
-  public static final String VERSION_CODE = "18031";
+  public static final String VERSION_NAME = "GreatVoyage-v4.7.1-11-ge5e347de7";
+  public static final String VERSION_CODE = "17686";
   private static final String VERSION = "4.7.2";
 
   public static String getVersion() {
