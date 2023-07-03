6ed9db403bc6d49ba5a4b2048177ed133b5fe60a
jiangyuanshu <317787106@qq.com>
2023-07-03 16:16:07 +0800

    feat(test): optimize NodeInfoServiceTest

--- framework/src/test/java/org/tron/core/services/NodeInfoServiceTest.java
+++ framework/src/test/java/org/tron/core/services/NodeInfoServiceTest.java
@@ -1,10 +1,14 @@
 package org.tron.core.services;
 
+import static org.mockito.Mockito.mock;
+
 import com.alibaba.fastjson.JSON;
 import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannelBuilder;
+import java.net.InetSocketAddress;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
+import org.mockito.Mockito;
 import org.tron.api.GrpcAPI.EmptyMessage;
 import org.tron.api.WalletGrpc;
 import org.tron.api.WalletGrpc.WalletBlockingStub;
@@ -13,6 +17,9 @@ import org.tron.common.entity.NodeInfo;
 import org.tron.common.utils.Sha256Hash;
 import org.tron.common.utils.client.Configuration;
 import org.tron.core.capsule.BlockCapsule;
+import org.tron.core.net.P2pEventHandlerImpl;
+import org.tron.core.net.peer.PeerManager;
+import org.tron.p2p.connection.Channel;
 import org.tron.program.Version;
 
 
@@ -21,12 +28,14 @@ public class NodeInfoServiceTest {
 
   private NodeInfoService nodeInfoService;
   private WitnessProductBlockService witnessProductBlockService;
+  private P2pEventHandlerImpl p2pEventHandler;
   private String fullnode = Configuration.getByPath("testng.conf").getStringList("fullnode.ip.list")
       .get(0);
 
   public NodeInfoServiceTest(TronApplicationContext context) {
     nodeInfoService = context.getBean("nodeInfoService", NodeInfoService.class);
     witnessProductBlockService = context.getBean(WitnessProductBlockService.class);
+    p2pEventHandler = context.getBean(P2pEventHandlerImpl.class);
   }
 
   public void test() {
@@ -36,6 +45,16 @@ public class NodeInfoServiceTest {
         200, ByteString.EMPTY);
     witnessProductBlockService.validWitnessProductTwoBlock(blockCapsule1);
     witnessProductBlockService.validWitnessProductTwoBlock(blockCapsule2);
+
+    //add peer
+    InetSocketAddress a1 = new InetSocketAddress("127.0.0.1", 10001);
+    Channel c1 = mock(Channel.class);
+    Mockito.when(c1.getInetSocketAddress()).thenReturn(a1);
+    Mockito.when(c1.getInetAddress()).thenReturn(a1.getAddress());
+    p2pEventHandler.onConnect(c1);
+    Assert.assertEquals(1, PeerManager.getPeers().size());
+
+    //test setConnectInfo
     NodeInfo nodeInfo = nodeInfoService.getNodeInfo();
     Assert.assertEquals(nodeInfo.getConfigNodeInfo().getCodeVersion(), Version.getVersion());
     Assert.assertEquals(nodeInfo.getCheatWitnessInfoMap().size(), 1);
