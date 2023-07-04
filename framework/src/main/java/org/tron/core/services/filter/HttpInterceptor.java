e8a921f33b605aa9151be9378d09d000f3cd90fc
wubin01 <wb_bupt@163.com>
2023-07-04 15:08:03 +0800

    feat(monitor): optimize code

--- framework/src/main/java/org/tron/core/services/filter/HttpInterceptor.java
+++ framework/src/main/java/org/tron/core/services/filter/HttpInterceptor.java
@@ -30,46 +30,38 @@ public class HttpInterceptor implements Filter {
   @Override
   public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
     try {
-      if (request instanceof HttpServletRequest) {
-        endpoint = ((HttpServletRequest) request).getRequestURI();
-
-        CharResponseWrapper responseWrapper = new CharResponseWrapper(
-            (HttpServletResponse) response);
-        chain.doFilter(request, responseWrapper);
-
-        HttpServletResponse resp = (HttpServletResponse) response;
-        int size = responseWrapper.getByteSize();
-
-        MetricsUtil.meterMark(MetricsKey.NET_API_OUT_TRAFFIC, size);
-        MetricsUtil.meterMark(MetricsKey.NET_API_QPS);
-
-        if (resp.getStatus() >= HTTP_BAD_REQUEST && resp.getStatus() <= HTTP_NOT_ACCEPTABLE) {
-          MetricsUtil.meterMark(MetricsKey.NET_API_FAIL_QPS);
-          Metrics.histogramObserve(MetricKeys.Histogram.HTTP_BYTES,
-                  size, MetricLabels.UNDEFINED, String.valueOf(responseWrapper.getStatus()));
-          return;
-        }
-
-        if (resp.getStatus() == HTTP_SUCCESS) {
-          MetricsUtil.meterMark(MetricsKey.NET_API_DETAIL_QPS + endpoint);
-        } else {
-          MetricsUtil.meterMark(MetricsKey.NET_API_FAIL_QPS);
-          MetricsUtil.meterMark(MetricsKey.NET_API_DETAIL_FAIL_QPS + endpoint);
-        }
-
-        MetricsUtil.meterMark(MetricsKey.NET_API_DETAIL_OUT_TRAFFIC + endpoint, size);
-        Metrics.histogramObserve(MetricKeys.Histogram.HTTP_BYTES,
-                size, endpoint, String.valueOf(responseWrapper.getStatus()));
-      } else {
+      if (!(request instanceof HttpServletRequest)) {
         chain.doFilter(request, response);
+        return;
       }
+      endpoint = ((HttpServletRequest) request).getRequestURI();
+      CharResponseWrapper responseWrapper = new CharResponseWrapper(
+              (HttpServletResponse) response);
+      chain.doFilter(request, responseWrapper);
+      HttpServletResponse resp = (HttpServletResponse) response;
+      int size = responseWrapper.getByteSize();
+      MetricsUtil.meterMark(MetricsKey.NET_API_OUT_TRAFFIC, size);
+      MetricsUtil.meterMark(MetricsKey.NET_API_QPS);
+      if (resp.getStatus() >= HTTP_BAD_REQUEST && resp.getStatus() <= HTTP_NOT_ACCEPTABLE) {
+        MetricsUtil.meterMark(MetricsKey.NET_API_FAIL_QPS);
+        Metrics.histogramObserve(MetricKeys.Histogram.HTTP_BYTES,
+                size, MetricLabels.UNDEFINED, String.valueOf(responseWrapper.getStatus()));
+        return;
+      }
+      if (resp.getStatus() == HTTP_SUCCESS) {
+        MetricsUtil.meterMark(MetricsKey.NET_API_DETAIL_QPS + endpoint);
+      } else {
+        MetricsUtil.meterMark(MetricsKey.NET_API_FAIL_QPS);
+        MetricsUtil.meterMark(MetricsKey.NET_API_DETAIL_FAIL_QPS + endpoint);
+      }
+      MetricsUtil.meterMark(MetricsKey.NET_API_DETAIL_OUT_TRAFFIC + endpoint, size);
+      Metrics.histogramObserve(MetricKeys.Histogram.HTTP_BYTES,
+              size, endpoint, String.valueOf(responseWrapper.getStatus()));
     } catch (Exception e) {
-      if (MetricsUtil.getMeters(MetricsKey.NET_API_DETAIL_QPS).containsKey(
-              MetricsKey.NET_API_DETAIL_QPS + endpoint)) {
-        MetricsUtil.meterMark(MetricsKey.NET_API_DETAIL_FAIL_QPS
-                + endpoint, 1);
-        MetricsUtil.meterMark(MetricsKey.NET_API_DETAIL_QPS
-                + endpoint, 1);
+      String key = MetricsKey.NET_API_DETAIL_QPS + endpoint;
+      if (MetricsUtil.getMeters(MetricsKey.NET_API_DETAIL_QPS).containsKey(key)) {
+        MetricsUtil.meterMark(key, 1);
+        MetricsUtil.meterMark(MetricsKey.NET_API_DETAIL_QPS + endpoint, 1);
       }
       MetricsUtil.meterMark(MetricsKey.NET_API_QPS, 1);
       MetricsUtil.meterMark(MetricsKey.NET_API_FAIL_QPS, 1);
@@ -79,7 +71,6 @@ public class HttpInterceptor implements Filter {
   @Override
   public void destroy() {
   }
-
 }
 
 
