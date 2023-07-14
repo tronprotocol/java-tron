029c0408c1341d1c449f7bc96de477d3572d8acd
forfreeday <forfreeday@163.com>
2023-07-14 20:14:58 +0800

    feat(Servlet): Optimize Http Servlet
    1. Optimized Util getAddress method
    2. add `GetBrokerageServletTest` test case
    3. add `GetRewardServletTest` test case

---
+++ framework/src/test/java/org/tron/core/services/http/GetBrokerageServletTest.java
@@ -0,0 +1,43 @@
+package org.tron.core.services.http;
+
+import static org.junit.Assert.assertEquals;
+
+import com.alibaba.fastjson.JSONObject;
+
+import java.io.UnsupportedEncodingException;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+public class GetBrokerageServletTest {
+
+  private MockHttpServletRequest request;
+  private MockHttpServletResponse response;
+
+  @Before
+  public void setUp() {
+    request = new MockHttpServletRequest();
+    request.setMethod("POST");
+    request.setContentType("application/x-www-form-urlencoded");
+    request.setCharacterEncoding("UTF-8");
+
+    response = new MockHttpServletResponse();
+  }
+
+  @Test
+  public void getBrokerageTest() {
+    request.addParameter("address", "");
+    GetRewardServlet getRewardServlet = new GetRewardServlet();
+    getRewardServlet.doPost(request, response);
+    try {
+      String contentAsString = response.getContentAsString();
+      JSONObject result = JSONObject.parseObject(contentAsString);
+      String content = (String) result.get("Error");
+      assertEquals(content, "INVALID address, invalid request parameter");
+    } catch (UnsupportedEncodingException e) {
+      e.printStackTrace();
+    }
+  }
+}
