6b88acfc2721b2b2cb56f58d0c620ae712c6f0f3
halibobo1205 <halibobo1205@gmail.com>
2023-07-28 16:28:48 +0800

    feat(db): add reward-cache db

---
+++ chainbase/src/main/java/org/tron/core/store/RewardCacheStore.java
@@ -0,0 +1,52 @@
+package org.tron.core.store;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.tron.common.utils.ByteArray;
+import org.tron.core.db.TronDatabase;
+import org.tron.core.db.common.iterator.DBIterator;
+
+@Slf4j(topic = "DB")
+@Component
+public class RewardCacheStore extends TronDatabase<byte[]> {
+
+  @Autowired
+  private RewardCacheStore(@Value("reward-cache") String dbName) {
+    super(dbName);
+  }
+
+  @Override
+  public byte[] get(byte[] key) {
+    return dbSource.getData(key);
+  }
+
+  @Override
+  public void put(byte[] key, byte[] item) {
+    dbSource.putData(key, item);
+  }
+
+  @Override
+  public void delete(byte[] key) {
+    dbSource.deleteData(key);
+  }
+
+  @Override
+  public boolean has(byte[] key) {
+    return dbSource.getData(key) != null;
+  }
+
+  public void putReward(byte[] key, long reward) {
+    put(key, ByteArray.fromLong(reward));
+  }
+
+  public long getReward(byte[] key) {
+    return ByteArray.toLong(get(key));
+  }
+
+  @Override
+  public DBIterator iterator() {
+    return ((DBIterator) dbSource.iterator());
+  }
+}
