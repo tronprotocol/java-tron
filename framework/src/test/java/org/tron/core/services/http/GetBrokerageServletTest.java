75d832e2391ac789d32354543ee379f34eb05878
liukai <kayle.liu@tron.network>
2023-08-14 19:32:51 +0800

    test(case): add routine use cases

--- framework/src/test/java/org/tron/core/services/http/GetBrokerageServletTest.java
+++ framework/src/test/java/org/tron/core/services/http/GetBrokerageServletTest.java
@@ -1,21 +1,36 @@
 package org.tron.core.services.http;
 
-import static org.junit.Assert.assertEquals;
-
 import com.alibaba.fastjson.JSONObject;
 
 import java.io.UnsupportedEncodingException;
+import javax.annotation.Resource;
 
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.mock.web.MockHttpServletRequest;
 import org.springframework.mock.web.MockHttpServletResponse;
+import org.tron.common.BaseTest;
+import org.tron.core.Constant;
+import org.tron.core.config.args.Args;
 
-public class GetBrokerageServletTest {
+public class GetBrokerageServletTest extends BaseTest {
 
   private MockHttpServletRequest request;
   private MockHttpServletResponse response;
+
+  @Resource
+  private  GetBrokerageServlet getBrokerageServlet;
+
+
+  static {
+    dbPath = "db_GetBrokerageServlet_test";
+    Args.setParam(
+            new String[]{
+                "--output-directory", dbPath,
+            }, Constant.TEST_CONF
+    );
+  }
 
   @Before
   public void setUp() {
@@ -28,19 +43,34 @@ public class GetBrokerageServletTest {
   }
 
   @Test
-  public void getBrokerageTest() {
-    request.addParameter("address", "");
-    GetRewardServlet getRewardServlet = new GetRewardServlet();
-    getRewardServlet.doPost(request, response);
+  public void getBrokerageValueTest() {
+    int expect = 20;
+    request.addParameter("address", "27VZHn9PFZwNh7o2EporxmLkpe157iWZVkh");
+    getBrokerageServlet.doPost(request, response);
     try {
       String contentAsString = response.getContentAsString();
       JSONObject result = JSONObject.parseObject(contentAsString);
-      int reward = (int)result.get("reward");
-      Assert.assertEquals(0, reward);
+      int brokerage = (int)result.get("brokerage");
+      Assert.assertEquals(expect, brokerage);
+    } catch (UnsupportedEncodingException e) {
+      Assert.fail(e.getMessage());
+    }
+  }
+
+  @Test
+  public void getBrokerageTest() {
+    int expect = 0;
+    request.addParameter("address", "");
+    getBrokerageServlet.doPost(request, response);
+    try {
+      String contentAsString = response.getContentAsString();
+      JSONObject result = JSONObject.parseObject(contentAsString);
+      int brokerage = (int)result.get("brokerage");
+      Assert.assertEquals(expect, brokerage);
       String content = (String) result.get("Error");
       Assert.assertNull(content);
     } catch (UnsupportedEncodingException e) {
-      e.printStackTrace();
+      Assert.fail(e.getMessage());
     }
   }
 }
