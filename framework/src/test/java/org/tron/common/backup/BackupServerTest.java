a9c4f4324c895f2f3c74eb7d7fb13add719419fe
halibobo1205 <82020050+halibobo1205@users.noreply.github.com>
2023-08-15 11:23:32 +0800

    feat(all): tune single Thread into SingleThreadExecutor (#5410)

---
+++ framework/src/test/java/org/tron/common/backup/BackupServerTest.java
@@ -0,0 +1,44 @@
+package org.tron.common.backup;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.tron.common.backup.socket.BackupServer;
+import org.tron.common.parameter.CommonParameter;
+import org.tron.core.Constant;
+import org.tron.core.config.args.Args;
+
+
+public class BackupServerTest {
+
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+  private BackupServer backupServer;
+
+  @Before
+  public void setUp() throws Exception {
+    Args.setParam(new String[]{"-d", temporaryFolder.newFolder().toString()}, Constant.TEST_CONF);
+    CommonParameter.getInstance().setBackupPort(80);
+    List<String> members = new ArrayList<>();
+    members.add("127.0.0.2");
+    CommonParameter.getInstance().setBackupMembers(members);
+    BackupManager backupManager = new BackupManager();
+    backupManager.init();
+    backupServer = new BackupServer(backupManager);
+  }
+
+  @After
+  public void tearDown() {
+    backupServer.close();
+    Args.clearParam();
+  }
+
+  @Test
+  public void test() throws InterruptedException {
+    backupServer.initServer();
+  }
+}
