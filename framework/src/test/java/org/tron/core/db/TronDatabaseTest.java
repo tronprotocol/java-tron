c4d630e138624a293b4ae723078d437ebbe1e97f
halibobo1205 <halibobo1205@gmail.com>
2023-08-18 10:10:36 +0800

    fix CI test

--- framework/src/test/java/org/tron/core/db/TronDatabaseTest.java
+++ framework/src/test/java/org/tron/core/db/TronDatabaseTest.java
@@ -1,18 +1,38 @@
 package org.tron.core.db;
 
 import com.google.protobuf.InvalidProtocolBufferException;
+import java.io.IOException;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.ExpectedException;
+import org.junit.rules.TemporaryFolder;
 import org.rocksdb.RocksDB;
+import org.tron.core.Constant;
+import org.tron.core.config.args.Args;
 import org.tron.core.exception.BadItemException;
 import org.tron.core.exception.ItemNotFoundException;
 
 public class TronDatabaseTest extends TronDatabase<String> {
 
+  @ClassRule
+  public static final TemporaryFolder temporaryFolder = new TemporaryFolder();
+
   static {
     RocksDB.loadLibrary();
+  }
+
+  @BeforeClass
+  public static void initArgs() throws IOException {
+    Args.setParam(new String[]{"-d", temporaryFolder.newFolder().toString()}, Constant.TEST_CONF);
+  }
+
+  @AfterClass
+  public static void destroy() {
+    Args.clearParam();
   }
 
   @Override
