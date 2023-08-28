a91e92a488a4f390270e91bc29527622b3686254
liukai <kayle.liu@tron.network>
2023-08-28 16:18:47 +0800

    test(case): add test cases
    1. add AccountAssetStoreTest
    2. add AssetIssueStoreTest
    3. add AssetIssueV2StoreTest
    4. add ContractStoreTest
    5. add DelegatedResourceStoreTest
    6. add DelegationStoreTest

---
+++ framework/src/test/java/org/tron/core/db/DelegatedResourceStoreTest.java
@@ -0,0 +1,73 @@
+package org.tron.core.db;
+
+import com.google.protobuf.ByteString;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.tron.common.BaseTest;
+import org.tron.common.utils.ByteArray;
+import org.tron.core.Constant;
+import org.tron.core.capsule.DelegatedResourceCapsule;
+import org.tron.core.config.args.Args;
+import org.tron.core.store.DelegatedResourceStore;
+
+import javax.annotation.Resource;
+
+public class DelegatedResourceStoreTest extends BaseTest {
+  private static final long BALANCE = 1_000_000;
+  private static final long EXPIRE_TIME = 1000L;
+  private static final String OWNER_ADDRESS = "111111111111";
+  private static final String RECEIVER_ADDRESS = "222222222222";
+  private static DelegatedResourceCapsule delegatedResourceCapsule;
+
+  @Resource
+  private DelegatedResourceStore delegatedResourceStore;
+
+  static {
+    dbPath = "db_DelegatedResourceStore_test";
+    Args.setParam(
+            new String[]{
+                    "--output-directory", dbPath,
+            },
+            Constant.TEST_CONF
+    );
+  }
+
+  @Before
+  public void init () {
+    delegatedResourceCapsule = create(OWNER_ADDRESS);
+    delegatedResourceStore.put(delegatedResourceCapsule.createDbKey(),
+            delegatedResourceCapsule);
+  }
+
+  @Test
+  public void testGet() {
+    DelegatedResourceCapsule delegatedResource = delegatedResourceStore
+            .get(delegatedResourceCapsule.createDbKey());
+    Assert.assertNotNull(delegatedResource);
+    Assert.assertEquals(delegatedResourceCapsule.getFrom(), delegatedResource.getFrom());
+  }
+
+  @Test
+  public void testPut() {
+    DelegatedResourceCapsule delegatedResourceCapsule = create("333333333333");
+    byte[] key = delegatedResourceCapsule.createDbKey();
+    delegatedResourceStore.put(key, delegatedResourceCapsule);
+
+    DelegatedResourceCapsule delegatedResourceCapsule1 = delegatedResourceStore.get(key);
+    Assert.assertNotNull(delegatedResourceCapsule1);
+    Assert.assertEquals(BALANCE, delegatedResourceCapsule1.getFrozenBalanceForEnergy());
+  }
+
+  public DelegatedResourceCapsule create(String address) {
+    byte[] ownerAddress = ByteArray.fromHexString(address);
+    byte[] receiverAddress = ByteArray.fromHexString(RECEIVER_ADDRESS);
+    DelegatedResourceCapsule delegatedResourceCapsule = new DelegatedResourceCapsule(
+            ByteString.copyFrom(ownerAddress),
+            ByteString.copyFrom(receiverAddress));
+
+    delegatedResourceCapsule.setFrozenBalanceForEnergy(BALANCE, EXPIRE_TIME);
+    return delegatedResourceCapsule;
+  }
+
+}
