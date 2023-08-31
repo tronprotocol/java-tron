91b6f333c5c601c1173fd6ab399aab0295938dab
chengtx01 <allen_cheng1@163.com>
2023-08-31 12:03:38 +0800

    test(net):add message handler test

--- framework/src/test/java/org/tron/core/net/messagehandler/FetchInvDataMsgHandlerTest.java
+++ framework/src/test/java/org/tron/core/net/messagehandler/FetchInvDataMsgHandlerTest.java
@@ -1,5 +1,65 @@
 package org.tron.core.net.messagehandler;
 
+import com.google.common.cache.Cache;
+import com.google.common.cache.CacheBuilder;
+import java.lang.reflect.Field;
+import java.util.concurrent.TimeUnit;
+import java.util.LinkedList;
+import java.util.List;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.tron.common.utils.ReflectUtils;
+import org.tron.common.utils.Sha256Hash;
+import org.tron.core.capsule.BlockCapsule;
+import org.tron.core.config.Parameter;
+import org.tron.core.net.TronNetDelegate;
+import org.tron.core.net.message.adv.BlockMessage;
+import org.tron.core.net.message.adv.FetchInvDataMessage;
+import org.tron.core.net.peer.Item;
+import org.tron.core.net.peer.PeerConnection;
+import org.tron.core.net.service.adv.AdvService;
+import org.tron.protos.Protocol;
+
+
+
 public class FetchInvDataMsgHandlerTest {
 
+  @Test
+  public void testProcessMessage() throws Exception {
+    FetchInvDataMsgHandler fetchInvDataMsgHandler = new FetchInvDataMsgHandler();
+    PeerConnection peer = Mockito.mock(PeerConnection.class);
+    TronNetDelegate tronNetDelegate = Mockito.mock(TronNetDelegate.class);
+    AdvService advService = Mockito.mock(AdvService.class);
+
+    Field field = FetchInvDataMsgHandler.class.getDeclaredField("tronNetDelegate");
+    field.setAccessible(true);
+    field.set(fetchInvDataMsgHandler, tronNetDelegate);
+
+    Mockito.when(tronNetDelegate.allowPBFT()).thenReturn(false);
+
+    BlockCapsule.BlockId blockId = new BlockCapsule.BlockId();
+    List<Sha256Hash> blockIds = new LinkedList<>();
+    blockIds.add(blockId);
+
+    Cache<Item, Long> advInvSpread = CacheBuilder.newBuilder().maximumSize(20000)
+        .expireAfterWrite(1, TimeUnit.HOURS).recordStats().build();
+    Mockito.when(peer.getAdvInvSpread()).thenReturn(advInvSpread);
+    Mockito.when(peer.isNeedSyncFromUs()).thenReturn(true);
+    Mockito.when(peer.isSyncFinish()).thenReturn(false);
+    Mockito.when(peer.getBlockBothHave()).thenReturn(blockId);
+    Cache<Sha256Hash, Long> syncBlockIdCache = CacheBuilder.newBuilder()
+        .maximumSize(2 * Parameter.NetConstants.SYNC_FETCH_BATCH_NUM).recordStats().build();
+    Mockito.when(peer.getSyncBlockIdCache()).thenReturn(syncBlockIdCache);
+    Mockito.when(peer.getLastSyncBlockId()).thenReturn(blockId);
+    BlockCapsule blockCapsule = new BlockCapsule(1, Sha256Hash.ZERO_HASH,
+        System.currentTimeMillis(), Sha256Hash.ZERO_HASH.getByteString());
+    Mockito.when(advService.getMessage(new Item(blockId, Protocol.Inventory.InventoryType.BLOCK)))
+        .thenReturn(new BlockMessage(blockCapsule));
+    ReflectUtils.setFieldValue(fetchInvDataMsgHandler, "advService", advService);
+
+    fetchInvDataMsgHandler.processMessage(peer,
+        new FetchInvDataMessage(blockIds, Protocol.Inventory.InventoryType.BLOCK));
+    Assert.assertNotNull(syncBlockIdCache.getIfPresent(blockId));
+  }
 }
---
+++ framework/src/test/java/org/tron/core/net/messagehandler/PbftDataSyncHandlerTest.java
@@ -0,0 +1,51 @@
+package org.tron.core.net.messagehandler;
+
+import com.google.protobuf.ByteString;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mockito;
+import org.tron.common.utils.Sha256Hash;
+import org.tron.core.ChainBaseManager;
+import org.tron.core.capsule.BlockCapsule;
+import org.tron.core.capsule.PbftSignCapsule;
+import org.tron.core.db.PbftSignDataStore;
+import org.tron.core.net.message.pbft.PbftCommitMessage;
+import org.tron.core.store.DynamicPropertiesStore;
+import org.tron.protos.Protocol;
+
+public class PbftDataSyncHandlerTest {
+  @Test
+  public void testProcessMessage() throws Exception {
+    PbftDataSyncHandler pbftDataSyncHandler = new PbftDataSyncHandler();
+    BlockCapsule blockCapsule = new BlockCapsule(1, Sha256Hash.ZERO_HASH,
+        System.currentTimeMillis(), ByteString.EMPTY);
+    Protocol.PBFTMessage.Raw.Builder rawBuilder = Protocol.PBFTMessage.Raw.newBuilder();
+    rawBuilder.setViewN(blockCapsule.getNum())
+        .setEpoch(0)
+        .setDataType(Protocol.PBFTMessage.DataType.BLOCK)
+        .setMsgType(Protocol.PBFTMessage.MsgType.PREPREPARE)
+        .setData(blockCapsule.getBlockId().getByteString());
+    Protocol.PBFTMessage.Raw raw = rawBuilder.build();
+    PbftSignCapsule pbftSignCapsule = new PbftSignCapsule(raw.toByteString(), new ArrayList<>());
+    PbftCommitMessage pbftCommitMessage = new PbftCommitMessage(pbftSignCapsule);
+    pbftDataSyncHandler.processMessage(null, pbftCommitMessage);
+    Assert.assertEquals(Protocol.PBFTMessage.Raw.parseFrom(
+        pbftCommitMessage.getPBFTCommitResult().getData()).getViewN(), 1);
+
+    DynamicPropertiesStore dynamicPropertiesStore = Mockito.mock(DynamicPropertiesStore.class);
+    PbftSignDataStore pbftSignDataStore = Mockito.mock(PbftSignDataStore.class);
+    ChainBaseManager chainBaseManager = Mockito.mock(ChainBaseManager.class);
+    Mockito.when(chainBaseManager.getDynamicPropertiesStore()).thenReturn(dynamicPropertiesStore);
+    Mockito.when(dynamicPropertiesStore.allowPBFT()).thenReturn(true);
+    Mockito.when(dynamicPropertiesStore.getMaintenanceTimeInterval()).thenReturn(600L);
+    Mockito.when(chainBaseManager.getPbftSignDataStore()).thenReturn(pbftSignDataStore);
+
+    Field field = PbftDataSyncHandler.class.getDeclaredField("chainBaseManager");
+    field.setAccessible(true);
+    field.set(pbftDataSyncHandler, chainBaseManager);
+
+    pbftDataSyncHandler.processPBFTCommitData(blockCapsule);
+  }
+}
