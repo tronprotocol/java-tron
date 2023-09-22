c78c42e77a8db0fcbd3ece5fe1e83cefc71debbd
lurais <107600021+lurais@users.noreply.github.com>
2023-09-22 17:02:41 +0800

    test(db): add test case (#5503)
    Co-authored-by: halibobo1205 <82020050+halibobo1205@users.noreply.github.com>
    Co-authored-by: morgan.peng <morgan.p@qq.com>

---
+++ framework/src/test/java/org/tron/core/db/MarketOrderStoreTest.java
@@ -0,0 +1,47 @@
+package org.tron.core.db;
+
+import com.google.protobuf.ByteString;
+import javax.annotation.Resource;
+import org.junit.Assert;
+import org.junit.Test;
+import org.tron.common.BaseTest;
+import org.tron.core.Constant;
+import org.tron.core.capsule.MarketOrderCapsule;
+import org.tron.core.config.args.Args;
+import org.tron.core.store.MarketOrderStore;
+import org.tron.protos.Protocol;
+
+public class MarketOrderStoreTest extends BaseTest {
+
+  @Resource
+  private MarketOrderStore marketOrderStore;
+
+  static {
+    Args.setParam(
+        new String[]{
+            "--output-directory", dbPath()
+        },
+        Constant.TEST_CONF
+    );
+  }
+
+  @Test
+  public void testGet() throws Exception {
+    byte[] orderId = "testGet".getBytes();
+    marketOrderStore.put(orderId,
+        new MarketOrderCapsule(Protocol.MarketOrder.newBuilder()
+            .setOrderId(ByteString.copyFrom(orderId))
+            .setSellTokenId(ByteString.copyFrom("addr1".getBytes()))
+            .setSellTokenQuantity(200L)
+            .setBuyTokenId(ByteString.copyFrom("addr2".getBytes()))
+            .setBuyTokenQuantity(100L)
+            .build()));
+    final MarketOrderCapsule result = marketOrderStore.get(orderId);
+    Assert.assertNotNull(result);
+    Assert.assertEquals(new String(result.getSellTokenId()), "addr1");
+    Assert.assertEquals(result.getSellTokenQuantity(), 200L);
+    Assert.assertEquals(new String(result.getBuyTokenId()), "addr2");
+    Assert.assertEquals(result.getBuyTokenQuantity(), 100L);
+  }
+
+}
