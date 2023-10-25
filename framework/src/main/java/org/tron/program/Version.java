381c52c5830ab60db02dd7a94d6e8c0dbe0ffef5
liukai <kayle.liu@tron.network>
2023-10-25 19:31:35 +0800

    update a new version. version name:GreatVoyage-v4.7.2-140-g9d13f9cb69,version code:18173

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -2,8 +2,8 @@ package org.tron.program;
 
 public class Version {
 
-  public static final String VERSION_NAME = "GreatVoyage-v4.7.1.1-343-g959e0199de";
-  public static final String VERSION_CODE = "18031";
+  public static final String VERSION_NAME = "GreatVoyage-v4.7.2-140-g9d13f9cb69";
+  public static final String VERSION_CODE = "18173";
   private static final String VERSION = "4.7.3";
 
   public static String getVersion() {
