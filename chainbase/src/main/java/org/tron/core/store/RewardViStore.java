cc753a80cc6c2b392386549cca714dc6b8d9326a
halibobo1205 <halibobo1205@gmail.com>
2023-12-15 16:15:43 +0800

    change old cal to new cal

--- chainbase/src/main/java/org/tron/core/store/RewardViStore.java
+++ chainbase/src/main/java/org/tron/core/store/RewardViStore.java
@@ -4,16 +4,14 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
-import org.tron.common.utils.ByteArray;
 import org.tron.core.db.TronDatabase;
-import org.tron.core.db.common.iterator.DBIterator;
 
 @Slf4j(topic = "DB")
 @Component
-public class RewardCacheStore extends TronDatabase<byte[]> {
+public class RewardViStore extends TronDatabase<byte[]> {
 
   @Autowired
-  private RewardCacheStore(@Value("reward-cache") String dbName) {
+  private RewardViStore(@Value("reward-vi") String dbName) {
     super(dbName);
   }
 
@@ -29,25 +27,11 @@ public class RewardCacheStore extends TronDatabase<byte[]> {
 
   @Override
   public void delete(byte[] key) {
-    dbSource.deleteData(key);
+    throw new UnsupportedOperationException();
   }
 
   @Override
   public boolean has(byte[] key) {
     return dbSource.getData(key) != null;
-  }
-
-  public void putReward(byte[] key, long reward) {
-    put(key, ByteArray.fromLong(reward));
-  }
-
-  public long getReward(byte[] key) {
-    byte[] value = get(key);
-    return value == null ? -1 : ByteArray.toLong(value);
-  }
-
-  @Override
-  public DBIterator iterator() {
-    return ((DBIterator) dbSource.iterator());
   }
 }
