74cd4fe8aedd36cb88f6f722cbe6dec8db86c686
halibobo1205 <halibobo1205@gmail.com>
2023-12-29 15:58:24 +0800

    remove query reward-vi root from node-info

--- common/src/main/java/org/tron/common/entity/NodeInfo.java
+++ common/src/main/java/org/tron/common/entity/NodeInfo.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-
-import lombok.Getter;
-import lombok.Setter;
 import org.tron.common.entity.NodeInfo.MachineInfo.DeadLockThreadInfo;
 import org.tron.common.entity.NodeInfo.MachineInfo.MemoryDescInfo;
 import org.tron.protos.Protocol;
@@ -31,12 +28,6 @@ public class NodeInfo {
   private MachineInfo machineInfo;
 
   private Map<String, String> cheatWitnessInfoMap = new HashMap<>();
-
-  /* rewardViRoot */
-
-  @Getter
-  @Setter
-  private String rewardViRoot;
 
   public long getBeginSyncNum() {
     return beginSyncNum;
