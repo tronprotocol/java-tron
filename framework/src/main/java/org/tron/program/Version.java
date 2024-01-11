3b7a4c3762ce61ddd731b48013d9472f1ca6f0c1
halibobo1205 <halibobo1205@gmail.com>
2024-01-11 11:14:45 +0800

    feat(version): update version to 4.7.3.1

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
   public static final String VERSION_NAME = "GreatVoyage-v4.7.2-140-g9d13f9cb69";
   public static final String VERSION_CODE = "18173";
-  private static final String VERSION = "4.7.3";
+  private static final String VERSION = "4.7.3.1";
 
   public static String getVersion() {
     return VERSION;
