555a839be9d5b350c496306628feb22876d373c8
Asuka <yanghang8612@163.com>
2024-11-28 13:19:14 +0800

    feat(version): update version to 4.7.7

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
   public static final String VERSION_NAME = "GreatVoyage-v4.7.5-64-g4103dfeb63";
   public static final String VERSION_CODE = "18372";
-  private static final String VERSION = "4.7.6";
+  private static final String VERSION = "4.7.7";
 
   public static String getVersion() {
     return VERSION;
