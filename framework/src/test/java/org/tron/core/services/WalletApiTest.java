e9795877a91cf58d8b976b28924013ffaa35bd66
Sunny Jiao <>
2025-07-01 15:11:52 +0800

    delete unused code

--- framework/src/test/java/org/tron/core/services/WalletApiTest.java
+++ framework/src/test/java/org/tron/core/services/WalletApiTest.java
@@ -14,14 +14,6 @@ import org.tron.api.WalletGrpc;
 import org.tron.common.application.Application;
 import org.tron.common.application.ApplicationFactory;
 import org.tron.common.application.TronApplicationContext;
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.stream.Collectors;
-import org.tron.common.utils.JsonUtilTest;
 import org.tron.common.utils.PublicMethod;
 import org.tron.core.Constant;
 import org.tron.core.config.DefaultConfig;
@@ -42,18 +34,8 @@ public class WalletApiTest {
   public static void init() throws IOException {
     Args.setParam(new String[]{ "-d", temporaryFolder.newFolder().toString(),
         "--p2p-disable", "true"}, Constant.TEST_CONF);
-    int rpcPort = PublicMethod.chooseRandomPort();
-    Args.getInstance().setRpcPort(rpcPort);
+    Args.getInstance().setRpcPort(PublicMethod.chooseRandomPort());
     Args.getInstance().setRpcEnable(true);
-
-    // http request for /wallet/xx on FullNode
-    Args.getInstance().setFullNodeHttpPort(rpcPort + 1);
-    Args.getInstance().setFullNodeHttpEnable(true);
-
-    // http request for /walletsolidity/xx on FullNode
-    Args.getInstance().setSolidityHttpPort(rpcPort + 2);
-    Args.getInstance().setSolidityNodeHttpEnable(true);
-
     context = new TronApplicationContext(DefaultConfig.class);
     appT = ApplicationFactory.create(context);
     appT.startup();
@@ -70,49 +52,6 @@ public class WalletApiTest {
     Assert.assertTrue(walletStub.listNodes(EmptyMessage.getDefaultInstance())
         .getNodesList().isEmpty());
   }
-
-  @Test
-  public void getPaginatedNowWitnessListTest() {
-    try {
-      String url = "http://127.0.0.1:" + Args.getInstance().getFullNodeHttpPort() +
-          "/wallet/getpaginatednowwitnesslist";
-      HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-      connection.setRequestMethod("POST");
-      connection.setRequestProperty("Content-Type", "application/json");
-      connection.setDoOutput(true);
-
-      // Create JSON payload
-      String jsonPayload = "{\"offset\": 0, \"limit\": 1000, \"visible\": true}";
-      connection.getOutputStream().write(jsonPayload.getBytes());
-      connection.getOutputStream().flush();
-
-      Assert.assertEquals("HTTP response code should be 200", 200, connection.getResponseCode());
-    } catch (Exception e) {
-      Assert.fail("HTTP request failed: " + e.getMessage());
-    }
-  }
-
-  @Test
-  public void getPaginatedNowWitnessListSolidityTest() {
-    try {
-      String url = "http://127.0.0.1:" + Args.getInstance().getSolidityHttpPort() +
-          "/walletsolidity/getpaginatednowwitnesslist";
-      HttpURLConnection connection = (HttpURLConnection) new URL(url).openConnection();
-      connection.setRequestMethod("POST");
-      connection.setRequestProperty("Content-Type", "application/json");
-      connection.setDoOutput(true);
-
-      // Create JSON payload
-      String jsonPayload = "{\"offset\": 0, \"limit\": 1000, \"visible\": true}";
-      connection.getOutputStream().write(jsonPayload.getBytes());
-      connection.getOutputStream().flush();
-
-      Assert.assertEquals("HTTP response code should be 200", 200, connection.getResponseCode());
-    } catch (Exception e) {
-      Assert.fail("HTTP solidity request failed: " + e.getMessage());
-    }
-  }
-
 
   @After
   public void destroy() {
