e9403b08c3f0e4ba625c93753c6530fb3a65e3f1
liuxincheng <lxcmyf@qq.com>
2025-07-07 15:40:28 +0800

    feat(approving): convert the approving window configuration to chain governance

--- common/src/main/java/org/tron/core/Constant.java
+++ common/src/main/java/org/tron/core/Constant.java
@@ -39,6 +39,12 @@ public class Constant {
   public static final long MAX_CONTRACT_RESULT_SIZE = 2L;
   public static final long PB_DEFAULT_ENERGY_LIMIT = 0L;
   public static final long CREATOR_DEFAULT_ENERGY_LIMIT = 1000 * 10_000L;
+  public static final long MIN_PROPOSAL_VOTING_WINDOW = 0L; // Number of blocks to 0 day
+  public static final long MAX_PROPOSAL_VOTING_WINDOW = 10512000L; // Number of blocks to 365 days
+  /**
+   * default number of blocks to 3 days
+   */
+  public static final long DEFAULT_PROPOSAL_VOTING_WINDOW = 86400L;
 
 
   // Numbers
@@ -405,4 +411,5 @@ public class Constant {
   public static final String COMMITTEE_ALLOW_TVM_CANCUN = "committee.allowTvmCancun";
 
   public static final String COMMITTEE_ALLOW_TVM_BLOB = "committee.allowTvmBlob";
+  public static final String COMMITTEE_PROPOSAL_VOTING_WINDOW = "committee.proposalVotingWindow";
 }
