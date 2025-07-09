26d3b4cfed1fc8c354bf008c92c1543811733c6c
federico <federico.zhen@tron.network>
2025-07-09 20:51:50 +0800

    make zk param initialization thread safe

--- framework/src/main/java/org/tron/core/zen/ZksnarkInitService.java
+++ framework/src/main/java/org/tron/core/zen/ZksnarkInitService.java
@@ -30,24 +30,30 @@ public class ZksnarkInitService {
       return;
     }
 
-    logger.info("init zk param begin");
+    synchronized (ZksnarkInitService.class) {
+      if (initialized.get()) {
+        logger.info("zk param already initialized");
+        return;
+      }
+      logger.info("init zk param begin");
 
-    String spendPath = getParamsFile("sapling-spend.params");
-    String spendHash = "25fd9a0d1c1be0526c14662947ae95b758fe9f3d7fb7f55e9b4437830dcc6215a7ce3ea465"
-        + "914b157715b7a4d681389ea4aa84438190e185d5e4c93574d3a19a";
+      String spendPath = getParamsFile("sapling-spend.params");
+      String spendHash = "25fd9a0d1c1be0526c14662947ae95b758fe9f3d7fb7f55e9b4437830dcc6215a7ce3ea"
+          + "465914b157715b7a4d681389ea4aa84438190e185d5e4c93574d3a19a";
 
-    String outputPath = getParamsFile("sapling-output.params");
-    String outputHash = "a1cb23b93256adce5bce2cb09cefbc96a1d16572675ceb691e9a3626ec15b5b546926ff1c"
-        + "536cfe3a9df07d796b32fdfc3e5d99d65567257bf286cd2858d71a6";
+      String outputPath = getParamsFile("sapling-output.params");
+      String outputHash = "a1cb23b93256adce5bce2cb09cefbc96a1d16572675ceb691e9a3626ec15b5b546926f"
+          + "f1c536cfe3a9df07d796b32fdfc3e5d99d65567257bf286cd2858d71a6";
 
-    try {
-      JLibrustzcash.librustzcashInitZksnarkParams(
-          new LibrustzcashParam.InitZksnarkParams(spendPath, spendHash, outputPath, outputHash));
+      try {
+        JLibrustzcash.librustzcashInitZksnarkParams(
+            new LibrustzcashParam.InitZksnarkParams(spendPath, spendHash, outputPath, outputHash));
+      } catch (ZksnarkException e) {
+        throw new TronError(e, TronError.ErrCode.ZCASH_INIT);
+      }
       initialized.set(true);
-    } catch (ZksnarkException e) {
-      throw new TronError(e, TronError.ErrCode.ZCASH_INIT);
+      logger.info("init zk param done");
     }
-    logger.info("init zk param done");
   }
 
   private static String getParamsFile(String fileName) {
