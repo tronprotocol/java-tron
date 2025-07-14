0a905ec071d4f10657067e9d0d8edab381793916
halibobo1205 <halibobo1205@gmail.com>
2025-07-14 17:56:14 +0800

    feat(test): add test for hashcode and web3_clientVersion

--- framework/src/test/java/org/tron/core/jsonrpc/JsonrpcServiceTest.java
+++ framework/src/test/java/org/tron/core/jsonrpc/JsonrpcServiceTest.java
@@ -969,4 +969,15 @@ public class JsonrpcServiceTest extends BaseTest {
       Assert.assertEquals("invalid block range params", e.getMessage());
     }
   }
+
+  @Test
+  public void testWeb3ClientVersion() {
+    try {
+      String[] versions = tronJsonRpc.web3ClientVersion().split("/");
+      String javaVersion = versions[versions.length - 1];
+      Assert.assertTrue("Java1.8".equals(javaVersion) || "Java17".equals(javaVersion));
+    } catch (Exception e) {
+      Assert.fail();
+    }
+  }
 }
