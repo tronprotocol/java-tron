c8504e2144db39956c9e959d7e3acebaa7215b60
jiangyuanshu <317787106@qq.com>
2025-09-02 18:53:56 +0800

    delete BaseNet, BaseNetTest

--- framework/src/test/java/org/tron/core/services/DelegationServiceTest.java
+++ framework/src/test/java/org/tron/core/services/DelegationServiceTest.java
@@ -5,29 +5,34 @@ import static org.tron.common.utils.client.Parameter.CommonConstant.ADD_PRE_FIX_
 
 import com.google.protobuf.ByteString;
 import io.grpc.ManagedChannelBuilder;
+import javax.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
 import org.tron.api.GrpcAPI.BytesMessage;
 import org.tron.api.GrpcAPI.TransactionExtention;
 import org.tron.api.WalletGrpc;
 import org.tron.api.WalletGrpc.WalletBlockingStub;
-import org.tron.common.application.TronApplicationContext;
+import org.tron.common.BaseTest;
+import org.tron.core.Constant;
 import org.tron.core.Wallet;
 import org.tron.core.capsule.AccountCapsule;
-import org.tron.core.db.Manager;
+import org.tron.core.config.args.Args;
 import org.tron.core.service.MortgageService;
 import org.tron.protos.contract.StorageContract.UpdateBrokerageContract;
 
 @Slf4j
-public class DelegationServiceTest {
+public class DelegationServiceTest extends BaseTest {
 
   private static String fullnode = "127.0.0.1:50051";
-  private MortgageService mortgageService;
-  private Manager manager;
+  @Resource
+  protected MortgageService mortgageService;
 
-  public DelegationServiceTest(TronApplicationContext context) {
-    mortgageService = context.getBean(MortgageService.class);
-    manager = context.getBean(Manager.class);
+  @BeforeClass
+  public static void init() {
+    Args.setParam(new String[] {"--output-directory", dbPath(), "--debug"},
+        Constant.TESTNET_CONF);
   }
 
   public static void testGrpc() {
@@ -42,7 +47,7 @@ public class DelegationServiceTest {
     System.out.println("getRewardInfo: " + walletStub.getRewardInfo(builder.build()).getNum());
     UpdateBrokerageContract.Builder updateBrokerageContract = UpdateBrokerageContract.newBuilder();
     updateBrokerageContract.setOwnerAddress(
-        ByteString.copyFrom(decodeFromBase58Check("TN3zfjYUmMFK3ZsHSsrdJoNRtGkQmZLBLz")))
+            ByteString.copyFrom(decodeFromBase58Check("TN3zfjYUmMFK3ZsHSsrdJoNRtGkQmZLBLz")))
         .setBrokerage(10);
     TransactionExtention transactionExtention = walletStub
         .updateBrokerage(updateBrokerageContract.build());
@@ -59,7 +64,7 @@ public class DelegationServiceTest {
     mortgageService.payStandbyWitness();
     Wallet.setAddressPreFixByte(ADD_PRE_FIX_BYTE_MAINNET);
     byte[] sr1 = decodeFromBase58Check("TLTDZBcPoJ8tZ6TTEeEqEvwYFk2wgotSfD");
-    long value = manager.getDelegationStore().getReward(cycle, sr1);
+    long value = dbManager.getDelegationStore().getReward(cycle, sr1);
     long tmp = 0;
     for (int i = 0; i < 27; i++) {
       tmp += 100000000 + i;
@@ -73,47 +78,48 @@ public class DelegationServiceTest {
     expect += 32000000;
     brokerageAmount = (long) (rate * 32000000);
     expect -= brokerageAmount;
-    value = manager.getDelegationStore().getReward(cycle, sr1);
+    value = dbManager.getDelegationStore().getReward(cycle, sr1);
     Assert.assertEquals(expect, value);
   }
 
   private void testWithdraw() {
     //init
-    manager.getDynamicPropertiesStore().saveCurrentCycleNumber(1);
+    dbManager.getDynamicPropertiesStore().saveCurrentCycleNumber(1);
     testPay(1);
-    manager.getDynamicPropertiesStore().saveCurrentCycleNumber(2);
+    dbManager.getDynamicPropertiesStore().saveCurrentCycleNumber(2);
     testPay(2);
     byte[] sr1 = decodeFromBase58Check("THKJYuUmMKKARNf7s2VT51g5uPY6KEqnat");
-    AccountCapsule accountCapsule = manager.getAccountStore().get(sr1);
+    AccountCapsule accountCapsule = dbManager.getAccountStore().get(sr1);
     byte[] sr27 = decodeFromBase58Check("TLTDZBcPoJ8tZ6TTEeEqEvwYFk2wgotSfD");
     accountCapsule.addVotes(ByteString.copyFrom(sr27), 10000000);
-    manager.getAccountStore().put(sr1, accountCapsule);
+    dbManager.getAccountStore().put(sr1, accountCapsule);
     //
     long allowance = accountCapsule.getAllowance();
     long value = mortgageService.queryReward(sr1) - allowance;
-    long reward1 = (long) ((double) manager.getDelegationStore().getReward(0, sr27) / 100000000
+    long reward1 = (long) ((double) dbManager.getDelegationStore().getReward(0, sr27) / 100000000
         * 10000000);
-    long reward2 = (long) ((double) manager.getDelegationStore().getReward(1, sr27) / 100000000
+    long reward2 = (long) ((double) dbManager.getDelegationStore().getReward(1, sr27) / 100000000
         * 10000000);
     long reward = reward1 + reward2;
     System.out.println("testWithdraw:" + value + ", reward:" + reward);
     Assert.assertEquals(reward, value);
     mortgageService.withdrawReward(sr1);
-    accountCapsule = manager.getAccountStore().get(sr1);
+    accountCapsule = dbManager.getAccountStore().get(sr1);
     allowance = accountCapsule.getAllowance() - allowance;
     System.out.println("withdrawReward:" + allowance);
     Assert.assertEquals(reward, allowance);
   }
 
+  @Test
   public void test() {
-    manager.getDynamicPropertiesStore().saveChangeDelegation(1);
-    manager.getDynamicPropertiesStore().saveConsensusLogicOptimization(1);
+    dbManager.getDynamicPropertiesStore().saveChangeDelegation(1);
+    dbManager.getDynamicPropertiesStore().saveConsensusLogicOptimization(1);
     byte[] sr27 = decodeFromBase58Check("TLTDZBcPoJ8tZ6TTEeEqEvwYFk2wgotSfD");
-    manager.getDelegationStore().setBrokerage(0, sr27, 10);
-    manager.getDelegationStore().setBrokerage(1, sr27, 20);
-    manager.getDelegationStore().setWitnessVote(0, sr27, 100000000);
-    manager.getDelegationStore().setWitnessVote(1, sr27, 100000000);
-    manager.getDelegationStore().setWitnessVote(2, sr27, 100000000);
+    dbManager.getDelegationStore().setBrokerage(0, sr27, 10);
+    dbManager.getDelegationStore().setBrokerage(1, sr27, 20);
+    dbManager.getDelegationStore().setWitnessVote(0, sr27, 100000000);
+    dbManager.getDelegationStore().setWitnessVote(1, sr27, 100000000);
+    dbManager.getDelegationStore().setWitnessVote(2, sr27, 100000000);
     testPay(0);
     testWithdraw();
   }
