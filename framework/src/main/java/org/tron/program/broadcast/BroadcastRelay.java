6b4d31c086ce0e5e30abe3c3a875a6d6a9dd31f3
Sunny Jiao <>
2025-09-22 15:19:54 +0800

    update remove tps

--- framework/src/main/java/org/tron/program/broadcast/BroadcastRelay.java
+++ framework/src/main/java/org/tron/program/broadcast/BroadcastRelay.java
@@ -1,20 +1,15 @@
 package org.tron.program.broadcast;
 
+import static org.tron.core.config.Parameter.ChainConstant.BLOCK_PRODUCED_INTERVAL;
 
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.util.Random;
 import java.util.concurrent.ConcurrentLinkedQueue;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import lombok.extern.slf4j.Slf4j;
 import org.tron.core.capsule.TransactionCapsule;
 import org.tron.core.db.Manager;
 import org.tron.core.net.TronNetService;
-import org.tron.core.net.message.adv.TransactionMessage;
 import org.tron.protos.Protocol.Transaction;
 
 @Slf4j(topic = "broadcastRelay")
@@ -27,13 +22,6 @@ public class BroadcastRelay {
 
   private final String output = "stress-test-output";
 
-  public int TPS = 100;
-
-  private final ExecutorService saveTransactionIDPool = Executors
-      .newFixedThreadPool(1, r -> new Thread(r, "save-relay-tx-id"));
-
-  private final Random random = new Random(System.currentTimeMillis());
-
   public BroadcastRelay(TronNetService tronNetService, Manager dbManager) {
     this.tronNetService = tronNetService;
     this.dbManager = dbManager;
@@ -45,13 +33,14 @@ public class BroadcastRelay {
       Transaction.Contract.ContractType.WitnessUpdateContract
   };
 
-  public boolean needSkip(Transaction transaction) {
-    Transaction.Contract.ContractType trxType = transaction.getRawData().getContract(0).getType();
+  public boolean needSkip(TransactionCapsule trx) {
+    Transaction.Contract.ContractType trxType =
+        trx.getInstance().getRawData().getContract(0).getType();
 
     // Check if the transaction type is in the skip list
     for (Transaction.Contract.ContractType skipType : SKIP_TRANSACTION_TYPES) {
       if (trxType == skipType) {
-        logger.info("Skipping transaction type: {}", trxType);
+        logger.info("Skipping transaction type: {} id: {}", trxType, trx.getTransactionId());
         return true;
       }
     }
@@ -60,64 +49,63 @@ public class BroadcastRelay {
 
   public void broadcastTransactions() {
     long trxCount = 0;
-
+    long skipCount = 0;
     long startTime = System.currentTimeMillis();
     logger.info("Start to process relay transaction broadcast task");
     try (FileInputStream fis = new FileInputStream(output + File.separator + "relay-tx.csv")) {
       Transaction transaction;
-      int cnt = 0;
-      long startTps = System.currentTimeMillis();
-      long endTps;
+
       while ((transaction = Transaction.parseDelimitedFrom(fis)) != null) {
+        // check for a special case of zero signatures, it means the block end, wait a while
+        if (transaction.getSignatureCount() == 0) {
+          long time =
+              BLOCK_PRODUCED_INTERVAL - System.currentTimeMillis() % BLOCK_PRODUCED_INTERVAL;
+          Thread.sleep(time + 50); // add a little more time to ensure block production given higher priority
+          logger.info("weak up from block end, wait: {} ms", time + 50);
+          continue; // skip this fake transaction
+        }
+
+        TransactionCapsule trx = new TransactionCapsule(transaction);
+        // Update transaction timestamp to current time to avoid expiration issues
+        trx.setTime(System.currentTimeMillis());
         // Skip VoteWitnessContract and WitnessUpdateContract transactions
-        if (needSkip(transaction)) {
+        if (needSkip(trx)) {
+          skipCount++;
           continue;
         }
 
         trxCount++;
-        if (cnt > TPS) {
-          endTps = System.currentTimeMillis();
-          if (endTps - startTps < 1000) {
-            Thread.sleep(1000 - (endTps - startTps));
-          }
-          cnt = 0;
-          startTps = System.currentTimeMillis();
-        } else {
-          try {
-            TransactionCapsule trx = new TransactionCapsule(transaction);
-            // Update transaction timestamp to current time to avoid expiration issues
-            trx.setTime(System.currentTimeMillis());
 
-            logger.info("dbManager push transaction start id: {}", trx.getTransactionId());
-            dbManager.pushTransaction(trx);
-            logger.info("dbManager process transaction success");
-            // TransactionMessage message = new TransactionMessage(transaction);
-            //            int peerCnt = tronNetService.fastBroadcastTransaction(message);
-            //            while (peerCnt <= 0) {
-            //              logger.warn("broadcast relay task has no available peers to broadcast, please wait");
-            //              Thread.sleep(100);
-            //              peerCnt = tronNetService.fastBroadcastTransaction(message);
-            //            }
-            //            if (trxCount % 1000 == 0) {
-            //              logger.info("total broadcast tx num: {}", trxCount);
-            //            }
-          } catch (Exception e) {
-            logger.info("dbManager process transaction failed");
-            e.printStackTrace();
-          }
-
-          cnt++;
+        try {
+          logger.info("dbManager push transaction start id: {}", trx.getTransactionId());
+          dbManager.pushTransaction(trx);
+          logger.info("dbManager process transaction success");
+          // TransactionMessage message = new TransactionMessage(transaction);
+          //            int peerCnt = tronNetService.fastBroadcastTransaction(message);
+          //            while (peerCnt <= 0) {
+          //              logger.warn("broadcast relay task has no available peers to broadcast, please wait");
+          //              Thread.sleep(100);
+          //              peerCnt = tronNetService.fastBroadcastTransaction(message);
+          //            }
+          //            if (trxCount % 1000 == 0) {
+          //              logger.info("total broadcast tx num: {}", trxCount);
+          //            }
+        } catch (Exception e) {
+          logger.info("dbManager process transaction failed");
+          e.printStackTrace();
         }
+
       }
 
     } catch (IOException e) {
       e.printStackTrace();
     } catch (InterruptedException e) {
-      Thread.currentThread().interrupt();
+      throw new RuntimeException(e);
     }
 
     long cost = System.currentTimeMillis() - startTime;
-    logger.info("relay trx size: {}, cost: {}, tps: {}", trxCount, cost,
+    logger.info("relay trx size: {}, skip trx size:{}, cost: {}, tps: {}", trxCount, skipCount,
+        cost,
         1.0 * trxCount / cost * 1000);
   }
 }
