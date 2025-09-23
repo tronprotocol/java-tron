4ee0028e2e53cd9b5b702ec3de934babc07ab597
Sunny Jiao <>
2025-09-23 10:29:59 +0800

    update config

--- framework/src/main/java/org/tron/program/broadcast/BroadcastRelay.java
+++ framework/src/main/java/org/tron/program/broadcast/BroadcastRelay.java
@@ -10,12 +10,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.tron.core.capsule.TransactionCapsule;
 import org.tron.core.db.Manager;
 import org.tron.core.net.TronNetService;
+import org.tron.core.net.message.adv.TransactionMessage;
 import org.tron.protos.Protocol.Transaction;
 
 @Slf4j(topic = "broadcastRelay")
 public class BroadcastRelay {
-
-  private final ConcurrentLinkedQueue<Transaction> transactionIDs = new ConcurrentLinkedQueue<>();
 
   private final TronNetService tronNetService;
   private final Manager dbManager;
@@ -34,8 +33,7 @@ public class BroadcastRelay {
   };
 
   public boolean needSkip(TransactionCapsule trx) {
-    Transaction.Contract.ContractType trxType =
-        trx.getInstance().getRawData().getContract(0).getType();
+    Transaction.Contract.ContractType trxType = trx.getInstance().getRawData().getContract(0).getType();
 
     // Check if the transaction type is in the skip list
     for (Transaction.Contract.ContractType skipType : SKIP_TRANSACTION_TYPES) {
@@ -56,10 +54,10 @@ public class BroadcastRelay {
       Transaction transaction;
 
       while ((transaction = Transaction.parseDelimitedFrom(fis)) != null) {
-        // check for a special case of zero signatures, it means the block end, wait a while
+        // check for a special case of zero signatures, it means the block end, wait a
+        // while
         if (transaction.getSignatureCount() == 0) {
-          long time =
-              BLOCK_PRODUCED_INTERVAL - System.currentTimeMillis() % BLOCK_PRODUCED_INTERVAL;
+          long time = BLOCK_PRODUCED_INTERVAL - System.currentTimeMillis() % BLOCK_PRODUCED_INTERVAL;
           Thread.sleep(time + 50); // add a little more time to ensure block production given higher priority
           logger.info("weak up from block end, wait: {} ms", time + 50);
           continue; // skip this fake transaction
@@ -80,19 +78,19 @@ public class BroadcastRelay {
           logger.info("dbManager push transaction start id: {}", trx.getTransactionId());
           dbManager.pushTransaction(trx);
           logger.info("dbManager process transaction success");
-          // TransactionMessage message = new TransactionMessage(transaction);
-          //            int peerCnt = tronNetService.fastBroadcastTransaction(message);
-          //            while (peerCnt <= 0) {
-          //              logger.warn("broadcast relay task has no available peers to broadcast, please wait");
-          //              Thread.sleep(100);
-          //              peerCnt = tronNetService.fastBroadcastTransaction(message);
-          //            }
-          //            if (trxCount % 1000 == 0) {
-          //              logger.info("total broadcast tx num: {}", trxCount);
-          //            }
+          TransactionMessage message = new TransactionMessage(transaction);
+          int peerCnt = tronNetService.fastBroadcastTransaction(message);
+          while (peerCnt <= 0) {
+            logger.warn("broadcast relay task has no available peers to broadcast, please wait");
+            Thread.sleep(100);
+            peerCnt = tronNetService.fastBroadcastTransaction(message);
+          }
+          if (trxCount % 1000 == 0) {
+            logger.info("total broadcast tx num: {}", trxCount);
+          }
         } catch (Exception e) {
           logger.info("dbManager process transaction failed");
-          failedCount ++;
+          failedCount++;
           e.printStackTrace();
         }
 
