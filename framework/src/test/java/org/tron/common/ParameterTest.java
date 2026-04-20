20c0de872f73eb733060441eae3389d63274d517
jiangyuanshu <317787106@qq.com>
2026-04-20 16:54:48 +0800

    fix the bug of ParameterTest

--- framework/src/test/java/org/tron/common/ParameterTest.java
+++ framework/src/test/java/org/tron/common/ParameterTest.java
@@ -241,16 +241,6 @@ public class ParameterTest {
     assertEquals(500, parameter.getPendingTransactionTimeout());
     parameter.setNodeMetricsEnable(false);
     assertFalse(parameter.isNodeMetricsEnable());
-    parameter.setMetricsStorageEnable(false);
-    assertFalse(parameter.isMetricsStorageEnable());
-    parameter.setInfluxDbIp("127.0.0.1");
-    assertEquals("127.0.0.1", parameter.getInfluxDbIp());
-    parameter.setInfluxDbPort(90);
-    assertEquals(90, parameter.getInfluxDbPort());
-    parameter.setInfluxDbDatabase("InfluxDb");
-    assertEquals("InfluxDb", parameter.getInfluxDbDatabase());
-    parameter.setMetricsReportInterval(100);
-    assertEquals(100, parameter.getMetricsReportInterval());
     parameter.setMetricsPrometheusPort(3000);
     assertEquals(3000, parameter.getMetricsPrometheusPort());
     parameter.setAgreeNodeCount(10);
