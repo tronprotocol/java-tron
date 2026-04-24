7d777ea52468a82a6d861902f967f1e55266b279
halibobo1205 <halibobo1205@gmail.com>
2026-04-24 15:40:16 +0800

    feat(proposal): add ALLOW_HARDEN_EXCHANGE_CALCULATION (code 98)

--- actuator/src/main/java/org/tron/core/utils/ProposalUtil.java
+++ actuator/src/main/java/org/tron/core/utils/ProposalUtil.java
@@ -902,6 +902,21 @@ public class ProposalUtil {
         }
         break;
       }
+      case ALLOW_HARDEN_EXCHANGE_CALCULATION: {
+        if (!forkController.pass(ForkBlockVersionEnum.VERSION_4_8_2)) {
+          throw new ContractValidateException(
+              "Bad chain parameter id [ALLOW_HARDEN_EXCHANGE_CALCULATION]");
+        }
+        if (dynamicPropertiesStore.getAllowHardenExchangeCalculation() == 1) {
+          throw new ContractValidateException(
+              "[ALLOW_HARDEN_EXCHANGE_CALCULATION] has been valid, no need to propose again");
+        }
+        if (value != 1) {
+          throw new ContractValidateException(
+              "This value[ALLOW_HARDEN_EXCHANGE_CALCULATION] is only allowed to be 1");
+        }
+        break;
+      }
       default:
         break;
     }
@@ -988,8 +1003,8 @@ public class ProposalUtil {
     PROPOSAL_EXPIRE_TIME(92), // (0, 31536003000)
     ALLOW_TVM_SELFDESTRUCT_RESTRICTION(94), // 0, 1
     ALLOW_TVM_OSAKA(96), // 0, 1
-    ALLOW_HARDEN_RESOURCE_CALCULATION(97); // 0, 1
-
+    ALLOW_HARDEN_RESOURCE_CALCULATION(97), // 0, 1
+    ALLOW_HARDEN_EXCHANGE_CALCULATION(98); // 0, 1
     private long code;
 
     ProposalType(long code) {
