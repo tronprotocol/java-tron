0444ddb630de47fe41d214551551ab607f18787e
halibobo1205 <halibobo1205@gmail.com>
2026-04-28 13:27:54 +0800

    refactor(common): drop unused JSONException(Throwable) constructor

--- common/src/main/java/org/tron/json/JSONException.java
+++ common/src/main/java/org/tron/json/JSONException.java
@@ -18,8 +18,4 @@ public class JSONException extends RuntimeException {
   public JSONException(String message, Throwable cause) {
     super(message, cause);
   }
-
-  public JSONException(Throwable cause) {
-    super(cause);
-  }
 }
