d188b5174016de94300af6d138e3d55b9623731b
jiangyuanshu <317787106@qq.com>
2026-05-09 19:23:33 +0800

    fix the bug of not stop solidity now

--- framework/src/main/java/org/tron/program/SolidityNode.java
+++ framework/src/main/java/org/tron/program/SolidityNode.java
@@ -72,11 +72,11 @@ public class SolidityNode implements ApplicationListener<ContextClosedEvent> {
   @PreDestroy
   private void shutdown() {
     flag = false;
-    ExecutorServiceManager.shutdownNow(getBlockExecutor, getBlockName);
-    ExecutorServiceManager.shutdownNow(processBlockExecutor, processBlockName);
     if (databaseGrpcClient != null) {
       databaseGrpcClient.shutdown();
     }
+    ExecutorServiceManager.shutdownAndAwaitTermination(getBlockExecutor, getBlockName);
+    ExecutorServiceManager.shutdownAndAwaitTermination(processBlockExecutor, processBlockName);
   }
 
   public void run() {
