a17f9a306d8013703f8b37cab53f5ea406f98a09
zz <aaa@bb.cc>
2026-06-02 19:43:02 +0800

    feat(version): update version to 4.8.1.1

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -4,7 +4,7 @@ public class Version {
 
   public static final String VERSION_NAME = "GreatVoyage-v4.8.0.1-1-g44a4bc8263";
   public static final String VERSION_CODE = "18636";
-  private static final String VERSION = "4.8.1";
+  private static final String VERSION = "4.8.1.1";
 
   public static String getVersion() {
     return VERSION;
