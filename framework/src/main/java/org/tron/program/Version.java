1b61a73d715161af672a56f71513c5d811e7b89a
halibobo1205 <halibobo1205@gmail.com>
2026-07-15 19:14:40 +0800

    update a new version. version name:GreatVoyage-v4.8.1.1-173-gaced0d5654,version code:18817

--- framework/src/main/java/org/tron/program/Version.java
+++ framework/src/main/java/org/tron/program/Version.java
@@ -2,8 +2,8 @@ package org.tron.program;
 
 public class Version {
 
-  public static final String VERSION_NAME = "GreatVoyage-v4.8.1-6-g52d7d9d23e";
-  public static final String VERSION_CODE = "18643";
+  public static final String VERSION_NAME = "GreatVoyage-v4.8.1.1-173-gaced0d5654";
+  public static final String VERSION_CODE = "18817";
   private static final String VERSION = "4.8.2";
 
   public static String getVersion() {
